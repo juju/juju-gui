30617d24c3726faf37109f5f21da66232c13af21
Kapil Thangavelu <kapil.foss@gmail.com>
2012-07-11 12:32:03 -0400

    commit wip

---
+++ src/app.js
@@ -0,0 +1,85 @@
+YUI().use('app', 'handlebars', 'jsonp', function(Y) {
+    
+
+   
+Charm = Y.Base.create('charm', Y.Model, [], {
+    ATTRS: {
+	charm_id: {value: null},
+	name: {value: null},
+	url: {value: null},
+	description: {value: null},
+	config: {value: null},
+	metadata: {value: null},
+	sha256: {value: null}
+    }
+});
+
+
+CharmList = Y.Base.create('charmList', Y.ModelList, [], {
+    ATTRS: {
+    }
+});
+
+
+Service = Y.Base.create('service', Y.Model, [], {
+    ATTRS: {
+	name: {value: null},
+	charm: {value: null},
+	config: {value: null},
+	constraints: {value: null},
+	exposed: {value: null},
+    }
+});
+
+
+ServiceList = Y.Base.create('serviceList', Y.ModelList, [], {
+    ATTRS: {
+    }
+});
+
+
+ServiceUnit = Y.Base.create('serviceUnit', Y.ModelList, [], {
+    ATTRS: {
+	name: {value: null},
+	service: {value: null},
+	machine: {value: null},
+	agent_state: {value: null},
+	relations: {value: null},
+	is_subordinate: {value: false},
+	open_ports: {value: null},
+	public_address: {value: null},
+	private_address: {value: null},
+    }
+}
+
+
+ServiceUnitList = Y.Base.create('serviceUnitList', Y.ModelList, [], {
+    ATTRS: {
+    }
+}
+
+
+Machine = Y.Base.create('machine', Y.Model, [], {
+    ATTRS: {
+    }
+});
+
+MachineList = Y.Base.create('machineList', Y.ModelList, [], {
+    ATTRS: {
+    }
+});
+
+
+      
+Relation = Y.Base.create('relation', Y.Model, [], {
+    ATTRS: {
+    }
+});
+
+RelationList = Y.Base.create('relationList', Y.ModelList, [], {
+    ATTRS: {
+    }
+});
+
+
+})
---
+++ src/models/charm.js
@@ -0,0 +1,13 @@
+/*
+
+Charm model
+
+*/
+Charm = Y.Base.create('charm', Y.Model, [], {
+    idAttrtibute: 'charm-id',
+
+    ATTRS: {
+	charm_id  : {value:  null},
+    }
+
+});
