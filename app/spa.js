357b8c2d97a562833b4778299bc47068d70afcfb
Kapil Thangavelu <kapil.foss@gmail.com>
2012-07-13 15:50:50 -0400

    clean up models

--- app/spa.js
+++ app/spa.js
@@ -1,80 +1,102 @@
 YUI().use('app', 'handlebars', 'jsonp', function(Y) {
     
+var Charm, CharmList;
+var Service, ServiceList;
+var ServiceUnit, ServiceUnitList;
+var Machine, MachineList;
+var Relation, RelationList;
 
+    
+var JujuGUI;
    
 Charm = Y.Base.create('charm', Y.Model, [], {
+    idAttribute: 'charm_id',
     ATTRS: {
-	charm_id: {value: null},
-	name: {value: null},
-	url: {value: null},
-	description: {value: null},
-	config: {value: null},
-	metadata: {value: null},
-	sha256: {value: null}
+	charm_id: {},
+	name: {},
+	url: {},
+	description: {},
+	config: {},
+	metadata: {},
+	sha256: {}
     }
 });
 
 
 CharmList = Y.Base.create('charmList', Y.ModelList, [], {
+    model: Charm,
     ATTRS: {
     }
 });
 
 
 Service = Y.Base.create('service', Y.Model, [], {
+    idAttribute: 'name',
     ATTRS: {
-	name: {value: null},
-	charm: {value: null},
-	config: {value: null},
-	constraints: {value: null},
-	exposed: {value: null},
+	name: {},
+	charm: {},
+	config: {},
+	constraints: {},
+	exposed: {},
+	relations: {}
     }
 });
 
 
 ServiceList = Y.Base.create('serviceList', Y.ModelList, [], {
+    model: Service,
     ATTRS: {
     }
 });
 
 
 ServiceUnit = Y.Base.create('serviceUnit', Y.ModelList, [], {
+    idAttribute: 'name',
     ATTRS: {
-	name: {value: null},
-	service: {value: null},
-	machine: {value: null},
-	agent_state: {value: null},
-	relations: {value: null},
-	is_subordinate: {value: false},
-	open_ports: {value: null},
-	public_address: {value: null},
-	private_address: {value: null},
+	name: {},
+	service: {},
+	machine: {},
+	agent_state: {},
+	// relations to unit relation state.
+	relations: {},
+	is_subordinate: {},
+	open_ports: {},
+	public_address: {},
+	private_address: {}
     }
-}
+});
 
 
 ServiceUnitList = Y.Base.create('serviceUnitList', Y.ModelList, [], {
+    model: ServiceUnit,
     ATTRS: {
     }
-}
+});
 
 
 Machine = Y.Base.create('machine', Y.Model, [], {
+    idAttribute: 'machine_id',
     ATTRS: {
-	public_address: {value: null},
-	private_address: {value: null}
+	machine_id: {},
+	public_address: {},
+	private_address: {}
     }
 });
 
 MachineList = Y.Base.create('machineList', Y.ModelList, [], {
+    model: Machine,
     ATTRS: {
     }
 });
 
 
-      
+
 Relation = Y.Base.create('relation', Y.Model, [], {
+    idAttribute: 'relation_id',
     ATTRS: {
+	relation_id: {},
+	type: {},
+	endpoints: {}
     }
 });
 
@@ -84,4 +106,4 @@ RelationList = Y.Base.create('relationList', Y.ModelList, [], {
 });
 
 
-})
+});
