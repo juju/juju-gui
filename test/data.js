eb03224d5cd7a0d2165fb2d5fd6f1ad123a3cd3c
Francesco Banconi <francesco.banconi@canonical.com>
2012-09-11 13:43:06 +0200

    Implement base structure for tests.

--- test/data.js
+++ test/data.js
@@ -69,7 +69,35 @@ YUI(GlobalConfig).add('juju-tests-data', function(Y) {
                     "owner": "negronjl"
                 }
             ]
+        },
+
+        charmQuery: '/charms/precise/postgresql/json',
+
+        charmResults: {
+            "maintainer": "Mark Mims <mark.mims@canonical.com>",
+            "series": "precise",
+            "owner": "charmers",
+            "provides": {
+                "db-admin": {
+                    "interface": "pgsql"
+                },
+                "db": {
+                    "interface": "pgsql"
+                }
+            },
+            "description": "PostgreSQL is a fully featured RDBMS.",
+            "name": "postgresql",
+            "summary": "object-relational SQL database (supported version)",
+            "bzr_branch": "lp:~charmers/charms/precise/postgresql/trunk",
+            "last_change": {
+                "committer": "David Owen <david.owen@canonical.com>",
+                "message": "Only reload for pg_hba updates",
+                "revno": 24,
+                "created": 1340206387.539
+            },
+            "proof": {}
         }
+
     };
 
 });
