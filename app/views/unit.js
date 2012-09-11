22f5be8e1708988c0195f5e6d2ba169df1c02d2f
Benji York <benji.york@canonical.com>
2012-09-11 12:31:03 +0000

    checkpoint

--- app/views/unit.js
+++ app/views/unit.js
@@ -1,41 +1,61 @@
-YUI.add("juju-view-unit", function(Y) {
+YUI.add('juju-view-unit', function(Y) {
 
-var views = Y.namespace("juju.views"),
+var views = Y.namespace('juju.views'),
+    models = Y.namespace("juju.models"),
     Templates = views.Templates;
 
 UnitView = Y.Base.create('UnitView', Y.View, [], {
     initializer: function () {
-      console.log("view.init.unit", this.get('unit'));
+        console.log('view.init.unit', this.get('unit'));
     },
 
-    template: Templates["unit"],
+    template: Templates['unit'],
 
     render: function () {
-	console.log("view.render.unit", this.get('unit'));
-	var container = this.get('container'),
-            db = this.get('db'),
-            unit = this.get("unit");
-	UnitView.superclass.render.apply(this, arguments);
-
-	if (! this.get('unit')) {
+        var container = this.get('container');
+        console.log('view.render.unit');
+        if (!this.get('unit')) {
+            // XXX I am assuming that once the unit data is ready all other
+            // data is as well.
+            // XXX put loading message back
             container.setHTML('<div class="alert">Loading...</div>');
-	    return this;
-	}; 
+            console.log('waiting on data');
+            return this;
+        };
 
-	container.setHTML(this.template(
-	    {'unit': this.get('unit')}));
-	return this;
+        var db = this.get('db'),
+            unit = this.get('unit').getAttrs(),
+            service = db.services.getById(unit.service).getAttrs(),
+            machine = db.machines.getById(unit.machine).getAttrs();
+
+        // XXX Why do I have to do this?  Is there a better way?
+        if (!db.charms.getById(service.charm)) {
+            charm = new models.Charm({id: service.charm});
+            db.charms.add(charm);
+        }
+        var charm = db.charms.getById(service.charm).getAttrs();
+
+        console.log('unit', unit);
+        console.log('charm', charm);
+        console.log('service', service);
+        UnitView.superclass.render.apply(this, arguments);
+
+        container.setHTML(this.template(
+            {'unit': unit,
+             'service': service,
+             'machine': machine}));
+        return this;
     }
 });
 
 
 views.unit = UnitView;
 
-}, "0.1.0", {
-    requires: ['d3',
-               'base',
-               'handlebars',
-               'node',
-               "view"]
+}, '0.1.0', {
+    requires: [
+        'base',
+        'handlebars',
+        'node',
+        'view']
 
 });
