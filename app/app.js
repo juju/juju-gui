05ec41e674707cf0f71ff2c94a530fad9d76b3b5
Brad Crittenden <bac@canonical.com>
2012-09-18 11:42:44 -0400

    Fix lint target in Makefile to restore tab.  Fixed subsequent accumulated lint.

--- app/app.js
+++ app/app.js
@@ -235,7 +235,7 @@ var JujuGUI = Y.Base.create('juju-gui', Y.App, [], {
     },
 
     show_environment: function (req) {
-        console.log("App: Route: Environment", req.path, req.pendingRoutes);
+        console.log('App: Route: Environment', req.path, req.pendingRoutes);
         this.showView('environment', {domain_models: this.db, env: this.env}, {render: true});
     },
 
