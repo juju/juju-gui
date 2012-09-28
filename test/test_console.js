858e051285904512444d7d600ad787d18658e128
Thiago Veronezi <thiago.veronezi@canonical.com>
2012-09-28 15:02:53 -0400

    checkpoint

---
+++ test/test_console.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function() {
+
+  describe('application console', function() {
+
+    it('should disable the console', function(done) {
+      var mock = consoleManager.getConsoleMock(),
+          logCalled = false,
+          message = null;
+
+      mock.log = function() {
+        logCalled = true;
+        message = arguments[0];
+      };
+
+      consoleManager.enable();
+      console.log('TEST');
+      assert.isFalse(logCalled);
+
+      consoleManager.disable();
+      console.log('TEST');
+      assert.isTrue(logCalled);
+      assert.equal('TEST', message);
+
+      done();
+    });
+
+  });
+
+})();
