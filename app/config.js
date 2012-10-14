b9ad76b61784281e69e23f8a3e8ae6c43d95c046
Gary Poster <gary.poster@canonical.com>
2012-10-13 20:57:37 -0400

    revert inadvertent commit of test configuration

--- app/config.js
+++ app/config.js
@@ -7,6 +7,5 @@ var juju_config = {
   // FIXME: turn off transitions until they are fixed.
   transitions: false,
   charm_store_url: 'http://jujucharms.com/',
-  // socket_url: 'ws://ec2-107-22-8-18.compute-1.amazonaws.com:80/ws'
-  socket_url: 'ws://ec2-50-112-209-147.us-west-2.compute.amazonaws.com:80/ws'
+  socket_url: 'ws://localhost:8081/ws'
 };
