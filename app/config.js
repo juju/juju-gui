3d75c9f3ffc55fa989463ff99b7dad7b5966e4fd
Francesco Banconi <francesco.banconi@canonical.com>
2012-10-25 10:30:45 +0200

    Switch to wss.

--- app/config.js
+++ app/config.js
@@ -7,5 +7,5 @@ var juju_config = {
   // FIXME: turn off transitions until they are fixed.
   transitions: false,
   charm_store_url: 'http://jujucharms.com/',
-  socket_url: 'ws://localhost:8081/ws'
+  socket_url: 'wss://localhost:8081/ws'
 };
