cfc7f924171205ee06b1987854aef98525e49591
Gary Poster <gary.poster@canonical.com>
2012-10-31 11:57:16 +0100

    make server should not run yuidoc every time.  flyby fix in README.

--- test-server.js
+++ test-server.js
@@ -6,22 +6,22 @@ var express = require('express'),
     path = require('path');
 
 
-server.configure(function () {
-    server.use(express.logger('dev'));
-    // 'static' is a reserved word so dot notation is not used to
-    // avoid annoying the linter.
-    server.use(express['static'](__dirname));
-    // fallback to looking in assets
-    server.use('/juju-ui', express['static'](__dirname + '/app/'));
-    server.use(express.bodyParser());
-    server.use(express.methodOverride());
+server.configure(function() {
+  server.use(express.logger('dev'));
+  // 'static' is a reserved word so dot notation is not used to
+  // avoid annoying the linter.
+  server.use(express['static'](__dirname));
+  // fallback to looking in assets
+  server.use('/juju-ui', express['static'](__dirname + '/app/'));
+  server.use(express.bodyParser());
+  server.use(express.methodOverride());
 });
 
 
 
 var port = 8084;
 
-server.listen(port, function () {
-    console.log('Server listening on ' + port);
+server.listen(port, function() {
+  console.log('Server listening on ' + port);
 });
 
