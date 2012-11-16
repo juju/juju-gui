13a5acb8b6af06547aa2e1e621c7ba6134a315b7
Francesco Banconi <francesco.banconi@canonical.com>
2012-11-16 09:20:18 +0100

    Refactored notifier widget tests.

--- test/test_notifier_widget.js
+++ test/test_notifier_widget.js
@@ -35,55 +35,56 @@ describe('notifier widget', function() {
     return notifier;
   };
 
+  // Assert the notifier box contains the expectedNumber of notifiers.
+  var assertNumNotifiers = function(expectedNumber) {
+    assert.equal(expectedNumber, notifierBox.get('children').size());
+  };
+
   it('should be able to display a notification', function() {
-    var notifier = makeNotifier(),
-        notifierNodes = notifierBox.all('.' + notifier.getClassName());
-    assert.equal(1, notifierNodes.size());
+    makeNotifier();
+    assertNumNotifiers(1);
   });
 
   it('should display the given title and message', function() {
-    var notifier = makeNotifier('mytitle', 'mymessage'),
-        notifierNode = notifierBox.one('.' + notifier.getClassName());
+    makeNotifier('mytitle', 'mymessage');
+    var notifierNode = notifierBox.one('*');
     assert.equal('mytitle', notifierNode.one('h3').getContent());
     assert.equal('mymessage', notifierNode.one('div').getContent());
   });
 
   it('should be able to display multiple notifications', function() {
-    var notifier = makeNotifier();
-    makeNotifier();
-    var notifierNodes = notifierBox.all('.' + notifier.getClassName());
-    assert.equal(2, notifierNodes.size());
+    var number = 10;
+    for (var i = 0; i < number; i += 1) {
+      makeNotifier();
+    }
+    assertNumNotifiers(number);
   });
 
   it('should display new notifications on top', function() {
     makeNotifier('mytitle1', 'mymessage1');
-    var notifier = makeNotifier('mytitle2', 'mymessage2'),
-        notifierNode = notifierBox.one('.' + notifier.getClassName());
+    makeNotifier('mytitle2', 'mymessage2');
+    var notifierNode = notifierBox.one('*');
     assert.equal('mytitle2', notifierNode.one('h3').getContent());
     assert.equal('mymessage2', notifierNode.one('div').getContent());
   });
 
   it('should destroy notifications after N milliseconds', function(done) {
-    var notifier = makeNotifier('mytitle', 'mymessage', 1),
-        selector = '.' + notifier.getClassName();
+    makeNotifier('mytitle', 'mymessage', 1);
     // A timeout of 250 milliseconds is used so that we ensure the destroying
     // animation can be completed.
     setTimeout(function() {
-      var notifierNodes = notifierBox.all(selector);
-      assert.equal(0, notifierNodes.size());
+      assertNumNotifiers(0);
       done();
     }, 250);
   });
 
   it('should prevent notification removal on mouse enter', function(done) {
-    var notifier = makeNotifier('mytitle', 'mymessage', 1),
-        selector = '.' + notifier.getClassName();
-    notifierBox.one(selector).simulate('mouseover');
+    makeNotifier('mytitle', 'mymessage', 1);
+    notifierBox.one('*').simulate('mouseover');
     // A timeout of 250 milliseconds is used so that we ensure the node is not
     // preserved by the destroying animation.
     setTimeout(function() {
-      var notifierNodes = notifierBox.all(selector);
-      assert.equal(1, notifierNodes.size());
+      assertNumNotifiers(1);
       done();
     }, 250);
   });
