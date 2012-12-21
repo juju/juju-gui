d6035f14ff2ec4ff682a612040f1d6d9ff292dd6
Brad Crittenden <bac@canonical.com>
2012-12-21 08:06:26 -0400

    Add panzoom test

---
+++ test/test_panzoom.js
@@ -0,0 +1,58 @@
+'use strict';
+
+describe('pan zoom module', function() {
+  var _setTimeout, _viewsHighlightRow, db, ERR_EV, juju, models, NO_OP,
+      viewContainer, views, Y, pz;
+
+  before(function() {
+    Y = YUI(GlobalConfig).use(['node',
+      'juju-models',
+      'juju-views',
+      'juju-gui',
+      'juju-env',
+      'juju-tests-utils',
+      'node-event-simulate'],
+    function(Y) {
+      juju = Y.namespace('juju');
+      models = Y.namespace('juju.models');
+      views = Y.namespace('juju.views');
+    });
+  });
+
+  beforeEach(function(done) {
+    Y = YUI(GlobalConfig).use(['node',
+      'juju-models',
+      'juju-views',
+      'juju-gui',
+      'juju-env',
+      'juju-tests-utils',
+      'node-event-simulate'],
+    function(Y) {
+      viewContainer = Y.Node.create('<div />');
+      viewContainer.appendTo(Y.one('body'));
+      viewContainer.hide();
+
+      db = new models.Database();
+      pz = new views.PanZoomModule({});
+      done();
+    });
+  });
+
+  afterEach(function() {
+    viewContainer.remove(true);
+  });
+
+  it('initial values are set',
+      function() {
+        pz._translate.should.eql([0, 0]);
+        pz._scale.should.equal(1.0);
+      });
+
+  // Test the zoom calculations.
+  it('scale handles fractional values',
+     function() {
+       pz.zoomHandler({scale: 0.6});
+       pz.slider.get('value').should.equal(61);
+     });
+
+});
