d1b121854dd628b8e77af6b383c7b6b8b7723b48
Brad Crittenden <bac@canonical.com>
2013-01-21 08:32:38 -0400

    checkpoint

--- test/test_service_module.js
+++ test/test_service_module.js
@@ -76,4 +76,74 @@ describe('service module', function() {
        location['gui.y'].should.equal(0);
      });
 
+  it('should ', function() {});
+
+  it('should ', function() {});
+  it('should ', function() {});
 });
+
+
+describe('service module events', function() {
+  var db, juju, models, view, viewContainer, views, Y;
+  before(function(done) {
+    Y = YUI(GlobalConfig).use(['node',
+      'juju-models',
+      'juju-views',
+       'juju-gui',
+       'juju-env',
+       'juju-tests-utils',
+       'node-event-simulate'],
+     function(Y) {
+       juju = Y.namespace('juju');
+       models = Y.namespace('juju.models');
+       views = Y.namespace('juju.views');
+       done();
+     });
+   });
+
+  beforeEach(function() {
+    viewContainer = Y.Node.create('<div />');
+    viewContainer.appendTo(Y.one('body'));
+    db = new models.Database();
+    db.services.add({id: 'haproxy'});
+    view = new views.environment({
+      container: viewContainer,
+      db: db,
+      env: {}
+    });
+    view.render();
+    view.rendered();
+  });
+
+  afterEach(function() {
+    if (viewContainer) {
+      viewContainer.remove(true);
+    }
+  });
+
+  it('should not show the menu after the service is double-clicked',
+      function() {
+        // Monkeypatch to avoid the click event handler bailing out early.
+        view.topo.modules.ServiceModule.service_boxes
+         .haproxy.containsPoint = function() {return true;};
+        var service = viewContainer.one('.service');
+        var menu = viewContainer.one('#service-menu');
+        service.simulate('click');
+        // Ideally the browser would not send the click event right away...
+        assert(menu.hasClass('active'));
+        service.simulate('dblclick');
+        assert.isFalse(menu.hasClass('active'));
+      });
+
+  it('should ignore clicks outside the service node',
+      function(){
+        // Monkeypatch to test the click event handler bailing out early.
+        view.topo.modules.ServiceModule.service_boxes
+         .haproxy.containsPoint = function() {return false;};
+        var service = viewContainer.one('.service');
+        var menu = viewContainer.one('#service-menu');
+        //assert.isFalse(menu.hasClass('active'));
+        service.simulate('click');
+        assert.isFalse(menu.hasClass('active'));
+      });
+ });
