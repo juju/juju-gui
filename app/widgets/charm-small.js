f1b69bf7f8394fab71a8563f2ef3755a13bb994f
j.c.sackett <jcsackett@canonical.com>
2013-03-01 14:19:45 +0000

    Comments are good, let's add some.

--- app/widgets/charm-small.js
+++ app/widgets/charm-small.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Provide the Charm Small widget.
+ *
+ * @module widgets
+ * @submodule widgets.charm-small
+ */
 YUI.add('charm-small', function(Y) {
 
   var ns = Y.namespace('juju.widgets');
@@ -8,8 +14,20 @@ YUI.add('charm-small', function(Y) {
 
     TEMPLATE: Y.namespace('juju.views').Templates['charm-small-widget'],
 
+    /**
+     * Initializer.
+     *
+     * @method initialize
+     * @return {undefined} Mutates only.
+     */
     initialize: function(cfg) {}, 
 
+    /**
+     * Create the nodes required by this widget and attach them to the DOM.
+     *
+     * @method renderUI
+     * @return {undefined} Mutates only.
+     */
     renderUI: function() {
       var content = this.TEMPLATE({
         iconfile: this.get('iconfile'),
@@ -21,6 +39,14 @@ YUI.add('charm-small', function(Y) {
       this.get('container').append(this.get('boundingBox'));
     },
 
+    /**
+     * Attach event listeners which bind the UI to the widget state.
+     * Hover causes the "Add" button to display.
+     * Clicking add fires the add signal.
+     *
+     * @method bindUI
+     * @return {undefined} Mutates only.
+     */
     bindUI: function() {
       var add_button = this.get('contentBox').one('button');
       this.on('mouseover', function() {
