fb3742a05b50ec5cdf9a1570c8d2b1cd1cfb99b9
j.c.sackett <jcsackett@canonical.com>
2013-03-05 16:55:00 +0000

    Initialization works.

--- app/widgets/charm-slider.js
+++ app/widgets/charm-slider.js
@@ -10,16 +10,16 @@
 YUI.add('browser-charm-slider', function(Y) {
 
   var ns = Y.namespace('juju.widgets.browser');
-  //ns.CharmSmall = Y.Base.create('CharmSmall', Y.Widget, [], {
-  //}, {
-    //ATTRS: { }
-  //});
+  ns.CharmSlider = Y.Base.create('CharmSlider', Y.Widget, [], {
+  }, {
+    ATTRS: {}
+  });
 
 }, '0.1.0', {
   requires: [
-    //'base',
+    'base',
     //'handlebars',
     //'juju-templates',
-    //'widget'
+    'widget'
   ]
 });
