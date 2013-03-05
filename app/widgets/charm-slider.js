5cef5e9917593c6bbda54f9905e2f54c80787d97
j.c.sackett <jcsackett@canonical.com>
2013-03-05 20:27:01 +0000

    Render test passes, other tests specced.

--- app/widgets/charm-slider.js
+++ app/widgets/charm-slider.js
@@ -2,24 +2,47 @@
 
 
 /**
- * Provide the Charm Small widget.
+ * Provide the Charm Slider widget.
  *
  * @module widgets
- * @submodule juju.widgets.browser.charm-small
+ * @submodule juju.widgets.browser.charm-slider
  */
 YUI.add('browser-charm-slider', function(Y) {
 
   var ns = Y.namespace('juju.widgets.browser');
   ns.CharmSlider = Y.Base.create('CharmSlider', Y.Widget, [], {
+
+    TEMPLATE: Y.namespace('juju.views').Templates['charm-slider'],
+
+    /**
+     * Create the nodes required by this widget and attach them to the DOM.
+     *
+     * @method renderUI
+     * @return {undefined} Mutates only.
+     */
+    renderUI: function() {
+      var content = this.TEMPLATE;
+      var cb = this.get('contentBox');
+      cb.setHTML(content);
+
+      var charmContainer = cb.one('.charm-small-container');
+      Y.Array.each(this.get('charms'), function(charm) {
+        charm.render(charmContainer);
+      });
+    }
   }, {
-    ATTRS: {}
+    ATTRS: {
+      charms: {
+        value: []
+      }
+    }
   });
 
 }, '0.1.0', {
   requires: [
     'base',
-    //'handlebars',
-    //'juju-templates',
+    'handlebars',
+    'juju-templates',
     'widget'
   ]
 });
--- test/test_charm_slider.js
+++ test/test_charm_slider.js
@@ -1,11 +1,11 @@
 'use strict';
 
-describe('charm slider', function() {
+describe.only('charm slider', function() {
   var container, Y;
 
   before(function(done) {
     Y = YUI(GlobalConfig).use(
-        ['browser-charm-slider', 'node'], function(Y) {
+        ['browser-charm-slider', 'browser-charm-small', 'node'], function(Y) {
           done();
         });
   });
@@ -19,8 +19,25 @@ describe('charm slider', function() {
     Y.one('#container').remove(true);
   });
 
-  it('should initialize', function() {
+  it('initializes', function() {
     var slider = new Y.juju.widgets.browser.CharmSlider();
     assert.isObject(slider);
   });
+
+  it('renders the charm small widgets its given', function() {
+    var charm = new Y.juju.widgets.browser.CharmSmall(); 
+    var cfg = {charms: [charm]};
+    var slider = new Y.juju.widgets.browser.CharmSlider(cfg);
+    slider.render(container);
+    assert.isObject(Y.one('.yui3-charmslider'));
+    assert.isObject(Y.one('.yui3-charmsmall'));
+  });
+
+  it('only shows one charm small widget at a time');
+
+  it('automatically scrolls through its charms');
+
+  it('has one button for each charm');
+
+  it('scrolls to charms when buttons are clicked');
 });
