5fc9e70d77ff5c1a1550069c2c63273ec9c15f22
Rick Harding <rick.harding@canonical.com>
2013-03-05 16:28:55 -0500

    more linter happiness

--- app/subapps/browser/views/fullscreen.js
+++ app/subapps/browser/views/fullscreen.js
@@ -5,19 +5,49 @@
 YUI.add('subapp-browser-fullscreen', function(Y) {
   var ns = Y.namespace('juju.browser.views');
 
+  /**
+   * Browser Sub App for the Juju Gui.
+   *
+   * @class FullScreen
+   * @extends {Y.View}
+   *
+   */
   ns.FullScreen = Y.Base.create('browser-view-fullscreen', Y.View, [], {
     // template: views.Templates.browser,
 
+    /**
+     * @attribute events
+     *
+     */
     events: {},
 
-    initializer: function() {},
+    /**
+     * Gereral YUI initializer.
+     *
+     * @method initializer
+     * @param {Object} cfg configuration object.
+     *
+     */
+    initializer: function(cfg) {},
 
+    /**
+     * Render out the view to the DOM.
+     *
+     * @method render
+     *
+     */
     render: function() {
       console.log('rendered in view one');
       this.get('container').setHTML('Hey! I\'m view one');
       return this;
     },
 
+    /**
+     * Destroy this view and clear from the dom world.
+     *
+     * @method destructor
+     *
+     */
     destructor: function() {}
 
   }, {
