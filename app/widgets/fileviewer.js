3de0888f11acfd04c44be08de6d3c35f542ff8df
j.c.sackett <jcsackett@canonical.com>
2013-03-12 20:27:27 +0000

    Comments

--- app/widgets/fileviewer.js
+++ app/widgets/fileviewer.js
@@ -1,14 +1,37 @@
 'use strict';
 
-
+/**
+ * Fileviewer widget; a syntax colored view of code with linenumbers.
+ *
+ * @namespace juju
+ * @module browser
+ * @submodule widgets.fileviewer
+ */
 YUI.add('browser-fileviewer-widget', function(Y) {
   var sub = Y.Lang.sub,
       ns = Y.namespace('juju.widgets.browser');
 
+  /**
+   * FileViewer
+   *
+   * @class FileViewer
+   * @extends {Y.Widget}
+   */
   ns.FileViewer = Y.Base.create('file-viewer', Y.Widget, [], {
-
+    /**
+     * The pre block template for the pretty printer
+     *
+     * @property TEMPLATE
+     * @type {String} 
+     */
     TEMPLATE: '<pre class="prettyprint linenums hidden">{code}</pre>',
 
+    /**
+     * Sets up the dom and processes the pretty printing.
+     *
+     * @method renderUI
+     * @param {undefined}; Mutates only. 
+     */
     renderUI: function() {
       var content = sub(this.TEMPLATE, {code: this.get('code')});
       this.get('contentBox').setHTML(content);
@@ -18,6 +41,11 @@ YUI.add('browser-fileviewer-widget', function(Y) {
 
   }, {
     ATTRS: {
+      /**
+       * @attribute code
+       * @default ''
+       * @type {String}
+       */
       code: {
         value: ''
       }
