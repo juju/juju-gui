a246e04be94f0113ea54ae0b084413eb53e22a32
Nicola Larosa <nicola.larosa@canonical.com>
2013-03-13 18:02:04 +0100

    Increase mocha-phantomjs timeout to avoid testing problems.

--- test-server.sh
+++ test-server.sh
@@ -8,7 +8,7 @@ if [ -n "$2" ]; then
     xdg-open http://localhost:8084/test/index.html
     fg %1
 else
-    mocha-phantomjs http://localhost:8084/test/index.html
+    mocha-phantomjs -t 40000 http://localhost:8084/test/index.html
     status=$?
     kill %1
     exit $status
