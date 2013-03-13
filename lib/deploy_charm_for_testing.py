0fc7080bf0cb3684430710beb4c95067bdcb990f
Benjamin Saller <bcsaller@gmail.com>
2013-03-13 15:13:15 -0400

    log around ssh internal ip usage

--- lib/deploy_charm_for_testing.py
+++ lib/deploy_charm_for_testing.py
@@ -103,6 +103,7 @@ def main(options=parse, print=print, juju=juju, wait_for_service=wait_for_servic
             internal_ip = subprocess.check_output(
                 "euca-describe-instances | grep INSTANCE | grep juju-juju-gui-testing-instance-1 | awk '{print $11;}'", shell=True).strip()
             os.environ['JUJU_INTERNAL_IP'] = internal_ip
+            print ('Storing Internal IP as %s' % internal_ip)
             subprocess.check_call("euca-associate-address -i %s %s" % (instance_id, instance_ip), shell=True)
             print('Assigned IP to %s' % instance_id)
 
