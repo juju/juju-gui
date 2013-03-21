0c37bdfe5017f59fb27a7d0646c1643a872d2da2
j.c.sackett <jcsackett@canonical.com>
2013-03-21 19:33:32 +0000

    Rewrote tests into mocha style.

--- test/test_indicator.js
+++ test/test_indicator.js
@@ -1,219 +1,151 @@
-/* Copyright (c) 2011, Canonical Ltd. All rights reserved. */
+'use strict';
 
-YUI.add('lp.indicator.test', function (Y) {
-    var tests = Y.namespace('lp.indicator.test');
-    tests.suite = new Y.Test.Suite('Indicator Tests');
-    var Assert = Y.Assert;
+describe('indicator', function() {
+  var container, Y;
 
-    // add the suite to the NS for the testrunner.js to find
-    tests.suite.add(new Y.Test.Case({
+  before(function(done) {
+    Y = YUI(GlobalConfig).use(['browser-indicator', 'node'], function(Y) {
+      done();
+    });
+  });
 
-        name: 'indicator_tests',
+  beforeEach(function() {
+    container = Y.Node.create('<div id="container"></div>');
+    Y.one('body').prepend(container);
+  });
 
-        setUp: function () {
-            this.div = Y.Node.create('<div/>');
-            // Generate an id so we can keep these around as we work.
-            this.div.generateID();
-            // We want to store this for the testing of the target, after that
-            // we can just use this.div.
-            this.div_id = this.div.get('id');
-            Y.one('body').appendChild(this.div);
-        },
+  afterEach(function() {
+    container.remove(true);
+  });
 
-        tearDown: function () {
-            // Delete the reference to this.div so we can recreate new ones for
-            // each test without worry.
-            this.div.remove();
-            delete this.div;
-            if (Y.Lang.isValue(this.indicator)) {
-                this.indicator.destroy();
-            }
-        },
+  it('exists', function() {
+    var indicator = new Y.juju.browser.widgets.Indicator();
+    assert.isObject(indicator);
+  });
 
-        test_target_attribute: function () {
-            // Constrain attribute should be set from passing in target.
-            var test_node = Y.one('#' + this.div_id);
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: test_node
-            });
-            this.indicator.render();
-            Assert.areEqual(test_node, this.indicator.get('target'));
-        },
+  it('gets target from config', function() {
+    var indicator = new Y.juju.browser.widgets.Indicator({target: container});
+    indicator.render();
+    assert.equal(container, indicator.get('target'));
+  });
 
-        test_indicator_appended_to_parent: function() {
-            // Indicator node is appended to target's parent, rather
-            // than target or body.
-            var child_div = Y.Node.create('<div/>');
-            // We need to create some nesting to really ensure
-            // the test is good.
-            this.div.appendChild(child_div);
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: child_div
-            });
-            this.indicator.render();
-            // this.div is actually the parentNode now.
-            Assert.areEqual(
-                this.div,
-                this.indicator.get('boundingBox').get('parentNode'));
-        },
+  it('appends itself to target parent', function() {
+    var child = Y.Node.create('<div/>');
+    container.appendChild(child);
+    var indicator = new Y.juju.browser.widgets.Indicator({target: child});
+    indicator.render();
+    assert.equal(container, indicator.get('boundingBox').get('parentNode'));
+  });
 
-        test_indicator_has_loading_icon: function () {
-            // The indicator should have a loading image added
-            // to the contentBox.
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: this.div
-            });
-            this.indicator.render();
-            var content = this.indicator.get('boundingBox');
-            var test = content.getContent();
-            var img = content.one('img');
-            Assert.areEqual('file:///@@/spinner-big', img.get('src'));
-        },
+  it('has a loading icon', function() {
+    var indicator = new Y.juju.browser.widgets.Indicator({target: container});
+    indicator.render();
+    var content = indicator.get('boundingBox'),
+        test = content.getContent(),
+        img = content.one('img');
+    assert.equal('file:///@@/spinner-big', img.get('src'));
+  });
 
-        test_indiciator_starts_invisible: function () {
-            // Indicator widgets should start hidden.
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: this.div
-            });
-            this.indicator.render();
-            Assert.isFalse(this.indicator.get('visible'));
-            Assert.isTrue(this.indicator.get('boundingBox').hasClass(
-                'yui3-overlay-indicator-hidden'));
-        },
+  it('starts invisible',  function() {
+    // Indicator widgets should start hidden.
+    var indicator = new Y.juju.browser.widgets.Indicator({target: container});
+    indicator.render();
+    assert.isFalse(indicator.get('visible'));
+    assert.isTrue(indicator.get('boundingBox').hasClass(
+        'yui3-browser-indicator-hidden'));
+  });
 
-        test_set_busy_shows_overlay: function() {
-            // setBusy should show the overlay.
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: this.div
-            });
-            this.indicator.render();
-            this.indicator.setBusy();
-            Assert.isTrue(this.indicator.get('visible'));
-            Assert.isFalse(this.indicator.get('boundingBox').hasClass(
-                'yui3-overlay-indicator-hidden'));
-        },
+  it('shows shows on setBusy', function() {
+    var indicator = new Y.juju.browser.widgets.Indicator({target: container});
+    indicator.render();
+    indicator.setBusy();
+    assert.isTrue(indicator.get('visible'));
+    assert.isFalse(indicator.get('boundingBox').hasClass(
+      'yui3-overlay-indicator-hidden'));
+  });
 
-        test_size_matches_on_set_busy: function() {
-            // Indicator should always resize when target changes size.
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: this.div
-            });
-            this.indicator.render();
-            // Mess with the size of target div.
-            var expected_width = 800;
-            var expected_height = 600;
-            this.div.set('offsetWidth', expected_width);
-            this.div.set('offsetHeight', expected_height);
-            Assert.areNotEqual(
-                expected_width,
-                this.indicator.get('boundingBox').get('offsetWidth'));
-            Assert.areNotEqual(
-                expected_height,
-                this.indicator.get('boundingBox').get('offsetHeight'));
-            this.indicator.setBusy();
-            Assert.areEqual(
-                expected_width,
-                this.indicator.get('boundingBox').get('offsetWidth'));
-            Assert.areEqual(
-                expected_height,
-                this.indicator.get('boundingBox').get('offsetHeight'));
-        },
+  it('size matches on setBusy', function() {
+    var indicator = new Y.juju.browser.widgets.Indicator({target: container});
+    indicator.render();
+    // Mess with the size of target div.
+    var expected_width = 800,
+        expected_height = 600;
+    container.set('offsetWidth', expected_width);
+    container.set('offsetHeight', expected_height);
+    assert.notEqual(
+      expected_width,
+      indicator.get('boundingBox').get('offsetWidth'));
+    assert.notEqual(
+      expected_height,
+      indicator.get('boundingBox').get('offsetHeight'));
+    indicator.setBusy();
+    assert.equal(
+      expected_width,
+      indicator.get('boundingBox').get('offsetWidth'));
+    assert.equal(
+      expected_height,
+      indicator.get('boundingBox').get('offsetHeight'));
+  });
 
-        test_position_matches_on_set_busy: function() {
-            // Indicator should always reposition itself before setBusy.
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: this.div
-            });
-            this.indicator.render();
-            // Change the position of target div.
-            var expected_xy = [100, 300];
-            this.div.setXY(expected_xy);
-            var actual_xy = this.indicator.get('boundingBox').getXY();
-            Assert.areNotEqual(expected_xy[0], actual_xy[0]);
-            Assert.areNotEqual(expected_xy[1], actual_xy[1]);
-            this.indicator.setBusy();
-            var final_xy = this.indicator.get('boundingBox').getXY();
-            Assert.areEqual(expected_xy[0], final_xy[0]);
-            Assert.areEqual(expected_xy[1], final_xy[1]);
+  it('position matches on setBusy', function() {
+    // Indicator should always reposition itself before setBusy.
+    var indicator = new Y.juju.browser.widgets.Indicator({target: container});
+    indicator.render();
+    // Change the position of target div.
+    var expected_xy = [100, 300],
+        actual_xy = indicator.get('boundingBox').getXY();
+    container.setXY(expected_xy);
+    assert.notEqual(expected_xy[0], actual_xy[0]);
+    assert.notEqual(expected_xy[1], actual_xy[1]);
+    indicator.setBusy();
+    var final_xy = indicator.get('boundingBox').getXY();
+    assert.equal(expected_xy[0], final_xy[0]);
+    assert.equal(expected_xy[1], final_xy[1]);
+  });
 
-        },
+  it('hides on success', function() {
+    var indicator = new Y.juju.browser.widgets.Indicator({target: container});
+    indicator.render();
+    indicator.setBusy();
+    indicator.success();
+    assert.isFalse(indicator.get('visible'));
+    assert.isTrue(indicator.get('boundingBox').hasClass(
+      'yui3-overlay-indicator-hidden'));
+  });
 
-        test_success_hides_overlay: function() {
-            // Calling success should hide the overlay.
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: this.div
-            });
-            this.indicator.render();
-            this.indicator.setBusy();
-            this.indicator.success();
-            Assert.isFalse(this.indicator.get('visible'));
-            Assert.isTrue(this.indicator.get('boundingBox').hasClass(
-                'yui3-overlay-indicator-hidden'));
-        },
+  it('can have a success callback', function() {
+    var called = false,
+        indicator = new Y.juju.browser.widgets.Indicator({
+      target: container,
+      success_action: function () {
+        called = true;
+      }
+    });
+    indicator.render();
+    indicator.success();
+    assert.isTrue(called);
+  });
 
-        test_success_callback: function() {
-            // We should be able to pass in a callback as success_action.
-            var called = false;
-            var callback = function() {
-                called = true;
-            };
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: this.div,
-                success_action: callback
-            });
-            this.indicator.render();
-            this.indicator.success();
-            Assert.isTrue(called);
-        },
+  it('hides on error', function() {
+    var indicator = new Y.juju.browser.widgets.Indicator({target: container});
+    indicator.render();
+    indicator.setBusy();
+    indicator.error();
+    assert.isFalse(indicator.get('visible'));
+    assert.isTrue(indicator.get('boundingBox').hasClass(
+      'yui3-overlay-indicator-hidden'));
+  });
 
-        test_focus_target_scrolls_success: function () {
-            // Provided function scroll_to_target should scroll to target.
-            var viewport = Y.DOM.viewportRegion();
-            this.div.set('offsetWidth', viewport.right + 1000);
-            this.div.set('offsetHeight', viewport.bottom + 1000);
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: this.div,
-                success_action: Y.lp.app.indicator.actions.scroll_to_target
-            });
-            this.indicator.render();
-            window.scrollTo(1000, 1000);
-            Assert.areEqual(1000, Y.DOM.docScrollX());
-            Assert.areEqual(1000, Y.DOM.docScrollY());
-            this.indicator.setBusy();
-            this.indicator.success();
-            var expected_xy = this.indicator.get('target').getXY();
-            Assert.areEqual(expected_xy[0], Y.DOM.docScrollX());
-            Assert.areEqual(expected_xy[1], Y.DOM.docScrollY());
-        },
-
-        test_error_hides_overlay: function () {
-            // Calling error should hide the overlay.
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: this.div
-            });
-            this.indicator.render();
-            this.indicator.setBusy();
-            this.indicator.error();
-            Assert.isFalse(this.indicator.get('visible'));
-            Assert.isTrue(this.indicator.get('boundingBox').hasClass(
-                'yui3-overlay-indicator-hidden'));
-        },
-
-        test_error_callback: function() {
-            // We should be able to pass in a callback as error_action.
-            var called = false;
-            var callback = function() {
-                called = true;
-            };
-            this.indicator = new Y.lp.app.indicator.OverlayIndicator({
-                target: this.div,
-                error_action: callback
-            });
-            this.indicator.render();
-            this.indicator.error();
-            Assert.isTrue(called);
-        }
-    }));
-
-
-}, '0.1', {'requires': ['test', 'lp.app.indicator']});
+  it('can have an error callback', function() {
+    var called = false,
+        indicator = new Y.juju.browser.widgets.Indicator({
+      target: container,
+      error_action: function() {
+        called = true; 
+      }
+    });
+    indicator.render();
+    indicator.error();
+    assert.isTrue(called);
+  });
+});
