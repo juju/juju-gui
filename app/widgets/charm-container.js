a792df9692d0aa5b97e9676d34c3ad9752840253
j.c.sackett <jcsackett@canonical.com>
2013-03-27 14:55:19 +0000

    Fleshed out charm-container.

--- app/widgets/charm-container.js
+++ app/widgets/charm-container.js
@@ -7,11 +7,52 @@ YUI.add('browser-charm-container', function(Y) {
   ns.CharmContainer = Y.Base.create('browser-charm-container', Y.Widget, [
     Y.WidgetParent
   ], {
-    TEMPLATE: '<div></div>'
+    _events: [],
+
+    TEMPLATE: Y.namespace('juju.views').Templates['charm-small-widget'],
+
+    initializer: function(cfg) {
+      ns.CharmContainer.superclass.initializer.apply(this, cfg);
+      var total = this.get('children').size,
+          extra = total - this.get('cutoff');
+      this.set('extra', extra);
+      this.set(
+          'cutoff_children',
+          this.get('children').slize(0,this.get('cutoff')));
+    },
+
+    renderUI: function() {
+
+    }
   }, {
     ATTRs: {
+      defaultChildType: {
+        value: Y.juju.widgets.browser.CharmSmall
+      },
+
       cutoff: {
         value: 3
+      },
+
+      extra: {},
+
+      name: {
+        value: ''
+      },
+
+      cutoff_children: {
+        value: []
       }
     }
   });
+
+}, '0.1.0', {
+  requires: [
+    'base',
+    'browser-charm-small',
+    'handlebars',
+    'juju-templates',
+    'widget',
+    'widget-parent'
+  ]
+});
