d301f483d0cb90e187749702b8e3c62bea7e6956
Nicola Larosa <nicola.larosa@canonical.com>
2013-04-10 17:08:47 +0200

    Fixes per reviews.

--- test/test_env_go.js
+++ test/test_env_go.js
@@ -668,6 +668,7 @@
         err = evt.err;
         service_name = evt.service_name;
       });
+      msg = conn.last_message();
       conn.msg({
         RequestId: msg.RequestId,
         Error: 'service "yoursql" not found'
@@ -677,17 +678,25 @@
     });
 
     it('can destroy a service', function() {
-      env.destroy_service('mysql');
-      msg = conn.last_message();
+      var service_name = '';
+      env.destroy_service('mysql', function(evt) {
+        service_name = evt.service_name;
+      });
       var expected = {
         Type: 'Client',
+        Request: 'ServiceDestroy',
         Params: {
           ServiceName: 'mysql'
         },
-        Request: 'ServiceDestroy',
         RequestId: msg.RequestId
       };
+      msg = conn.last_message();
+      conn.msg({
+        RequestId: msg.RequestId,
+        Error: 'service "yoursql" not found'
+      });
       assert.deepEqual(expected, msg);
+      assert.equal(service_name, 'mysql');
     });
 
     it('handles failed destroy service', function() {
@@ -696,6 +705,7 @@
         err = evt.err;
         service_name = evt.service_name;
       });
+      msg = conn.last_message();
       conn.msg({
         RequestId: msg.RequestId,
         Error: 'service "yoursql" not found'
