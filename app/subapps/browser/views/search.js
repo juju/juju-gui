db663a7369c64faa8cf6ec817575532463590d28
j.c.sackett <jcsackett@canonical.com>
2013-04-15 13:18:59 +0000

    Stubbed out view.

--- app/subapps/browser/views/search.js
+++ app/subapps/browser/views/search.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 /**
  * Provides searching functionality for the charm browser.
  *
@@ -11,6 +12,17 @@ YUI.add('subapp-browser-searchview', function(Y) {
   var ns = Y.namespace('juju.browser.views'),
       views = Y.namespace('juju.views'),
       widgets = Y.namespace('juju.widgets');
+
+  ns.BrowserSearchView = Y.Base.create('browser-view-searchview', Y.View, [
+    widgets.browser.IndicatorManager,
+    Y.Event.EventTracker
+  ], {
+    render: function() {
+
+    }
+  },{
+    ATTRS: {}
+  });
 
 }, '0.1.0', {
   requires: [
