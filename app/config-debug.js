62ad4378790fd01c4bef8abb9c72e7e451135311
Rick Harding <rick.harding@canonical.com>
2013-04-29 09:29:41 -0400

    Remove the debug endpoint

--- app/config-debug.js
+++ app/config-debug.js
@@ -12,14 +12,13 @@ var juju_config = {
   // SubApps.
   consoleEnabled: true,
   charm_store_url: 'http://jujucharms.com/',
-  charmworldURL: 'http://ec2-54-224-248-114.compute-1.amazonaws.com/',
-  // charmworldURL: 'http://staging.jujucharms.com/',
-  // The config has three socket settings.  socket_port and socket_protocol
-  // modify the current application url to determine the websocket url (always
-  // adding "/ws" as the final path).  socket_url sets the entire websocket
-  // url.  For backwards compatibility in the GUI charm, if you provide the
-  // socket port and/or protocol *and* the socket_url, the socket_url will be
-  // ignored (the port/protocol behavior overrides socket_url).
+  charmworldURL: 'http://staging.jujucharms.com/',
+  The config has three socket settings.  socket_port and socket_protocol
+  modify the current application url to determine the websocket url (always
+  adding "/ws" as the final path).  socket_url sets the entire websocket
+  url.  For backwards compatibility in the GUI charm, if you provide the
+  socket port and/or protocol *and* the socket_url, the socket_url will be
+  ignored (the port/protocol behavior overrides socket_url).
   socket_protocol: 'ws',
   socket_port: 8081,
   user: 'admin',
