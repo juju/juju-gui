d11316a2708395b8ea7261b3a79dcae0efd73b1c
Rick Harding <rick.harding@canonical.com>
2013-04-29 10:26:11 -0400

    Fix comment

--- app/config-debug.js
+++ app/config-debug.js
@@ -13,12 +13,12 @@ var juju_config = {
   consoleEnabled: true,
   charm_store_url: 'http://jujucharms.com/',
   charmworldURL: 'http://staging.jujucharms.com/',
-  The config has three socket settings.  socket_port and socket_protocol
-  modify the current application url to determine the websocket url (always
-  adding "/ws" as the final path).  socket_url sets the entire websocket
-  url.  For backwards compatibility in the GUI charm, if you provide the
-  socket port and/or protocol *and* the socket_url, the socket_url will be
-  ignored (the port/protocol behavior overrides socket_url).
+  // The config has three socket settings.  socket_port and socket_protocol
+  // modify the current application url to determine the websocket url (always
+  // adding "/ws" as the final path).  socket_url sets the entire websocket
+  // url.  For backwards compatibility in the GUI charm, if you provide the
+  // socket port and/or protocol *and* the socket_url, the socket_url will be
+  // ignored (the port/protocol behavior overrides socket_url).
   socket_protocol: 'ws',
   socket_port: 8081,
   user: 'admin',
