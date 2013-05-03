e11197e2453d0e32cddad37531a6528e4da0aedc
Benjamin Saller <bcsaller@gmail.com>
2013-05-03 00:10:41 -0700

    missing files

---
+++ app/store/env/simulator.js
@@ -0,0 +1,304 @@
+'use strict';
+
+/**
+
+  Simulate various types of change to a sandbox environment.
+  These can include things like
+    - unit/relation failure/recovery
+    - unit count changes
+    - landscape annotation changes
+    - position annotation changes
+
+
+  @module env
+  @submodule env.simulator
+*/
+
+YUI.add('juju-fakebackend-simulator', function(Y) {
+
+  var models = Y.namespace('juju.models');
+  // How often should we run by default (ms).
+  var DEFAULT_INTERVAL = 3000;
+  var RAND = function(prob) {
+    return Math.random() <= prob;
+  };
+
+  var DEFAULT_AGENTS = {
+    landscape: {
+      start: function(context) {
+        // For landscape to work we need to ensure the top level
+        // env has the proper setup. This happens once.
+        var db = context.state.db;
+        var envAnno = db.environment.get('annotations');
+        envAnno['landscape-url'] = 'http://landscape.com';
+        envAnno['landscape-computers'] = '/computers/criteria/environment:test';
+        envAnno['landscape-reboot-alert-url'] =
+          '+alert:computer-reboot/info#power';
+        envAnno['landscape-security-alert-url'] =
+          '+alert:security-upgrades/packages/list?filter=security';
+        context.state.updateAnnotations('env', envAnno);
+      },
+
+      select: {
+        list: 'units',
+        random: 0.01
+      },
+
+      run: function(context) {
+        context.selection.each(function(unit) {
+          // Toggle landscape attributes as though they
+          var annotations = unit.annotations || {};
+          var changed = false;
+          if (!annotations['landscape-computer']) {
+            annotations['landscape-computer'] = '+unit:' + unit.urlName;
+          }
+
+          // Toggle some annotations.
+          if (RAND(0.3)) {
+            annotations['landscape-needs-reboot'] = !annotations[
+              'landscape-needs-reboot'];
+              changed = true;
+          }
+          if (RAND(0.3)) {
+            annotations['landscape-security-upgrades'] = !annotations[
+              'landscape-security-upgrades'];
+            changed = true;
+          }
+          if (changed) {
+            context.state.updateAnnotations(unit.id, annotations);
+          }
+        });
+      }
+    },
+
+    unitStatus: {
+      select: {
+        list: 'units',
+        random: 0.05
+      },
+      run: function(context) {
+        context.selection.each(function(unit) {
+          var roll = Math.random();
+          if (roll <= 0.25) {
+            unit.agent_state = 'started';
+          } else if (roll <= 0.5) {
+            unit.agent_state = 'install-error';
+          } else if (roll <= 0.75) {
+            unit.agent_state = 'pending';
+          }
+          //TODO: get_relations_for_service()
+          // choose one and set it as a unit.relation_errors entry.
+          // update the changes['relations'];
+
+          // Put in delta since there is no API for this.
+          context.state.changes.units[unit.id] = [unit, true];
+        });
+
+      }
+    }
+
+  };
+
+  /**
+    Agents of backend change. Created automatically,
+    see Simulator.ATTRS.agents
+
+    @class Agent
+    */
+  function Agent(config) {
+    Agent.superclass.constructor.apply(this, arguments);
+  }
+  Agent.NAME = 'Agent';
+  Agent.ATTRS = {
+ };
+
+  Y.extend(Agent, Y.Base, {
+    /**
+      This tells `Y.Base` that it should create ad-hoc attributes for config
+      properties passed to Model's constructor. This makes it possible to
+      instantiate a model and set a bunch of attributes without having to subclass
+      `Y.Model` and declare all those attributes first.
+
+      @property _allowAdHocAttrs
+      @type Boolean
+      @default true
+      @protected
+      @since 3.5.0
+     **/
+    _allowAdHocAttrs: true,
+
+    getContext: function () {
+      var context = this.getAttrs();
+      delete context.initialized;
+      delete context.destroyed;
+      return context;
+    },
+
+    start: function() {
+      var context = this.getContext();
+      if (context.start) {
+        context.start(context);
+      }
+    },
+
+    select: function(context) {
+      var select = context.select;
+      var db = context.state.db;
+      if (!select) {
+        return;
+      }
+
+      if (select.list) {
+        context.selection = db[select.list];
+      }
+      if (select.filter) {
+        // filter should return {asList: true}
+        context.selection = select.filter(context);
+      }
+      if (select.random) {
+        // This requires that a selection is present.
+        context.selection = context.selection.filter(
+          {asList: true}, function () {
+          return Math.random() <= select.random;
+        });
+      }
+    },
+
+    run: function() {
+      var context = this.getContext();
+
+      // Update selection to act on.
+      if (context.select) {
+        this.select(context);
+      }
+      context.run(context);
+    }
+  });
+
+  /**
+  Humble make-believe manager.
+
+  @class Simulator
+  */
+  function Simulator(config) {
+    // Invoke Base constructor, passing through arguments.
+    Simulator.superclass.constructor.apply(this, arguments);
+  }
+
+  Simulator.NAME = 'Simulator';
+  Simulator.ATTRS = {
+    agents: {},
+    state: {},
+    useDefaultAgents: {value: true},
+    interval: {value: DEFAULT_INTERVAL}
+  };
+
+  Y.extend(Simulator, Y.Base, {
+
+    /**
+    Initializes.
+
+    @method initializer
+    @return {undefined} Nothing.
+    */
+    initializer: function() {
+      this._agents = null;
+      this._scheduler = null;
+      /**
+       * Fired on each interval of the scheduler
+       * after agents have run.
+       * @event tick
+       */
+      this.publish('tick');
+
+      // WHen our agents property changes
+      // regenerate our agents mapping.
+      if (this.get('useDefaultAgents')) {
+        var agents = this.get('agents') || {};
+        this.set('agents', Y.mix(agents, DEFAULT_AGENTS));
+      }
+      this._updateAgents();
+      this.after('stateChange', this._updateAgents, this);
+      this.after('agentsChange', this._updateAgents, this);
+      return this;
+    },
+
+    /**
+    Cleanup on destruction.
+    @method destructor
+    */
+    destructor : function() {
+      this.stop();
+    },
+
+    _updateAgents: function(evt) {
+      var decls = this.get('agents');
+      var state = this.get('state');
+      var agents = {};
+
+      Y.each(decls, function(spec, name) {
+        spec = spec || {};
+        spec.state = state;
+        agents[name] = new Agent(spec);
+        agents[name].start();
+      });
+      this._agents = agents;
+    },
+
+    /**
+    Start each agent firing 'tick' events when pushing changes.
+
+    @method start
+    @chainable
+    */
+    start: function() {
+      var self = this,
+          state = this.get('state');
+      if (this._scheduler) {
+        // Already started, so restart.
+        this.stop();
+      }
+
+      var scheduler = function() {
+        Y.each(self._agents, function(agent, name) {
+          agent.run(state);
+        });
+        self.fire('tick');
+      };
+
+      this._scheduler = Y.later(self.get('interval'),
+                                self, scheduler, undefined,
+                                true);
+
+      // Invoke on start as well.
+      scheduler();
+      return this;
+
+    },
+
+    /**
+    Stop agents from firing or mutating backend.
+    @method stop
+    @chainable
+    */
+    stop: function() {
+      if (this._scheduler) {
+        this._scheduler.cancel();
+        this._scheduler = null;
+      }
+      return this;
+    }
+
+  });
+  Y.namespace('juju.environments').Simulator = Simulator;
+
+
+}, '0.1.0', {
+  requires: [
+    'base',
+    'event',
+    'juju-models',
+    'promise',
+    'yui-later'
+  ]
+});
---
+++ test/test_simulator.js
@@ -0,0 +1,90 @@
+
+'use strict';
+
+(function() {
+
+  describe('FakeBackend.simulator', function() {
+    var requires = ['node',
+      'juju-tests-utils', 'juju-models', 'juju-charm-models',
+      'juju-fakebackend-simulator'];
+    var Y, state, Simulator, simulator, utils;
+
+    before(function(done) {
+      Y = YUI(GlobalConfig).use(requires, function(Y) {
+        Simulator = Y.namespace('juju.environments').Simulator;
+        utils = Y.namespace('juju-tests.utils');
+        done();
+      });
+    });
+
+    beforeEach(function(done) {
+      state = utils.makeFakeBackendWithCharmStore();
+      var fixture = utils.loadFixture('data/sample-improv.json', false);
+      state.importEnvironment(fixture, function(result) {
+        done();
+      });
+    });
+
+    afterEach(function() {
+      simulator.destroy();
+      state.destroy();
+    });
+
+    it('should be able to spawn a simulation engine', function(done) {
+      simulator = new Simulator({
+        state: state,
+        useDefaultAgents: false
+      });
+      simulator.on('tick', function() {
+        // and the default simulator should tick when running.
+        // the state was properly assigned.
+        assert.equal(this.get('state'), state);
+        done();
+      });
+      simulator.start();
+    });
+
+    it('should be able to run decalred agents', function(done) {
+      var db = state.db;
+      var service = db.services.getById('wordpress');
+      var units = db.units.get_units_for_service(service);
+      simulator = new Simulator({
+        state: state,
+        useDefaultAgents: false,
+        agents: {
+          test: {
+            select: {
+              list: 'services',
+              random: 1.0, // Trigger the random path, but 100% of the time.
+              filter: function(context) {
+                var selection = context.selection.filter(
+                  {asList: true},
+                  function(service) {
+                    // Filter down to a list of one for testing.
+                    return service.get('id') === 'wordpress';
+                  });
+                return selection;
+              }},
+              run: function(context) {
+                context.selection.each(function(service) {
+                  // Add 10 units
+                  context.state.addUnit(service.get('id'), 10);
+                });
+              }
+          }}});
+
+      simulator.on('tick', function() {
+        // Verify that our sample agent
+        // added units.
+        units = db.units.get_units_for_service(service);
+        assert.equal(units.length, 11);
+        done();
+      });
+
+     assert.equal(units.length, 1);
+     simulator.start();
+    });
+
+  });
+
+})();
