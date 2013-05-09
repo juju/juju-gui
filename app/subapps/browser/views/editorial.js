994759c051422ad10891994b248bcd1cd97afcff
Rick Harding <rick.harding@canonical.com>
2013-05-09 11:39:03 -0400

    make the linter happy

--- app/subapps/browser/views/editorial.js
+++ app/subapps/browser/views/editorial.js
@@ -22,224 +22,224 @@ YUI.add('subapp-browser-editorial', function(Y) {
    * @extends {juju.browser.views.Editorial}
    *
    */
-  ns.EditorialView = Y.Base.create('browser-view-sidebar', ns.CharmResults,
-    [], {
-    template: views.Templates.editorial,
+  ns.EditorialView = Y.Base.create('browser-view-sidebar', ns.CharmResults, [],
+      {
+        template: views.Templates.editorial,
 
-    // How many of each charm container do we show by default.
-    cutoffs: {
-      sidebar: {
-        featured: 2,
-        popular: 2,
-        'new': 2
-      },
-      fullscreen: {
-        featured: 6,
-        popular: 3,
-        'new': 3
-      }
-    },
-
-    events: {
-      '.charm-token': {
-        click: '_handleCharmSelection'
-      }
-    },
-
-    /**
-        When selecting a charm from the list make sure we re-route the app to
-        the details view with that charm selected.
-
-        @method _handleCharmSelection
-        @param {Event} ev the click event handler for the charm selected.
-
-     */
-    _handleCharmSelection: function(ev) {
-      ev.halt();
-      var charm = ev.currentTarget;
-      var charmID = charm.getData('charmid');
-
-      // Update the UI for the active one.
-      if (!this.get('isFullscreen')) {
-        this._updateActive(ev.currentTarget);
-      }
-
-      var change = {
-        charmID: charmID
-      };
-
-      this.fire('viewNavigate', {change: change});
-    },
-
-    /**
-      Update the node in the editorial list marked as 'active'.
-
-      @method _updateActive
-      @param {Node} clickTarget the charm-token clicked on to activate.
-
-    */
-    _updateActive: function(clickTarget) {
-      // Remove the active class from any nodes that have it.
-      Y.all('.yui3-charmtoken.active').removeClass('active');
-
-      // Add it to the current node.
-      if (clickTarget) {
-        clickTarget.ancestor('.yui3-charmtoken').addClass('active');
-      }
-    },
-
-    /**
-     * Generates a message to the user based on a bad api call.
-     * @method apiFailure
-     * @param {Object} data the json decoded response text.
-     * @param {Object} request the original io_request object for debugging.
-     */
-    apiFailure: function(data, request) {
-      this._apiFailure(data, request, 'Failed to load editorial content.');
-    },
-
-    /**
-     * General YUI initializer.
-     *
-     * @method initializer
-     * @param {Object} cfg configuration object.
-     *
-     */
-    initializer: function(cfg) {
-      // Hold onto charm data so we can pass model instances to other views when
-      // charms are selected.
-      this._cacheCharms = new models.BrowserCharmList();
-
-      // Watch for changse to the activeID so that we can mark/unmark active
-      // as required.
-      this.on('activeIDChange', function(ev) {
-        var id = ev.newVal;
-        if (id) {
-          id = this.get('container').one(
-              '.charm-token[data-charmid="' + id + '"]');
-        }
-        this._updateActive(id);
-      });
-    },
-
-    /**
-     * Load the editorial content into the container specified.
-     *
-     * @method render
-     * @param {Node} container An optional node to override where it's going.
-     *
-     */
-    render: function() {
-      var tpl = this.template(this.getAttrs()),
-          tplNode = Y.Node.create(tpl),
-          store = this.get('store');
-
-      this.showIndicator(this.get('renderTo'));
-
-      // By default we grab the editorial content from the api to use for
-      // display.
-      this.get('store').interesting({
-        'success': function(data) {
-          var cutoffs;
-          // Add featured charms
-          var featuredCharms = this.get('store').resultsToCharmlist(
-              data.result.featured);
-          var featuredContainer = tplNode.one('.featured');
-          if (this.get('isFullscreen')) {
-            cutoffs = this.cutoffs.fullscreen;
-          } else {
-            cutoffs = this.cutoffs.sidebar;
-          }
-
-          var containerCfg = {
-            additionalChildConfig: {
-              size: this.get('isFullscreen') ? 'large' : 'small'
-            }
-          };
-
-          var featuredCharmContainer = new widgets.browser.CharmContainer(
-              Y.merge({
-                name: 'Featured Charms',
-                cutoff: cutoffs.featured,
-                children: featuredCharms.map(function(charm) {
-                  return charm.getAttrs();
-                })},
-              containerCfg));
-          featuredCharmContainer.render(featuredContainer);
-
-          // Add popular charms
-          var popularCharms = this.get('store').resultsToCharmlist(
-              data.result.popular);
-          var popularContainer = tplNode.one('.popular');
-          var popularCharmContainer = new widgets.browser.CharmContainer(
-              Y.merge({
-                name: 'Popular Charms',
-                cutoff: cutoffs.popular,
-                children: popularCharms.map(function(charm) {
-                  return charm.getAttrs();
-                })},
-              containerCfg));
-          popularCharmContainer.render(popularContainer);
-
-          // Add in the charm tokens for the new as well.
-          var newContainer = tplNode.one('.new');
-          var newCharms = this.get('store').resultsToCharmlist(
-              data.result['new']);
-          var newCharmContainer = new widgets.browser.CharmContainer(
-              Y.merge({
-                name: 'New Charms',
-                cutoff: cutoffs['new'],
-                children: newCharms.map(function(charm) {
-                  return charm.getAttrs();
-                })},
-              containerCfg));
-          newCharmContainer.render(newContainer);
-
-          var container = this.get('container');
-          container.append(tplNode);
-          this.get('renderTo').setHTML(container);
-          this.hideIndicator(this.get('renderTo'));
-
-          // Add the charms to the cache for use in other views.
-          // Start with a reset to empty any current cached models.
-          this._cacheCharms.reset(newCharms);
-          this._cacheCharms.add(popularCharms);
-          this._cacheCharms.add(featuredCharms);
-          this.charmContainers = [
-            featuredCharmContainer,
-            newCharmContainer,
-            popularCharmContainer
-          ];
-
-          // Set the active charm if available.
-          var active = this.get('activeID');
-          if (active) {
-            this._updateActive(
-                container.one('.charm-token[data-charmid="' + active + '"]')
-            );
+        // How many of each charm container do we show by default.
+        cutoffs: {
+          sidebar: {
+            featured: 2,
+            popular: 2,
+            'new': 2
+          },
+          fullscreen: {
+            featured: 6,
+            popular: 3,
+            'new': 3
           }
         },
 
-        'failure': this.apiFailure
-      }, this);
-    },
+        events: {
+          '.charm-token': {
+            click: '_handleCharmSelection'
+          }
+        },
 
-    /**
-     * Destroy this view and clear from the dom world.
-     *
-     * @method destructor
-     *
-     */
-    destructor: function() {
-      if (this.charmContainers) {
-        Y.Array.each(this.charmContainers, function(container) {
-          container.destroy();
-        });
-      }
-    }
-  }, {
-    ATTRS: {}
-  });
+        /**
+            When selecting a charm from the list make sure we re-route the app to
+            the details view with that charm selected.
+
+            @method _handleCharmSelection
+            @param {Event} ev the click event handler for the charm selected.
+
+         */
+        _handleCharmSelection: function(ev) {
+          ev.halt();
+          var charm = ev.currentTarget;
+          var charmID = charm.getData('charmid');
+
+          // Update the UI for the active one.
+          if (!this.get('isFullscreen')) {
+            this._updateActive(ev.currentTarget);
+          }
+
+          var change = {
+            charmID: charmID
+          };
+
+          this.fire('viewNavigate', {change: change});
+        },
+
+        /**
+          Update the node in the editorial list marked as 'active'.
+
+          @method _updateActive
+          @param {Node} clickTarget the charm-token clicked on to activate.
+
+        */
+        _updateActive: function(clickTarget) {
+          // Remove the active class from any nodes that have it.
+          Y.all('.yui3-charmtoken.active').removeClass('active');
+
+          // Add it to the current node.
+          if (clickTarget) {
+            clickTarget.ancestor('.yui3-charmtoken').addClass('active');
+          }
+        },
+
+        /**
+         * Generates a message to the user based on a bad api call.
+         * @method apiFailure
+         * @param {Object} data the json decoded response text.
+         * @param {Object} request the original io_request object for debugging.
+         */
+        apiFailure: function(data, request) {
+          this._apiFailure(data, request, 'Failed to load editorial content.');
+        },
+
+        /**
+         * General YUI initializer.
+         *
+         * @method initializer
+         * @param {Object} cfg configuration object.
+         *
+         */
+        initializer: function(cfg) {
+          // Hold onto charm data so we can pass model instances to other views when
+          // charms are selected.
+          this._cacheCharms = new models.BrowserCharmList();
+
+          // Watch for changse to the activeID so that we can mark/unmark active
+          // as required.
+          this.on('activeIDChange', function(ev) {
+            var id = ev.newVal;
+            if (id) {
+              id = this.get('container').one(
+                  '.charm-token[data-charmid="' + id + '"]');
+            }
+            this._updateActive(id);
+          });
+        },
+
+        /**
+         * Load the editorial content into the container specified.
+         *
+         * @method render
+         * @param {Node} container An optional node to override where it's going.
+         *
+         */
+        render: function() {
+          var tpl = this.template(this.getAttrs()),
+              tplNode = Y.Node.create(tpl),
+              store = this.get('store');
+
+          this.showIndicator(this.get('renderTo'));
+
+          // By default we grab the editorial content from the api to use for
+          // display.
+          this.get('store').interesting({
+            'success': function(data) {
+              var cutoffs;
+              // Add featured charms
+              var featuredCharms = this.get('store').resultsToCharmlist(
+                  data.result.featured);
+              var featuredContainer = tplNode.one('.featured');
+              if (this.get('isFullscreen')) {
+                cutoffs = this.cutoffs.fullscreen;
+              } else {
+                cutoffs = this.cutoffs.sidebar;
+              }
+
+              var containerCfg = {
+                additionalChildConfig: {
+                  size: this.get('isFullscreen') ? 'large' : 'small'
+                }
+              };
+
+              var featuredCharmContainer = new widgets.browser.CharmContainer(
+                  Y.merge({
+                    name: 'Featured Charms',
+                    cutoff: cutoffs.featured,
+                    children: featuredCharms.map(function(charm) {
+                      return charm.getAttrs();
+                    })},
+                  containerCfg));
+              featuredCharmContainer.render(featuredContainer);
+
+              // Add popular charms
+              var popularCharms = this.get('store').resultsToCharmlist(
+                  data.result.popular);
+              var popularContainer = tplNode.one('.popular');
+              var popularCharmContainer = new widgets.browser.CharmContainer(
+                  Y.merge({
+                    name: 'Popular Charms',
+                    cutoff: cutoffs.popular,
+                    children: popularCharms.map(function(charm) {
+                      return charm.getAttrs();
+                    })},
+                  containerCfg));
+              popularCharmContainer.render(popularContainer);
+
+              // Add in the charm tokens for the new as well.
+              var newContainer = tplNode.one('.new');
+              var newCharms = this.get('store').resultsToCharmlist(
+                  data.result['new']);
+              var newCharmContainer = new widgets.browser.CharmContainer(
+                  Y.merge({
+                    name: 'New Charms',
+                    cutoff: cutoffs['new'],
+                    children: newCharms.map(function(charm) {
+                      return charm.getAttrs();
+                    })},
+                  containerCfg));
+              newCharmContainer.render(newContainer);
+
+              var container = this.get('container');
+              container.append(tplNode);
+              this.get('renderTo').setHTML(container);
+              this.hideIndicator(this.get('renderTo'));
+
+              // Add the charms to the cache for use in other views.
+              // Start with a reset to empty any current cached models.
+              this._cacheCharms.reset(newCharms);
+              this._cacheCharms.add(popularCharms);
+              this._cacheCharms.add(featuredCharms);
+              this.charmContainers = [
+                featuredCharmContainer,
+                newCharmContainer,
+                popularCharmContainer
+              ];
+
+              // Set the active charm if available.
+              var active = this.get('activeID');
+              if (active) {
+                this._updateActive(
+                    container.one('.charm-token[data-charmid="' + active + '"]')
+                );
+              }
+            },
+
+            'failure': this.apiFailure
+          }, this);
+        },
+
+        /**
+         * Destroy this view and clear from the dom world.
+         *
+         * @method destructor
+         *
+         */
+        destructor: function() {
+          if (this.charmContainers) {
+            Y.Array.each(this.charmContainers, function(container) {
+              container.destroy();
+            });
+          }
+        }
+      }, {
+        ATTRS: {}
+      });
 
 }, '0.1.0', {
   requires: [
--- app/subapps/browser/views/search.js
+++ app/subapps/browser/views/search.js
@@ -14,156 +14,158 @@ YUI.add('subapp-browser-searchview', function(Y) {
       widgets = Y.namespace('juju.widgets'),
       models = Y.namespace('juju.models');
 
-  ns.BrowserSearchView = Y.Base.create('browser-view-searchview', ns.CharmResults, [
-    views.utils.apiFailingView,
-    widgets.browser.IndicatorManager,
-    Y.Event.EventTracker
-  ], {
-    template: views.Templates.search,
+  ns.BrowserSearchView = Y.Base.create(
+      'browser-view-searchview',
+      ns.CharmResults, [
+        views.utils.apiFailingView,
+        widgets.browser.IndicatorManager,
+        Y.Event.EventTracker], {
+        template: views.Templates.search,
 
-    _bindEvents: function() {
-      this.events['.filterControl a'] = {
-        click: '_toggleFilters'
-      };
-    },
+        _bindEvents: function() {
+          this.events['.filterControl a'] = {
+            click: '_toggleFilters'
+          };
+        },
 
-    /**
-       When a filter is changed, catch the event and build a change object for
-       the subapp to generate a new route for.
+        /**
+           When a filter is changed, catch the event and build a change object
+           for the subapp to generate a new route for.
 
-       @method _filterChanged
-       @param {Event} ev the change event detected from the widget.
+           @method _filterChanged
+           @param {Event} ev the change event detected from the widget.
 
-     */
-    _filterChanged: function(ev) {
-      var filters = this.get('filters');
-      filters[ev.change.field] = ev.change.value;
-      var change = {
-        search: true,
-        filter: {}
-      };
-      change.filter[ev.change.field] = ev.change.value;
-      this.fire('viewNavigate', {change: change});
-    },
+         */
+        _filterChanged: function(ev) {
+          var filters = this.get('filters');
+          filters[ev.change.field] = ev.change.value;
+          var change = {
+            search: true,
+            filter: {}
+          };
+          change.filter[ev.change.field] = ev.change.value;
+          this.fire('viewNavigate', {change: change});
+        },
 
-    /**
-       Show/hide the filters based on the click of this control.
+        /**
+           Show/hide the filters based on the click of this control.
 
-       @method _toggleFilters
-       @param {Event} ev The click event from YUI.
+           @method _toggleFilters
+           @param {Event} ev The click event from YUI.
 
-     */
-    _toggleFilters: function(ev) {
-      ev.halt();
+         */
+        _toggleFilters: function(ev) {
+          ev.halt();
 
-      var control = ev.currentTarget;
-      var newTarget = control.hasClass('less') ? 'more' : 'less';
-      newTarget = this.get('container').one('.filterControl .' + newTarget);
+          var control = ev.currentTarget;
+          var newTarget = control.hasClass('less') ? 'more' : 'less';
+          newTarget = this.get('container').one('.filterControl .' + newTarget);
 
-      control.hide();
-      newTarget.show();
+          control.hide();
+          newTarget.show();
 
-      if (newTarget.hasClass('less')) {
-        this.get('container').one('.search-filters').show();
-      } else {
-        this.get('container').one('.search-filters').hide();
-      }
-    },
-
-    /**
-       Renders the search results from the the store query.
-
-       @method _renderSearchResults
-
-     */
-    _renderSearchResults: function(results) {
-      var target = this.get('renderTo'),
-          tpl = this.template({
-            count: results.size(),
-            isFullscreen: this.get('isFullscreen')
-          }),
-          tplNode = Y.Node.create(tpl),
-          results_container = tplNode.one('.search-results'),
-          filter_container = tplNode.one('.search-filters');
-
-      results.map(function(charm) {
-        var ct = new widgets.browser.CharmToken(Y.merge(
-            charm.getAttrs(), {
-              size: this.get('isFullscreen') ? 'large' : 'small'
-            }));
-        ct.render(results_container);
-      }, this);
-      this._renderFilterWidget(filter_container);
-      this.get('container').setHTML(tplNode);
-      target.setHTML(this.get('container'));
-      // XXX: We shouldn't have to do this; calling .empty before rending
-      // should reset where the node's overflow is scrolled to, but it
-      // doesn't. Se we scroll the heading into view to ensure the view
-      // renders at the top of the content.
-      target.one('.search-title').scrollIntoView();
-    },
-
-    /**
-       Render the filter controls widget into the search page.
-
-       @method _renderfilterWidget
-       @param {Node} container the node to drop the filter control into.
-
-     */
-    _renderFilterWidget: function(container) {
-      this.filters = new widgets.browser.Filter({
-        filters: this.get('filters')
-      });
-
-      this.filters.render(container);
-      this.addEvent(
-          this.filters.on(
-              this.filters.EV_FILTER_CHANGED, this._filterChanged, this)
-      );
-    },
-
-    /**
-     * Renders the searchview, rendering search results for the view's search
-     * text.
-     *
-     * @method render
-     */
-    render: function() {
-      this.showIndicator(this.get('renderTo'));
-      // This is only rendered once from the subapp and so the filters is the
-      // initial set from the application. All subsequent renders go through
-      // the subapp so we don't have to keep the filters in sync here.
-      // If caching/reusing comes into play though an event to track the
-      // change of the filters ATTR would make sense to re-draw.
-      this.get('store').search(this.get('filters'), {
-        'success': function(data) {
-          var results = this.get('store').resultsToCharmlist(data.result);
-          this._renderSearchResults(results);
-          this.hideIndicator(this.get('renderTo'));
-
-          // Set the active charm if available.
-          var active = this.get('activeID');
-          if (active) {
-            this._updateActive(
-                this.get('container').one('.charm-token[data-charmid="' + active + '"]')
-            );
+          if (newTarget.hasClass('less')) {
+            this.get('container').one('.search-filters').show();
+          } else {
+            this.get('container').one('.search-filters').hide();
           }
         },
-        'failure': this.apiFailure
-      }, this);
-    }
-  }, {
-    ATTRS: {
-      /**
-         The search data object which is a Filter instance.
 
-         @attribute filters
-         @default undefined
-         @type {Filter}
-       */
-      filters: {}
-    }
-  });
+        /**
+           Renders the search results from the the store query.
+
+           @method _renderSearchResults
+
+         */
+        _renderSearchResults: function(results) {
+          var target = this.get('renderTo'),
+              tpl = this.template({
+                count: results.size(),
+                isFullscreen: this.get('isFullscreen')
+              }),
+              tplNode = Y.Node.create(tpl),
+              results_container = tplNode.one('.search-results'),
+              filter_container = tplNode.one('.search-filters');
+
+          results.map(function(charm) {
+            var ct = new widgets.browser.CharmToken(Y.merge(
+                charm.getAttrs(), {
+                  size: this.get('isFullscreen') ? 'large' : 'small'
+                }));
+            ct.render(results_container);
+          }, this);
+          this._renderFilterWidget(filter_container);
+          this.get('container').setHTML(tplNode);
+          target.setHTML(this.get('container'));
+          // XXX: We shouldn't have to do this; calling .empty before rending
+          // should reset where the node's overflow is scrolled to, but it
+          // doesn't. Se we scroll the heading into view to ensure the view
+          // renders at the top of the content.
+          target.one('.search-title').scrollIntoView();
+        },
+
+        /**
+           Render the filter controls widget into the search page.
+
+           @method _renderfilterWidget
+           @param {Node} container the node to drop the filter control into.
+
+         */
+        _renderFilterWidget: function(container) {
+          this.filters = new widgets.browser.Filter({
+            filters: this.get('filters')
+          });
+
+          this.filters.render(container);
+          this.addEvent(
+              this.filters.on(
+                  this.filters.EV_FILTER_CHANGED, this._filterChanged, this)
+          );
+        },
+
+        /**
+           Renders the searchview, rendering search results for the view's
+           search text.
+
+           @method render
+         */
+        render: function() {
+          this.showIndicator(this.get('renderTo'));
+          // This is only rendered once from the subapp and so the filters is
+          // the initial set from the application. All subsequent renders go
+          // through the subapp so we don't have to keep the filters in sync
+          // here.  If caching/reusing comes into play though an event to
+          // track the change of the filters ATTR would make sense to re-draw.
+          this.get('store').search(this.get('filters'), {
+            'success': function(data) {
+              var results = this.get('store').resultsToCharmlist(data.result);
+              this._renderSearchResults(results);
+              this.hideIndicator(this.get('renderTo'));
+
+              // Set the active charm if available.
+              var active = this.get('activeID');
+              if (active) {
+                this._updateActive(
+                    this.get('container').one(
+                        '.charm-token[data-charmid="' + active + '"]')
+                );
+              }
+            },
+            'failure': this.apiFailure
+          }, this);
+        }
+      }, {
+        ATTRS: {
+          /**
+             The search data object which is a Filter instance.
+
+             @attribute filters
+             @default undefined
+             @type {Filter}
+           */
+          filters: {}
+        }
+      });
 
 }, '0.1.0', {
   requires: [
