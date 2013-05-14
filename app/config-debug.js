007d76d81ff0dbd42401258c980f4275ef997eef
Matthew Scott <matthew.scott@canonical.com>
2013-05-14 09:09:07 -0600

    Reverting simulator change.

--- app/config-debug.js
+++ app/config-debug.js
@@ -26,7 +26,7 @@ var juju_config = {
   apiBackend: 'python', // Value can be 'python' or 'go'.
   sandbox: true,
   // When in sandbox mode should we create events to simulate a live env.
-  simulateEvents: false,
+  simulateEvents: true,
   readOnly: false,
   login_help: 'For this demonstration, use the password "admin" to connect.'
 };
