11c994358d3c2f5a27969896b8c7d61342655bbc
j.c.sackett <jcsackett@canonical.com>
2013-06-05 16:04:35 +0000

    _stripViewMode better normalizes the id.

--- app/subapps/browser/browser.js
+++ app/subapps/browser/browser.js
@@ -234,7 +234,7 @@ YUI.add('subapp-browser', function(Y) {
       // Clear out any parts of /sidebar/search, /sidebar, or /search from the
       // id. See if we still really have an id.
       var match =
-          /^(sidebar|fullscreen|minimized|search|test\/index\.html)\/?(search)?/;
+          /^(sidebar|fullscreen|minimized|search|test\/index\.html)\/?(search)?\/?/;
 
       if (id && id.match(match)) {
         // Strip it out.
