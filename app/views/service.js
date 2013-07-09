3fec129492cf9f606d8ee2fd19b5817fc263bc9b
Benji York <benji.york@canonical.com>
2013-07-09 15:09:02 -0500

    checkpoint

--- app/views/service.js
+++ app/views/service.js
@@ -1136,7 +1136,7 @@ YUI.add('juju-view-service', function(Y) {
       @param {Y.EventFacade} evt The click event.
     */
     showDestroyServicePrompt: function(evt) {
-      evt.container.one('.destroy-service-prompt').removeClass('hidden');
+      evt.container.one('.destroy-service-prompt').addClass('open');
     },
 
     /**
@@ -1146,7 +1146,7 @@ YUI.add('juju-view-service', function(Y) {
       @param {Y.EventFacade} evt The click event.
     */
     hideDestroyServicePrompt: function(evt) {
-      evt.container.one('.destroy-service-prompt').addClass('hidden');
+      evt.container.one('.destroy-service-prompt').removeClass('open');
     },
 
     initiateServiceDestroy: function(evt) {
