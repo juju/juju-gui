2ccbbeb875a96055d8004ebb01910497b05fe672
Rick Harding <rick.harding@canonical.com>
2013-07-10 17:11:37 +0000

    Lint

--- test/test_browser_app.js
+++ test/test_browser_app.js
@@ -73,8 +73,6 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
     // Ensure the search results are rendered inside the container.
     it('must correctly render the initial browser ui', function() {
       var container = Y.one('#subapp-browser');
-
-      var container = Y.one('#subapp-browser');
       view = new FullScreen({
         store: new Y.juju.Charmworld2({
           apiHost: 'http://localhost'
