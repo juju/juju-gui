fc7f9840f14ba0c202aa4f2f258c305c80321b3d
Rick Harding <rick.harding@canonical.com>
2013-07-12 21:25:35 +0000

    Fix comment

--- app/widgets/charm-search.js
+++ app/widgets/charm-search.js
@@ -40,7 +40,7 @@ YUI.add('browser-search-widget', function(Y) {
    * @extends {Y.Widget}
    * @event EV_CLEAR_SEARCH the widget requests all search reset.
    * @event EV_SEARCH_CHANGED the widgets notifies that the search input has
-   * changed.
+    changed.
    * @event EV_SEARCH_GOHOME Signal that the user clicked the home button.
    *
    */
