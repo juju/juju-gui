c7f626d68b34ca69c029039487f48d8b66f83e75
Rick Harding <rick.harding@canonical.com>
2013-07-16 09:17:30 -0400

    Lint

--- app/store/charm.js
+++ app/store/charm.js
@@ -302,6 +302,16 @@ YUI.add('juju-charm-store', function(Y) {
         filename].join('/');
     },
 
+    /**
+      Generate the API path to a charm icon.
+      This is useful when generating links and references in HTML to the
+      charm's icon and is constructing the correct icon based on reviewed
+      status and categories on the charm.
+
+      @method iconpath
+      @param {String} charmID The id of the charm to grab the icon for.
+
+     */
     iconpath: function(charmID) {
       return this.get('apiHost') + [
         this._apiRoot,
