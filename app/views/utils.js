7cd9a84dd5c64348c332f085d085c0f963aa143e
Matthew Scott <matthew.scott@canonical.com>
2013-07-16 15:17:19 -0400

    Charm icons

--- app/views/utils.js
+++ app/views/utils.js
@@ -1047,24 +1047,25 @@ YUI.add('juju-view-utils', function(Y) {
       }
     });
     this.store = store;
-    services.each(function() {
-      var id = this.get('id');
+    services.each(function(service) {
+      var id = service.get('id');
       if (result[id] !== undefined) {
-        result[id].model = this;
+        result[id].model = service;
       } else {
-        result[id] = new BoundingBox(module, this);
+        result[id] = new BoundingBox(module, service);
       }
-      if (this.get('icon') === undefined) {
+      if (service.get('icon') === undefined) {
         var icon;
         // Get the charm ID from the service.  In some cases, this will be the
         // charm URL with a protocol, which will need to be removed.  The
         // following regular expression removes everything up to the colon
         // portion of the quote and leaves behind a charm ID.
-        var charmID = this.get('charm').replace(/^.*:/, '');
+        var charmID = service.get('charm').replace(/^.*:/, '');
         // Get the icon url from the store
-        this.set('icon', icon);
+        icon = store.iconpath(charmID);
+        service.set('icon', icon);
       }
-      result[id].icon = this.get('icon');
+      result[id].icon = service.get('icon');
     }, this);
     return result;
   };
