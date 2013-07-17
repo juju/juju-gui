a7759ab42fcf268d742578079a9c3b8e7085bc55
j.c.sackett <jcsackett@canonical.com>
2013-07-17 15:18:11 +0000

    Remove charmStore.find, it's not used.

--- app/store/charm.js
+++ app/store/charm.js
@@ -50,49 +50,6 @@ YUI.add('juju-charm-store', function(Y) {
     },
 
     /**
-     * @method find
-     * @param {string} query Either a string that is passed directly to the
-     *   search url, or a hash that is marshalled to the correct format (e.g.,
-     *   {series:precise owner:charmers}).
-     * @return {Object} CharmId instances grouped by series and ordered
-     *   within the groups according to the CharmId compare function.
-     */
-    find: function(query, options) {
-      if (!Y.Lang.isString(query)) {
-        var operator = query.op || 'intersection',
-            join_string = {union: ' OR ', intersection: ' '}[operator],
-            tmp = [];
-        delete query.op;
-        if (!Y.Lang.isValue(join_string)) {
-          throw 'Developer error: unknown operator ' + operator;
-        }
-        Y.each(query, function(val, key) {
-          if (Y.Lang.isString(val)) {
-            val = [val];
-          }
-          Y.each(val, function(v) {
-            tmp.push(key + ':' + v);
-          });
-        });
-        query = escape(tmp.join(join_string));
-      }
-      this.get('datasource').sendRequest({
-        request: 'search/json?search_text=' + query,
-        callback: {
-          'success': Y.bind(function(io_request) {
-            // To see an example of what is being obtained, look at
-            // http://jujucharms.com/search/json?search_text=mysql .
-            var result_set = Y.JSON.parse(
-                io_request.response.results[0].responseText);
-            options.success(
-                this._normalizeCharms(
-                result_set.results, options.list, options.defaultSeries));
-          }, this),
-          'failure': options.failure
-        }});
-    },
-
-    /**
      * Convert the charm data into Charm instances, using only id and
      * relevance.  Group them into series.  The series are arranged with first
      * the defaultSeries, if any, and then all other available series arranged
