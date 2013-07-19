223d83affe5554c420d025d7191de346970b99f8
j.c.sackett <jcsackett@canonical.com>
2013-07-19 18:05:56 +0000

    Clean up flow.

--- test/utils.js
+++ test/utils.js
@@ -137,21 +137,19 @@ YUI(GlobalConfig).add('juju-tests-utils', function(Y) {
       return charms;
     })(),
 
-    TestCharmStore: Y.Base.create(
-        'test-charm-store', Y.juju.CharmStore, [], {
-          loadByPath: function(path, options) {
-            var charmName = path.split('/')[2];
-            // Ignore version as changing across all
-            // testing artifacts is a pain.
-            charmName = charmName.split('-', 1);
-            if (charmName in jujuTests.utils._cached_charms) {
-              options.success(jujuTests.utils._cached_charms[charmName]);
-            } else {
-              options.failure(new Error('Unable to load charm ' + charmName));
-            }
-          }
+    TestCharmStore: Y.Base.create('test-charm-store', Y.juju.CharmStore, [], {
+      loadByPath: function(path, options) {
+        var charmName = path.split('/')[2];
+        // Ignore version as changing across all
+        // testing artifacts is a pain.
+        charmName = charmName.split('-', 1);
+        if (charmName in jujuTests.utils._cached_charms) {
+          options.success(jujuTests.utils._cached_charms[charmName]);
+        } else {
+          options.failure(new Error('Unable to load charm ' + charmName));
         }
-    ),
+      }
+    }),
 
     makeFakeBackend: function() {
       var fakeStore = new Y.juju.Charmworld2({});
