d3f33004d62045b6fd938fff551e046fd5b23547
j.c.sackett <jcsackett@canonical.com>
2013-08-09 20:14:26 +0000

    Converted database to Browsercharms.

--- app/models/models.js
+++ app/models/models.js
@@ -699,8 +699,7 @@ YUI.add('juju-models', function(Y) {
       // Single model for environment database is bound to.
       this.environment = new Environment();
       this.services = new ServiceList();
-      this.charms = new models.CharmList();
-      this.browserCharms = new models.BrowserCharmList();
+      this.charms = new models.BrowserCharmList();
       this.relations = new RelationList();
       this.notifications = new NotificationList();
 
