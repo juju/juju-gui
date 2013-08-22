3a2e090923a4201e7af61e6b83e562c66345690d
Rick Harding <rick.harding@canonical.com>
2013-08-22 17:06:04 +0000

    Back to all tests

--- test/test_sandbox_python.js
+++ test/test_sandbox_python.js
@@ -20,7 +20,7 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (function() {
 
-  describe.only('sandbox.PyJujuAPI', function() {
+  describe('sandbox.PyJujuAPI', function() {
     var requires = [
       'juju-env-sandbox', 'juju-tests-utils', 'juju-env-python',
       'juju-models', 'promise'];
