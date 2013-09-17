28adb9cd9ee24b36f0ace7b88fc41649180f3e45
Gary Poster <gary.poster@canonical.com>
2013-09-17 11:44:32 -0400

    relation menu should open and close with inspector

--- app/views/topology/service.js
+++ app/views/topology/service.js
@@ -107,12 +107,6 @@ YUI.add('juju-topology-service', function(Y) {
         // Only update position if we're not already in a drag state (the
         // current drag supercedes any previous annotations).
         var fromGhost = d.model.get('placeFromGhostPosition');
-        if (fromGhost) {
-          // This flag has served its purpose, at initialization time on the
-          // canvas.  Remove it, so future changes will have the usual
-          // behavior.
-          d.model.set('placeFromGhostPosition', false);
-        }
         if (!d.inDrag) {
           var useTransitions = self.get('useTransitions') && !fromGhost;
           self.drag.call(this, d, self, {x: x, y: y}, useTransitions);
@@ -172,6 +166,18 @@ YUI.add('juju-topology-service', function(Y) {
               .attr({'class': 'sub-rel-count',
           'x': 64,
           'y': 47 * 0.8});
+
+    // Handle the last step of models that were made locally from ghosts.
+    node.filter(function(d) {
+      return d.model.get('placeFromGhostPosition');
+    }).each(function(d) {
+      // Show the service menu from the start.
+      self.showServiceMenu(d);
+      // This flag has served its purpose, at initialization time on the
+      // canvas.  Remove it, so future changes will have the usual
+      // behavior.
+      d.model.set('placeFromGhostPosition', false);
+    });
 
     // Landscape badge
     if (landscape) {
@@ -454,8 +460,7 @@ YUI.add('juju-topology-service', function(Y) {
     initializer: function(options) {
       ServiceModule.superclass.constructor.apply(this, arguments);
       // Set a default
-      this.set('currentServiceClickAction', 'toggleServiceMenu');
-
+      this.set('currentServiceClickAction', 'showServiceMenu');
     },
 
     /**
