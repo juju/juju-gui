7f77c5db30cb9b9b26d4a32312486e8c5c19a242
Benjamin Saller <bcsaller@gmail.com>
2013-09-26 11:49:46 -0700

    use random ports when starting test server to proctect vs existing servers confusing make check

--- test-server.js
+++ test-server.js
@@ -21,29 +21,29 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 // process.argv[2] will be 'debug' or 'prod'
 
 var express = require('express'),
-    server = express(),
+    http = require('http'),
     fs = require('fs'),
     path = require('path');
 
-server.configure(function() {
-  server.use(express.logger('dev'));
+var app = express();
+app.configure(function() {
+
+  app.use(express.logger('dev'));
   // 'static' is a reserved word so dot notation is not used to
   // avoid annoying the linter.
-  server.use(express['static'](__dirname));
+  app.use(express['static'](__dirname));
   // fallback to looking in assets
-  server.use('/juju-ui', express['static'](
+  app.use('/juju-ui', express['static'](
       __dirname + '/build-' + process.argv[2] + '/juju-ui'));
-  server.use(express.bodyParser());
-  server.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
 });
 
-server.get('/juju-ui/:file', function(req, res) {
+app.get('/juju-ui/:file', function(req, res) {
   var fileName = req.params.file;
   res.sendfile('build-' + process.argv[2] + '/juju-ui/' + fileName);
 });
 
-var port = 8084;
-
-server.listen(port, function() {
-  console.log(process.argv[2] + ' server listening on ' + port);
-});
+var server = http.createServer(app);
+server.listen(0);
+console.log('http://0.0.0.0:' + server.address().port + '/test/index.html');
