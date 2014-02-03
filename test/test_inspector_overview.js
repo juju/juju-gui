e9bcd82794e66843cf81ab897b7e401d9ed30b33
Benji York <benji@benjiyork.com>
2014-02-03 14:01:48 -0600

    automatically close (and reopen) the sidebar
    ...when large display elements take over the screen.

--- test/test_inspector_overview.js
+++ test/test_inspector_overview.js
@@ -34,8 +34,7 @@ describe('Inspector Overview', function() {
           jujuViews = Y.namespace('juju.views');
           juju = Y.namespace('juju');
           charmConfig = utils.loadFixture(
-          'data/mediawiki-api-response.json',
-          true);
+              'data/mediawiki-api-response.json', true);
           done();
         });
 
@@ -1024,6 +1023,53 @@ describe('Inspector Overview', function() {
         assert.deepEqual(buttonList, results);
       });
     });
+  });
+
+  describe('viewport takeover handling', function() {
+
+    it('showUnitDetails fires inspectorTakeoverStarting', function() {
+      inspector = setUpInspector(null, true);
+      var fauxEvent = {
+        halt: function() {},
+        currentTarget: {
+          getData: function(name) {
+            assert.equal(name, 'unit');
+            return 'mediawiki/1';
+          }
+        }
+      };
+      inspector.viewletManager.set('db', {
+        services: {
+          getById: function() {return db.services.getById('mediawiki');}
+        },
+        relations: {
+          get_relations_for_service: function() {return [];}
+        }
+      });
+
+      inspector.showUnitDetails(fauxEvent);
+    });
+
+    it('onShowCharmDetails fires inspectorTakeoverStarting', function() {
+      inspector = setUpInspector(null, true);
+      var fauxEvent = {
+        halt: function() {},
+        currentTarget: {
+          getData: function(name) {
+            assert.equal(name, 'charmid');
+            return 'precise/mediawiki-14';
+          }
+        }
+      };
+      inspector.viewletManager.set('db', {
+        charms: {
+          getById: function(id) {return db.charms.getById(id);}
+        }
+      });
+
+      inspector.onShowCharmDetails(fauxEvent);
+    });
+
   });
 
 });
