99bc31f990dce1410229ae70842b157c2ed0201f
Jeff Pihach <jeff.pihach@canonical.com>
2014-03-11 14:33:12 -0600

    Created a request series inspector which extends the inspector base class

---
+++ app/views/inspectors/request-series.js
@@ -0,0 +1,62 @@
+/*
+This file is part of the Juju GUI, which lets users view and manage Juju
+environments within a graphical interface (https://launchpad.net/juju-gui).
+Copyright (C) 2012-2013 Canonical Ltd.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License version 3, as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+'use strict';
+
+
+YUI.add('request-series-inspector', function(Y) {
+  var ns = Y.namespace('juju.views'),
+      viewlets = Y.namespace('juju.viewlets');
+
+  var name = 'request-series-inspector';
+
+  ns.RequestSeriesInspector = Y.Base.create(name, ns.Inspector, [], {
+
+    views: {
+      requestSeries: Y.juju.viewlets.RequestSeries
+    },
+
+    /**
+      This render method is called by the inspector-base class on render.
+
+      @method renderUI
+    */
+    renderUI: function() {
+      this.views.requestSeries.setAttrs({
+        file: this.get('file'),
+        env: this.get('env'),
+        db: this.get('db')
+      });
+      this.showViewlet('requestSeries');
+    }
+
+  }, {
+    ATTRS: {
+      file: {},
+      env: {},
+      db: {}
+    }
+  });
+
+}, '', {
+  requires: [
+    'viewlet-view-base',
+    'inspector-base',
+    'request-series-view'
+  ]
+});
