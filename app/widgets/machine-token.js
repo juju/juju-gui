0e506c5ac7f75830ad5afd9a89d0e68672e4210b
Huw Wilkins <huw.wilkins@canonical.com>
2014-04-30 10:33:03 -0700

    Fixed the active state of machine tokens.

--- app/widgets/machine-token.js
+++ app/widgets/machine-token.js
@@ -92,7 +92,8 @@ YUI.add('machine-token', function(Y) {
         render: function() {
           var container = this.get('container'),
               machine = this.get('machine');
-          machine.formattedHardware = this._formatHardware(machine.hardware);
+          machine.formattedHardware = this._formatHardware(
+              Y.clone(machine.hardware));
           container.setHTML(this.template(machine));
           container.addClass('machine-token');
           container.setAttribute('data-id', machine.id);
