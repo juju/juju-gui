6c12b4573eeee2b74fd7a5eb4098ef5c487c2eee
Kyle Adams <kyle.adams@canonical.com>
2014-07-31 13:18:36 -0400

    Remove testing cruft.

--- test/test_model_handlers.js
+++ test/test_model_handlers.js
@@ -202,7 +202,7 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
         assert.strictEqual('example.com/foo', machine.public_address);
       });
 
-      it.only('skips machine create if a service is unassociated', function() {
+      it('skips machine create if a service is unassociated', function() {
         db.services.add({id: 'django'});
         var change = {
           Name: 'django/2',
