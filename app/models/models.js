9f85f6663819ebcfa7a034f9f8ef3f937bc82a9b
Kyle Adams <kyle.adams@canonical.com>
2014-11-13 11:31:01 -0500

    Documentation cleanup.

--- app/models/models.js
+++ app/models/models.js
@@ -742,6 +742,13 @@ YUI.add('juju-models', function(Y) {
       return ghostService;
     },
 
+    /**
+      Process service changes coming in from juju-core.
+
+      @method process_delta
+      @param {String} action The type of change: 'change', 'remove', 'add'
+      @param {Object} data The data for the model being updated
+    */
     process_delta: function(action, data) {
       _process_delta(this, action, data, {exposed: false});
     }
@@ -854,6 +861,14 @@ YUI.add('juju-models', function(Y) {
       return name.replace('unit-', '').replace(/^(.+)-(\d+)$/, '$1/$2');
     },
 
+    /**
+      Process unit changes coming in from juju-core.
+
+      @method process_delta
+      @param {String} action The type of change: 'change', 'remove', 'add'
+      @param {Object} data The data for the model being updated
+      @param {Object} db The database containing the model being updated
+    */
     process_delta: function(action, data, db) {
       // If a charm_url is included in the data (that is, the Go backend
       // provides it), get the old charm so that we can compare charm URLs
@@ -1471,6 +1486,13 @@ YUI.add('juju-models', function(Y) {
       });
     },
 
+    /**
+      Process machine changes coming in from juju-core.
+
+      @method process_delta
+      @param {String} action The type of change: 'change', 'remove', 'add'
+      @param {Object} data The data for the model being updated
+    */
     process_delta: function(action, data) {
       _process_delta(this, action, data, {});
     },
@@ -1629,6 +1651,14 @@ YUI.add('juju-models', function(Y) {
   var RelationList = Y.Base.create('relationList', Y.ModelList, [], {
     model: Relation,
 
+    /**
+      Process relation changes coming in from juju-core.
+
+      @method process_delta
+      @param {String} action The type of change: 'change', 'remove', 'add'
+      @param {Object} data The data for the model being updated
+      @param {Object} db The database containing the model being updated
+    */
     process_delta: function(action, data, db) {
       // If the action is remove we need to parse the models before they are
       // removed from the db so that we can remove them from the relation
@@ -2435,8 +2465,8 @@ YUI.add('juju-models', function(Y) {
       @method setMVVisibility
       @param {String} serviceId The service id to compare to the units
         services in the machine.
-      @param {Boolean} visible If the machine with units matching the supplied
-        service should be visible or not.
+      @param {Boolean} highlight If the machine with units matching the supplied
+        service should be highlighted or not.
     */
     setMVVisibility: function(serviceId, highlight) {
       var highlightIndex = this._highlightedServices.indexOf(serviceId);
