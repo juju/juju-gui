6a24efe34835f9b20b5c06de6a58402cf8431953
Brad Crittenden <bac@canonical.com>
2015-03-09 09:38:35 -0400

    Use sudo selectively in the install script.

--- Makefile
+++ Makefile
@@ -172,7 +172,7 @@ help:
 
 sysdeps:
 	sudo apt-get install -y software-properties-common
-	sudo sh install-sysdeps.sh
+	sh install-sysdeps.sh
 	npm install --cache-min=999999999
 
 build-shared/juju-ui/templates.js: $(TEMPLATE_TARGETS) bin/generateTemplates
--- install-sysdeps.sh
+++ install-sysdeps.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
-add-apt-repository -y ppa:chris-lea/node.js
-apt-get update
-apt-get install -y nodejs imagemagick python-sphinx python-yaml \
+# Take care to only use 'sudo' when required.
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs imagemagick python-sphinx python-yaml \
   python-tz python-virtualenv python-shelltoolbox python-tornado \
   python-gflags g++ xvfb git firefox
 
-npm install -g jshint@2.1.3 mocha-phantomjs@3.3.1 phantomjs@1.9.2-6
+sudo npm install -g jshint@2.1.3 mocha-phantomjs@3.3.1 phantomjs@1.9.2-6
