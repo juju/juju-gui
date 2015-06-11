0ccd017055120f7db980fbad6ecfb2721cb413f6
Huw Wilkins <huw.wilkins@canonical.com>
2015-06-12 08:49:39 +1000

    Removed LESS files and compilation.

--- config.js
+++ config.js
@@ -6,10 +6,6 @@ var config = {
       __dirname + '/app/templates/',
       __dirname + '/app/subapps/browser/templates/'
     ],
-    'view_dirs': [
-      __dirname + '/lib/views/',
-      __dirname + '/lib/views/browser'
-    ],
     'scss_dirs': [
       __dirname + '/app/assets/css/'
     ],
