faab6442b96aac67d60402fc0e459384cd7b009a
j.c.sackett <j.c.sackett@gmail.com>
2015-08-11 17:06:03 -0400

    Fix config issue.

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ var config = {
     ],
     'scss_dirs': [
       // Must have the top level path first.
-      __dirname + '/jujugui/static/gui/src/app/assets/css/'
-      __dirname + '/jujugui/static/gui/src/app/assets/css/browser'
-      __dirname + '/jujugui/static/gui/src/app/assets/css/inspector'
+      __dirname + '/jujugui/static/gui/src/app/assets/css/',
+      __dirname + '/jujugui/static/gui/src/app/assets/css/browser',
+      __dirname + '/jujugui/static/gui/src/app/assets/css/inspector',
       __dirname + '/jujugui/static/gui/src/app/assets/css/machine-view'
     ],
     'public_dir': __dirname + '/jujugui/static/gui/build/app'
