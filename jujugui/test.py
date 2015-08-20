3ddf7e9685e25f453a1ee412e1ce55ac2f38c061
Jeff Pihach <jeff.pihach@canonical.com>
2015-08-20 15:23:54 -0600

    setup server to serve the test files for browser based tests

---
+++ jujugui/test.py
@@ -0,0 +1,7 @@
+from pyramid.config import Configurator
+
+def main(global_config, **settings):
+    config = Configurator(settings=settings)
+    config.add_static_view('/test', 'jujugui:static/gui/src/test')
+    config.include('jujugui.gui')
+    return config.make_wsgi_app()
