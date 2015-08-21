19372e7a32d51bc9a1afb5fd23a359dc0d1e0a64
j.c.sackett <j.c.sackett@gmail.com>
2015-08-21 16:04:55 -0400

    Remove standalone wsgi assets app.

--- jujugui/__init__.py
+++ jujugui/__init__.py
@@ -12,12 +12,5 @@ def main(global_config, **settings):
 
 def make_application(config):
     """Set up the routes and return the WSGI application."""
-    # We use two separate included app/routes so that we can
-    # have the gui parts behind a separate route from the
-    # assets when we embed it in e.g. the storefront.
-    # NOTE: kadams54, 2015-08-04: It's very important that assets be listed
-    # first; if it isn't, then the jujugui.gui routes override those specified
-    # in assets and any asset requests will go to the main app.
-    config.include('jujugui.assets')
     config.include('jujugui.gui')
     return config.make_wsgi_app()
