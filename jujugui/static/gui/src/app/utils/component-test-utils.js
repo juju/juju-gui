f3bced1b394bdde8dcd1ee090cf37d9054d382a2
Jeff Pihach <jeff.pihach@canonical.com>
2015-09-14 19:12:17 -0600

    Added start of test utils for the component tests

---
+++ jujugui/static/gui/src/app/utils/component-test-utils.js
@@ -0,0 +1,45 @@
+/*
+This file is part of the Juju GUI, which lets users view and manage Juju
+environments within a graphical interface (https://launchpad.net/juju-gui).
+Copyright (C) 2015 Canonical Ltd.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License version 3, as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+'use strict';
+
+var jsTestUtils = {
+
+  /**
+    shalloRender provides a convenience wrapper around the React
+    testUtils createRenderer method.
+
+    The createRenderer isn't well documented but you can view it's source
+    code:
+      https://github.com/facebook/react/blob/
+      dc2570e1ceebd2b4be7ebe0990f8524f6b53ea7c/src/test/ReactTestUtils.js#L347
+
+    @method shallowRender
+    @param {Object} component The components to render.
+    @param {Boolean} returnRenderer Whether or not it should return the
+      component instance or just the rendered output.
+    @return {Object} See returnRenderer parameter.
+  */
+  shallowRender: function(component, returnRenderer) {
+    var shallowRenderer = testUtils.createRenderer();
+    shallowRenderer.render(component);
+    return (returnRenderer) ?
+      shallowRenderer : shallowRenderer.getRenderOutput();
+  }
+
+};
