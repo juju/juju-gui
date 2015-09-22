105758b58bba010057a95082bdb73a0ddc780fdc
Jeff Pihach <jeff.pihach@canonical.com>
2015-09-22 15:11:28 -0600

    pass the charm data into the inspector and render the configuration values and defaults

--- jujugui/static/gui/src/app/app.js
+++ jujugui/static/gui/src/app/app.js
@@ -827,6 +827,7 @@ YUI.add('juju-gui', function(Y) {
         });
         return;
       }
+      var charm = app.db.charms.getById(service.get('charm'));
       var state = this.state;
       var utils = views.utils;
       React.render(
@@ -836,6 +837,7 @@ YUI.add('juju-gui', function(Y) {
           metadata={metadata}>
           <components.Inspector
             service={service}
+            charm={charm}
             addGhostAndEcsUnits={utils.addGhostAndEcsUnits.bind(
                 this, this.db, this.env, service)}
             createMachinesPlaceUnits={utils.createMachinesPlaceUnits.bind(
