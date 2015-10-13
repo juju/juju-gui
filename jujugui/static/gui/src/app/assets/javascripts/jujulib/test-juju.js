f14f6d659e07aed76de0ca829756d1508219067a
j.c.sackett <j.c.sackett@gmail.com>
2015-10-13 09:23:25 -0400

    Tests.

--- jujugui/static/gui/src/app/assets/javascripts/jujulib/test-juju.js
+++ jujugui/static/gui/src/app/assets/javascripts/jujulib/test-juju.js
@@ -57,6 +57,40 @@ describe('jujulib', function() {
       });
     });
 
+    it('lists servers', function(done) {
+      var bakery = {
+        sendGetRequest: function(path, success, failure) {
+          var xhr = _makeXHRRequest({"state-servers": ["foo"]});
+          success(xhr);
+        }
+      };
+      env = new window.jujulib.environment('http://example.com', bakery);
+      env.listServers(function(data) {
+        assert.deepEqual(data, ['foo']);
+        done();
+      }, function() {
+        assert.fail('failure callback should not have been called.');
+        done();
+      });
+    });
+
+    it('handles errors listing servers', function(done) {
+      var err = 'bad wolf';
+      var bakery = {
+        sendGetRequest: function(path, success, failure) {
+          failure(err);
+        }
+      };
+      env = new window.jujulib.environment('http://example.com', bakery);
+      env.listEnvironments(function(data) {
+        assert.fail('success callback should not have been called.');
+        done();
+      }, function(error) {
+        assert.equal(error, err);
+        done();
+      });
+    });
+
     it('gets environment data', function(done) {
       var bakery = {
         sendGetRequest: function(path, success, failure) {
@@ -87,6 +121,59 @@ describe('jujulib', function() {
         assert.equal(error, err);
         done();
       });
+    });
+
+    it('can create a new environment', function(done) {
+      var bakery = {
+        sendPostRequest: function(path, data, success, failure) {
+          assert.equal(path, 'http://example.com/v1/env/rose');
+          assert.deepEqual(
+            JSON.parse(data), {
+              'state-server': 'foo',
+              password: 'password',
+              name: 'fnord',
+              templates: ['rose/template']
+            });
+          var xhr = _makeXHRRequest({});
+          success(xhr);
+        },
+        sendGetRequest: function(path, success, failure) {
+          var xhr = _makeXHRRequest({"state-servers": [{path: "foo"}]});
+          success(xhr);
+        }
+      };
+
+      env = new window.jujulib.environment('http://example.com', bakery);
+      env.newEnvironment('rose', 'fnord', 'rose/template', 'password', function(data) {
+        done();
+      }, function(error) {
+        assert.fail('Failure callback should not have been called.');
+        done();
+      });
+    });
+
+    it('handles errors creating a new environment', function(done) {
+      var err = 'bad wolf';
+      var bakery = {
+        sendPostRequest: function(path, data, success, failure) {
+          assert.equal(path, 'http://example.com/v1/env/rose');
+          failure(err);
+        },
+        sendGetRequest: function(path, success, failure) {
+          var xhr = _makeXHRRequest({"state-servers": [{path: "foo"}]});
+          success(xhr);
+        }
+      };
+
+      env = new window.jujulib.environment('http://example.com', bakery);
+      env.newEnvironment('rose', 'fnord', 'rose/template', 'password', function(data) {
+        done();
+        assert.fail('Success callback should not have been called.');
+      }, function(error) {
+        assert.equal(error, err)
+        done();
+      });
+
     });
   });
 
