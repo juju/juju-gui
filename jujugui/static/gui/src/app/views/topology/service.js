6de3db3834e66ad66c45e80cb2ee0341d71b1beb
Huw Wilkins <huw.wilkins@canonical.com>
2015-12-10 12:44:16 +1100

    Don't center on the services on deploy.

--- jujugui/static/gui/src/app/views/topology/service.js
+++ jujugui/static/gui/src/app/views/topology/service.js
@@ -1394,11 +1394,6 @@ YUI.add('juju-topology-service', function(Y) {
           });
         }
       }
-      // Find the centroid of our hull of services and inform the
-      // topology.
-      if (vertices.length) {
-        this.findCentroid(vertices);
-      }
 
       // enter
       node
