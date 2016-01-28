f37f5450c891007b90e0f544497e50bb25d14991
Kyle Adams <kadams54@gmail.com>
2016-01-28 13:40:55 -0500

    Use the new library.

--- jujugui/static/gui/src/app/app.js
+++ jujugui/static/gui/src/app/app.js
@@ -999,7 +999,7 @@ YUI.add('juju-gui', function(Y) {
           getEntity={charmstore.getEntity.bind(charmstore)}
           getFile={charmstore.getFile.bind(charmstore)}
           getDiagramURL={charmstore.getDiagramURL.bind(charmstore)}
-          renderMarkdown={Y.Markdown.toHTML.bind(this)}
+          renderMarkdown={marked.bind(this)}
           deployService={this.deployService.bind(this)}
           appState={state.get('current')}
           changeState={this.changeState.bind(this)}
