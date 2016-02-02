d19910dc64ba4ec4c21493e5da27e2b322329730
Jeff Pihach <jeff.pihach@canonical.com>
2016-02-02 11:25:24 -0600

    do not clear the bakery macaroon on window unload, we'll add a proper logout later

--- jujugui/static/gui/src/app/store/env/bakery.js
+++ jujugui/static/gui/src/app/store/env/bakery.js
@@ -298,9 +298,6 @@ YUI.add('juju-env-bakery', function(Y) {
       _successfulDischarges: function (originalRequest, jsonMacaroon) {
         var prefix = this.macaroonName + '=';
         document.cookie = prefix + btoa(JSON.stringify(jsonMacaroon));
-        window.addEventListener('unload', function() {
-          document.cookie = prefix + ';';
-        });
         originalRequest();
       },
 
