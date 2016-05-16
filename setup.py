fff48ae9238586cf33ea9dc435edb1981d8a2872
j.c.sackett <j.c.sackett@gmail.com>
2016-05-16 10:08:41 -0400

    Bump version: 2.1.3 → 2.1.4

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ install_requires = [str(req.req) for req in requirements]
 tests_require = [str(req.req) for req in test_requirements]
 
 setup(name='jujugui',
-      version='2.1.3',
+      version='2.1.4',
       description='jujugui',
       classifiers=[
           "Programming Language :: Python",
