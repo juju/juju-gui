deb7d0bee71d384e416f68fa46c7850a84298f18
Jeff Pihach <jeff.pihach@canonical.com>
2016-06-16 17:19:21 +0200

    Fix execution scope of plans api call

--- jujugui/static/gui/src/app/app.js
+++ jujugui/static/gui/src/app/app.js
@@ -1124,7 +1124,7 @@ YUI.add('juju-gui', function(Y) {
             displayPlans={utils.compareSemver(
               this.get('jujuCoreVersion'), '2') > -1}
             modelUUID={this.get('jujuEnvUUID')}
-            showActivePlan={this.plans.showActivePlan}
+            showActivePlan={this.plans.showActivePlan.bind(this.plans)}
             setCharm={this.env.setCharm.bind(this.env)}
             getCharm={this.env.get_charm.bind(this.env)}
             getUnitStatusCounts={utils.getUnitStatusCounts}
