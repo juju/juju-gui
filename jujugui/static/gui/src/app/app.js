5fad69213738423321cc3df2aa6f70bd271d282c
Madison Scott-Clary <madison.scott-clary@canonical.com>
2016-06-22 12:13:08 -0600

    First pass at switching based on version

--- jujugui/static/gui/src/app/app.js
+++ jujugui/static/gui/src/app/app.js
@@ -471,7 +471,12 @@ YUI.add('juju-gui', function(Y) {
           // requests to the juju-core API.
           envOptions.webHandler = new webModule.WebHandler();
         }
-        this._init(cfg, new environments.GoEnvironment(envOptions), state);
+        var environment = environments.GoEnvironment;
+        if (
+          views.utils.compareSemver(this.get('jujuCoreVersion'), '1') > -1) {
+            environment = environments.GoLegacyEnvironment;
+        }
+        this._init(cfg, new environment(envOptions), state);
       });
     },
 
