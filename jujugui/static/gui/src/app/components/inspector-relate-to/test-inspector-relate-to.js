949796f2e55c94e0ee01ba9ce863d4c9db5b9e73
Jeff Pihach <jeff.pihach@canonical.com>
2016-07-04 10:54:05 -0600

    Delete invalid test suite content

--- jujugui/static/gui/src/app/components/inspector-relate-to/test-inspector-relate-to.js
+++ jujugui/static/gui/src/app/components/inspector-relate-to/test-inspector-relate-to.js
@@ -1,7 +1,7 @@
 /*
 This file is part of the Juju GUI, which lets users view and manage Juju
 environments within a graphical interface (https://launchpad.net/juju-gui).
-Copyright (C) 2015 Canonical Ltd.
+Copyright (C) 2016 Canonical Ltd.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU Affero General Public License version 3, as published by
@@ -23,39 +23,11 @@ var juju = {components: {}}; // eslint-disable-line no-unused-vars
 chai.config.includeStack = true;
 chai.config.truncateThreshold = 0;
 
-describe('InspectorExpose', function() {
+describe('InspectorRelateTo', function() {
 
   beforeAll(function(done) {
     // By loading this file it adds the component to the juju components.
     YUI().use('inspector-relate-to', function() { done(); });
   });
 
-  it('can render correctly if not exposed', function() {
-    var getStub = sinon.stub();
-    getStub.withArgs('id').returns('demo');
-    getStub.withArgs('exposed').returns(false);
-    var service = {get: getStub};
-    var toggle = {key: 'expose-toggle'};
-    var output = jsTestUtils.shallowRender(
-        <juju.components.InspectorExpose
-          service={service} />);
-    var toggleItem = output.props.children[0].props.children;
-    assert.deepEqual(output,
-        <div className="inspector-expose">
-            <div className="inspector-expose__control">
-              <juju.components.BooleanConfig
-                key={toggle.key}
-                ref={toggle.key}
-                option={toggle}
-                onChange={toggleItem.props.onChange}
-                label="Expose service"
-                config={false} />
-            </div>
-            <p className="inspector-expose__warning">
-              Exposing this service may make it publicly accessible from
-              the web
-            </p>
-            {undefined}
-        </div>);
-  });
 });
