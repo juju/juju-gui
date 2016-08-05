3a6d4d9d46e24fa75548c276ed1811ee952428f9
Jeff Pihach <jeff.pihach@canonical.com>
2016-08-05 15:02:29 -0600

    Update sysdeps to add xvfb and chromium-browser

--- install-sysdeps.sh
+++ install-sysdeps.sh
@@ -5,6 +5,6 @@ sudo add-apt-repository -y ppa:yellow/ppa
 sudo apt-get update
 sudo apt-get install -y nodejs python-sphinx python-yaml \
   python-tz python-virtualenv python-shelltoolbox python-tornado \
-  python-gflags g++ xvfb git firefox
+  python-gflags g++ xvfb git chromium-browser
 
 sudo npm install -g jshint@2.1.3 react-tools
