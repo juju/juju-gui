723db5ad5bbf824b7e9662844e81215dd8bad0e9
Huw Wilkins <huw.wilkins@canonical.com>
2016-09-01 17:05:56 +1000

    Replace Y.Array.reduce with .reduce().

--- jujugui/static/gui/src/app/models/models.js
+++ jujugui/static/gui/src/app/models/models.js
@@ -1280,8 +1280,8 @@ YUI.add('juju-models', function(Y) {
      */
     update_service_unit_aggregates: function(service) {
       var aggregate = this.get_informative_states_for_service(service);
-      var sum = Y.Array.reduce(
-          Y.Object.values(aggregate[0]), 0, function(a, b) {return a + b;});
+      var sum = Y.Object.values(aggregate[0]).reduce(
+        (a, b) => {return a + b;}, 0);
       var previous_unit_count = service.get('unit_count');
       service.set('unit_count', sum);
       service.set('aggregated_status', aggregate[0]);
