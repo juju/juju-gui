bf9de1a07541de53d0a7313e6bb43ba6c1e29b8b
Kyle Adams <kadams54@gmail.com>
2016-09-08 14:25:15 -0400

    Handle long boolean config titles.

--- jujugui/static/gui/src/app/components/boolean-config/boolean-config.js
+++ jujugui/static/gui/src/app/components/boolean-config/boolean-config.js
@@ -90,21 +90,23 @@ YUI.add('boolean-config', function() {
     render: function() {
       return (
         <div className="boolean-config">
-          <div className="boolean-config--title">{this.props.label}</div>
-          <div className="boolean-config--toggle">
-            <input
-              disabled={this.props.disabled}
-              type="checkbox"
-              id={this.props.option.key}
-              onClick={this._stopBubble}
-              onChange={this._handleChange}
-              checked={this.state.value}
-              className="boolean-config--input" />
-            <label
-              htmlFor={this.props.option.key}
-              className="boolean-config--label">
-              <div className="boolean-config--handle"></div>
-            </label>
+          <div className="boolean-config--toggle-container">
+            <div className="boolean-config--title">{this.props.label}</div>
+            <div className="boolean-config--toggle">
+              <input
+                disabled={this.props.disabled}
+                type="checkbox"
+                id={this.props.option.key}
+                onClick={this._stopBubble}
+                onChange={this._handleChange}
+                checked={this.state.value}
+                className="boolean-config--input" />
+              <label
+                htmlFor={this.props.option.key}
+                className="boolean-config--label">
+                <div className="boolean-config--handle"></div>
+              </label>
+            </div>
           </div>
           <div className="boolean-config--description"
             dangerouslySetInnerHTML={{__html: this.props.option.description}}>
