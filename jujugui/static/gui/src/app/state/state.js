f942a21ac38ed1f944dc9be4d5b0412689b2a900
Jeff Pihach <jeff.pihach@canonical.com>
2016-10-18 15:33:33 -0600

    Initial commit of the new state module.

---
+++ jujugui/static/gui/src/app/state/state.js
@@ -0,0 +1,43 @@
+/*
+This file is part of the Juju GUI, which lets users view and manage Juju
+environments within a graphical interface (https://launchpad.net/juju-gui).
+Copyright (C) 2016 Canonical Ltd.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License version 3, as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+'use strict';
+
+if (typeof this.jujugui === 'undefined') {
+  this.jujugui = {};
+}
+
+/** Class representing the State of the Juju GUI */
+this.jujugui.State = class State {
+  /**
+    Create a new instance of the GUI state.
+    @param {Object} cfg - The configuration options for a new State instance.
+  */
+  constructor(cfg) {
+    /**
+      The baseURL to use when generating URLs.
+      @type {String}
+    */
+    this.baseURL = cfg.baseURL;
+    /**
+      The list of dispatchers for the various components from the url.
+      @type {Object}
+    */
+    this.dispatchers = cfg.dispatchers;
+  }
+};
