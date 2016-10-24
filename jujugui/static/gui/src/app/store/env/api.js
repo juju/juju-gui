d9b6eac1186505f34ca85de0dbab592a1d911a76
Huw Wilkins <huw.wilkins@canonical.com>
2016-10-24 14:33:26 +1100

    Add comment about why the name can't be null.

--- jujugui/static/gui/src/app/store/env/api.js
+++ jujugui/static/gui/src/app/store/env/api.js
@@ -228,7 +228,9 @@ YUI.add('juju-env-api', function(Y) {
       // would not be updated as when the model disconnected then the value was
       // not reset. The reason we need the value to be reset is so that when it
       // is set to the new value it fires a value change event, which in turn
-      // updates the name set in the db. See onEnvironmentNameChange.
+      // updates the name set in the db. See onEnvironmentNameChange. For some
+      // reason this can't be set to null as then it displays the name as null
+      // instead of picking up that the name has not been set.
       this.setConnectedAttr('environmentName', undefined);
     },
 
