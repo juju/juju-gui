4c2ef0df2712463c229af9e0158b30e9464789e6
Jeff Pihach <jeff.pihach@canonical.com>
2016-11-15 11:13:42 -0600

    Bump version: 2.2.2 → 2.2.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = parse_requirements("requirements.txt")
 tests_requires = parse_requirements("test-requirements.txt")
 
 setup(name='jujugui',
-      version='2.2.2',
+      version='2.2.3',
       description='jujugui',
       classifiers=[
           "Programming Language :: Python",
