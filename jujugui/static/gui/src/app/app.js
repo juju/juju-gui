df54a09ddf43acb3f0fd09ffd2839202ed483429
Huw Wilkins <huw.wilkins@canonical.com>
2016-11-17 10:23:13 +1100

    Handle the canvas change state events.

--- jujugui/static/gui/src/app/app.js
+++ jujugui/static/gui/src/app/app.js
@@ -1827,6 +1827,8 @@ YUI.add('juju-gui', function(Y) {
           this._clearDeployment.bind(this)]
       ]);
 
+      // Listen for change state events from the canvas components.
+      this.on('*:changeState', this._changeState, this);
       return state;
     },
 
@@ -1869,9 +1871,7 @@ YUI.add('juju-gui', function(Y) {
       @param {Object} e The event facade.
     */
     _changeState: function(e) {
-      var state = e.details[0];
-      var url = this.state.generateUrl(state);
-      this.navigate(url);
+      this.state.changeState(e.details[0]);
     },
 
     /**
