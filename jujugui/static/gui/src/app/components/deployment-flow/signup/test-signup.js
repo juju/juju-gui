f9c883466f6dc46566506526d2d33f4128b1fe72
Anthony Dillon <me@anthonydillon.com>
2016-11-17 17:52:02 +0000

    Update the sign up copy and add a button to sign up

--- jujugui/static/gui/src/app/components/deployment-flow/signup/test-signup.js
+++ jujugui/static/gui/src/app/components/deployment-flow/signup/test-signup.js
@@ -47,59 +47,96 @@ describe('DeploymentSignup', function() {
         changeState={changeState}
         title="Prawns on the barbie">
         <div className="deployment-signup">
-          <div className="six-col">
-            <h2>Deploy your model using Juju</h2>
-            <p>
-              By deploying your model, all of your services will be deployed
-              to the cloud provider of your choice, using the configuration
-              values you provided.  In the next step, you will be able to
-              sign up and choose your cloud provider.
-            </p>
-            <p>
-              Continue to the&nbsp;
-              <juju.components.GenericButton
-                action={instance._displayFlow}
-                type="inline-neutral"
-                title="deployment demo of Juju" />
-            </p>
-          </div>
-          <div className="twelve-col">
-            <h2>Lorem ipsum dolor sit</h2>
-            <p className="six-col">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-              do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <ul>
-              <li className="six-col">
-                Lorem ipsum
-              </li>
-              <li className="six-col last-col">
-                Onsectetur adipisicing elit
-              </li>
-              <li className="six-col">
-              Sed do eiusmod
-              </li>
-              <li className="six-col last-col">
-              Tempor incididunt ut
-              </li>
-              <li className="six-col">Labore et dolore magna aliqua</li>
-            </ul>
-            <form className="twelve-col">
-              <juju.components.GenericInput
-                label="Lorem ipsum dolor"
-                required={true}
-                ref="email"
-                validate={[{
-                  regex: /\S+/,
-                  error: 'This field is required.'
-                }]}
-                value="" />
+          <div className="row border-bottom">
+            <h2>Install Juju to deploy locally</h2>
+            <div className="six-col">
+              <p className="intro">
+                Local deployment uses LXD containers, allowing you to
+                recreate your production environment on your own machine.
+                This minimises portability issues when deploying to a public
+                cloud, OpenStack or bare metal.
+              </p>
+              <p className="intro">To deploy locally:</p>
+              <ol className="deployment-signup__numbered-list">
+                <li className="deployment-signup__numbered-list-item">
+                  Download your model
+                </li>
+                <li className="deployment-signup__numbered-list-item">
+                  <a href="https://jujucharms.com/docs">
+                    Install Juju&nbsp;&rsaquo;
+                  </a>
+                </li>
+                <li className="deployment-signup__numbered-list-item">
+                  Add your model to deploy
+                </li>
+              </ol>
+              <p>
+                Continue to the&nbsp;
                 <juju.components.GenericButton
-                  action={instance._handleSignup}
-                  type="inline-positive"
-                  submit={true}
-                  title="Lorem ipsum" />
-              </form>
+                  action={instance._displayFlow}
+                  type="inline-neutral"
+                  title="Deployment demo of Juju" />
+              </p>
+            </div>
+            <div className="prepend-one four-col last-col">
+              <juju.components.SvgIcon
+                className="juju-logo"
+                name="juju-logo"
+                size="100%" />
+            </div>
+          </div>
+          <div className="row">
+            <h2>A new way to deploy</h2>
+            <div className="six-col">
+              <p>
+                Coming soon: deploy from hosted Juju direct to public clouds.
+                For early access to this feature, sign up for the beta.
+              </p>
+              <ul>
+                <li>
+                  Deploy to all major public clouds directly from your browser
+                </li>
+                <li>
+                  Hosted and managed Juju controllers
+                </li>
+                <li>
+                  Identity management across all models
+                </li>
+                <li>
+                  Reusable shareable models with unlimited users
+                </li>
+              </ul>
+              <p>
+                <a href="https://jujucharms.com/beta"
+                  target="_blank"
+                  className="button--inline-positive"
+                  onClick={instance._handleSignup}>
+                    Sign up for early access
+                </a>
+              </p>
+            </div>
+            <div className="six-col last-col">
+              <ul className="inline-logos no-bullets">
+                <li className="inline-logos__item">
+                  <juju.components.SvgIcon
+                    className="inline-logos__image"
+                    name="aws"
+                    size="100%" />
+                </li>
+                <li className="inline-logos__item">
+                  <juju.components.SvgIcon
+                    className="inline-logos__image"
+                    name="google"
+                    size="100%" />
+                </li>
+                <li className="inline-logos__item">
+                  <juju.components.SvgIcon
+                    className="inline-logos__image"
+                    name="azure"
+                    size="100%" />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </juju.components.DeploymentPanel>);
@@ -115,8 +152,8 @@ describe('DeploymentSignup', function() {
         modelName="Lamington">
         <span>content</span>
       </juju.components.DeploymentSignup>);
-    output.props.children.props.children[0].props.children[2].props.children[1]
-      .props.action();
+    output.props.children.props.children[0].props.children[1].props.children[3]
+      .props.children[1].props.action();
     assert.equal(changeState.callCount, 1);
     assert.deepEqual(changeState.args[0][0], {
       sectionC: {
