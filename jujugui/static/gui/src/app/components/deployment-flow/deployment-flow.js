0a023559bd99b0bab77a4d1c3e7fb94e04da616f
Huw Wilkins <huw.wilkins@canonical.com>
2017-01-25 11:12:49 +1100

    Display the agreement checkbox.

--- jujugui/static/gui/src/app/components/deployment-flow/deployment-flow.js
+++ jujugui/static/gui/src/app/components/deployment-flow/deployment-flow.js
@@ -146,9 +146,10 @@ YUI.add('deployment-flow', function() {
           visible = true;
           break;
         case 'agreements':
+          const terms = this._getTerms();
           completed = false;
           disabled = false;
-          visible = true;
+          visible = terms && terms.length > 0;
           break;
       }
       return {
@@ -301,6 +302,30 @@ YUI.add('deployment-flow', function() {
         args.config = {'authorized-keys': this.state.sshKey};
       }
       this.props.deploy(this._handleClose, true, this.state.modelName, args);
+    },
+
+    /**
+      Get the list of terms for the uncommitted apps.
+
+      @method _getTerms
+      @returns {Array} The list of terms.
+    */
+    _getTerms: function() {
+      let termIds = [];
+      this.props.applications.forEach(app => {
+        // Get the terms from the app's charm.
+        const terms = this.props.charmsGetById(app.get('charm')).get('terms');
+        if (terms && terms.length > 0) {
+          // If there are terms then add them if they haven't already been
+          // recorded.
+          terms.forEach(id => {
+            if (termIds.indexOf(id) === -1) {
+              termIds.push(id);
+            }
+          });
+        }
+      });
+      return termIds;
     },
 
     /**
@@ -645,19 +670,22 @@ YUI.add('deployment-flow', function() {
       @returns {Object} The markup.
     */
     _generateAgreementsSection: function() {
-      var status = this._getSectionStatus('agreements');
+      const status = this._getSectionStatus('agreements');
       if (!status.visible) {
         return;
       }
+      const disabled = this.props.acl.isReadOnly();
       return (
-        <juju.components.DeploymentTerms
-          acl={this.props.acl}
-          charmsGetById={this.props.charmsGetById}
-          getAgreements={this.props.getAgreements}
-          applications={this.props.applications}
-          setTerms={this._setTerms}
-          showTerms={this.props.showTerms}
-          terms={this.state.terms} />);
+          <div className="deployment-flow__deploy-option">
+            <input className="deployment-flow__deploy-checkbox"
+              disabled={disabled}
+              id="terms"
+              type="checkbox" />
+            <label className="deployment-flow__deploy-label"
+              htmlFor="terms">
+              I agree to all terms.
+            </label>
+          </div>);
     },
 
     /**
@@ -742,7 +770,6 @@ YUI.add('deployment-flow', function() {
     'deployment-section',
     'deployment-services',
     'deployment-ssh-key',
-    'deployment-terms',
     'generic-button',
     'generic-input'
   ]
