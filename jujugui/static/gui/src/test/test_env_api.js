82b93fa65d09de0de6a47529b38ff387fc8008a1
Jeff Pihach <jeff.pihach@canonical.com>
2017-03-21 14:01:24 -0500

    Added missing dependencies to env tests.

--- jujugui/static/gui/src/test/test_env_api.js
+++ jujugui/static/gui/src/test/test_env_api.js
@@ -101,7 +101,8 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
     before(function(done) {
       Y = YUI(GlobalConfig).use([
         'environment-change-set',
-        'juju-tests-utils'
+        'juju-tests-utils',
+        'juju-env-api'
       ], function(Y) {
         juju = Y.namespace('juju');
         utils = Y.namespace('juju-tests.utils');
