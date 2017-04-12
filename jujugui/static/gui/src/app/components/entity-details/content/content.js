da9006bf47a9517244c5551a8434fc22454aa1b3
Luke Wesley-Holley <luke@lukewh.com>
2017-04-12 15:45:39 +0100

    Updated styling of textarea

--- jujugui/static/gui/src/app/components/entity-details/content/content.js
+++ jujugui/static/gui/src/app/components/entity-details/content/content.js
@@ -427,6 +427,37 @@ YUI.add('entity-content', function() {
         </div>);
     },
 
+    /**
+      Generate the Juju card example
+    */
+    _generateCard: function() {
+      const entityModel = this.props.entityModel;
+      const entity = entityModel.toEntity();
+      const storeId = entity.type === 'charm' ?
+        entity.storeId : entity.id.split('cs:').join('');
+      const script = '<script ' +
+      'src="https://assets.ubuntu.com/v1/juju-cards-v1.5.0.js"></script>\n' +
+      '<div class="juju-card" data-id="' + storeId +'"></div>';
+
+      return (<div className="entity-content__card section clearfix">
+        <h3 className="section__title">
+          Embed this charm
+        </h3>
+        <p>
+          Add this card to your website by copying the code below.&nbsp;
+          <a href="https://jujucharms.com/community/cards" target="_blank"
+            className="link">
+            Learn more
+          </a>.
+        </p>
+        <textarea
+          rows="2" cols="70" readOnly="readonly" wrap="off"
+          className="twelve-col" defaultValue={script}></textarea>
+        <h4>Preview</h4>
+        <div className="juju-card" data-id={storeId}></div>
+      </div>);
+    },
+
     componentDidMount: function() {
       if (window.jujuCards) {
         window.jujuCards();
@@ -435,11 +466,6 @@ YUI.add('entity-content', function() {
 
     render: function() {
       const entityModel = this.props.entityModel;
-      const entity = entityModel.toEntity();
-      const type = entity.type;
-      const storeId = entity.type === 'charm' ? entity.storeId : entity.id.split('cs:').join('');
-      const script = `<script src="https://assets.ubuntu.com/v1/juju-cards-v1.5.0.js"></script>
-<div class="juju-card" data-id="${storeId}"></div>`;
       return (
         <div className="entity-content">
           {this._generateDescription(entityModel)}
@@ -462,19 +488,7 @@ YUI.add('entity-content', function() {
                   pluralize={this.props.pluralize} />
                 <juju.components.EntityContentRevisions
                   revisions={entityModel.get('revisions')} />
-                <div className="card section clearfix" id="cards">
-                  <h3 className="section__title">
-                    Embed this charm
-                  </h3>
-                  <p>
-                    Add this card to your website by copying the code below. Learn more.
-                  </p>
-                  <textarea
-                    rows="2" cols="70" readOnly="readonly" wrap="off"
-                    className="twelve-col" defaultValue={script}></textarea>
-                  <h4>Preview</h4>
-                  <div className="juju-card" data-id={storeId}></div>
-                </div>
+                {this._generateCard()}
               </div>
             </div>
           </div>
