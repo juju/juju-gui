c66bc58b93f9c6fb60365c4ee9ff9b2a101d019f
Huw Wilkins <huw.wilkins@canonical.com>
2017-04-21 14:17:48 +1000

    Add cookie getting/setting without YUI.

--- jujugui/static/gui/src/app/extensions/app-cookies-extension.js
+++ jujugui/static/gui/src/app/extensions/app-cookies-extension.js
@@ -34,6 +34,52 @@ YUI.add('app-cookies-extension', function(Y) {
   Cookies.prototype = {
 
     /**
+      Get the value for a cookie.
+
+      @method _getCookie
+      @param cookie {String} The name of the cookie.
+      @return {String} The value of the cookie.
+    */
+    _getCookie: function(cookie) {
+      let value = null;
+      document.cookie.split(';').some(pair => {
+        const parts = pair.split('=');
+        if (parts[0].trim() === cookie) {
+          value = parts.length == 2 && parts[1].trim() || '';
+          return true;
+        }
+      });
+      return value;
+    },
+
+    /**
+      Set a cookie. This is here so that we can test that the right values were
+      passed.
+
+      @method _setCookie
+      @param cookie {String} A cookie to set.
+    */
+    _setDocumentCookie: function(cookie) {
+      document.cookie = cookie;
+    },
+
+    /**
+      Set the value for a cookie.
+
+      @method _setCookie
+      @param cookie {String} The name of the cookie.
+      @param value {String} The value of the cookie.
+      @param expiry {String} The expiry for the cookie.
+    */
+    _setCookie: function(cookie, value, expiry) {
+      let pairs = [`${cookie}=${value}`];
+      if (expiry) {
+        pairs.push(`expiry=${expiry}`);
+      }
+      this._setDocumentCookie(pairs.join('; '));
+    },
+
+    /**
       Check that the user accepted cookie usage, and if not display a cookie
       usage warning.
 
@@ -41,7 +87,7 @@ YUI.add('app-cookies-extension', function(Y) {
       @return {undefined} Side-effects only.
     */
     check: function() {
-      if (Y.Cookie.get('_cookies_accepted') !== 'true' &&
+      if (this._getCookie('_cookies_accepted') !== 'true' &&
           !localStorage.getItem('disable-cookie')) {
         this.node.classList.add('display-cookie-notice');
         document.querySelector('.link-cta').addEventListener('click',
@@ -62,8 +108,8 @@ YUI.add('app-cookies-extension', function(Y) {
     */
     close: function() {
       this.node.classList.remove('display-cookie-notice');
-      Y.Cookie.set('_cookies_accepted', 'true',
-          {expires: new Date('January 12, 2025')});
+      this._setCookie(
+        '_cookies_accepted', 'true', new Date('January 12, 2025'));
     }
 
   };
@@ -72,7 +118,5 @@ YUI.add('app-cookies-extension', function(Y) {
 
 }, '0.1.0', {
   requires: [
-    'base',
-    'cookie'
   ]
 });
