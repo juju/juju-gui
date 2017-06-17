72d0133fb7732e49e0a25f0880504015260f61c3
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 11:46:33 +1000

    Update DeploymentBarNotification component.

--- jujugui/static/gui/src/app/components/deployment-bar/notification/notification.js
+++ jujugui/static/gui/src/app/components/deployment-bar/notification/notification.js
@@ -18,87 +18,87 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('deployment-bar-notification', function() {
+const DeploymentBarNotification = React.createClass({
+  timeout: null,
 
-  juju.components.DeploymentBarNotification = React.createClass({
-    timeout: null,
+  propTypes: {
+    change: React.PropTypes.object
+  },
 
-    propTypes: {
-      change: React.PropTypes.object
-    },
+  /**
+    Fade out the notification.
 
-    /**
-      Fade out the notification.
+    @method _hideNotification
+  */
+  _hideNotification: function() {
+    var node;
+    try {
+      // This will throw a warning that it can't be found sometimes, that's
+      // fine to ignore.
+      node = this.refs.deploymentBarNotificationContainer;
+      // Fade out the notification.
+      node.classList.remove('deployment-bar__notification--visible');
+    } catch (e) {
+      // The notification has already been removed.
+    }
+  },
 
-      @method _hideNotification
-    */
-    _hideNotification: function() {
-      var node;
-      try {
-        // This will throw a warning that it can't be found sometimes, that's
-        // fine to ignore.
-        node = this.refs.deploymentBarNotificationContainer;
-        // Fade out the notification.
-        node.classList.remove('deployment-bar__notification--visible');
-      } catch (e) {
-        // The notification has already been removed.
-      }
-    },
+  componentDidMount: function() {
+    this._showNotification(null);
+  },
 
-    componentDidMount: function() {
-      this._showNotification(null);
-    },
+  componentDidUpdate: function(prevProps, prevState) {
+    this._showNotification(prevProps);
+  },
 
-    componentDidUpdate: function(prevProps, prevState) {
-      this._showNotification(prevProps);
-    },
+  /**
+    Show the notification with the supplied change.
 
-    /**
-      Show the notification with the supplied change.
-
-      @method _showNotification
-      @param {Object} prevProps The previous props for the component.
-    */
-    _showNotification: function(prevProps) {
-      if (this.props.change) {
-        var newId = this.props.change.id;
-        var oldId = prevProps && prevProps.change ? prevProps.change.id : null;
-        // Only show the notification if we've received a new id.
-        if (newId !== oldId) {
-          var node = ReactDOM.findDOMNode(this);
-          // Fade in the notification.
-          if (node) {
-            node.classList.add('deployment-bar__notification--visible');
-            window.clearTimeout(this.timeout);
-            this.timeout = window.setTimeout(this._hideNotification, 4000);
-          }
+    @method _showNotification
+    @param {Object} prevProps The previous props for the component.
+  */
+  _showNotification: function(prevProps) {
+    if (this.props.change) {
+      var newId = this.props.change.id;
+      var oldId = prevProps && prevProps.change ? prevProps.change.id : null;
+      // Only show the notification if we've received a new id.
+      if (newId !== oldId) {
+        var node = ReactDOM.findDOMNode(this);
+        // Fade in the notification.
+        if (node) {
+          node.classList.add('deployment-bar__notification--visible');
+          window.clearTimeout(this.timeout);
+          this.timeout = window.setTimeout(this._hideNotification, 4000);
         }
       }
-    },
-
-    /**
-      Hide the notification when clicked
-
-      @method _handleClick
-    */
-    _handleClick: function() {
-      window.clearTimeout(this.timeout);
-      this._hideNotification();
-    },
-
-    render: function() {
-      var description;
-      if (this.props.change) {
-        description = this.props.change.description;
-      }
-      return (
-        <div className="deployment-bar__notification"
-          onClick={this._handleClick}
-          ref="deploymentBarNotificationContainer">
-          {description}
-        </div>
-      );
     }
-  });
+  },
 
+  /**
+    Hide the notification when clicked
+
+    @method _handleClick
+  */
+  _handleClick: function() {
+    window.clearTimeout(this.timeout);
+    this._hideNotification();
+  },
+
+  render: function() {
+    var description;
+    if (this.props.change) {
+      description = this.props.change.description;
+    }
+    return (
+      <div className="deployment-bar__notification"
+        onClick={this._handleClick}
+        ref="deploymentBarNotificationContainer">
+        {description}
+      </div>
+    );
+  }
+});
+
+YUI.add('deployment-bar-notification', function() {
+  juju.components.DeploymentBarNotification = DeploymentBarNotification;
 }, '0.1.0', { requires: []});
