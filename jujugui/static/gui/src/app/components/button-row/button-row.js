02daebdc45e4fe95e231025ca6b9208fd9e6e9be
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 10:27:52 +1000

    Update ButtonRow component.

--- jujugui/static/gui/src/app/components/button-row/button-row.js
+++ jujugui/static/gui/src/app/components/button-row/button-row.js
@@ -18,62 +18,62 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
+const ButtonRow = React.createClass({
+  displayName: 'ButtonRow',
+
+  propTypes: {
+    buttons: React.PropTypes.array.isRequired
+  },
+
+  /**
+    Returns the classes for the footer based on the provided props.
+    @method _generateClasses
+    @returns {String} The collection of class names.
+  */
+  _generateClasses: function() {
+    var classes = {};
+    var buttonsLength = this.props.buttons.length;
+    classes['button-row--multiple'] = buttonsLength > 0;
+    classes['button-row--count-' + buttonsLength] = true;
+    return classNames(
+      'button-row',
+      classes
+    );
+  },
+
+  /**
+    Creates the buttons based on the provided props.
+    @method _generateButtons
+    @param {Array} buttons The properties of the buttons to generate.
+    @returns {Array} Collection of buttons.
+  */
+  _generateButtons: function(buttons) {
+    var components = [];
+    buttons.forEach((button) => {
+      components.push(
+        <juju.components.GenericButton
+          key={button.title}
+          title={button.title}
+          action={button.action}
+          disabled={button.disabled}
+          submit={button.submit}
+          type={button.type} />);
+    });
+    return components;
+  },
+
+  render: function() {
+    var buttons = this._generateButtons(this.props.buttons);
+    return (
+      <div className={this._generateClasses()}>
+        {buttons}
+      </div>
+    );
+  }
+});
+
 YUI.add('button-row', function() {
-
-  juju.components.ButtonRow = React.createClass({
-    displayName: 'ButtonRow',
-
-    propTypes: {
-      buttons: React.PropTypes.array.isRequired
-    },
-
-    /**
-      Returns the classes for the footer based on the provided props.
-      @method _generateClasses
-      @returns {String} The collection of class names.
-    */
-    _generateClasses: function() {
-      var classes = {};
-      var buttonsLength = this.props.buttons.length;
-      classes['button-row--multiple'] = buttonsLength > 0;
-      classes['button-row--count-' + buttonsLength] = true;
-      return classNames(
-        'button-row',
-        classes
-      );
-    },
-
-    /**
-      Creates the buttons based on the provided props.
-      @method _generateButtons
-      @param {Array} buttons The properties of the buttons to generate.
-      @returns {Array} Collection of buttons.
-    */
-    _generateButtons: function(buttons) {
-      var components = [];
-      buttons.forEach((button) => {
-        components.push(
-          <juju.components.GenericButton
-            key={button.title}
-            title={button.title}
-            action={button.action}
-            disabled={button.disabled}
-            submit={button.submit}
-            type={button.type} />);
-      });
-      return components;
-    },
-
-    render: function() {
-      var buttons = this._generateButtons(this.props.buttons);
-      return (
-        <div className={this._generateClasses()}>
-          {buttons}
-        </div>
-      );
-    }
-  });
-
+  juju.components.ButtonRow = ButtonRow;
 }, '0.1.0', { requires: [
   'generic-button'
 ]});
