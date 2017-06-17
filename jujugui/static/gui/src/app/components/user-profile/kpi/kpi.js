7f8275ce9ff0c1327cbea07a4e0343d07bcf7477
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-18 07:34:37 +1000

    Update UserProfileEntityKPI component.

--- jujugui/static/gui/src/app/components/user-profile/kpi/kpi.js
+++ jujugui/static/gui/src/app/components/user-profile/kpi/kpi.js
@@ -18,69 +18,69 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
+const UserProfileEntityKPI = React.createClass({
+  displayName: 'UserProfileEntityKPI',
+
+  propTypes: {
+    d3: React.PropTypes.object.isRequired,
+    metricTypes: React.PropTypes.array.isRequired,
+    metrics: React.PropTypes.array.isRequired
+  },
+
+  getInitialState: function() {
+    return {
+      currentMetric: []
+    };
+  },
+
+  /**
+    Tell the metric component to render a graph of a specific metric.
+
+    @method _showMetric
+    @param {String} metric The metric to show (selected from metricTypes)
+  */
+  _showMetric: function(metric) {
+    // TODO:
+    // the ability to select a metric does not work properly, and we don't
+    // know enough about the eventual metrics of charms to guess how; for
+    // now, just render the first metric at hand. See also the comment in
+    // in componentDidMount
+    // Makyo 2017-04-03
+    metric = this.props.metrics[0].metric;
+    this.setState({
+      currentMetric: this.props.metrics.filter(d => d.metric === metric)
+    });
+  },
+
+  /**
+    Show the metric when the component has mounted
+  */
+  componentDidMount: function() {
+    // TODO:
+    // the select isn't working; this should be the on-change callback
+    // but for now just call it
+    // Makyo 2017-04-03
+    this._showMetric('bad-wolf');
+  },
+
+  /**
+     Render a metric into the entity
+     TODO: in the future, this will include an instance of inset-select
+     to choose which metric.
+  */
+  render: function() {
+    return (
+      <div className="twelve-col last-col">
+        <juju.components.UserProfileEntityMetric
+          d3={this.props.d3}
+          metric={this.state.currentMetric} />
+      </div>);
+  }
+
+});
+
 YUI.add('user-profile-entity-kpi', function() {
-
-  juju.components.UserProfileEntityKPI = React.createClass({
-    displayName: 'UserProfileEntityKPI',
-
-    propTypes: {
-      d3: React.PropTypes.object.isRequired,
-      metricTypes: React.PropTypes.array.isRequired,
-      metrics: React.PropTypes.array.isRequired
-    },
-
-    getInitialState: function() {
-      return {
-        currentMetric: []
-      };
-    },
-
-    /**
-      Tell the metric component to render a graph of a specific metric.
-
-      @method _showMetric
-      @param {String} metric The metric to show (selected from metricTypes)
-    */
-    _showMetric: function(metric) {
-      // TODO:
-      // the ability to select a metric does not work properly, and we don't
-      // know enough about the eventual metrics of charms to guess how; for
-      // now, just render the first metric at hand. See also the comment in
-      // in componentDidMount
-      // Makyo 2017-04-03
-      metric = this.props.metrics[0].metric;
-      this.setState({
-        currentMetric: this.props.metrics.filter(d => d.metric === metric)
-      });
-    },
-
-    /**
-      Show the metric when the component has mounted
-    */
-    componentDidMount: function() {
-      // TODO:
-      // the select isn't working; this should be the on-change callback
-      // but for now just call it
-      // Makyo 2017-04-03
-      this._showMetric('bad-wolf');
-    },
-
-    /**
-       Render a metric into the entity
-       TODO: in the future, this will include an instance of inset-select
-       to choose which metric.
-    */
-    render: function() {
-      return (
-        <div className="twelve-col last-col">
-          <juju.components.UserProfileEntityMetric
-            d3={this.props.d3}
-            metric={this.state.currentMetric} />
-        </div>);
-    }
-
-  });
-
+  juju.components.UserProfileEntityKPI = UserProfileEntityKPI;
 }, '', {
   requires: [
     'user-profile-entity-metric'
