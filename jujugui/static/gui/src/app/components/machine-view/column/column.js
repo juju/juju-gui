31b3b5cdb21f8d9b20c52ecb084792b3d145d7d9
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 15:17:22 +1000

    Update MachineViewColumn component.

--- jujugui/static/gui/src/app/components/machine-view/column/column.js
+++ jujugui/static/gui/src/app/components/machine-view/column/column.js
@@ -18,118 +18,116 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('machine-view-column', function() {
+const dropTarget = {
+  /**
+    Called when something is dropped on the component.
+    See: http://gaearon.github.io/react-dnd/docs-drop-target.html
 
-  var dropTarget = {
-    /**
-      Called when something is dropped on the component.
-      See: http://gaearon.github.io/react-dnd/docs-drop-target.html
-
-      @method drop
-      @param {Object} props The component props.
-      @param {Object} monitor A DropTargetMonitor.
-      @param {Object} component The component that is being dropped onto.
-    */
-    drop: function (props, monitor, component) {
-      // Don't try and place the unit if it was dropped on a nested component.
-      if (monitor.didDrop()) {
-        return;
-      };
-      var item = monitor.getItem();
-      if (props.droppable) {
-        props.dropUnit(item.unit, null, props.type);
-      }
-    },
-
-    /**
-      Called to check if the unit is allowed to be dropped on the column.
-
-      @method canDrop
-      @param {Object} props The component props.
-      @param {Object} monitor A DropTargetMonitor.
-    */
-    canDrop: function (props, monitor) {
-      return !props.acl.isReadOnly() && props.droppable;
+    @method drop
+    @param {Object} props The component props.
+    @param {Object} monitor A DropTargetMonitor.
+    @param {Object} component The component that is being dropped onto.
+  */
+  drop: function (props, monitor, component) {
+    // Don't try and place the unit if it was dropped on a nested component.
+    if (monitor.didDrop()) {
+      return;
+    };
+    var item = monitor.getItem();
+    if (props.droppable) {
+      props.dropUnit(item.unit, null, props.type);
     }
-  };
+  },
 
   /**
-    Provides props to be injected into the component.
+    Called to check if the unit is allowed to be dropped on the column.
 
-    @method collect
-    @param {Object} connect The connector.
+    @method canDrop
+    @param {Object} props The component props.
     @param {Object} monitor A DropTargetMonitor.
   */
-  function collect(connect, monitor) {
-    return {
-      connectDropTarget: connect.dropTarget(),
-      canDrop: monitor.canDrop(),
-      isOver: monitor.isOver({shallow: true})
-    };
+  canDrop: function (props, monitor) {
+    return !props.acl.isReadOnly() && props.droppable;
   }
+};
 
-  var MachineViewColumn = React.createClass({
-    propTypes: {
-      acl: React.PropTypes.object.isRequired,
-      activeMenuItem: React.PropTypes.string,
-      canDrop: React.PropTypes.bool.isRequired,
-      children: React.PropTypes.oneOfType([
-        React.PropTypes.object,
-        React.PropTypes.array
-      ]),
-      connectDropTarget: React.PropTypes.func.isRequired,
-      dropUnit: React.PropTypes.func,
-      droppable: React.PropTypes.bool.isRequired,
-      isOver: React.PropTypes.bool.isRequired,
-      menuItems: React.PropTypes.array,
-      title: React.PropTypes.string.isRequired,
-      toggle: React.PropTypes.object,
-      type: React.PropTypes.string
-    },
+/**
+  Provides props to be injected into the component.
 
-    /**
-      Generate the classes for the component.
+  @method collect
+  @param {Object} connect The connector.
+  @param {Object} monitor A DropTargetMonitor.
+*/
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    canDrop: monitor.canDrop(),
+    isOver: monitor.isOver({shallow: true})
+  };
+}
 
-      @method _generateClasses
-      @returns {String} The collection of class names.
-    */
-    _generateClasses: function() {
-      return classNames(
-        'machine-view__column', {
-          'machine-view__column--droppable':
-            this.props.droppable && this.props.canDrop,
-          'machine-view__column--drop': this.props.isOver
-        });
-    },
+const MachineViewColumn = ReactDnD.DropTarget(
+  'unit', dropTarget, collect)(React.createClass({
+  propTypes: {
+    acl: React.PropTypes.object.isRequired,
+    activeMenuItem: React.PropTypes.string,
+    canDrop: React.PropTypes.bool.isRequired,
+    children: React.PropTypes.oneOfType([
+      React.PropTypes.object,
+      React.PropTypes.array
+    ]),
+    connectDropTarget: React.PropTypes.func.isRequired,
+    dropUnit: React.PropTypes.func,
+    droppable: React.PropTypes.bool.isRequired,
+    isOver: React.PropTypes.bool.isRequired,
+    menuItems: React.PropTypes.array,
+    title: React.PropTypes.string.isRequired,
+    toggle: React.PropTypes.object,
+    type: React.PropTypes.string
+  },
 
-    render: function() {
-      var props = this.props;
-      return this.props.connectDropTarget(
-        <div className={this._generateClasses()}>
-          <juju.components.MachineViewHeader
-            acl={this.props.acl}
-            activeMenuItem={props.activeMenuItem}
-            droppable={props.droppable}
-            dropUnit={props.dropUnit}
-            menuItems={props.menuItems}
-            title={props.title}
-            toggle={props.toggle}
-            type={props.type} />
-          <div className="machine-view__column-content">
-            {this.props.children}
-            <div className="machine-view__column-drop-target">
-              <juju.components.SvgIcon name="add_16"
-                size="16" />
-            </div>
+  /**
+    Generate the classes for the component.
+
+    @method _generateClasses
+    @returns {String} The collection of class names.
+  */
+  _generateClasses: function() {
+    return classNames(
+      'machine-view__column', {
+        'machine-view__column--droppable':
+          this.props.droppable && this.props.canDrop,
+        'machine-view__column--drop': this.props.isOver
+      });
+  },
+
+  render: function() {
+    var props = this.props;
+    return this.props.connectDropTarget(
+      <div className={this._generateClasses()}>
+        <juju.components.MachineViewHeader
+          acl={this.props.acl}
+          activeMenuItem={props.activeMenuItem}
+          droppable={props.droppable}
+          dropUnit={props.dropUnit}
+          menuItems={props.menuItems}
+          title={props.title}
+          toggle={props.toggle}
+          type={props.type} />
+        <div className="machine-view__column-content">
+          {this.props.children}
+          <div className="machine-view__column-drop-target">
+            <juju.components.SvgIcon name="add_16"
+              size="16" />
           </div>
         </div>
-      );
-    }
-  });
+      </div>
+    );
+  }
+}));
 
-  juju.components.MachineViewColumn = ReactDnD.DropTarget(
-    'unit', dropTarget, collect)(MachineViewColumn);
-
+YUI.add('machine-view-column', function() {
+  juju.components.MachineViewColumn = MachineViewColumn;
 }, '0.1.0', {
   requires: [
     'machine-view-header',
