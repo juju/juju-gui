969caaea820672805d2e3e8baa16f6c029189a72
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 10:58:11 +1000

    Update CreateModelButton component.

--- jujugui/static/gui/src/app/components/create-model-button/create-model-button.js
+++ jujugui/static/gui/src/app/components/create-model-button/create-model-button.js
@@ -18,53 +18,53 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('create-model-button', function() {
+const CreateModelButton = React.createClass({
+  propTypes: {
+    action: React.PropTypes.func,
+    changeState: React.PropTypes.func.isRequired,
+    disabled: React.PropTypes.bool,
+    switchModel: React.PropTypes.func.isRequired,
+    title: React.PropTypes.string,
+    type: React.PropTypes.string
+  },
 
-  juju.components.CreateModelButton = React.createClass({
-    propTypes: {
-      action: React.PropTypes.func,
-      changeState: React.PropTypes.func.isRequired,
-      disabled: React.PropTypes.bool,
-      switchModel: React.PropTypes.func.isRequired,
-      title: React.PropTypes.string,
-      type: React.PropTypes.string
-    },
+  getDefaultProps: function() {
+    return {
+      type: 'inline-neutral',
+      title: 'Create new'
+    };
+  },
 
-    getDefaultProps: function() {
-      return {
-        type: 'inline-neutral',
-        title: 'Create new'
-      };
-    },
-
-    _createNewModel: function() {
-      const props = this.props;
-      if (props.disabled) {
-        return;
-      }
-      // We want to explicitly close the profile when switching to a new
-      // model to resolve a race condition with the new model setup.
-      props.changeState({profile: null});
-      props.switchModel(null);
-      if (this.props.action) {
-        this.props.action();
-      }
-    },
-
-    render: function() {
-      const disabled = this.props.disabled || false;
-      return (
-        <div className="create-new-model">
-          <juju.components.GenericButton
-            action={this._createNewModel}
-            disabled={disabled}
-            type={this.props.type}
-            title={this.props.title} />
-        </div>
-      );
+  _createNewModel: function() {
+    const props = this.props;
+    if (props.disabled) {
+      return;
     }
-  });
+    // We want to explicitly close the profile when switching to a new
+    // model to resolve a race condition with the new model setup.
+    props.changeState({profile: null});
+    props.switchModel(null);
+    if (this.props.action) {
+      this.props.action();
+    }
+  },
 
+  render: function() {
+    const disabled = this.props.disabled || false;
+    return (
+      <div className="create-new-model">
+        <juju.components.GenericButton
+          action={this._createNewModel}
+          disabled={disabled}
+          type={this.props.type}
+          title={this.props.title} />
+      </div>
+    );
+  }
+});
+
+YUI.add('create-model-button', function() {
+  juju.components.CreateModelButton = CreateModelButton;
 }, '', {
   requires: [
     'generic-button'
