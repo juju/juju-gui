10c7efe3705360e747d0d6a403a970faf929c41d
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-18 07:33:42 +1000

    Update UserProfileEntityList component.

--- jujugui/static/gui/src/app/components/user-profile/entity-list/entity-list.js
+++ jujugui/static/gui/src/app/components/user-profile/entity-list/entity-list.js
@@ -18,344 +18,344 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('user-profile-entity-list', function() {
+const UserProfileEntityList = React.createClass({
+  // broadcastStatus is necessary for communicating loading status back to
+  // the parent SectionLoadWatcher.
+  propTypes: {
+    broadcastStatus: React.PropTypes.func,
+    changeState: React.PropTypes.func.isRequired,
+    charmstore: React.PropTypes.object.isRequired,
+    d3: React.PropTypes.object,
+    getDiagramURL: React.PropTypes.func.isRequired,
+    getKpiMetrics: React.PropTypes.func,
+    type: React.PropTypes.string.isRequired,
+    user: React.PropTypes.string
+  },
 
-  juju.components.UserProfileEntityList = React.createClass({
-    // broadcastStatus is necessary for communicating loading status back to
-    // the parent SectionLoadWatcher.
-    propTypes: {
-      broadcastStatus: React.PropTypes.func,
-      changeState: React.PropTypes.func.isRequired,
-      charmstore: React.PropTypes.object.isRequired,
-      d3: React.PropTypes.object,
-      getDiagramURL: React.PropTypes.func.isRequired,
-      getKpiMetrics: React.PropTypes.func,
-      type: React.PropTypes.string.isRequired,
-      user: React.PropTypes.string
-    },
+  getInitialState: function() {
+    this.xhrs = [];
 
-    getInitialState: function() {
-      this.xhrs = [];
+    return {
+      entityList: [],
+      loadingEntities: false
+    };
+  },
 
-      return {
-        entityList: [],
-        loadingEntities: false
-      };
-    },
+  getDefaultProps: function() {
+    // Just in case broadcastStatus isn't passed in (e.g., in tests), calls
+    // to it should not fail, so default to an empty function.
+    return {
+      broadcastStatus: function() {}
+    };
+  },
 
-    getDefaultProps: function() {
-      // Just in case broadcastStatus isn't passed in (e.g., in tests), calls
-      // to it should not fail, so default to an empty function.
-      return {
-        broadcastStatus: function() {}
-      };
-    },
+  componentWillMount: function() {
+    if (this.props.user) {
+      this._fetchEntities(this.props);
+    }
+  },
 
-    componentWillMount: function() {
-      if (this.props.user) {
-        this._fetchEntities(this.props);
-      }
-    },
+  componentWillUnmount: function() {
+    this.xhrs.forEach((xhr) => {
+      xhr && xhr.abort && xhr.abort();
+    });
+  },
 
-    componentWillUnmount: function() {
-      this.xhrs.forEach((xhr) => {
-        xhr && xhr.abort && xhr.abort();
+  componentWillReceiveProps: function(nextProps) {
+    const props = this.props;
+    // Compare next and previous charmstore users in a data-safe manner.
+    if (props.user !== nextProps.user) {
+      this._fetchEntities(nextProps);
+    }
+  },
+
+  /**
+    Requests a list from charmstore of the user's entities.
+
+    @method _fetchEntities
+    @param {Object} props the component properties to use.
+  */
+  _fetchEntities:  function(props) {
+    const charmstore = props.charmstore;
+    if (charmstore && charmstore.list && props.user) {
+      this.props.broadcastStatus('starting');
+      const callback = this._fetchEntitiesCallback;
+      // Delay the call until after the state change to prevent race
+      // conditions.
+      this.setState({loadingEntities: true}, () => {
+        const xhr = charmstore.list(props.user, callback, props.type);
+        this.xhrs.push(xhr);
       });
-    },
+    }
+  },
 
-    componentWillReceiveProps: function(nextProps) {
-      const props = this.props;
-      // Compare next and previous charmstore users in a data-safe manner.
-      if (props.user !== nextProps.user) {
-        this._fetchEntities(nextProps);
-      }
-    },
+  /**
+    Callback for the request to list a user's entities.
 
-    /**
-      Requests a list from charmstore of the user's entities.
-
-      @method _fetchEntities
-      @param {Object} props the component properties to use.
-    */
-    _fetchEntities:  function(props) {
-      const charmstore = props.charmstore;
-      if (charmstore && charmstore.list && props.user) {
-        this.props.broadcastStatus('starting');
-        const callback = this._fetchEntitiesCallback;
-        // Delay the call until after the state change to prevent race
-        // conditions.
-        this.setState({loadingEntities: true}, () => {
-          const xhr = charmstore.list(props.user, callback, props.type);
-          this.xhrs.push(xhr);
-        });
-      }
-    },
-
-    /**
-      Callback for the request to list a user's entities.
-
-      @method _fetchEntitiesCallback
-      @param {String} error The error from the request, or null.
-      @param {Object} data The data from the request.
-    */
-    _fetchEntitiesCallback: function(error, data) {
-      this.setState({loadingEntities: false}, () => {
-        const broadcastStatus = this.props.broadcastStatus;
-        if (error) {
-          broadcastStatus('error');
-          console.error('Can not retrieve entities: ', error);
-          return;
-        }
-        if (!data || !data.length || data.length === 0) {
-          broadcastStatus('empty');
-        } else {
-          broadcastStatus('ok');
-        }
-        this.setState({entityList: data});
-      });
-    },
-
-    /**
-      Generate a list of tags.
-
-      @method _generateTags
-      @param {Array} tagList A list of tags.
-      @param {String} id The id of the entity.
-      @returns {Object} A list of tag components.
-    */
-    _generateTags: function(tagList, id) {
-      if (!tagList) {
+    @method _fetchEntitiesCallback
+    @param {String} error The error from the request, or null.
+    @param {Object} data The data from the request.
+  */
+  _fetchEntitiesCallback: function(error, data) {
+    this.setState({loadingEntities: false}, () => {
+      const broadcastStatus = this.props.broadcastStatus;
+      if (error) {
+        broadcastStatus('error');
+        console.error('Can not retrieve entities: ', error);
         return;
       }
-      const tags = [];
-      tagList.forEach((tag) => {
-        tags.push(
-          <li className="user-profile__comma-item"
-            key={id + '-' + tag}>
-            {tag}
-          </li>);
-      });
-      return (
-        <ul className="user-profile__list-tags">
-          {tags}
-        </ul>);
-    },
-
-    /**
-      Generate a list of series.
-
-      @method _generateSeries
-      @param {Array} series A list of series.
-      @param {String} id The id of the entity.
-      @returns {Object} A list of series components.
-    */
-    _generateSeries: function(series, id) {
-      if (!series) {
-        return;
+      if (!data || !data.length || data.length === 0) {
+        broadcastStatus('empty');
+      } else {
+        broadcastStatus('ok');
       }
-      const listItems = [];
-      series.forEach((release) => {
-        listItems.push(
-          <li className="user-profile__comma-item"
-            key={id + '-' + release}>
-            {release}
-          </li>);
-      });
-      return (
-        <ul className="user-profile__list-series">
-          {listItems}
-        </ul>);
-    },
+      this.setState({entityList: data});
+    });
+  },
 
-    /**
-      Construct the URL for a service icon.
+  /**
+    Generate a list of tags.
 
-      @method _getIcon
-      @param {String} id The service ID.
-      @returns {String} The icon URL.
-    */
-    _getIcon: function(id) {
-      if (!id) {
-        return;
-      }
-      const cs = this.props.charmstore;
-      const path = id.replace('cs:', '');
-      return `${cs.url}/${path}/icon.svg`;
-    },
+    @method _generateTags
+    @param {Array} tagList A list of tags.
+    @param {String} id The id of the entity.
+    @returns {Object} A list of tag components.
+  */
+  _generateTags: function(tagList, id) {
+    if (!tagList) {
+      return;
+    }
+    const tags = [];
+    tagList.forEach((tag) => {
+      tags.push(
+        <li className="user-profile__comma-item"
+          key={id + '-' + tag}>
+          {tag}
+        </li>);
+    });
+    return (
+      <ul className="user-profile__list-tags">
+        {tags}
+      </ul>);
+  },
 
-    /**
-      Generate the details for the provided bundle.
+  /**
+    Generate a list of series.
 
-      @method _generateBundleRow
-      @param {Object} bundle A bundle object.
-      @returns {Array} The markup for the row.
-    */
-    _generateBundleRow: function(bundle) {
-      const id = bundle.id;
-      const services = [];
-      const applications = bundle.applications || bundle.services || {};
-      const serviceNames = Object.keys(applications);
-      serviceNames.forEach((serviceName, idx) => {
-        const service = applications[serviceName];
-        const id = service.charm;
-        const key = `icon-${idx}-${id}`;
-        services.push(
+    @method _generateSeries
+    @param {Array} series A list of series.
+    @param {String} id The id of the entity.
+    @returns {Object} A list of series components.
+  */
+  _generateSeries: function(series, id) {
+    if (!series) {
+      return;
+    }
+    const listItems = [];
+    series.forEach((release) => {
+      listItems.push(
+        <li className="user-profile__comma-item"
+          key={id + '-' + release}>
+          {release}
+        </li>);
+    });
+    return (
+      <ul className="user-profile__list-series">
+        {listItems}
+      </ul>);
+  },
+
+  /**
+    Construct the URL for a service icon.
+
+    @method _getIcon
+    @param {String} id The service ID.
+    @returns {String} The icon URL.
+  */
+  _getIcon: function(id) {
+    if (!id) {
+      return;
+    }
+    const cs = this.props.charmstore;
+    const path = id.replace('cs:', '');
+    return `${cs.url}/${path}/icon.svg`;
+  },
+
+  /**
+    Generate the details for the provided bundle.
+
+    @method _generateBundleRow
+    @param {Object} bundle A bundle object.
+    @returns {Array} The markup for the row.
+  */
+  _generateBundleRow: function(bundle) {
+    const id = bundle.id;
+    const services = [];
+    const applications = bundle.applications || bundle.services || {};
+    const serviceNames = Object.keys(applications);
+    serviceNames.forEach((serviceName, idx) => {
+      const service = applications[serviceName];
+      const id = service.charm;
+      const key = `icon-${idx}-${id}`;
+      services.push(
+        <img className="user-profile__list-icon"
+          key={key}
+          src={this._getIcon(id)}
+          title={service.charm} />);
+    });
+    const unitCount = bundle.unitCount || <span>&nbsp;</span>;
+    return (
+      <juju.components.UserProfileEntity
+        changeState={this.props.changeState}
+        entity={bundle}
+        getDiagramURL={this.props.getDiagramURL}
+        key={id}
+        type="bundle">
+        <span className={'user-profile__list-col five-col ' +
+          'user-profile__list-name'}>
+          {bundle.name}
+          {this._generateTags(bundle.tags, id)}
+        </span>
+        <span className={'user-profile__list-col three-col ' +
+          'user-profile__list-icons'}>
+          {services}
+        </span>
+        <span className={'user-profile__list-col three-col prepend-one ' +
+          'last-col'}>
+          {unitCount}
+        </span>
+      </juju.components.UserProfileEntity>);
+  },
+
+  /**
+    Generate the header for the bundles.
+
+    @method _generateBundleHeader
+    @returns {Array} The markup for the header.
+  */
+  _generateBundleHeader: function() {
+    return (
+      <li className="user-profile__list-header twelve-col">
+        <span className="user-profile__list-col five-col">
+          Name
+        </span>
+        <span className={'user-profile__list-col three-col ' +
+          'user-profile__list-icons'}>
+          Charms
+        </span>
+        <span className={'user-profile__list-col three-col prepend-one ' +
+          'last-col'}>
+          Units
+        </span>
+      </li>);
+  },
+
+  /**
+    Generate the details for the provided charm.
+
+    @method _generateCharmRow
+    @param {Object} charm A charm object.
+    @returns {Array} The markup for the row.
+  */
+  _generateCharmRow: function(charm) {
+    const id = charm.id;
+    // Ensure the icon is set.
+    charm.icon = charm.icon || this._getIcon(id);
+    return (
+      <juju.components.UserProfileEntity
+        changeState={this.props.changeState}
+        d3={this.props.d3}
+        entity={charm}
+        key={id}
+        getKpiMetrics={this.props.getKpiMetrics}
+        type="charm">
+        <span className={'user-profile__list-col five-col ' +
+          'user-profile__list-name'}>
+          {charm.name}
+          {this._generateTags(charm.tags, id)}
+        </span>
+        <span className={'user-profile__list-col three-col ' +
+          'user-profile__list-icons'}>
           <img className="user-profile__list-icon"
-            key={key}
-            src={this._getIcon(id)}
-            title={service.charm} />);
-      });
-      const unitCount = bundle.unitCount || <span>&nbsp;</span>;
-      return (
-        <juju.components.UserProfileEntity
-          changeState={this.props.changeState}
-          entity={bundle}
-          getDiagramURL={this.props.getDiagramURL}
-          key={id}
-          type="bundle">
-          <span className={'user-profile__list-col five-col ' +
-            'user-profile__list-name'}>
-            {bundle.name}
-            {this._generateTags(bundle.tags, id)}
-          </span>
-          <span className={'user-profile__list-col three-col ' +
-            'user-profile__list-icons'}>
-            {services}
-          </span>
-          <span className={'user-profile__list-col three-col prepend-one ' +
-            'last-col'}>
-            {unitCount}
-          </span>
-        </juju.components.UserProfileEntity>);
-    },
+            src={charm.icon}
+            title={charm.name} />
+        </span>
+        <span className={'user-profile__list-col prepend-one three-col ' +
+          'last-col'}>
+          {this._generateSeries(charm.series, id)}
+        </span>
+      </juju.components.UserProfileEntity>);
+  },
 
-    /**
-      Generate the header for the bundles.
+  /**
+    Generate the header for the charms.
 
-      @method _generateBundleHeader
-      @returns {Array} The markup for the header.
-    */
-    _generateBundleHeader: function() {
-      return (
-        <li className="user-profile__list-header twelve-col">
-          <span className="user-profile__list-col five-col">
-            Name
-          </span>
-          <span className={'user-profile__list-col three-col ' +
-            'user-profile__list-icons'}>
-            Charms
-          </span>
-          <span className={'user-profile__list-col three-col prepend-one ' +
-            'last-col'}>
-            Units
-          </span>
-        </li>);
-    },
+    @method _generateCharmHeader
+    @returns {Array} The markup for the header.
+  */
+  _generateCharmHeader: function() {
+    return (
+      <li className="user-profile__list-header twelve-col">
+        <span className="user-profile__list-col eight-col">
+          Name
+        </span>
+        <span className={'user-profile__list-col prepend-one three-col ' +
+          'last-col'}>
+          Series
+        </span>
+      </li>);
+  },
 
-    /**
-      Generate the details for the provided charm.
-
-      @method _generateCharmRow
-      @param {Object} charm A charm object.
-      @returns {Array} The markup for the row.
-    */
-    _generateCharmRow: function(charm) {
-      const id = charm.id;
-      // Ensure the icon is set.
-      charm.icon = charm.icon || this._getIcon(id);
-      return (
-        <juju.components.UserProfileEntity
-          changeState={this.props.changeState}
-          d3={this.props.d3}
-          entity={charm}
-          key={id}
-          getKpiMetrics={this.props.getKpiMetrics}
-          type="charm">
-          <span className={'user-profile__list-col five-col ' +
-            'user-profile__list-name'}>
-            {charm.name}
-            {this._generateTags(charm.tags, id)}
-          </span>
-          <span className={'user-profile__list-col three-col ' +
-            'user-profile__list-icons'}>
-            <img className="user-profile__list-icon"
-              src={charm.icon}
-              title={charm.name} />
-          </span>
-          <span className={'user-profile__list-col prepend-one three-col ' +
-            'last-col'}>
-            {this._generateSeries(charm.series, id)}
-          </span>
-        </juju.components.UserProfileEntity>);
-    },
-
-    /**
-      Generate the header for the charms.
-
-      @method _generateCharmHeader
-      @returns {Array} The markup for the header.
-    */
-    _generateCharmHeader: function() {
-      return (
-        <li className="user-profile__list-header twelve-col">
-          <span className="user-profile__list-col eight-col">
-            Name
-          </span>
-          <span className={'user-profile__list-col prepend-one three-col ' +
-            'last-col'}>
-            Series
-          </span>
-        </li>);
-    },
-
-    render: function() {
-      const type = this.props.type;
-      const classes = classNames(
-        `user-profile__${type}-list`,
-        { 'twelve-col': this.state.loadingEntities }
-      );
-      if (this.state.loadingEntities) {
-        return (
-          <div className={classes}>
-            <juju.components.Spinner />
-          </div>
-        );
-      }
-      const list = this.state.entityList;
-      if (!list || list.length === 0) {
-        return null;
-      }
-      let generateRow,
-          header,
-          title;
-      if (type === 'bundle') {
-        generateRow = this._generateBundleRow;
-        header = this._generateBundleHeader();
-        title = 'Bundles';
-      } else if (type === 'charm') {
-        generateRow = this._generateCharmRow;
-        header = this._generateCharmHeader();
-        title = 'Charms';
-      }
-      const rows = list.map(generateRow);
+  render: function() {
+    const type = this.props.type;
+    const classes = classNames(
+      `user-profile__${type}-list`,
+      { 'twelve-col': this.state.loadingEntities }
+    );
+    if (this.state.loadingEntities) {
       return (
         <div className={classes}>
-          <div className="user-profile__header twelve-col no-margin-bottom">
-            {title}
-            <span className="user-profile__size">
-              ({list.length})
-            </span>
-          </div>
-          <ul className="user-profile__list twelve-col">
-            {header}
-            {rows}
-          </ul>
+          <juju.components.Spinner />
         </div>
       );
     }
+    const list = this.state.entityList;
+    if (!list || list.length === 0) {
+      return null;
+    }
+    let generateRow,
+        header,
+        title;
+    if (type === 'bundle') {
+      generateRow = this._generateBundleRow;
+      header = this._generateBundleHeader();
+      title = 'Bundles';
+    } else if (type === 'charm') {
+      generateRow = this._generateCharmRow;
+      header = this._generateCharmHeader();
+      title = 'Charms';
+    }
+    const rows = list.map(generateRow);
+    return (
+      <div className={classes}>
+        <div className="user-profile__header twelve-col no-margin-bottom">
+          {title}
+          <span className="user-profile__size">
+            ({list.length})
+          </span>
+        </div>
+        <ul className="user-profile__list twelve-col">
+          {header}
+          {rows}
+        </ul>
+      </div>
+    );
+  }
 
-  });
+});
 
+YUI.add('user-profile-entity-list', function() {
+  juju.components.UserProfileEntityList = UserProfileEntityList;
 }, '', {
   requires: [
     'loading-spinner',
