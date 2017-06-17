ed07a79e5ca34c9fd328bc887db97a7e6b747648
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 10:48:28 +1000

    Update AddressForm component.

--- jujugui/static/gui/src/app/components/address-form/address-form.js
+++ jujugui/static/gui/src/app/components/address-form/address-form.js
@@ -18,262 +18,262 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('address-form', function() {
+const AddressForm = React.createClass({
+  displayName: 'AddressForm',
 
-  juju.components.AddressForm = React.createClass({
-    displayName: 'AddressForm',
+  propTypes: {
+    addNotification: React.PropTypes.func,
+    address: React.PropTypes.object,
+    disabled: React.PropTypes.bool,
+    getCountries: React.PropTypes.func,
+    showName: React.PropTypes.bool,
+    showPhone: React.PropTypes.bool,
+    validateForm: React.PropTypes.func
+  },
 
-    propTypes: {
-      addNotification: React.PropTypes.func,
-      address: React.PropTypes.object,
-      disabled: React.PropTypes.bool,
-      getCountries: React.PropTypes.func,
-      showName: React.PropTypes.bool,
-      showPhone: React.PropTypes.bool,
-      validateForm: React.PropTypes.func
-    },
+  getDefaultProps: function() {
+    return {
+      address: {},
+      showName: true,
+      showPhone: true
+    };
+  },
 
-    getDefaultProps: function() {
-      return {
-        address: {},
-        showName: true,
-        showPhone: true
-      };
-    },
+  getInitialState: function() {
+    this.xhrs = [];
+    return {
+      countries: [],
+      loading: false
+    };
+  },
 
-    getInitialState: function() {
-      this.xhrs = [];
-      return {
-        countries: [],
-        loading: false
-      };
-    },
+  componentWillMount: function() {
+    this._getCountries();
+  },
 
-    componentWillMount: function() {
-      this._getCountries();
-    },
+  componentWillUnmount: function() {
+    this.xhrs.forEach((xhr) => {
+      xhr && xhr.abort && xhr.abort();
+    });
+  },
 
-    componentWillUnmount: function() {
-      this.xhrs.forEach((xhr) => {
-        xhr && xhr.abort && xhr.abort();
-      });
-    },
+  /**
+    Get a list of countries.
 
-    /**
-      Get a list of countries.
-
-      @method _getCountries
-    */
-    _getCountries: function() {
-      this.setState({loading: true}, () => {
-        const xhr = this.props.getCountries((error, countries) => {
-          if (error) {
-            const message = 'Could not load country info';
-            this.props.addNotification({
-              title: message,
-              message: `${message}: ${error}`,
-              level: 'error'
-            });
-            console.error(message, error);
-            return;
-          }
-          this.setState({
-            countries: countries || [],
-            loading: false
+    @method _getCountries
+  */
+  _getCountries: function() {
+    this.setState({loading: true}, () => {
+      const xhr = this.props.getCountries((error, countries) => {
+        if (error) {
+          const message = 'Could not load country info';
+          this.props.addNotification({
+            title: message,
+            message: `${message}: ${error}`,
+            level: 'error'
           });
+          console.error(message, error);
+          return;
+        }
+        this.setState({
+          countries: countries || [],
+          loading: false
         });
-        this.xhrs.push(xhr);
       });
-    },
+      this.xhrs.push(xhr);
+    });
+  },
 
-    /**
-      Validate the form.
+  /**
+    Validate the form.
 
-      @method validate
-      @returns {Boolean} Whether the form is valid.
-    */
-    validate: function() {
-      let fields = [
-        'line1',
-        'line2',
-        'city',
-        'state',
-        'postcode',
-        'country'
-      ];
-      if (this.props.showName) {
-        fields.push('name');
-      }
-      if (this.props.showPhone) {
-        fields.push('phoneNumber');
-      }
-      return this.props.validateForm(fields, this.refs);
-    },
+    @method validate
+    @returns {Boolean} Whether the form is valid.
+  */
+  validate: function() {
+    let fields = [
+      'line1',
+      'line2',
+      'city',
+      'state',
+      'postcode',
+      'country'
+    ];
+    if (this.props.showName) {
+      fields.push('name');
+    }
+    if (this.props.showPhone) {
+      fields.push('phoneNumber');
+    }
+    return this.props.validateForm(fields, this.refs);
+  },
 
-    /**
-      Get address data.
+  /**
+    Get address data.
 
-      @method getValue
-    */
-    getValue: function() {
-      const refs = this.refs;
+    @method getValue
+  */
+  getValue: function() {
+    const refs = this.refs;
+    return {
+      name: this.props.showName ? refs.name.getValue() : null,
+      line1: refs.line1.getValue(),
+      line2: refs.line2.getValue(),
+      city: refs.city.getValue(),
+      county: refs.state.getValue(),
+      postcode: refs.postcode.getValue(),
+      country: refs.country.getValue(),
+      phones: this.props.showPhone ? [refs.phoneNumber.getValue()] : null
+    };
+  },
+
+  /**
+    Generate the country values for a select box.
+
+    @method _generateCountryOptions
+    @returns {Array} The list of country options.
+  */
+  _generateCountryOptions: function() {
+    return this.state.countries.map(country => {
       return {
-        name: this.props.showName ? refs.name.getValue() : null,
-        line1: refs.line1.getValue(),
-        line2: refs.line2.getValue(),
-        city: refs.city.getValue(),
-        county: refs.state.getValue(),
-        postcode: refs.postcode.getValue(),
-        country: refs.country.getValue(),
-        phones: this.props.showPhone ? [refs.phoneNumber.getValue()] : null
+        label: country.name,
+        value: country.code
       };
-    },
+    });
+  },
 
-    /**
-      Generate the country values for a select box.
+  /**
+    Generate the name field.
 
-      @method _generateCountryOptions
-      @returns {Array} The list of country options.
-    */
-    _generateCountryOptions: function() {
-      return this.state.countries.map(country => {
-        return {
-          label: country.name,
-          value: country.code
-        };
-      });
-    },
+    @returns {Object} The name field markup.
+  */
+  _generateNameField: function() {
+    if (!this.props.showName) {
+      return null;
+    }
+    const address = this.props.address;
+    return (
+      <juju.components.GenericInput
+        disabled={this.props.disabled}
+        label="Full name"
+        ref="name"
+        required={true}
+        validate={[{
+          regex: /\S+/,
+          error: 'This field is required.'
+        }]}
+        value={address.name} />);
+  },
 
-    /**
-      Generate the name field.
+  /**
+    Generate the phone field.
 
-      @returns {Object} The name field markup.
-    */
-    _generateNameField: function() {
-      if (!this.props.showName) {
-        return null;
-      }
-      const address = this.props.address;
-      return (
+    @returns {Object} The phone field markup.
+  */
+  _generatePhoneField: function() {
+    if (!this.props.showPhone) {
+      return null;
+    }
+    const address = this.props.address;
+    return (
+      <div className="twelve-col">
         <juju.components.GenericInput
           disabled={this.props.disabled}
-          label="Full name"
-          ref="name"
+          label="Phone number"
+          ref="phoneNumber"
           required={true}
           validate={[{
             regex: /\S+/,
             error: 'This field is required.'
           }]}
-          value={address.name} />);
-    },
+          value={(address.phones || []).join(', ')} />
+      </div>);
+  },
 
-    /**
-      Generate the phone field.
-
-      @returns {Object} The phone field markup.
-    */
-    _generatePhoneField: function() {
-      if (!this.props.showPhone) {
-        return null;
-      }
-      const address = this.props.address;
-      return (
-        <div className="twelve-col">
+  render: function() {
+    const address = this.props.address;
+    let content;
+    if (this.state.loading) {
+      content = (
+        <juju.components.Spinner />);
+    } else {
+      const required = {
+        regex: /\S+/,
+        error: 'This field is required.'
+      };
+      const disabled = this.props.disabled;
+      let countryCode = 'GB';
+      // Map the country name to code so that the select box can display the
+      // correct value.
+      this.state.countries.some(country => {
+        if (country.name === address.country) {
+          countryCode = country.code;
+          return true;
+        }
+      });
+      content = (
+        <div>
+          <juju.components.InsetSelect
+            disabled={disabled}
+            label="Country"
+            options={this._generateCountryOptions()}
+            ref="country"
+            value={countryCode} />
+          {this._generateNameField()}
           <juju.components.GenericInput
-            disabled={this.props.disabled}
-            label="Phone number"
-            ref="phoneNumber"
+            disabled={disabled}
+            label="Address line 1"
+            ref="line1"
             required={true}
-            validate={[{
-              regex: /\S+/,
-              error: 'This field is required.'
-            }]}
-            value={(address.phones || []).join(', ')} />
-        </div>);
-    },
-
-    render: function() {
-      const address = this.props.address;
-      let content;
-      if (this.state.loading) {
-        content = (
-          <juju.components.Spinner />);
-      } else {
-        const required = {
-          regex: /\S+/,
-          error: 'This field is required.'
-        };
-        const disabled = this.props.disabled;
-        let countryCode = 'GB';
-        // Map the country name to code so that the select box can display the
-        // correct value.
-        this.state.countries.some(country => {
-          if (country.name === address.country) {
-            countryCode = country.code;
-            return true;
-          }
-        });
-        content = (
-          <div>
-            <juju.components.InsetSelect
-              disabled={disabled}
-              label="Country"
-              options={this._generateCountryOptions()}
-              ref="country"
-              value={countryCode} />
-            {this._generateNameField()}
-            <juju.components.GenericInput
-              disabled={disabled}
-              label="Address line 1"
-              ref="line1"
-              required={true}
-              validate={[required]}
-              value={address.line1}/>
-            <juju.components.GenericInput
-              disabled={disabled}
-              label="Address line 2 (optional)"
-              ref="line2"
-              required={false}
-              value={address.line2} />
-            <juju.components.GenericInput
-              disabled={disabled}
-              label="State/province"
-              ref="state"
-              required={true}
-              validate={[required]}
-              value={address.county} />
-            <div className="twelve-col">
-              <div className="six-col">
-                <juju.components.GenericInput
-                  disabled={disabled}
-                  label="Town/city"
-                  ref="city"
-                  required={true}
-                  validate={[required]}
-                  value={address.city} />
-              </div>
-              <div className="six-col last-col">
-                <juju.components.GenericInput
-                  disabled={disabled}
-                  label="Postcode"
-                  ref="postcode"
-                  required={true}
-                  validate={[required]}
-                  value={address.postcode} />
-              </div>
-              {this._generatePhoneField()}
+            validate={[required]}
+            value={address.line1}/>
+          <juju.components.GenericInput
+            disabled={disabled}
+            label="Address line 2 (optional)"
+            ref="line2"
+            required={false}
+            value={address.line2} />
+          <juju.components.GenericInput
+            disabled={disabled}
+            label="State/province"
+            ref="state"
+            required={true}
+            validate={[required]}
+            value={address.county} />
+          <div className="twelve-col">
+            <div className="six-col">
+              <juju.components.GenericInput
+                disabled={disabled}
+                label="Town/city"
+                ref="city"
+                required={true}
+                validate={[required]}
+                value={address.city} />
             </div>
-          </div>);
-      }
-      return (
-        <div className="address-form">
-          {content}
-        </div>
-      );
+            <div className="six-col last-col">
+              <juju.components.GenericInput
+                disabled={disabled}
+                label="Postcode"
+                ref="postcode"
+                required={true}
+                validate={[required]}
+                value={address.postcode} />
+            </div>
+            {this._generatePhoneField()}
+          </div>
+        </div>);
     }
+    return (
+      <div className="address-form">
+        {content}
+      </div>
+    );
+  }
 
-  });
+});
 
+YUI.add('address-form', function() {
+  juju.components.AddressForm = AddressForm;
 }, '0.1.0', {
   requires: [
     'generic-input',
