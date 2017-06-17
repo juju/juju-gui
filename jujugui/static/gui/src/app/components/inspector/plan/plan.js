689cfeea1404ca3c5244a1d5a19feab8484341f3
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 15:01:09 +1000

    Update InspectorPlan component.

--- jujugui/static/gui/src/app/components/inspector/plan/plan.js
+++ jujugui/static/gui/src/app/components/inspector/plan/plan.js
@@ -18,57 +18,57 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('inspector-plan', function() {
+const InspectorPlan = React.createClass({
 
-  juju.components.InspectorPlan = React.createClass({
+  propTypes: {
+    acl: React.PropTypes.object.isRequired,
+    currentPlan: React.PropTypes.object
+  },
 
-    propTypes: {
-      acl: React.PropTypes.object.isRequired,
-      currentPlan: React.PropTypes.object
-    },
+  /**
+    Generates the elements if the applicaton has a plan selected.
 
-    /**
-      Generates the elements if the applicaton has a plan selected.
-
-      @method _generatePlanDetails
-      @return {Function} The React elements for the UI.
-    */
-    _generatePlanDetails: function() {
-      var currentPlan = this.props.currentPlan;
-      return (
-        <div className="inspector-plan__details">
-          <div className="inspector-plan__title">{currentPlan.url}</div>
-          <div className="inspector-plan__price">{currentPlan.price}</div>
-          <div className="inspector-plan__description">
-            {currentPlan.description}
-          </div>
-        </div>);
-    },
-
-    /**
-      Generates the elements if the application does not have a plan selected.
-
-      @method _generateNoPlans
-      @return {Function} The React elements for the UI.
-    */
-    _generateNoPlans: function() {
-      return (
-        <div className="inspector-plan__no-plan">
-          You have no active plan
-        </div>);
-    },
-
-    render: function() {
-      return (
-        <div className="inspector-plan">
-          {this.props.currentPlan ?
-            this._generatePlanDetails() : this._generateNoPlans()}
+    @method _generatePlanDetails
+    @return {Function} The React elements for the UI.
+  */
+  _generatePlanDetails: function() {
+    var currentPlan = this.props.currentPlan;
+    return (
+      <div className="inspector-plan__details">
+        <div className="inspector-plan__title">{currentPlan.url}</div>
+        <div className="inspector-plan__price">{currentPlan.price}</div>
+        <div className="inspector-plan__description">
+          {currentPlan.description}
         </div>
-      );
-    }
+      </div>);
+  },
 
-  });
+  /**
+    Generates the elements if the application does not have a plan selected.
 
+    @method _generateNoPlans
+    @return {Function} The React elements for the UI.
+  */
+  _generateNoPlans: function() {
+    return (
+      <div className="inspector-plan__no-plan">
+        You have no active plan
+      </div>);
+  },
+
+  render: function() {
+    return (
+      <div className="inspector-plan">
+        {this.props.currentPlan ?
+          this._generatePlanDetails() : this._generateNoPlans()}
+      </div>
+    );
+  }
+
+});
+
+YUI.add('inspector-plan', function() {
+  juju.components.InspectorPlan = InspectorPlan;
 }, '0.1.0', { requires: [
   'button-row'
 ]});
