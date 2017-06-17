d8287c3ca5ffe19d73e786d4f557e3c9b19cc175
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 13:13:06 +1000

    Update InspectorChangeVersionItem component.

--- jujugui/static/gui/src/app/components/inspector/change-version/item/item.js
+++ jujugui/static/gui/src/app/components/inspector/change-version/item/item.js
@@ -18,50 +18,50 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
+const InspectorChangeVersionItem = React.createClass({
+
+  propTypes: {
+    acl: React.PropTypes.object.isRequired,
+    buttonAction: React.PropTypes.func.isRequired,
+    downgrade: React.PropTypes.bool.isRequired,
+    itemAction: React.PropTypes.func.isRequired,
+    url: React.PropTypes.object.isRequired
+  },
+
+  /**
+    Generate the button label for a downgrade or upgrade.
+
+    @method _generateButtonLabel
+    @returns {String} The button label.
+  */
+  _generateButtonLabel: function() {
+    return this.props.downgrade ? 'Downgrade' : 'Upgrade';
+  },
+
+  render: function() {
+    const path = this.props.url.path();
+    const props = this.props;
+    return (
+      <li className="inspector-current-version__item"
+        role="button" tabIndex="0"
+        onClick={props.itemAction}>
+        <span title={path} className="inspector-current-version__title">
+          version {props.url.revision}
+        </span>
+        <juju.components.GenericButton
+          disabled={props.acl.isReadOnly()}
+          key={path}
+          type='inline-neutral'
+          title={this._generateButtonLabel()}
+          action={props.buttonAction} />
+      </li>
+    );
+  }
+
+});
+
 YUI.add('inspector-change-version-item', function() {
-
-  juju.components.InspectorChangeVersionItem = React.createClass({
-
-    propTypes: {
-      acl: React.PropTypes.object.isRequired,
-      buttonAction: React.PropTypes.func.isRequired,
-      downgrade: React.PropTypes.bool.isRequired,
-      itemAction: React.PropTypes.func.isRequired,
-      url: React.PropTypes.object.isRequired
-    },
-
-    /**
-      Generate the button label for a downgrade or upgrade.
-
-      @method _generateButtonLabel
-      @returns {String} The button label.
-    */
-    _generateButtonLabel: function() {
-      return this.props.downgrade ? 'Downgrade' : 'Upgrade';
-    },
-
-    render: function() {
-      const path = this.props.url.path();
-      const props = this.props;
-      return (
-        <li className="inspector-current-version__item"
-          role="button" tabIndex="0"
-          onClick={props.itemAction}>
-          <span title={path} className="inspector-current-version__title">
-            version {props.url.revision}
-          </span>
-          <juju.components.GenericButton
-            disabled={props.acl.isReadOnly()}
-            key={path}
-            type='inline-neutral'
-            title={this._generateButtonLabel()}
-            action={props.buttonAction} />
-        </li>
-      );
-    }
-
-  });
-
+  juju.components.InspectorChangeVersionItem = InspectorChangeVersionItem;
 }, '0.1.0', { requires: [
   'generic-button'
 ]});
