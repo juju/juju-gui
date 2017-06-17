3348ae58d8fb42899ebee14dc9d8a62514040a3e
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-18 07:23:21 +1000

    Update Popup component.

--- jujugui/static/gui/src/app/components/popup/popup.js
+++ jujugui/static/gui/src/app/components/popup/popup.js
@@ -18,112 +18,112 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('popup', function() {
+/**
+  Popup provides a React component for modal confirmation of an
+  action.
+*/
+const Popup = React.createClass({
+  displayName: 'Popup',
+
+  propTypes: {
+    buttons: React.PropTypes.array,
+    children: React.PropTypes.oneOfType([
+      React.PropTypes.object,
+      React.PropTypes.array
+    ]),
+    className: React.PropTypes.string,
+    close: React.PropTypes.func,
+    title: React.PropTypes.string,
+    type: React.PropTypes.string
+  },
+
+  getDefaultProps: function() {
+    return {
+      type: 'narrow',
+      className: ''
+    };
+  },
 
   /**
-    Popup provides a React component for modal confirmation of an
-    action.
+   Generate the buttons component if required.
+
+   @method _generateButtons
   */
-  juju.components.Popup = React.createClass({
-    displayName: 'Popup',
-
-    propTypes: {
-      buttons: React.PropTypes.array,
-      children: React.PropTypes.oneOfType([
-        React.PropTypes.object,
-        React.PropTypes.array
-      ]),
-      className: React.PropTypes.string,
-      close: React.PropTypes.func,
-      title: React.PropTypes.string,
-      type: React.PropTypes.string
-    },
-
-    getDefaultProps: function() {
-      return {
-        type: 'narrow',
-        className: ''
-      };
-    },
-
-    /**
-     Generate the buttons component if required.
-
-     @method _generateButtons
-    */
-    _generateButtons: function() {
-      const buttons = this.props.buttons;
-      if (buttons) {
-        return (
-          <juju.components.ButtonRow
-            buttons={buttons} />);
-      }
-    },
-
-    /**
-     Generate the close component if required.
-
-     @method _generateClose
-    */
-    _generateClose: function() {
-      const close = this.props.close;
-      if (close) {
-        return (
-          <div className="popup__close">
-            <juju.components.GenericButton
-              action={close}
-              type="base"
-              icon="close_16" />
-          </div>);
-      }
-    },
-
-    /**
-     Generate the title if required.
-
-     @method _generateTitle
-    */
-    _generateTitle: function() {
-      const title = this.props.title;
-      if (title) {
-        return (
-          <h3 className="popup__title">
-            {title}
-          </h3>);
-      }
-    },
-
-    /**
-      Generate the classes based on the props.
-
-      @method _generateClasses
-      @returns {String} The collection of class names.
-    */
-    _generateClasses: function() {
-      return classNames(
-        'popup__panel',
-        `popup__panel--${this.props.type}`,
-        this.props.className
-      );
-    },
-
-    render: function() {
+  _generateButtons: function() {
+    const buttons = this.props.buttons;
+    if (buttons) {
       return (
-        <juju.components.Panel
-          instanceName="popup"
-          visible={true}>
-          <div className={this._generateClasses()}>
-            {this._generateClose()}
-            {this._generateTitle()}
-            {this.props.children}
-            {this._generateButtons()}
-          </div>
-        </juju.components.Panel>
-      );
+        <juju.components.ButtonRow
+          buttons={buttons} />);
     }
+  },
 
-  });
+  /**
+   Generate the close component if required.
 
+   @method _generateClose
+  */
+  _generateClose: function() {
+    const close = this.props.close;
+    if (close) {
+      return (
+        <div className="popup__close">
+          <juju.components.GenericButton
+            action={close}
+            type="base"
+            icon="close_16" />
+        </div>);
+    }
+  },
+
+  /**
+   Generate the title if required.
+
+   @method _generateTitle
+  */
+  _generateTitle: function() {
+    const title = this.props.title;
+    if (title) {
+      return (
+        <h3 className="popup__title">
+          {title}
+        </h3>);
+    }
+  },
+
+  /**
+    Generate the classes based on the props.
+
+    @method _generateClasses
+    @returns {String} The collection of class names.
+  */
+  _generateClasses: function() {
+    return classNames(
+      'popup__panel',
+      `popup__panel--${this.props.type}`,
+      this.props.className
+    );
+  },
+
+  render: function() {
+    return (
+      <juju.components.Panel
+        instanceName="popup"
+        visible={true}>
+        <div className={this._generateClasses()}>
+          {this._generateClose()}
+          {this._generateTitle()}
+          {this.props.children}
+          {this._generateButtons()}
+        </div>
+      </juju.components.Panel>
+    );
+  }
+
+});
+
+YUI.add('popup', function() {
+  juju.components.Popup = Popup;
 }, '', {
   requires: [
     'button-row',
