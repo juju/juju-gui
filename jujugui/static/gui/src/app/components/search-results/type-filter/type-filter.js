0b93c8cd4847892c63bf36cd1fa7ea654286191f
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-18 07:26:10 +1000

    Update SearchResultsTypeFilter component.

--- jujugui/static/gui/src/app/components/search-results/type-filter/type-filter.js
+++ jujugui/static/gui/src/app/components/search-results/type-filter/type-filter.js
@@ -18,85 +18,85 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
+const SearchResultsTypeFilter = React.createClass({
+
+  propTypes: {
+    changeState: React.PropTypes.func.isRequired,
+    currentType: React.PropTypes.string
+  },
+
+  /**
+    Generate the base classes from the props.
+
+    @method _generateClasses
+    @param {Boolean} selected Whether the filter is selected.
+    @returns {String} The collection of class names.
+  */
+  _generateClasses: function(selected) {
+    return classNames(
+      {selected: selected}
+    );
+  },
+
+  /**
+    Generate a list of filter items.
+
+    @method _generateFilterItems
+    @returns {Object} The components.
+  */
+  _generateFilterItems: function() {
+    var components = [];
+    var currentType = this.props.currentType;
+    var items = [{
+      label: 'All',
+      selected: !currentType,
+      action: null
+    }, {
+      label: 'Charms',
+      selected: currentType === 'charm',
+      action: 'charm'
+    }, {
+      label: 'Bundles',
+      selected: currentType === 'bundle',
+      action: 'bundle'
+    }];
+    items.forEach(function(item) {
+      components.push(
+        <li className={this._generateClasses(item.selected)}
+          onClick={this._handleFilterClick.bind(this, item.action)}
+          key={item.label}
+          tabIndex="0" role="button">
+          {item.label}
+        </li>);
+    }, this);
+    return components;
+  },
+
+  /**
+    Filter the search results by the provided type.
+
+    @method _handleFilterClick
+    @param {String} type The bound type.
+  */
+  _handleFilterClick: function(type) {
+    this.props.changeState({
+      search: {
+        type: type
+      }
+    });
+  },
+
+  render: function() {
+    return (
+      <nav className="six-col list-block__type">
+        <ul>
+          {this._generateFilterItems()}
+        </ul>
+      </nav>
+    );
+  }
+});
+
 YUI.add('search-results-type-filter', function(Y) {
-
-  juju.components.SearchResultsTypeFilter = React.createClass({
-
-    propTypes: {
-      changeState: React.PropTypes.func.isRequired,
-      currentType: React.PropTypes.string
-    },
-
-    /**
-      Generate the base classes from the props.
-
-      @method _generateClasses
-      @param {Boolean} selected Whether the filter is selected.
-      @returns {String} The collection of class names.
-    */
-    _generateClasses: function(selected) {
-      return classNames(
-        {selected: selected}
-      );
-    },
-
-    /**
-      Generate a list of filter items.
-
-      @method _generateFilterItems
-      @returns {Object} The components.
-    */
-    _generateFilterItems: function() {
-      var components = [];
-      var currentType = this.props.currentType;
-      var items = [{
-        label: 'All',
-        selected: !currentType,
-        action: null
-      }, {
-        label: 'Charms',
-        selected: currentType === 'charm',
-        action: 'charm'
-      }, {
-        label: 'Bundles',
-        selected: currentType === 'bundle',
-        action: 'bundle'
-      }];
-      items.forEach(function(item) {
-        components.push(
-          <li className={this._generateClasses(item.selected)}
-            onClick={this._handleFilterClick.bind(this, item.action)}
-            key={item.label}
-            tabIndex="0" role="button">
-            {item.label}
-          </li>);
-      }, this);
-      return components;
-    },
-
-    /**
-      Filter the search results by the provided type.
-
-      @method _handleFilterClick
-      @param {String} type The bound type.
-    */
-    _handleFilterClick: function(type) {
-      this.props.changeState({
-        search: {
-          type: type
-        }
-      });
-    },
-
-    render: function() {
-      return (
-        <nav className="six-col list-block__type">
-          <ul>
-            {this._generateFilterItems()}
-          </ul>
-        </nav>
-      );
-    }
-  });
-
+  juju.components.SearchResultsTypeFilter = SearchResultsTypeFilter;
 }, '0.1.0', {requires: []});
