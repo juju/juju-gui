ac9812248a90f277e61c7192bc7939e5df70ab20
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-18 07:29:23 +1000

    Update StringConfigInput component.

--- jujugui/static/gui/src/app/components/string-config/input/input.js
+++ jujugui/static/gui/src/app/components/string-config/input/input.js
@@ -18,53 +18,53 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
+const StringConfigInput = React.createClass({
+  displayName: 'StringConfigInput',
+
+  propTypes: {
+    config: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.number
+    ]),
+    disabled: React.PropTypes.bool,
+    setValue: React.PropTypes.func.isRequired
+  },
+
+  getInitialState: function() {
+    return {
+      value: this.props.config
+    };
+  },
+
+  shouldComponentUpdate: function(nextProps, nextState) {
+    return nextState.value !== this.refs.editableInput.innerText;
+  },
+
+  /**
+    When the value is updated in the input element then update the state
+    with its innerText.
+
+    @param {Object} evt The input or blur event objects.
+  */
+  _updateValue: function(evt) {
+    const value = evt.currentTarget.innerText;
+    this.props.setValue(value);
+    this.setState({value:  value});
+  },
+
+  render: function() {
+    return (
+      <div className="string-config-input"
+        contentEditable={!this.props.disabled}
+        dangerouslySetInnerHTML={{__html: this.state.value}}
+        onBlur={this._updateValue}
+        onInput={this._updateValue}
+        ref="editableInput">
+      </div>
+    );
+  }
+});
+
 YUI.add('string-config-input', function() {
-
-  juju.components.StringConfigInput = React.createClass({
-    displayName: 'StringConfigInput',
-
-    propTypes: {
-      config: React.PropTypes.oneOfType([
-        React.PropTypes.string,
-        React.PropTypes.number
-      ]),
-      disabled: React.PropTypes.bool,
-      setValue: React.PropTypes.func.isRequired
-    },
-
-    getInitialState: function() {
-      return {
-        value: this.props.config
-      };
-    },
-
-    shouldComponentUpdate: function(nextProps, nextState) {
-      return nextState.value !== this.refs.editableInput.innerText;
-    },
-
-    /**
-      When the value is updated in the input element then update the state
-      with its innerText.
-
-      @param {Object} evt The input or blur event objects.
-    */
-    _updateValue: function(evt) {
-      const value = evt.currentTarget.innerText;
-      this.props.setValue(value);
-      this.setState({value:  value});
-    },
-
-    render: function() {
-      return (
-        <div className="string-config-input"
-          contentEditable={!this.props.disabled}
-          dangerouslySetInnerHTML={{__html: this.state.value}}
-          onBlur={this._updateValue}
-          onInput={this._updateValue}
-          ref="editableInput">
-        </div>
-      );
-    }
-  });
-
+  juju.components.StringConfigInput = StringConfigInput;
 }, '0.1.0', { requires: [] });
