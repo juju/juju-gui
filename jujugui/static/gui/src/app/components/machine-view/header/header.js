bf24f09ec68efa4c3c28f91f5ae99a77ee5c1a18
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 15:18:23 +1000

    Update MachineViewHeader component.

--- jujugui/static/gui/src/app/components/machine-view/header/header.js
+++ jujugui/static/gui/src/app/components/machine-view/header/header.js
@@ -18,124 +18,123 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('machine-view-header', function() {
+const dropTarget = {
+  /**
+    Called when something is dropped on the header.
+    See: http://gaearon.github.io/react-dnd/docs-drop-target.html
 
-  var dropTarget = {
-    /**
-      Called when something is dropped on the header.
-      See: http://gaearon.github.io/react-dnd/docs-drop-target.html
-
-      @method drop
-      @param {Object} props The component props.
-      @param {Object} monitor A DropTargetMonitor.
-      @param {Object} component The component that is being dropped onto.
-    */
-    drop: function (props, monitor, component) {
-      var item = monitor.getItem();
-      if (props.droppable) {
-        props.dropUnit(item.unit, null, props.type);
-      }
-    },
-
-    /**
-      Called to check if the unit is allowed to be dropped on the header.
-
-      @method canDrop
-      @param {Object} props The component props.
-      @param {Object} monitor A DropTargetMonitor.
-    */
-    canDrop: function (props, monitor) {
-      return !props.acl.isReadOnly() && props.droppable;
+    @method drop
+    @param {Object} props The component props.
+    @param {Object} monitor A DropTargetMonitor.
+    @param {Object} component The component that is being dropped onto.
+  */
+  drop: function (props, monitor, component) {
+    var item = monitor.getItem();
+    if (props.droppable) {
+      props.dropUnit(item.unit, null, props.type);
     }
-  };
+  },
 
   /**
-    Provides props to be injected into the component.
+    Called to check if the unit is allowed to be dropped on the header.
 
-    @method collect
-    @param {Object} connect The connector.
+    @method canDrop
+    @param {Object} props The component props.
     @param {Object} monitor A DropTargetMonitor.
   */
-  function collect(connect, monitor) {
-    return {
-      connectDropTarget: connect.dropTarget(),
-      canDrop: monitor.canDrop(),
-      isOver: monitor.isOver()
-    };
+  canDrop: function (props, monitor) {
+    return !props.acl.isReadOnly() && props.droppable;
   }
+};
 
-  var MachineViewHeader = React.createClass({
-    propTypes: {
-      acl: React.PropTypes.object.isRequired,
-      activeMenuItem: React.PropTypes.string,
-      canDrop: React.PropTypes.bool.isRequired,
-      connectDropTarget: React.PropTypes.func.isRequired,
-      dropUnit: React.PropTypes.func,
-      droppable: React.PropTypes.bool.isRequired,
-      isOver: React.PropTypes.bool.isRequired,
-      menuItems: React.PropTypes.array,
-      title: React.PropTypes.string.isRequired,
-      toggle: React.PropTypes.object,
-      type: React.PropTypes.string
-    },
+/**
+  Provides props to be injected into the component.
 
-    /**
-      Generate a menu for the supplied controls.
+  @method collect
+  @param {Object} connect The connector.
+  @param {Object} monitor A DropTargetMonitor.
+*/
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    canDrop: monitor.canDrop(),
+    isOver: monitor.isOver()
+  };
+}
 
-      @method _generateControl
-      @returns {Object} A more menu component
-    */
-    _generateControl: function() {
-      var menuItems = this.props.menuItems;
-      var toggle = this.props.toggle;
-      if (menuItems) {
-        return (
-          <juju.components.MoreMenu
-            activeItem={this.props.activeMenuItem}
-            items={menuItems} />);
-      } else if (toggle) {
-        var icon = toggle.toggleOn ? 'close_16_white' : 'add-light-16';
-        return (
-          <juju.components.GenericButton
-            action={toggle.action}
-            disabled={toggle.disabled}
-            type='inline-positive'
-            icon={icon} />);
-      }
-    },
+const MachineViewHeader = ReactDnD.DropTarget(
+  'unit', dropTarget, collect)(React.createClass({
+  propTypes: {
+    acl: React.PropTypes.object.isRequired,
+    activeMenuItem: React.PropTypes.string,
+    canDrop: React.PropTypes.bool.isRequired,
+    connectDropTarget: React.PropTypes.func.isRequired,
+    dropUnit: React.PropTypes.func,
+    droppable: React.PropTypes.bool.isRequired,
+    isOver: React.PropTypes.bool.isRequired,
+    menuItems: React.PropTypes.array,
+    title: React.PropTypes.string.isRequired,
+    toggle: React.PropTypes.object,
+    type: React.PropTypes.string
+  },
 
-    /**
-      Generate the classes for the component.
+  /**
+    Generate a menu for the supplied controls.
 
-      @method _generateClasses
-      @returns {String} The collection of class names.
-    */
-    _generateClasses: function() {
-      return classNames(
-        'machine-view__header', {
-          'machine-view__header--droppable':
-            this.props.droppable && this.props.canDrop,
-          'machine-view__header--drop': this.props.isOver
-        });
-    },
+    @method _generateControl
+    @returns {Object} A more menu component
+  */
+  _generateControl: function() {
+    var menuItems = this.props.menuItems;
+    var toggle = this.props.toggle;
+    if (menuItems) {
+      return (
+        <juju.components.MoreMenu
+          activeItem={this.props.activeMenuItem}
+          items={menuItems} />);
+    } else if (toggle) {
+      var icon = toggle.toggleOn ? 'close_16_white' : 'add-light-16';
+      return (
+        <juju.components.GenericButton
+          action={toggle.action}
+          disabled={toggle.disabled}
+          type='inline-positive'
+          icon={icon} />);
+    }
+  },
 
-    render: function() {
-      return this.props.connectDropTarget(
-        <div className={this._generateClasses()}>
-          {this.props.title}
-          {this._generateControl()}
-          <div className="machine-view__header-drop-target">
-            <div className="machine-view__header-drop-message">
-              Create new {this.props.type}
-            </div>
+  /**
+    Generate the classes for the component.
+
+    @method _generateClasses
+    @returns {String} The collection of class names.
+  */
+  _generateClasses: function() {
+    return classNames(
+      'machine-view__header', {
+        'machine-view__header--droppable':
+          this.props.droppable && this.props.canDrop,
+        'machine-view__header--drop': this.props.isOver
+      });
+  },
+
+  render: function() {
+    return this.props.connectDropTarget(
+      <div className={this._generateClasses()}>
+        {this.props.title}
+        {this._generateControl()}
+        <div className="machine-view__header-drop-target">
+          <div className="machine-view__header-drop-message">
+            Create new {this.props.type}
           </div>
         </div>
-      );
-    }
-  });
+      </div>
+    );
+  }
+}));
 
-  juju.components.MachineViewHeader = ReactDnD.DropTarget(
-    'unit', dropTarget, collect)(MachineViewHeader);
+YUI.add('machine-view-header', function() {
+  juju.components.MachineViewHeader = MachineViewHeader;
 
 }, '0.1.0', {
   requires: [
