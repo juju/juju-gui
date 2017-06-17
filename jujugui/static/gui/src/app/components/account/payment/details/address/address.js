ec4d5d1480a50bdac7d8b458c28ba72f1d55dc93
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 10:37:21 +1000

    Update AccountPaymentDetailsAddress component.

--- jujugui/static/gui/src/app/components/account/payment/details/address/address.js
+++ jujugui/static/gui/src/app/components/account/payment/details/address/address.js
@@ -18,109 +18,108 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('account-payment-details-address', function() {
+const AccountPaymentDetailsAddress = React.createClass({
+  displayName: 'AccountPaymentDetailsAddress',
 
-  juju.components.AccountPaymentDetailsAddress = React.createClass({
-    displayName: 'AccountPaymentDetailsAddress',
+  propTypes: {
+    acl: React.PropTypes.object.isRequired,
+    addAddress: React.PropTypes.func.isRequired,
+    addNotification: React.PropTypes.func.isRequired,
+    address: React.PropTypes.object.isRequired,
+    close: React.PropTypes.func.isRequired,
+    getCountries: React.PropTypes.func.isRequired,
+    removeAddress: React.PropTypes.func.isRequired,
+    showEdit: React.PropTypes.bool,
+    updateAddress: React.PropTypes.func.isRequired,
+    updated: React.PropTypes.func.isRequired,
+    username: React.PropTypes.string.isRequired,
+    validateForm: React.PropTypes.func.isRequired
+  },
 
-    propTypes: {
-      acl: React.PropTypes.object.isRequired,
-      addAddress: React.PropTypes.func.isRequired,
-      addNotification: React.PropTypes.func.isRequired,
-      address: React.PropTypes.object.isRequired,
-      close: React.PropTypes.func.isRequired,
-      getCountries: React.PropTypes.func.isRequired,
-      removeAddress: React.PropTypes.func.isRequired,
-      showEdit: React.PropTypes.bool,
-      updateAddress: React.PropTypes.func.isRequired,
-      updated: React.PropTypes.func.isRequired,
-      username: React.PropTypes.string.isRequired,
-      validateForm: React.PropTypes.func.isRequired
-    },
+  getInitialState: function() {
+    this.xhrs = [];
+    return {};
+  },
 
-    getInitialState: function() {
-      this.xhrs = [];
-      return {};
-    },
+  componentWillUnmount: function() {
+    this.xhrs.forEach((xhr) => {
+      xhr && xhr.abort && xhr.abort();
+    });
+  },
 
-    componentWillUnmount: function() {
-      this.xhrs.forEach((xhr) => {
-        xhr && xhr.abort && xhr.abort();
-      });
-    },
+  /**
+    Update an address.
 
-    /**
-      Update an address.
-
-      @method _updateAddress
-    */
-    _updateAddress: function() {
-      const valid = this.props.validateForm(['addressForm'], this.refs);
-      if (!valid) {
-        return;
-      }
-      const address = this.refs.addressForm.getValue();
-      const username = this.props.username;
-      const xhr = this.props.updateAddress(
-        username, this.props.address.id, address, error => {
-          if (error) {
-            const message = 'Could not update address';
-            this.props.addNotification({
-              title: message,
-              message: `${message}: ${error}`,
-              level: 'error'
-            });
-            console.error(message, error);
-            return;
-          }
-          this.props.updated();
-          this.props.close();
-        });
-      this.xhrs.push(xhr);
-    },
-
-    render: function() {
-      const disabled = this.props.acl.isReadOnly();
-      return (
-        <juju.components.ExpandingRow
-          classes={{
-            'account-payment-details-address': true,
-            'twelve-col': true
-          }}
-          clickable={false}
-          expanded={this.props.showEdit}>
-          <juju.components.AddressForm
-            disabled={true}
-            address={this.props.address}
-            getCountries={this.props.getCountries} />
-          <div className="twelve-col account-payment-details-address__edit">
-            <juju.components.AddressForm
-              addNotification={this.props.addNotification}
-              address={this.props.address}
-              disabled={disabled}
-              getCountries={this.props.getCountries}
-              ref="addressForm"
-              validateForm={this.props.validateForm} />
-            <div className={
-              'twelve-col account-payment-details-address__buttons'}>
-              <juju.components.GenericButton
-                action={this.props.close}
-                disabled={disabled}
-                type="inline-neutral"
-                title="Cancel" />
-              <juju.components.GenericButton
-                action={this._updateAddress}
-                disabled={disabled}
-                type="inline-positive"
-                title="Update" />
-            </div>
-          </div>
-        </juju.components.ExpandingRow>
-      );
+    @method _updateAddress
+  */
+  _updateAddress: function() {
+    const valid = this.props.validateForm(['addressForm'], this.refs);
+    if (!valid) {
+      return;
     }
+    const address = this.refs.addressForm.getValue();
+    const username = this.props.username;
+    const xhr = this.props.updateAddress(
+      username, this.props.address.id, address, error => {
+        if (error) {
+          const message = 'Could not update address';
+          this.props.addNotification({
+            title: message,
+            message: `${message}: ${error}`,
+            level: 'error'
+          });
+          console.error(message, error);
+          return;
+        }
+        this.props.updated();
+        this.props.close();
+      });
+    this.xhrs.push(xhr);
+  },
 
-  });
+  render: function() {
+    const disabled = this.props.acl.isReadOnly();
+    return (
+      <juju.components.ExpandingRow
+        classes={{
+          'account-payment-details-address': true,
+          'twelve-col': true
+        }}
+        clickable={false}
+        expanded={this.props.showEdit}>
+        <juju.components.AddressForm
+          disabled={true}
+          address={this.props.address}
+          getCountries={this.props.getCountries} />
+        <div className="twelve-col account-payment-details-address__edit">
+          <juju.components.AddressForm
+            addNotification={this.props.addNotification}
+            address={this.props.address}
+            disabled={disabled}
+            getCountries={this.props.getCountries}
+            ref="addressForm"
+            validateForm={this.props.validateForm} />
+          <div className={
+            'twelve-col account-payment-details-address__buttons'}>
+            <juju.components.GenericButton
+              action={this.props.close}
+              disabled={disabled}
+              type="inline-neutral"
+              title="Cancel" />
+            <juju.components.GenericButton
+              action={this._updateAddress}
+              disabled={disabled}
+              type="inline-positive"
+              title="Update" />
+          </div>
+        </div>
+      </juju.components.ExpandingRow>
+    );
+  }
 
+});
+YUI.add('account-payment-details-address', function() {
+  juju.components.AccountPaymentDetailsAddress = AccountPaymentDetailsAddress;
 }, '', {
   requires: [
     'address-form',
