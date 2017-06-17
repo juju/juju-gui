831ce1773c3009b589b8db3b6eaf51b44bc38053
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 13:18:08 +1000

    Update OverviewAction component.

--- jujugui/static/gui/src/app/components/inspector/overview-action/overview-action.js
+++ jujugui/static/gui/src/app/components/inspector/overview-action/overview-action.js
@@ -18,110 +18,110 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('overview-action', function() {
+const OverviewAction = React.createClass({
 
-  juju.components.OverviewAction = React.createClass({
+  propTypes: {
+    action: React.PropTypes.func.isRequired,
+    icon: React.PropTypes.string,
+    linkAction: React.PropTypes.func,
+    linkTitle: React.PropTypes.string,
+    title: React.PropTypes.string.isRequired,
+    value: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.number
+    ]),
+    valueType: React.PropTypes.string
+  },
 
-    propTypes: {
-      action: React.PropTypes.func.isRequired,
-      icon: React.PropTypes.string,
-      linkAction: React.PropTypes.func,
-      linkTitle: React.PropTypes.string,
-      title: React.PropTypes.string.isRequired,
-      value: React.PropTypes.oneOfType([
-        React.PropTypes.string,
-        React.PropTypes.number
-      ]),
-      valueType: React.PropTypes.string
-    },
+  baseClass: 'overview-action',
 
-    baseClass: 'overview-action',
-
-    /**
-      Returns the supplied classes with the 'hidden' class applied if the
-      value is falsey.
-      @method _valueClasses
-      @returns {String} The collection of class names.
-    */
-    _valueClasses: function() {
-      return classNames(
-        this.baseClass + '__value',
-        this.props.valueType ?
-          this.baseClass + '__value--type-' + this.props.valueType : '',
-        {
-          hidden: !this.props.value
-        }
-      );
-    },
-
-    /**
-      Returns the supplied classes with the 'hidden' class applied if the
-      value is falsey.
-      @method _linkClasses
-      @returns {String} The collection of class names.
-    */
-    _linkClasses: function() {
-      return classNames(
-        this.baseClass + '__link',
-        {
-          hidden: !this.props.linkAction
-        }
-      );
-    },
-
-    /**
-      Call the supplied link action
-
-      @method _handleLinkClick
-      @param {Object} e The click event.
-    */
-    _handleLinkClick: function(e) {
-      e.stopPropagation();
-      this.props.linkAction();
-    },
-
-    /**
-      Call the supplied link action
-
-      @method _handleLinkClick
-      @param {Object} e The click event.
-    */
-    _generateIcon: function(e) {
-      var icon = this.props.icon;
-      if (!icon) {
-        return;
+  /**
+    Returns the supplied classes with the 'hidden' class applied if the
+    value is falsey.
+    @method _valueClasses
+    @returns {String} The collection of class names.
+  */
+  _valueClasses: function() {
+    return classNames(
+      this.baseClass + '__value',
+      this.props.valueType ?
+        this.baseClass + '__value--type-' + this.props.valueType : '',
+      {
+        hidden: !this.props.value
       }
-      return (
-        <span className="overview-action__icon">
-          <juju.components.SvgIcon name={icon}
-            size="16" />
-        </span>
-      );
-    },
+    );
+  },
 
-    render: function() {
-      var titleClass = this.baseClass + '__title';
-      return (
-        <li className={this.baseClass}
-          onClick={this.props.action}
-          title={this.props.title} tabIndex="0" role="button">
-          {this._generateIcon()}
-          <span className={titleClass}>
-            {this.props.title}
-          </span>
-          <span className={this._linkClasses()}
-            onClick={this._handleLinkClick}>
-            {this.props.linkTitle}
-          </span>
-          <span className={this._valueClasses()}>
-            {this.props.value}
-          </span>
-        </li>
-      );
+  /**
+    Returns the supplied classes with the 'hidden' class applied if the
+    value is falsey.
+    @method _linkClasses
+    @returns {String} The collection of class names.
+  */
+  _linkClasses: function() {
+    return classNames(
+      this.baseClass + '__link',
+      {
+        hidden: !this.props.linkAction
+      }
+    );
+  },
+
+  /**
+    Call the supplied link action
+
+    @method _handleLinkClick
+    @param {Object} e The click event.
+  */
+  _handleLinkClick: function(e) {
+    e.stopPropagation();
+    this.props.linkAction();
+  },
+
+  /**
+    Call the supplied link action
+
+    @method _handleLinkClick
+    @param {Object} e The click event.
+  */
+  _generateIcon: function(e) {
+    var icon = this.props.icon;
+    if (!icon) {
+      return;
     }
+    return (
+      <span className="overview-action__icon">
+        <juju.components.SvgIcon name={icon}
+          size="16" />
+      </span>
+    );
+  },
 
-  });
+  render: function() {
+    var titleClass = this.baseClass + '__title';
+    return (
+      <li className={this.baseClass}
+        onClick={this.props.action}
+        title={this.props.title} tabIndex="0" role="button">
+        {this._generateIcon()}
+        <span className={titleClass}>
+          {this.props.title}
+        </span>
+        <span className={this._linkClasses()}
+          onClick={this._handleLinkClick}>
+          {this.props.linkTitle}
+        </span>
+        <span className={this._valueClasses()}>
+          {this.props.value}
+        </span>
+      </li>
+    );
+  }
 
+});
+
+YUI.add('overview-action', function() {
+  juju.components.OverviewAction = OverviewAction;
 }, '0.1.0', { requires: [
   'svg-icon'
 ]});
