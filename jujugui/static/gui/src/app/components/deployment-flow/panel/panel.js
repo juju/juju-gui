cf2cc3749884c23d12e4ef6b63748012fca63835
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 11:56:16 +1000

    Update DeploymentPanel component.

--- jujugui/static/gui/src/app/components/deployment-flow/panel/panel.js
+++ jujugui/static/gui/src/app/components/deployment-flow/panel/panel.js
@@ -18,69 +18,69 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('deployment-panel', function() {
+const DeploymentPanel = React.createClass({
+  displayName: 'DeploymentPanel',
 
-  juju.components.DeploymentPanel = React.createClass({
-    displayName: 'DeploymentPanel',
+  propTypes: {
+    changeState: React.PropTypes.func.isRequired,
+    children: React.PropTypes.oneOfType([
+      React.PropTypes.object,
+      React.PropTypes.array
+    ]),
+    sendAnalytics: React.PropTypes.func,
+    title: React.PropTypes.string
+  },
 
-    propTypes: {
-      changeState: React.PropTypes.func.isRequired,
-      children: React.PropTypes.oneOfType([
-        React.PropTypes.object,
-        React.PropTypes.array
-      ]),
-      sendAnalytics: React.PropTypes.func,
-      title: React.PropTypes.string
-    },
+  /**
+    Handle closing the panel when the close button is clicked.
 
-    /**
-      Handle closing the panel when the close button is clicked.
+    @method _handleClose
+  */
+  _handleClose: function() {
+    this.props.sendAnalytics(
+      'Button click',
+      'Cancel deployment'
+    );
+    this.props.changeState({
+      gui: {deploy: null},
+      profile: null,
+      special: {dd: null}
+    });
+  },
 
-      @method _handleClose
-    */
-    _handleClose: function() {
-      this.props.sendAnalytics(
-        'Button click',
-        'Cancel deployment'
-      );
-      this.props.changeState({
-        gui: {deploy: null},
-        profile: null,
-        special: {dd: null}
-      });
-    },
-
-    render: function() {
-      return (
-        <juju.components.Panel
-          instanceName="deployment-flow-panel"
-          visible={true}>
-          <div className="deployment-panel">
-            <div className="deployment-panel__header">
-              <div className="deployment-panel__close">
-                <juju.components.GenericButton
-                  action={this._handleClose}
-                  type="neutral"
-                  title="Back to canvas" />
-              </div>
-              <div className="deployment-panel__header-name">
-                {this.props.title}
-              </div>
+  render: function() {
+    return (
+      <juju.components.Panel
+        instanceName="deployment-flow-panel"
+        visible={true}>
+        <div className="deployment-panel">
+          <div className="deployment-panel__header">
+            <div className="deployment-panel__close">
+              <juju.components.GenericButton
+                action={this._handleClose}
+                type="neutral"
+                title="Back to canvas" />
             </div>
-            <div className="deployment-panel__content">
-              <div className="twelve-col">
-                <div className="inner-wrapper">
-                  {this.props.children}
-                </div>
+            <div className="deployment-panel__header-name">
+              {this.props.title}
+            </div>
+          </div>
+          <div className="deployment-panel__content">
+            <div className="twelve-col">
+              <div className="inner-wrapper">
+                {this.props.children}
               </div>
             </div>
           </div>
-        </juju.components.Panel>
-      );
-    }
+        </div>
+      </juju.components.Panel>
+    );
+  }
 
-  });
+});
 
+YUI.add('deployment-panel', function() {
+  juju.components.DeploymentPanel = DeploymentPanel;
 }, '0.1.0', {
   requires: [
     'generic-button',
