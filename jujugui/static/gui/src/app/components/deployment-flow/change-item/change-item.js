bcb72de92b6a05a092c9fcbf8cbf5d4d2fb5328e
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 11:49:48 +1000

    Update DeploymentChangeItem component.

--- jujugui/static/gui/src/app/components/deployment-flow/change-item/change-item.js
+++ jujugui/static/gui/src/app/components/deployment-flow/change-item/change-item.js
@@ -18,70 +18,70 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('deployment-change-item', function() {
+const DeploymentChangeItem = React.createClass({
 
-  juju.components.DeploymentChangeItem = React.createClass({
+  propTypes: {
+    change: React.PropTypes.object.isRequired,
+    showTime: React.PropTypes.bool
+  },
 
-    propTypes: {
-      change: React.PropTypes.object.isRequired,
-      showTime: React.PropTypes.bool
-    },
+  getDefaultProps: function() {
+    return {showTime: true};
+  },
 
-    getDefaultProps: function() {
-      return {showTime: true};
-    },
+  /**
+    Generate the icon node.
 
-    /**
-      Generate the icon node.
-
-      @method _generateIcon
-      @param {String} icon The icon to display.
-      @returns {Object} The icon node.
-    */
-    _generateIcon: function(icon) {
-      var node;
-      var className = 'deployment-change-item__icon';
-      if (icon.indexOf('.svg') > -1) {
-        node = <img src={icon} alt="" className={className} />;
-      } else {
-        node = (
-          <juju.components.SvgIcon name={icon}
-            className={className}
-            size="16" />);
-      }
-      return node;
-    },
-
-    /**
-      Generate the time if required.
-
-      @method _generateTime
-      @returns {Object} The time markup.
-    */
-    _generateTime: function() {
-      if (!this.props.showTime) {
-        return;
-      }
-      return (
-        <span className="deployment-change-item__time">
-          {this.props.change.time}
-        </span>);
-    },
-
-    render: function() {
-      var change = this.props.change;
-      return (
-        <li className="deployment-change-item">
-          <span className="deployment-change-item__change">
-            {this._generateIcon(change.icon)}
-            {change.description}
-          </span>
-          {this._generateTime()}
-        </li>
-      );
+    @method _generateIcon
+    @param {String} icon The icon to display.
+    @returns {Object} The icon node.
+  */
+  _generateIcon: function(icon) {
+    var node;
+    var className = 'deployment-change-item__icon';
+    if (icon.indexOf('.svg') > -1) {
+      node = <img src={icon} alt="" className={className} />;
+    } else {
+      node = (
+        <juju.components.SvgIcon name={icon}
+          className={className}
+          size="16" />);
     }
-  });
+    return node;
+  },
 
+  /**
+    Generate the time if required.
+
+    @method _generateTime
+    @returns {Object} The time markup.
+  */
+  _generateTime: function() {
+    if (!this.props.showTime) {
+      return;
+    }
+    return (
+      <span className="deployment-change-item__time">
+        {this.props.change.time}
+      </span>);
+  },
+
+  render: function() {
+    var change = this.props.change;
+    return (
+      <li className="deployment-change-item">
+        <span className="deployment-change-item__change">
+          {this._generateIcon(change.icon)}
+          {change.description}
+        </span>
+        {this._generateTime()}
+      </li>
+    );
+  }
+});
+
+YUI.add('deployment-change-item', function() {
+  juju.components.DeploymentChangeItem = DeploymentChangeItem;
 }, '0.1.0', { requires: [
   'svg-icon'
 ]});
