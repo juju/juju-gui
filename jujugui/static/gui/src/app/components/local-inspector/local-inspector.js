1110c5d98be1bb1f2da253d4e930d4ef57788cba
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 15:12:54 +1000

    Update LocalInspector component.

--- jujugui/static/gui/src/app/components/local-inspector/local-inspector.js
+++ jujugui/static/gui/src/app/components/local-inspector/local-inspector.js
@@ -18,232 +18,232 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('local-inspector', function() {
+const LocalInspector = React.createClass({
 
-  juju.components.LocalInspector = React.createClass({
+  propTypes: {
+    acl: React.PropTypes.object.isRequired,
+    changeState: React.PropTypes.func.isRequired,
+    file: React.PropTypes.object.isRequired,
+    localType: React.PropTypes.string.isRequired,
+    series: React.PropTypes.object.isRequired,
+    services: React.PropTypes.object.isRequired,
+    upgradeServiceUsingLocalCharm: React.PropTypes.func.isRequired,
+    uploadLocalCharm: React.PropTypes.func.isRequired
+  },
 
-    propTypes: {
-      acl: React.PropTypes.object.isRequired,
-      changeState: React.PropTypes.func.isRequired,
-      file: React.PropTypes.object.isRequired,
-      localType: React.PropTypes.string.isRequired,
-      series: React.PropTypes.object.isRequired,
-      services: React.PropTypes.object.isRequired,
-      upgradeServiceUsingLocalCharm: React.PropTypes.func.isRequired,
-      uploadLocalCharm: React.PropTypes.func.isRequired
-    },
+  /**
+    Get the current state of the local inspector.
 
-    /**
-      Get the current state of the local inspector.
+    @method getInitialState
+    @returns {String} The current state.
+  */
+  getInitialState: function() {
+    // Setting a default state object.
+    return this.generateState(this.props);
+  },
 
-      @method getInitialState
-      @returns {String} The current state.
-    */
-    getInitialState: function() {
-      // Setting a default state object.
-      return this.generateState(this.props);
-    },
+  componentWillReceiveProps: function(nextProps) {
+    this.setState(this.generateState(nextProps));
+  },
 
-    componentWillReceiveProps: function(nextProps) {
-      this.setState(this.generateState(nextProps));
-    },
+  /**
+    Change the state to reflect the chosen component.
 
-    /**
-      Change the state to reflect the chosen component.
+    @method _changeActiveComponent
+    @param {String} newComponent The component to switch to.
+  */
+  _changeActiveComponent: function(newComponent) {
+    var nextProps = this.state;
+    nextProps.activeComponent = newComponent;
+    this.setState(this.generateState(nextProps));
+  },
 
-      @method _changeActiveComponent
-      @param {String} newComponent The component to switch to.
-    */
-    _changeActiveComponent: function(newComponent) {
-      var nextProps = this.state;
-      nextProps.activeComponent = newComponent;
-      this.setState(this.generateState(nextProps));
-    },
+  /**
+    Generates the state for the local inspector based on the app state.
 
-    /**
-      Generates the state for the local inspector based on the app state.
+    @method generateState
+    @param {Object} nextProps The props which were sent to the component.
+    @return {Object} A generated state object which can be passed to setState.
+  */
+  generateState: function(nextProps) {
+    return {
+      activeComponent: nextProps.activeComponent || nextProps.localType ||
+        this.props.localType
+    };
+  },
 
-      @method generateState
-      @param {Object} nextProps The props which were sent to the component.
-      @return {Object} A generated state object which can be passed to setState.
-    */
-    generateState: function(nextProps) {
-      return {
-        activeComponent: nextProps.activeComponent || nextProps.localType ||
-          this.props.localType
-      };
-    },
+  /**
+    Generates the active component based on the state.
 
-    /**
-      Generates the active component based on the state.
-
-      @method _generateComponent
-      @param {Object} activeComponent The component to display.
-      @return {Object} A generated component.
-    */
-    _generateComponent: function(activeComponent) {
-      var component;
-      switch (activeComponent) {
-        case 'new':
-          var series = this.props.series;
-          var seriesOptions = Object.keys(series).map((key) => {
-            return (
-              <option value={key} key={key}>
-                {series[key].name}
-              </option>);
-          });
-          component = (
-            <div>
-              <p>Choose a series to deploy this charm</p>
-              <select ref="series" defaultValue="trusty"
-                className="local-inspector__series"
-                disabled={this.props.acl.isReadOnly()}>
-                {seriesOptions}
-              </select>
-            </div>
-          );
-          break;
-        case 'update':
-          component = (
-            <ul className="local-inspector__list">
-              {this._generateServiceList()}
-            </ul>
-          );
-          break;
-      }
-      return component;
-    },
-
-    /**
-      Generate a list of services
-
-      @method _generateServiceList
-    */
-    _generateServiceList: function() {
-      var props = this.props;
-      var services = props.services.toArray();
-      if (services.length === 0) {
-        return <li>No existing services</li>;
-      }
-      var items = [];
-      services.forEach(function(service) {
-        var serviceId = service.get('id');
-        items.push(
-          <li key={serviceId}>
-            <label>
-              <input type="checkbox" data-id={serviceId}
-                disabled={props.acl.isReadOnly()}
-                ref={'service-' + serviceId} />
-              {service.get('name')}
-            </label>
-          </li>
-        );
-      });
-      return items;
-    },
-
-    /**
-      Handle closing the local inspector.
-
-      @method _close
-    */
-    _close: function() {
-      this.props.changeState({
-        gui: {
-          inspector: null
-        }});
-    },
-
-    /**
-      Handle uploading the charm.
-
-      @method _handleUpload
-    */
-    _handleUpload: function() {
-      this.props.uploadLocalCharm(this.refs.series.value, this.props.file);
-    },
-
-    /**
-      Handle updating services.
-
-      @method _handleUpdate
-    */
-    _handleUpdate: function() {
-      var refs = this.refs;
-      var services = this.props.services;
-      var selectedServices = Object.keys(refs).filter((ref) => {
-        var input = refs[ref];
-        if (ref.split('-')[0] === 'service' && input.checked) {
-          return true;
-        }
-        return false;
-      });
-      if (selectedServices.length > 0) {
-        var serviceList = selectedServices.map((serviceId) => {
-          return services.getById(serviceId.split('-')[1]);
+    @method _generateComponent
+    @param {Object} activeComponent The component to display.
+    @return {Object} A generated component.
+  */
+  _generateComponent: function(activeComponent) {
+    var component;
+    switch (activeComponent) {
+      case 'new':
+        var series = this.props.series;
+        var seriesOptions = Object.keys(series).map((key) => {
+          return (
+            <option value={key} key={key}>
+              {series[key].name}
+            </option>);
         });
-        this.props.upgradeServiceUsingLocalCharm(serviceList, this.props.file);
-        this._close();
-      }
-    },
-
-    render: function() {
-      var isReadOnly = this.props.acl.isReadOnly();
-      var localType = this.props.localType;
-      var file = this.props.file;
-      var size = (file.size / 1024).toFixed(2);
-      var buttons = [{
-        title: 'Cancel',
-        action: this._close,
-        type: 'base'
-      }, {
-        title: 'Upload',
-        action: this.state.activeComponent === 'new' ?
-          this._handleUpload : this._handleUpdate,
-        disabled: isReadOnly,
-        type: 'neutral'
-      }];
-      return (
-        <div className="inspector-view local-inspector">
-          <juju.components.InspectorHeader
-            backCallback={this._close}
-            title="Local charm" />
-          <div className="inspector-content local-inspector__section">
-            <div className="local-inspector__file">
-              <p>File: {file.name}</p>
-              <p>Size: {size}kb</p>
-            </div>
-            <ul className="local-inspector__list">
-              <li>
-                <label>
-                  <input type="radio" name="action"
-                    defaultChecked={localType === 'new'}
-                    disabled={isReadOnly}
-                    onChange={this._changeActiveComponent.bind(this, 'new')} />
-                  Deploy local
-                </label>
-              </li>
-              <li>
-                <label>
-                  <input type="radio" name="action"
-                    defaultChecked={localType === 'update'}
-                    disabled={isReadOnly}
-                    onChange={
-                      this._changeActiveComponent.bind(this, 'update')} />
-                  Upgrade local
-                </label>
-              </li>
-            </ul>
-            {this._generateComponent(this.state.activeComponent)}
+        component = (
+          <div>
+            <p>Choose a series to deploy this charm</p>
+            <select ref="series" defaultValue="trusty"
+              className="local-inspector__series"
+              disabled={this.props.acl.isReadOnly()}>
+              {seriesOptions}
+            </select>
           </div>
-          <juju.components.ButtonRow
-            buttons={buttons} />
-        </div>
-      );
+        );
+        break;
+      case 'update':
+        component = (
+          <ul className="local-inspector__list">
+            {this._generateServiceList()}
+          </ul>
+        );
+        break;
     }
+    return component;
+  },
 
-  });
+  /**
+    Generate a list of services
 
+    @method _generateServiceList
+  */
+  _generateServiceList: function() {
+    var props = this.props;
+    var services = props.services.toArray();
+    if (services.length === 0) {
+      return <li>No existing services</li>;
+    }
+    var items = [];
+    services.forEach(function(service) {
+      var serviceId = service.get('id');
+      items.push(
+        <li key={serviceId}>
+          <label>
+            <input type="checkbox" data-id={serviceId}
+              disabled={props.acl.isReadOnly()}
+              ref={'service-' + serviceId} />
+            {service.get('name')}
+          </label>
+        </li>
+      );
+    });
+    return items;
+  },
+
+  /**
+    Handle closing the local inspector.
+
+    @method _close
+  */
+  _close: function() {
+    this.props.changeState({
+      gui: {
+        inspector: null
+      }});
+  },
+
+  /**
+    Handle uploading the charm.
+
+    @method _handleUpload
+  */
+  _handleUpload: function() {
+    this.props.uploadLocalCharm(this.refs.series.value, this.props.file);
+  },
+
+  /**
+    Handle updating services.
+
+    @method _handleUpdate
+  */
+  _handleUpdate: function() {
+    var refs = this.refs;
+    var services = this.props.services;
+    var selectedServices = Object.keys(refs).filter((ref) => {
+      var input = refs[ref];
+      if (ref.split('-')[0] === 'service' && input.checked) {
+        return true;
+      }
+      return false;
+    });
+    if (selectedServices.length > 0) {
+      var serviceList = selectedServices.map((serviceId) => {
+        return services.getById(serviceId.split('-')[1]);
+      });
+      this.props.upgradeServiceUsingLocalCharm(serviceList, this.props.file);
+      this._close();
+    }
+  },
+
+  render: function() {
+    var isReadOnly = this.props.acl.isReadOnly();
+    var localType = this.props.localType;
+    var file = this.props.file;
+    var size = (file.size / 1024).toFixed(2);
+    var buttons = [{
+      title: 'Cancel',
+      action: this._close,
+      type: 'base'
+    }, {
+      title: 'Upload',
+      action: this.state.activeComponent === 'new' ?
+        this._handleUpload : this._handleUpdate,
+      disabled: isReadOnly,
+      type: 'neutral'
+    }];
+    return (
+      <div className="inspector-view local-inspector">
+        <juju.components.InspectorHeader
+          backCallback={this._close}
+          title="Local charm" />
+        <div className="inspector-content local-inspector__section">
+          <div className="local-inspector__file">
+            <p>File: {file.name}</p>
+            <p>Size: {size}kb</p>
+          </div>
+          <ul className="local-inspector__list">
+            <li>
+              <label>
+                <input type="radio" name="action"
+                  defaultChecked={localType === 'new'}
+                  disabled={isReadOnly}
+                  onChange={this._changeActiveComponent.bind(this, 'new')} />
+                Deploy local
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type="radio" name="action"
+                  defaultChecked={localType === 'update'}
+                  disabled={isReadOnly}
+                  onChange={
+                    this._changeActiveComponent.bind(this, 'update')} />
+                Upgrade local
+              </label>
+            </li>
+          </ul>
+          {this._generateComponent(this.state.activeComponent)}
+        </div>
+        <juju.components.ButtonRow
+          buttons={buttons} />
+      </div>
+    );
+  }
+
+});
+
+YUI.add('local-inspector', function() {
+  juju.components.LocalInspector = LocalInspector;
 }, '0.1.0', {requires: [
   'button-row',
   'inspector-header'
