d57784753c47e6e451504435562a5fcd08b5a60f
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 15:20:08 +1000

    Update MachineViewMachineUnit component.

--- jujugui/static/gui/src/app/components/machine-view/machine-unit/machine-unit.js
+++ jujugui/static/gui/src/app/components/machine-view/machine-unit/machine-unit.js
@@ -18,114 +18,113 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('machine-view-machine-unit', function() {
-  var dragSource = {
-    /**
-      Called when the component starts the drag.
-      See: http://gaearon.github.io/react-dnd/docs-drag-source.html
+const dragSource = {
+  /**
+    Called when the component starts the drag.
+    See: http://gaearon.github.io/react-dnd/docs-drag-source.html
 
-      @method beginDrag
-      @param {Object} props The component props.
-    */
-    beginDrag: function(props) {
-      return {unit: props.unit};
-    },
-
-    /**
-      Called to check if the component is allowed to be dragged.
-      See: http://gaearon.github.io/react-dnd/docs-drag-source.html
-
-      @method canDrag
-      @param {Object} props The component props.
-    */
-    canDrag: function(props) {
-      return !props.acl.isReadOnly() && !props.unit.agent_state;
-    }
-  };
+    @method beginDrag
+    @param {Object} props The component props.
+  */
+  beginDrag: function(props) {
+    return {unit: props.unit};
+  },
 
   /**
-    Provides props to be injected into the component.
+    Called to check if the component is allowed to be dragged.
+    See: http://gaearon.github.io/react-dnd/docs-drag-source.html
 
-    @method collect
-    @param {Object} connect The connector.
-    @param {Object} monitor A DropTargetMonitor.
+    @method canDrag
+    @param {Object} props The component props.
   */
-  function collect(connect, monitor) {
-    return {
-      canDrag: monitor.canDrag(),
-      connectDragSource: connect.dragSource(),
-      isDragging: monitor.isDragging()
-    };
+  canDrag: function(props) {
+    return !props.acl.isReadOnly() && !props.unit.agent_state;
   }
+};
 
-  var MachineViewMachineUnit = React.createClass({
-    propTypes: {
-      acl: React.PropTypes.object.isRequired,
-      canDrag: React.PropTypes.bool.isRequired,
-      connectDragSource: React.PropTypes.func.isRequired,
-      isDragging: React.PropTypes.bool.isRequired,
-      machineType: React.PropTypes.string.isRequired,
-      removeUnit: React.PropTypes.func,
-      service: React.PropTypes.object.isRequired,
-      unit: React.PropTypes.object.isRequired
-    },
+/**
+  Provides props to be injected into the component.
 
-    /**
-      Generate the classes for the unit.
+  @method collect
+  @param {Object} connect The connector.
+  @param {Object} monitor A DropTargetMonitor.
+*/
+function collect(connect, monitor) {
+  return {
+    canDrag: monitor.canDrag(),
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  };
+}
 
-      @method _generateClasses
-      @returns {String} The collection of class names.
-    */
-    _generateClasses: function() {
-      var unit = this.props.unit;
-      var agentState = unit.agent_state;
-      var status = unit.deleted || !agentState ? 'uncommitted' : agentState;
-      var classes = {
-        'machine-view__machine-unit--draggable': this.props.canDrag,
-        'machine-view__machine-unit--dragged': this.props.isDragging
-      };
-      classes['machine-view__machine-unit--' + status] = true;
-      return classNames(
-        'machine-view__machine-unit',
-        classes);
-    },
+const MachineViewMachineUnit = ReactDnD.DragSource(
+  'unit', dragSource, collect)(React.createClass({
+  propTypes: {
+    acl: React.PropTypes.object.isRequired,
+    canDrag: React.PropTypes.bool.isRequired,
+    connectDragSource: React.PropTypes.func.isRequired,
+    isDragging: React.PropTypes.bool.isRequired,
+    machineType: React.PropTypes.string.isRequired,
+    removeUnit: React.PropTypes.func,
+    service: React.PropTypes.object.isRequired,
+    unit: React.PropTypes.object.isRequired
+  },
 
-    render: function() {
-      var menu;
-      var title;
-      var service = this.props.service;
-      var unit = this.props.unit;
-      if (this.props.machineType === 'container') {
-        var menuItems = [{
-          label: 'Destroy',
-          action: !this.props.acl.isReadOnly() &&
-            this.props.removeUnit.bind(null, unit.id)
-        }];
-        menu = (
-          <juju.components.MoreMenu
-            items={menuItems} />);
-        title = unit.displayName;
-      }
-      // Wrap the returned components in the drag source method.
-      return this.props.connectDragSource(
-        <li className={this._generateClasses()}>
-          <span className="machine-view__machine-unit-icon">
-            <img
-              alt={unit.displayName}
-              className="machine-view__machine-unit-icon-img"
-              src={service.get('icon')}
-              title={unit.displayName} />
-          </span>
-          {title}
-          {menu}
-        </li>
-      );
+  /**
+    Generate the classes for the unit.
+
+    @method _generateClasses
+    @returns {String} The collection of class names.
+  */
+  _generateClasses: function() {
+    var unit = this.props.unit;
+    var agentState = unit.agent_state;
+    var status = unit.deleted || !agentState ? 'uncommitted' : agentState;
+    var classes = {
+      'machine-view__machine-unit--draggable': this.props.canDrag,
+      'machine-view__machine-unit--dragged': this.props.isDragging
+    };
+    classes['machine-view__machine-unit--' + status] = true;
+    return classNames(
+      'machine-view__machine-unit',
+      classes);
+  },
+
+  render: function() {
+    var menu;
+    var title;
+    var service = this.props.service;
+    var unit = this.props.unit;
+    if (this.props.machineType === 'container') {
+      var menuItems = [{
+        label: 'Destroy',
+        action: !this.props.acl.isReadOnly() &&
+          this.props.removeUnit.bind(null, unit.id)
+      }];
+      menu = (
+        <juju.components.MoreMenu
+          items={menuItems} />);
+      title = unit.displayName;
     }
-  });
+    // Wrap the returned components in the drag source method.
+    return this.props.connectDragSource(
+      <li className={this._generateClasses()}>
+        <span className="machine-view__machine-unit-icon">
+          <img
+            alt={unit.displayName}
+            className="machine-view__machine-unit-icon-img"
+            src={service.get('icon')}
+            title={unit.displayName} />
+        </span>
+        {title}
+        {menu}
+      </li>
+    );
+  }
+}));
 
-  juju.components.MachineViewMachineUnit = ReactDnD.DragSource(
-    'unit', dragSource, collect)(MachineViewMachineUnit);
-
+YUI.add('machine-view-machine-unit', function() {
+  juju.components.MachineViewMachineUnit = MachineViewMachineUnit;
 }, '0.1.0', {
   requires: [
     'machine-view-add-machine',
