d5e1aa28b44c03b44ac473fd997a415deaf597ee
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 10:38:20 +1000

    Update AccountPaymentDetails component.

--- jujugui/static/gui/src/app/components/account/payment/details/details.js
+++ jujugui/static/gui/src/app/components/account/payment/details/details.js
@@ -18,159 +18,159 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('account-payment-details', function() {
+const AccountPaymentDetails = React.createClass({
+  displayName: 'AccountPaymentDetails',
 
-  juju.components.AccountPaymentDetails = React.createClass({
-    displayName: 'AccountPaymentDetails',
+  propTypes: {
+    acl: React.PropTypes.object.isRequired,
+    addAddress: React.PropTypes.func.isRequired,
+    addBillingAddress: React.PropTypes.func.isRequired,
+    addNotification: React.PropTypes.func.isRequired,
+    getCountries: React.PropTypes.func.isRequired,
+    paymentUser: React.PropTypes.object.isRequired,
+    removeAddress: React.PropTypes.func.isRequired,
+    removeBillingAddress: React.PropTypes.func.isRequired,
+    updateAddress: React.PropTypes.func.isRequired,
+    updateBillingAddress: React.PropTypes.func.isRequired,
+    updateUser: React.PropTypes.func.isRequired,
+    username: React.PropTypes.string.isRequired,
+    validateForm: React.PropTypes.func.isRequired
+  },
 
-    propTypes: {
-      acl: React.PropTypes.object.isRequired,
-      addAddress: React.PropTypes.func.isRequired,
-      addBillingAddress: React.PropTypes.func.isRequired,
-      addNotification: React.PropTypes.func.isRequired,
-      getCountries: React.PropTypes.func.isRequired,
-      paymentUser: React.PropTypes.object.isRequired,
-      removeAddress: React.PropTypes.func.isRequired,
-      removeBillingAddress: React.PropTypes.func.isRequired,
-      updateAddress: React.PropTypes.func.isRequired,
-      updateBillingAddress: React.PropTypes.func.isRequired,
-      updateUser: React.PropTypes.func.isRequired,
-      username: React.PropTypes.string.isRequired,
-      validateForm: React.PropTypes.func.isRequired
-    },
+  getInitialState: function() {
+    return {
+      addressEdit: false,
+      billingAddressEdit: false
+    };
+  },
 
-    getInitialState: function() {
-      return {
-        addressEdit: false,
-        billingAddressEdit: false
-      };
-    },
+  /**
+    Show the edit address form.
 
-    /**
-      Show the edit address form.
+    @method _toggleAddressEdit
+  */
+  _toggleAddressEdit: function() {
+    this.setState({addressEdit: !this.state.addressEdit});
+  },
 
-      @method _toggleAddressEdit
-    */
-    _toggleAddressEdit: function() {
-      this.setState({addressEdit: !this.state.addressEdit});
-    },
+  /**
+    Show the edit address form.
 
-    /**
-      Show the edit address form.
+    @method _toggleBillingAddressEdit
+  */
+  _toggleBillingAddressEdit: function() {
+    this.setState({billingAddressEdit: !this.state.billingAddressEdit});
+  },
 
-      @method _toggleBillingAddressEdit
-    */
-    _toggleBillingAddressEdit: function() {
-      this.setState({billingAddressEdit: !this.state.billingAddressEdit});
-    },
+  /**
+    Generate the user's details.
 
-    /**
-      Generate the user's details.
-
-      @method _generateDetails
-    */
-    _generateDetails: function() {
-      const user = this.props.paymentUser;
-      const business = user.business;
-      const disabled = this.props.acl.isReadOnly();
-      return (
-        <div className="account__payment-details-view twelve-col">
-          <div className="account__payment-details-fields">
+    @method _generateDetails
+  */
+  _generateDetails: function() {
+    const user = this.props.paymentUser;
+    const business = user.business;
+    const disabled = this.props.acl.isReadOnly();
+    return (
+      <div className="account__payment-details-view twelve-col">
+        <div className="account__payment-details-fields">
+          <juju.components.GenericInput
+            disabled={true}
+            label="Name"
+            value={user.name} />
+          <juju.components.GenericInput
+            disabled={true}
+            label="Email address"
+            value={user.email} />
+          {business ? (
             <juju.components.GenericInput
               disabled={true}
-              label="Name"
-              value={user.name} />
+              label="VAT number (optional)"
+              value={user.vat} />) : null}
+          {business ? (
             <juju.components.GenericInput
               disabled={true}
-              label="Email address"
-              value={user.email} />
-            {business ? (
-              <juju.components.GenericInput
-                disabled={true}
-                label="VAT number (optional)"
-                value={user.vat} />) : null}
-            {business ? (
-              <juju.components.GenericInput
-                disabled={true}
-                label="Business name"
-                value={user.businessName} />) : null}
-          </div>
-          <h4>
-            Addresses
-            {this.state.addressEdit ? null : (
-              <juju.components.GenericButton
-                action={this._toggleAddressEdit}
-                disabled={disabled}
-                type="inline-neutral"
-                title="Edit" />)}
-          </h4>
-          {this._generateAddresses(user.addresses)}
-          <h4>
-            Billing addresses
-            {this.state.billingAddressEdit ? null : (
-              <juju.components.GenericButton
-                action={this._toggleBillingAddressEdit}
-                disabled={disabled}
-                type="inline-neutral"
-                title="Edit" />)}
-          </h4>
-          {this._generateAddresses(user.billingAddresses, true)}
-        </div>);
-    },
-
-    /**
-      Generate the address fields.
-
-      @method _generateAddresses
-      @param {Array} adresses A list of address.
-      @param {Boolean} billing Whether this is a billing address.
-      @returns {Object} The markup for the addresses.
-    */
-    _generateAddresses: function(addresses, billing=false) {
-      let list = addresses.map(address => {
-        return (
-          <juju.components.AccountPaymentDetailsAddress
-            acl={this.props.acl}
-            addNotification={this.props.addNotification}
-            addAddress={
-              billing ? this.props.addBillingAddress : this.props.addAddress}
-            address={address}
-            close={
-              billing ? this._toggleBillingAddressEdit :
-                this._toggleAddressEdit}
-            getCountries={this.props.getCountries}
-            key={address.name}
-            removeAddress={
-              billing ? this.props.removeBillingAddress :
-                this.props.removeAddress}
-            showEdit={
-              billing ? this.state.billingAddressEdit : this.state.addressEdit}
-            updateAddress={
-              billing ? this.props.updateBillingAddress :
-                this.props.updateAddress}
-            updated={this.props.updateUser}
-            username={this.props.username}
-            validateForm={this.props.validateForm} />);
-      });
-      return (
-        <ul className="account__payment-details-addresses">
-          {list}
-        </ul>);
-    },
-
-    render: function() {
-      return (
-        <div className="account__section">
-          <h2 className="account__title twelve-col">
-            Account details
-          </h2>
-          {this._generateDetails()}
+              label="Business name"
+              value={user.businessName} />) : null}
         </div>
-      );
-    }
+        <h4>
+          Addresses
+          {this.state.addressEdit ? null : (
+            <juju.components.GenericButton
+              action={this._toggleAddressEdit}
+              disabled={disabled}
+              type="inline-neutral"
+              title="Edit" />)}
+        </h4>
+        {this._generateAddresses(user.addresses)}
+        <h4>
+          Billing addresses
+          {this.state.billingAddressEdit ? null : (
+            <juju.components.GenericButton
+              action={this._toggleBillingAddressEdit}
+              disabled={disabled}
+              type="inline-neutral"
+              title="Edit" />)}
+        </h4>
+        {this._generateAddresses(user.billingAddresses, true)}
+      </div>);
+  },
 
-  });
+  /**
+    Generate the address fields.
 
+    @method _generateAddresses
+    @param {Array} adresses A list of address.
+    @param {Boolean} billing Whether this is a billing address.
+    @returns {Object} The markup for the addresses.
+  */
+  _generateAddresses: function(addresses, billing=false) {
+    let list = addresses.map(address => {
+      return (
+        <juju.components.AccountPaymentDetailsAddress
+          acl={this.props.acl}
+          addNotification={this.props.addNotification}
+          addAddress={
+            billing ? this.props.addBillingAddress : this.props.addAddress}
+          address={address}
+          close={
+            billing ? this._toggleBillingAddressEdit :
+              this._toggleAddressEdit}
+          getCountries={this.props.getCountries}
+          key={address.name}
+          removeAddress={
+            billing ? this.props.removeBillingAddress :
+              this.props.removeAddress}
+          showEdit={
+            billing ? this.state.billingAddressEdit : this.state.addressEdit}
+          updateAddress={
+            billing ? this.props.updateBillingAddress :
+              this.props.updateAddress}
+          updated={this.props.updateUser}
+          username={this.props.username}
+          validateForm={this.props.validateForm} />);
+    });
+    return (
+      <ul className="account__payment-details-addresses">
+        {list}
+      </ul>);
+  },
+
+  render: function() {
+    return (
+      <div className="account__section">
+        <h2 className="account__title twelve-col">
+          Account details
+        </h2>
+        {this._generateDetails()}
+      </div>
+    );
+  }
+
+});
+
+YUI.add('account-payment-details', function() {
+  juju.components.AccountPaymentDetails = AccountPaymentDetails;
 }, '', {
   requires: [
     'account-payment-details-address',
