4b28cd8efe80593f81578a0adf4d1d177e918aa6
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 10:54:26 +1000

    Update CardForm component.

--- jujugui/static/gui/src/app/components/card-form/card-form.js
+++ jujugui/static/gui/src/app/components/card-form/card-form.js
@@ -18,73 +18,73 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
+const CardForm = React.createClass({
+  displayName: 'CardForm',
+
+  propTypes: {
+    acl: React.PropTypes.object.isRequired,
+    createCardElement: React.PropTypes.func.isRequired,
+    validateForm: React.PropTypes.func.isRequired
+  },
+
+  componentDidMount: function() {
+    this.props.createCardElement(card => {
+      this.card = card;
+      if (this.refs.cardNode) {
+        this.card.mount('.card-form__card');
+      }
+    });
+  },
+
+  /**
+    Validate the form.
+
+    @method validate
+    @returns {Boolean} Whether the form is valid.
+  */
+  validate: function() {
+    const fields = [
+      'name'
+    ];
+    return this.props.validateForm(fields, this.refs);
+  },
+
+  /**
+    Get card data.
+
+    @method getValue
+  */
+  getValue: function() {
+    return {
+      card: this.card,
+      name: this.refs.name.getValue()
+    };
+  },
+
+  render: function() {
+    const disabled = this.props.acl.isReadOnly();
+    const required = {
+      regex: /\S+/,
+      error: 'This field is required.'
+    };
+    return (
+      <div className="card-form">
+        <juju.components.GenericInput
+          disabled={disabled}
+          label="Name on card"
+          ref="name"
+          required={true}
+          validate={[required]} />
+        <div className="card-form__card"
+          ref="cardNode"></div>
+      </div>
+    );
+  }
+
+});
+
 YUI.add('card-form', function() {
-
-  juju.components.CardForm = React.createClass({
-    displayName: 'CardForm',
-
-    propTypes: {
-      acl: React.PropTypes.object.isRequired,
-      createCardElement: React.PropTypes.func.isRequired,
-      validateForm: React.PropTypes.func.isRequired
-    },
-
-    componentDidMount: function() {
-      this.props.createCardElement(card => {
-        this.card = card;
-        if (this.refs.cardNode) {
-          this.card.mount('.card-form__card');
-        }
-      });
-    },
-
-    /**
-      Validate the form.
-
-      @method validate
-      @returns {Boolean} Whether the form is valid.
-    */
-    validate: function() {
-      const fields = [
-        'name'
-      ];
-      return this.props.validateForm(fields, this.refs);
-    },
-
-    /**
-      Get card data.
-
-      @method getValue
-    */
-    getValue: function() {
-      return {
-        card: this.card,
-        name: this.refs.name.getValue()
-      };
-    },
-
-    render: function() {
-      const disabled = this.props.acl.isReadOnly();
-      const required = {
-        regex: /\S+/,
-        error: 'This field is required.'
-      };
-      return (
-        <div className="card-form">
-          <juju.components.GenericInput
-            disabled={disabled}
-            label="Name on card"
-            ref="name"
-            required={true}
-            validate={[required]} />
-          <div className="card-form__card"
-            ref="cardNode"></div>
-        </div>
-      );
-    }
-
-  });
-
+  juju.components.CardForm = CardForm;
 }, '0.1.0', {
   requires: [
     'generic-input'
