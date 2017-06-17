3fd11d90c3c198f2cc4a218ddc83d76027c6ce1e
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-17 10:55:11 +1000

    Update Charmbrowser component.

--- jujugui/static/gui/src/app/components/charmbrowser/charmbrowser.js
+++ jujugui/static/gui/src/app/components/charmbrowser/charmbrowser.js
@@ -18,239 +18,239 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-YUI.add('charmbrowser-component', function() {
+const Charmbrowser = React.createClass({
+  displayName: 'Charmbrowser',
+  propTypes: {
+    acl: React.PropTypes.object.isRequired,
+    addNotification: React.PropTypes.func.isRequired,
+    apiUrl: React.PropTypes.string.isRequired,
+    apiVersion: React.PropTypes.string.isRequired,
+    appState: React.PropTypes.object.isRequired,
+    charmstoreSearch: React.PropTypes.func.isRequired,
+    charmstoreURL: React.PropTypes.string.isRequired,
+    deployService: React.PropTypes.func.isRequired,
+    deployTarget: React.PropTypes.func.isRequired,
+    getBundleYAML: React.PropTypes.func.isRequired,
+    getDiagramURL: React.PropTypes.func.isRequired,
+    getEntity: React.PropTypes.func.isRequired,
+    getFile: React.PropTypes.func.isRequired,
+    getModelName: React.PropTypes.func.isRequired,
+    gisf: React.PropTypes.bool.isRequired,
+    importBundleYAML: React.PropTypes.func.isRequired,
+    listPlansForCharm: React.PropTypes.func.isRequired,
+    makeEntityModel: React.PropTypes.func.isRequired,
+    renderMarkdown: React.PropTypes.func.isRequired,
+    series: React.PropTypes.object.isRequired,
+    setPageTitle: React.PropTypes.func.isRequired,
+    showTerms: React.PropTypes.func.isRequired,
+    staticURL: React.PropTypes.string,
+    urllib: React.PropTypes.func.isRequired,
+    utils: React.PropTypes.object.isRequired
+  },
 
-  juju.components.Charmbrowser = React.createClass({
-    displayName: 'Charmbrowser',
-    propTypes: {
-      acl: React.PropTypes.object.isRequired,
-      addNotification: React.PropTypes.func.isRequired,
-      apiUrl: React.PropTypes.string.isRequired,
-      apiVersion: React.PropTypes.string.isRequired,
-      appState: React.PropTypes.object.isRequired,
-      charmstoreSearch: React.PropTypes.func.isRequired,
-      charmstoreURL: React.PropTypes.string.isRequired,
-      deployService: React.PropTypes.func.isRequired,
-      deployTarget: React.PropTypes.func.isRequired,
-      getBundleYAML: React.PropTypes.func.isRequired,
-      getDiagramURL: React.PropTypes.func.isRequired,
-      getEntity: React.PropTypes.func.isRequired,
-      getFile: React.PropTypes.func.isRequired,
-      getModelName: React.PropTypes.func.isRequired,
-      gisf: React.PropTypes.bool.isRequired,
-      importBundleYAML: React.PropTypes.func.isRequired,
-      listPlansForCharm: React.PropTypes.func.isRequired,
-      makeEntityModel: React.PropTypes.func.isRequired,
-      renderMarkdown: React.PropTypes.func.isRequired,
-      series: React.PropTypes.object.isRequired,
-      setPageTitle: React.PropTypes.func.isRequired,
-      showTerms: React.PropTypes.func.isRequired,
-      staticURL: React.PropTypes.string,
-      urllib: React.PropTypes.func.isRequired,
-      utils: React.PropTypes.object.isRequired
-    },
+  /**
+    Get the current state of the charmbrowser.
 
-    /**
-      Get the current state of the charmbrowser.
+    @method getInitialState
+    @returns {String} The current state.
+  */
+  getInitialState: function() {
+    // Setting a default state object.
+    var state = this.generateState(this.props);
+    state.scrollPosition = 0;
+    return state;
+  },
 
-      @method getInitialState
-      @returns {String} The current state.
-    */
-    getInitialState: function() {
-      // Setting a default state object.
-      var state = this.generateState(this.props);
-      state.scrollPosition = 0;
-      return state;
-    },
+  componentDidMount: function() {
+    this.refs.charmbrowser.addEventListener('scroll', this._onScroll);
+  },
 
-    componentDidMount: function() {
-      this.refs.charmbrowser.addEventListener('scroll', this._onScroll);
-    },
-
-    componentWillUnmount: function() {
-      if (this.refs.charmbrowser) {
-        this.refs.charmbrowser.removeEventListener('scroll', this._onScroll);
-      }
-    },
-
-    /**
-      Scroll the charmbrowser to an element with an id that matches the
-      hash state.
-
-      @param container {Object} a DOM node that contains the element with the
-        id to scroll to.
-    */
-    _scrollCharmbrowser: function(container) {
-      const hash = this.props.appState.current.hash;
-      const target = container.querySelector(`#${hash}`);
-      // The charmbrowser element does the scrolling.
-      const charmbrowser = ReactDOM.findDOMNode(this).querySelector(
-        '.charmbrowser');
-      if (target && charmbrowser) {
-        // Set the scroll position to the element's top position taking into
-        // account the sticky header size.
-        charmbrowser.scrollTop += target.getBoundingClientRect().top - 200;
-      }
-    },
-
-    /**
-      Set the scroll position state.
-
-      @method _onScroll
-      @param {Object} e The scroll event
-    */
-    _onScroll: function(e) {
-      if (this.state.activeComponent === 'entity-details') {
-        this.setState({scrollPosition: e.target.scrollTop});
-      }
-    },
-
-    /**
-      Closes the charmbrowser.
-
-      @method _close
-    */
-    _close: function() {
-      this.props.appState.changeState({
-        hash: null,
-        root: null,
-        search: null,
-        store: null
-      });
-    },
-
-    /**
-      Generates the state for the charmbrowser based on the app state.
-
-      @method generateState
-      @param {Object} nextProps The props which were sent to the component.
-      @return {Object} A generated state object which can be passed to setState.
-    */
-    generateState: function(nextProps) {
-      let state = {};
-      let activeComponent = 'store';
-      const currentState = nextProps.appState.current;
-      if (currentState.store) {
-        activeComponent = 'entity-details';
-      } else if (currentState.search) {
-        activeComponent = 'search-results';
-      }
-      // If the contents of the charmbrowser changes we need to scroll the
-      // container to the top.
-      if (this.state && activeComponent !== this.state.activeComponent) {
-        this.refs.charmbrowser.scrollTop = 0;
-        state.scrollPosition = 0;
-      }
-      state.activeComponent = activeComponent;
-      return state;
-    },
-
-    /**
-      Generate the content based on the state.
-
-      @method _generateContent
-      @return {Object} The child components for the content.
-    */
-    _generateContent: function() {
-      let activeChild;
-      const utils = this.props.utils;
-      const appState = this.props.appState;
-      const currentState = appState.current;
-      const changeState = appState.changeState.bind(appState);
-      switch (this.state.activeComponent) {
-        case 'store':
-          activeChild = (
-            <juju.components.Store
-              changeState={changeState}
-              gisf={this.props.gisf}
-              staticURL={this.props.staticURL}
-              charmstoreURL={this.props.charmstoreURL}
-              apiVersion={this.props.apiVersion}
-              setPageTitle={this.props.setPageTitle} />
-          );
-          break;
-        case 'search-results':
-          const search = currentState.search;
-          activeChild = (
-            <juju.components.SearchResults
-              acl={this.props.acl}
-              changeState={changeState}
-              generatePath={appState.generatePath.bind(appState)}
-              charmstoreSearch={this.props.charmstoreSearch}
-              deployTarget={this.props.deployTarget}
-              getName={utils.getName}
-              makeEntityModel={this.props.makeEntityModel}
-              owner={search.owner}
-              provides={search.provides}
-              query={search.text}
-              requires={search.requires}
-              series={search.series}
-              seriesList={this.props.series}
-              setPageTitle={this.props.setPageTitle}
-              sort={search.sort}
-              tags={search.tags}
-              type={search.type} />
-          );
-          break;
-        case 'entity-details':
-          // TODO frankban: do we still really need this?
-          const id = currentState.store || `~${currentState.user}`;
-          activeChild = (
-            <juju.components.EntityDetails
-              acl={this.props.acl}
-              addNotification={this.props.addNotification}
-              apiUrl={this.props.apiUrl}
-              changeState={changeState}
-              importBundleYAML={this.props.importBundleYAML}
-              getBundleYAML={this.props.getBundleYAML}
-              getEntity={this.props.getEntity}
-              deployService={this.props.deployService}
-              getDiagramURL={this.props.getDiagramURL}
-              getModelName={this.props.getModelName}
-              getFile={this.props.getFile}
-              hash={currentState.hash}
-              scrollPosition={this.state.scrollPosition}
-              renderMarkdown={this.props.renderMarkdown}
-              id={id}
-                // This is used to force a component remount when the entity
-                // changes, for instance a charm detail page has a link to
-                // another charm detail page.
-              key={id}
-              pluralize={utils.pluralize}
-              listPlansForCharm={this.props.listPlansForCharm}
-              makeEntityModel={this.props.makeEntityModel}
-              scrollCharmbrowser={this._scrollCharmbrowser}
-              setPageTitle={this.props.setPageTitle}
-              showTerms={this.props.showTerms}
-              urllib={this.props.urllib}
-            />
-          );
-          break;
-      }
-      return activeChild;
-    },
-
-    componentWillReceiveProps: function(nextProps) {
-      this.setState(this.generateState(nextProps));
-    },
-
-    render: function() {
-      return (
-        <juju.components.Panel
-          clickAction={this._close}
-          instanceName="white-box"
-          focus={false}
-          visible={true}>
-          <div className="charmbrowser"
-            ref="charmbrowser">
-            {this._generateContent()}
-          </div>
-        </juju.components.Panel>
-      );
+  componentWillUnmount: function() {
+    if (this.refs.charmbrowser) {
+      this.refs.charmbrowser.removeEventListener('scroll', this._onScroll);
     }
+  },
 
-  });
+  /**
+    Scroll the charmbrowser to an element with an id that matches the
+    hash state.
 
+    @param container {Object} a DOM node that contains the element with the
+      id to scroll to.
+  */
+  _scrollCharmbrowser: function(container) {
+    const hash = this.props.appState.current.hash;
+    const target = container.querySelector(`#${hash}`);
+    // The charmbrowser element does the scrolling.
+    const charmbrowser = ReactDOM.findDOMNode(this).querySelector(
+      '.charmbrowser');
+    if (target && charmbrowser) {
+      // Set the scroll position to the element's top position taking into
+      // account the sticky header size.
+      charmbrowser.scrollTop += target.getBoundingClientRect().top - 200;
+    }
+  },
+
+  /**
+    Set the scroll position state.
+
+    @method _onScroll
+    @param {Object} e The scroll event
+  */
+  _onScroll: function(e) {
+    if (this.state.activeComponent === 'entity-details') {
+      this.setState({scrollPosition: e.target.scrollTop});
+    }
+  },
+
+  /**
+    Closes the charmbrowser.
+
+    @method _close
+  */
+  _close: function() {
+    this.props.appState.changeState({
+      hash: null,
+      root: null,
+      search: null,
+      store: null
+    });
+  },
+
+  /**
+    Generates the state for the charmbrowser based on the app state.
+
+    @method generateState
+    @param {Object} nextProps The props which were sent to the component.
+    @return {Object} A generated state object which can be passed to setState.
+  */
+  generateState: function(nextProps) {
+    let state = {};
+    let activeComponent = 'store';
+    const currentState = nextProps.appState.current;
+    if (currentState.store) {
+      activeComponent = 'entity-details';
+    } else if (currentState.search) {
+      activeComponent = 'search-results';
+    }
+    // If the contents of the charmbrowser changes we need to scroll the
+    // container to the top.
+    if (this.state && activeComponent !== this.state.activeComponent) {
+      this.refs.charmbrowser.scrollTop = 0;
+      state.scrollPosition = 0;
+    }
+    state.activeComponent = activeComponent;
+    return state;
+  },
+
+  /**
+    Generate the content based on the state.
+
+    @method _generateContent
+    @return {Object} The child components for the content.
+  */
+  _generateContent: function() {
+    let activeChild;
+    const utils = this.props.utils;
+    const appState = this.props.appState;
+    const currentState = appState.current;
+    const changeState = appState.changeState.bind(appState);
+    switch (this.state.activeComponent) {
+      case 'store':
+        activeChild = (
+          <juju.components.Store
+            changeState={changeState}
+            gisf={this.props.gisf}
+            staticURL={this.props.staticURL}
+            charmstoreURL={this.props.charmstoreURL}
+            apiVersion={this.props.apiVersion}
+            setPageTitle={this.props.setPageTitle} />
+        );
+        break;
+      case 'search-results':
+        const search = currentState.search;
+        activeChild = (
+          <juju.components.SearchResults
+            acl={this.props.acl}
+            changeState={changeState}
+            generatePath={appState.generatePath.bind(appState)}
+            charmstoreSearch={this.props.charmstoreSearch}
+            deployTarget={this.props.deployTarget}
+            getName={utils.getName}
+            makeEntityModel={this.props.makeEntityModel}
+            owner={search.owner}
+            provides={search.provides}
+            query={search.text}
+            requires={search.requires}
+            series={search.series}
+            seriesList={this.props.series}
+            setPageTitle={this.props.setPageTitle}
+            sort={search.sort}
+            tags={search.tags}
+            type={search.type} />
+        );
+        break;
+      case 'entity-details':
+        // TODO frankban: do we still really need this?
+        const id = currentState.store || `~${currentState.user}`;
+        activeChild = (
+          <juju.components.EntityDetails
+            acl={this.props.acl}
+            addNotification={this.props.addNotification}
+            apiUrl={this.props.apiUrl}
+            changeState={changeState}
+            importBundleYAML={this.props.importBundleYAML}
+            getBundleYAML={this.props.getBundleYAML}
+            getEntity={this.props.getEntity}
+            deployService={this.props.deployService}
+            getDiagramURL={this.props.getDiagramURL}
+            getModelName={this.props.getModelName}
+            getFile={this.props.getFile}
+            hash={currentState.hash}
+            scrollPosition={this.state.scrollPosition}
+            renderMarkdown={this.props.renderMarkdown}
+            id={id}
+              // This is used to force a component remount when the entity
+              // changes, for instance a charm detail page has a link to
+              // another charm detail page.
+            key={id}
+            pluralize={utils.pluralize}
+            listPlansForCharm={this.props.listPlansForCharm}
+            makeEntityModel={this.props.makeEntityModel}
+            scrollCharmbrowser={this._scrollCharmbrowser}
+            setPageTitle={this.props.setPageTitle}
+            showTerms={this.props.showTerms}
+            urllib={this.props.urllib}
+          />
+        );
+        break;
+    }
+    return activeChild;
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    this.setState(this.generateState(nextProps));
+  },
+
+  render: function() {
+    return (
+      <juju.components.Panel
+        clickAction={this._close}
+        instanceName="white-box"
+        focus={false}
+        visible={true}>
+        <div className="charmbrowser"
+          ref="charmbrowser">
+          {this._generateContent()}
+        </div>
+      </juju.components.Panel>
+    );
+  }
+
+});
+
+YUI.add('charmbrowser-component', function() {
+  juju.components.Charmbrowser = Charmbrowser;
 }, '0.1.0', {
   requires: [
     'entity-details',
