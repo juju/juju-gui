040b000272991ddf75118ee9277dc72dd173aa11
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-26 08:41:21 +1000

    Update ExpandingProgress to use es6 class.

--- jujugui/static/gui/src/app/components/expanding-progress/expanding-progress.js
+++ jujugui/static/gui/src/app/components/expanding-progress/expanding-progress.js
@@ -18,13 +18,13 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-const ExpandingProgress = React.createClass({
+class ExpandingProgress extends React.Component {
+  constructor() {
+    super();
+    this.state = {active: false};
+  }
 
-  getInitialState: function() {
-    return {active: false};
-  },
-
-  componentDidMount: function() {
+  componentDidMount() {
     // componentDidMount appears to actually fire before the element is in
     // the DOM so this class gets triggered too early causing the css
     // transitions to not be applied. This setTimeout hack makes sure that
@@ -32,9 +32,9 @@ const ExpandingProgress = React.createClass({
     setTimeout(() => {
       this.setState({active: true});
     });
-  },
+  }
 
-  render: function() {
+  render() {
     var classes = classNames('expanding-progress', {
       'expanding-progress--active' : this.state.active
     });
@@ -42,7 +42,7 @@ const ExpandingProgress = React.createClass({
       <div className={classes}></div>
     );
   }
-});
+};
 
 YUI.add('expanding-progress', function() {
   juju.components.ExpandingProgress = ExpandingProgress;
