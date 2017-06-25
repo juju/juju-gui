32677de50fc60a61d88011185c9d9fd3b31f77a3
Huw Wilkins <huw.wilkins@canonical.com>
2017-06-26 09:06:53 +1000

    Update HeaderLogo to use es6 class.

--- jujugui/static/gui/src/app/components/header-logo/header-logo.js
+++ jujugui/static/gui/src/app/components/header-logo/header-logo.js
@@ -22,19 +22,12 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
   We want the header logo to be a component as we may need to change what
   happens when it's clicked.
 */
-const HeaderLogo = React.createClass({
-
-  propTypes: {
-    gisf: React.PropTypes.bool.isRequired,
-    homePath: React.PropTypes.string,
-    showProfile: React.PropTypes.func
-  },
-
+class HeaderLogo extends React.Component {
   /**
     Calls the showProfile prop
     @param {Object} e The click event.
   */
-  _showProfile: function(e) {
+  _showProfile(e) {
     if (this.props.gisf) {
       // If we're in gisf then we want to allow the href to
       // continue as normal
@@ -42,28 +35,34 @@ const HeaderLogo = React.createClass({
     }
     e.preventDefault();
     this.props.showProfile();
-  },
+  }
 
   /**
     Returns the Juju logo SvgIcon component.
     @return {Object} The SvgIcon component of the Juju logo.
   */
-  _svg: function() {
+  _svg() {
     return (<juju.components.SvgIcon name="juju-logo"
       className="svg-icon"
       width="90" height="35" />);
-  },
+  }
 
-  render: function() {
+  render() {
     return (
       <a href={this.props.homePath}
-        onClick={this._showProfile}
+        onClick={this._showProfile.bind(this)}
         role="button"
         title="Home">
         {this._svg()}
       </a>);
   }
-});
+};
+
+HeaderLogo.propTypes = {
+  gisf: React.PropTypes.bool.isRequired,
+  homePath: React.PropTypes.string,
+  showProfile: React.PropTypes.func
+};
 
 YUI.add('header-logo', function() {
   juju.components.HeaderLogo = HeaderLogo;
