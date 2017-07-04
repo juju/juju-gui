089b5894a49f0ca70254f5b3b65b6d072a417d9f
Jeff Pihach <jeff.pihach@canonical.com>
2017-07-04 14:06:47 -0500

    Depending on the active section prop and state, render the appropriate placeholder content.

--- jujugui/static/gui/src/app/components/profile/profile.js
+++ jujugui/static/gui/src/app/components/profile/profile.js
@@ -4,6 +4,44 @@
 
 class Profile extends React.Component {
 
+  constructor(props) {
+    super(props);
+    const activeSection = decodeURIComponent(this.props.activeSection || 'models'); // eslint-disable-line
+    this.state = {activeSection};
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({activeSection: nextProps.activeSection});
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.activeSection !== this.state.activeSection) {
+      return true;
+    }
+    return false;
+  }
+
+  _generateContent() {
+    switch(this.state.activeSection) {
+      case 'models':
+        return 'Models';
+        break;
+      case 'bundles':
+        return 'Bundles';
+        break;
+      case 'charms':
+        return 'Charms';
+        break;
+      case 'cloud credentials':
+        return 'Cloud Credentials';
+        break;
+      default:
+        // Render the default as the model list for now, but the Summary later.
+        return 'Models';
+        break;
+    }
+  }
+
   render() {
     return (
       <juju.components.Panel
@@ -12,9 +50,12 @@ class Profile extends React.Component {
         <juju.components.ProfileHeader />
         <div className="inner-wrapper">
           <div className="three-col">
-            <juju.components.ProfileNavigation />
+            <juju.components.ProfileNavigation
+              changeState={this.props.changeState}/>
           </div>
-          <div className="six-col last-col"></div>
+          <div className="six-col last-col">
+            {this._generateContent()}
+          </div>
         </div>
       </juju.components.Panel>
     );
@@ -22,7 +63,10 @@ class Profile extends React.Component {
 
 };
 
-Profile.propTypes = {};
+Profile.propTypes = {
+  activeSection: React.PropTypes.string,
+  changeState: React.PropTypes.func.isRequired
+};
 
 YUI.add('profile', function() {
   juju.components.Profile = Profile;
