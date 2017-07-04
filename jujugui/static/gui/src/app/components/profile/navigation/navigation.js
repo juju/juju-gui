0c2963069412a177f378e28a8ad40796191c6795
Jeff Pihach <jeff.pihach@canonical.com>
2017-07-04 14:05:51 -0500

    Add the ability to navigate using changeState to the profile navigation.

--- jujugui/static/gui/src/app/components/profile/navigation/navigation.js
+++ jujugui/static/gui/src/app/components/profile/navigation/navigation.js
@@ -7,13 +7,28 @@ class ProfileNavigation extends React.Component {
   constructor() {
     super();
     this.state = {
-      activeSection: 'Models'
+      activeSection: 'models'
     };
+  }
+
+  _changeState(label) {
+    this.props.changeState({
+      // We're using a hash for the state for intra profile navigation because
+      // of the ambiguous paths can not easily support yet another collision
+      // case on top of model and entity names.
+      hash: encodeURIComponent(label.toLowerCase())
+    });
   }
 
   render() {
     const links = ['Models', 'Bundles', 'Charms', 'Cloud Credentials']
-      .map(label => <li className={this.state.activeSection === label ? 'active' : ''}>{label}</li>); // eslint-disable-line
+      .map(label =>
+        <li
+          className={this.state.activeSection === label.toLowerCase() ? 'active' : ''} // eslint-disable-line
+          key={label}
+          onClick={this._changeState.bind(this, label)}>
+          {label}
+        </li>);
     return (
       <div className="profile-navigation">
         <ul>
@@ -24,7 +39,9 @@ class ProfileNavigation extends React.Component {
 
 };
 
-ProfileNavigation.propTypes = {};
+ProfileNavigation.propTypes = {
+  changeState: React.PropTypes.func.isRequired
+};
 
 YUI.add('profile-navigation', function() {
   juju.components.ProfileNavigation = ProfileNavigation;
