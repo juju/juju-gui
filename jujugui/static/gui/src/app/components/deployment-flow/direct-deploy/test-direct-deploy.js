8958ef191b6c03778fd49dcea00e776b0c9afd15
Huw Wilkins <huw.wilkins@canonical.com>
2017-07-11 15:44:11 +1000

    Clean up direct deploy states. Display the machine count. Display the single charm icon.

--- jujugui/static/gui/src/app/components/deployment-flow/direct-deploy/test-direct-deploy.js
+++ jujugui/static/gui/src/app/components/deployment-flow/direct-deploy/test-direct-deploy.js
@@ -30,111 +30,184 @@ describe('DirectDeploy', function() {
     YUI().use('deployment-direct-deploy', function() { done(); });
   });
 
-  it('renders the Direct deploy for a charm', () => {
-    const id = 'cs:apache-21';
+  it('can display the loading spinner', () => {
     const renderer = jsTestUtils.shallowRender(
       <juju.components.DeploymentDirectDeploy
         changeState={sinon.stub()}
-        ddData={{id: id}}
+        ddData={{id: 'cs:bundle/kubernetes-core-8'}}
         generatePath={sinon.stub()}
         getDiagramURL={sinon.stub()}
         getEntity={sinon.stub()}
         makeEntityModel={sinon.stub()}
         renderMarkdown={sinon.stub()} />, true);
     const output = renderer.getRenderOutput();
-    const instance = renderer.getMountedInstance();
     const expected = (
       <juju.components.DeploymentSection
         instance="deployment-direct-deploy">
-        <div className="six-col last-col deployment-direct-deploy__image">
-          {null}
-          <div className="deployment-direct-deploy__edit-model">
-            <juju.components.GenericButton
-              action={instance._handleClose}
-              type="inline-neutral">
-              Edit model
-            </juju.components.GenericButton>
+        <juju.components.Spinner />
+      </juju.components.DeploymentSection>);
+    expect(output).toEqualJSX(expected);
+  });
+
+  it('can show a message for an invalid bundle', () => {
+    const renderer = jsTestUtils.shallowRender(
+      <juju.components.DeploymentDirectDeploy
+        changeState={sinon.stub()}
+        ddData={{id: 'cs:bundle/kubernetes-core-8'}}
+        generatePath={sinon.stub()}
+        getDiagramURL={sinon.stub()}
+        getEntity={sinon.stub().callsArgWith(1, null, null)}
+        makeEntityModel={sinon.stub()}
+        renderMarkdown={sinon.stub()} />, true);
+    const output = renderer.getRenderOutput();
+    const expected = (
+      <juju.components.DeploymentSection
+        instance="deployment-direct-deploy">
+        <div>
+          This {'bundle'} could not be found.
+        </div>
+      </juju.components.DeploymentSection>);
+    expect(output).toEqualJSX(expected);
+  });
+
+  it('can show a message for an invalid charm', () => {
+    const renderer = jsTestUtils.shallowRender(
+      <juju.components.DeploymentDirectDeploy
+        changeState={sinon.stub()}
+        ddData={{id: 'cs:apache-21'}}
+        generatePath={sinon.stub()}
+        getDiagramURL={sinon.stub()}
+        getEntity={sinon.stub().callsArgWith(1, null, null)}
+        makeEntityModel={sinon.stub()}
+        renderMarkdown={sinon.stub()} />, true);
+    const output = renderer.getRenderOutput();
+    const expected = (
+      <juju.components.DeploymentSection
+        instance="deployment-direct-deploy">
+        <div>
+          This {'charm'} could not be found.
+        </div>
+      </juju.components.DeploymentSection>);
+    expect(output).toEqualJSX(expected);
+  });
+
+  it('renders the Direct deploy for a charm', () => {
+    const charm = {
+      toEntity: sinon.stub().returns({
+        displayName: 'Apache 2',
+        iconPath: 'http://example.com/icon.svg'
+      })
+    };
+    const renderMarkdown = sinon.stub();
+    const renderer = jsTestUtils.shallowRender(
+      <juju.components.DeploymentDirectDeploy
+        changeState={sinon.stub()}
+        ddData={{id: 'cs:apache-21'}}
+        generatePath={sinon.stub()}
+        getDiagramURL={sinon.stub()}
+        getEntity={sinon.stub().callsArgWith(1, null, [charm])}
+        makeEntityModel={sinon.stub().returns(charm)}
+        renderMarkdown={renderMarkdown} />, true);
+    const instance = renderer.getMountedInstance();
+    const output = renderer.getRenderOutput();
+    const expected = (
+      <juju.components.DeploymentSection
+        instance="deployment-direct-deploy">
+        <div>
+          <div className="deployment-direct-deploy__description six-col">
+            <h4>You are about to deploy:</h4>
+            <h2 className="deployment-direct-deploy__title">
+              Apache 2
+            </h2>
+            <juju.components.EntityContentDescription
+              entityModel={charm}
+              renderMarkdown={renderMarkdown} />
+            <ul>
+              <li>
+                It will run on {1}&nbsp;
+                machine{''} in your cloud.
+              </li>
+            </ul>
+            {null}
+          </div>
+          <div className="six-col last-col no-margin-bottom">
+            <div className="deployment-direct-deploy__image">
+              <div className="deployment-direct-deploy__image-block">
+                <img alt="Apache 2"
+                  className="deployment-direct-deploy__image-block-icon"
+                  src="http://example.com/icon.svg"
+                  width="96" />
+              </div>
+            </div>
+            <div className="deployment-direct-deploy__edit-model">
+              <juju.components.GenericButton
+                action={instance._handleClose.bind(this)}
+                type="inline-neutral">
+                Edit model
+              </juju.components.GenericButton>
+            </div>
           </div>
         </div>
       </juju.components.DeploymentSection>);
     expect(output).toEqualJSX(expected);
-
-    const finalRenderer = jsTestUtils.shallowRender(expected, true);
-    const finalOutput = finalRenderer.getRenderOutput();
-    const finalExpected = (
-      <div className="deployment-section
-        twelve-col
-        deployment-section--active
-        deployment-direct-deploy">
-        <h3 className="deployment-section__title" />
-        <div className="six-col last-col deployment-direct-deploy__image">
-          {null}
-          <div className="deployment-direct-deploy__edit-model">
-            <juju.components.GenericButton
-              action={instance._handleClose}
-              type="inline-neutral">
-              Edit model
-            </juju.components.GenericButton>
-          </div>
-        </div>
-      </div>);
-    expect(finalOutput).toEqualJSX(finalExpected);
   });
 
   it('renders the Direct Deploy for a bundle', () => {
-    const id = 'cs:bundles/kubernetes-core-8';
+    const bundle = {
+      toEntity: sinon.stub().returns({
+        displayName: 'Kubernetes core',
+        machineCount: 4
+      })
+    };
+    const renderMarkdown = sinon.stub();
+    const getDiagramURL = sinon.stub();
     const renderer = jsTestUtils.shallowRender(
       <juju.components.DeploymentDirectDeploy
         changeState={sinon.stub()}
-        ddData={{id: id}}
+        ddData={{id: 'cs:bundle/kubernetes-core-8'}}
         generatePath={sinon.stub()}
-        getDiagramURL={sinon.stub()}
-        getEntity={sinon.stub()}
-        makeEntityModel={sinon.stub()}
-        renderMarkdown={sinon.stub()} />, true);
-    const output = renderer.getRenderOutput();
+        getDiagramURL={getDiagramURL}
+        getEntity={sinon.stub().callsArgWith(1, null, [bundle])}
+        makeEntityModel={sinon.stub().returns(bundle)}
+        renderMarkdown={renderMarkdown} />, true);
     const instance = renderer.getMountedInstance();
+    const output = renderer.getRenderOutput();
     const expected = (
       <juju.components.DeploymentSection
         instance="deployment-direct-deploy">
-        <div className="six-col last-col deployment-direct-deploy__image">
-          <juju.components.EntityContentDiagram
-            getDiagramURL={instance.props.getDiagramURL}
-            id={id} />
-          <div className="deployment-direct-deploy__edit-model">
-            <juju.components.GenericButton
-              action={instance._handleClose}
-              type="inline-neutral">
-              Edit model
-            </juju.components.GenericButton>
+        <div>
+          <div className="deployment-direct-deploy__description six-col">
+            <h4>You are about to deploy:</h4>
+            <h2 className="deployment-direct-deploy__title">
+              Kubernetes core
+            </h2>
+            <juju.components.EntityContentDescription
+              entityModel={bundle}
+              renderMarkdown={renderMarkdown} />
+            <ul>
+              <li>
+                It will run on {4}&nbsp;
+                machine{'s'} in your cloud.
+              </li>
+            </ul>
+            {null}
+          </div>
+          <div className="six-col last-col no-margin-bottom">
+            <div className="deployment-direct-deploy__image">
+              <juju.components.EntityContentDiagram
+                getDiagramURL={getDiagramURL}
+                id="cs:bundle/kubernetes-core-8" />
+            </div>
+            <div className="deployment-direct-deploy__edit-model">
+              <juju.components.GenericButton
+                action={instance._handleClose.bind(this)}
+                type="inline-neutral">
+                Edit model
+              </juju.components.GenericButton>
+            </div>
           </div>
         </div>
-      </juju.components.DeploymentSection>
-    );
+      </juju.components.DeploymentSection>);
     expect(output).toEqualJSX(expected);
-
-    const finalRenderer = jsTestUtils.shallowRender(expected, true);
-    const finalOutput = finalRenderer.getRenderOutput();
-    const finalExpected = (
-      <div className="deployment-section
-        twelve-col
-        deployment-section--active
-        deployment-direct-deploy">
-        <h3 className="deployment-section__title" />
-        <div className="six-col last-col deployment-direct-deploy__image">
-          <juju.components.EntityContentDiagram
-            getDiagramURL={instance.props.getDiagramURL}
-            id={id} />
-          <div className="deployment-direct-deploy__edit-model">
-            <juju.components.GenericButton
-              action={instance._handleClose}
-              type="inline-neutral">
-              Edit model
-            </juju.components.GenericButton>
-          </div>
-        </div>
-      </div>
-    );
-    expect(finalOutput).toEqualJSX(finalExpected);
   });
 });
