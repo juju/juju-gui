c4be6b5dc5eabbed2f72c22b33ea5ad928573bbf
Huw Wilkins <huw.wilkins@canonical.com>
2017-07-28 15:37:47 +1000

    Add a basic status component.

---
+++ jujugui/static/gui/src/app/components/status/status.js
@@ -0,0 +1,34 @@
+/* Copyright (C) 2017 Canonical Ltd. */
+
+'use strict';
+
+/** Status React component used to display Juju status. */
+class Status extends React.Component {
+
+  render() {
+    return (
+      <juju.components.Panel
+        instanceName="status-view"
+        visible={true}>
+        <div className="status-view__content">
+          Status
+        </div>
+      </juju.components.Panel>
+    );
+  }
+
+};
+
+
+Status.propTypes = {
+  acl: PropTypes.object,
+  addNotification: PropTypes.func.isRequired
+};
+
+YUI.add('status', function() {
+  juju.components.Status = Status;
+}, '', {
+  requires: [
+    'panel-component'
+  ]
+});
---
+++ jujugui/static/gui/src/app/components/status/test-status.js
@@ -0,0 +1,33 @@
+/* Copyright (C) 2017 Canonical Ltd. */
+
+'use strict';
+
+var juju = {components: {}}; // eslint-disable-line no-unused-vars
+
+describe('Status', function() {
+  beforeAll(function(done) {
+    // By loading this file it adds the component to the juju components.
+    YUI().use('status', function() {
+      done();
+    });
+  });
+
+  it('can render', () => {
+    const renderer = jsTestUtils.shallowRender(
+      <window.juju.components.Status
+        acl={{}}
+        addNotification={sinon.stub()} />, true);
+    const output = renderer.getRenderOutput();
+    const expected = (
+      <juju.components.Panel
+        instanceName="status-view"
+        visible={true}>
+        <div className="status-view__content">
+          Status
+        </div>
+      </juju.components.Panel>
+    );
+    expect(output).toEqualJSX(expected);
+  });
+
+});
