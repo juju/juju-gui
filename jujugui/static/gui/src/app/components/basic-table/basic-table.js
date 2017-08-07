b6a79259bda4ba2bf75692ea090dade4126310b1
Huw Wilkins <huw.wilkins@canonical.com>
2017-08-07 16:12:23 +1000

    Added a basic table component.

---
+++ jujugui/static/gui/src/app/components/basic-table/basic-table.js
@@ -0,0 +1,112 @@
+/* Copyright (C) 2017 Canonical Ltd. */
+
+'use strict';
+
+/** Basic table React component used to display data in a table structure. */
+class BasicTable extends React.Component {
+  constructor() {
+    super();
+    this.columnClasses = [
+      'one-col',
+      'two-col',
+      'three-col',
+      'four-col',
+      'five-col',
+      'six-col',
+      'seven-col',
+      'eight-col',
+      'nine-col',
+      'ten-col',
+      'eleven-col',
+      'twelve-col'
+    ];
+  }
+
+  /**
+    Generate the a row.
+    @param header {Boolean} Whether this is a header row.
+    @param columnValues {Array} The list of column values.
+    @param index {Int} The row index (used for unique keys).
+    @returns {Object} The row to render.
+  */
+  _generateRow(header, columnValues, index) {
+    const columnsNumber = this.props.columns.length;
+    const columns = this.props.columns.map((column, i) => {
+      let conditionalClasses = {'last-col': i + 1 === columnsNumber};
+      // Map the column size to the appropriate CSS class.
+      conditionalClasses[this.columnClasses[parseInt(column.size) - 1]] = true;
+      if (column.classes) {
+        column.classes.forEach(className => {
+          conditionalClasses[className] = true;
+        });
+      }
+      const classes = classNames(conditionalClasses);
+      let content;
+      if (header) {
+        content = column.title;
+      } else {
+        content = columnValues[i] || '';
+      }
+      return (
+        <div className={classes}
+          key={i}>
+          {content}
+        </div>);
+    });
+    const classes = classNames(
+      'twelve-col',
+      {
+        'basic-table__header': header,
+        'basic-table__row': !header
+      });
+    return (
+      <li className={classes}
+        key={index || 0}>
+        {columns}
+      </li>);
+  }
+
+  /**
+    Generate the row content.
+    @returns {Object} The rows to render.
+  */
+  _generateContent() {
+    return this.props.rows.map((row, i) => {
+      return this._generateRow(false, row, i+1);
+    });
+  }
+
+  render() {
+    return (
+      <ul className="basic-table twelve-col">
+        {this._generateRow(true)}
+        {this._generateContent()}
+      </ul>
+    );
+  }
+};
+
+BasicTable.propTypes = {
+  // The columns array should contain objects in the following format:
+  // [{
+  //  title: 'Column title',
+  //  size: 3, // The width of the column (between 1 and 12).
+  //  classes: ['class1', 'class2', ...]
+  // }, ...]
+  columns: PropTypes.array.isRequired,
+  // The rows property should contain an array of arrays containing the
+  // column content. e.g.
+  // [
+  //  ['row 1 column 1', 'row 1 column 2', ...],
+  //  ['row 2 column 1', 'row 2 column 2', ...],
+  //  ...
+  // ]
+  rows: PropTypes.array.isRequired
+};
+
+YUI.add('basic-table', function() {
+  juju.components.BasicTable = BasicTable;
+}, '', {
+  requires: [
+  ]
+});
---
+++ jujugui/static/gui/src/app/components/basic-table/test-basic-table.js
@@ -0,0 +1,83 @@
+/* Copyright (C) 2017 Canonical Ltd. */
+
+'use strict';
+
+var juju = {components: {}}; // eslint-disable-line no-unused-vars
+
+chai.config.includeStack = true;
+chai.config.truncateThreshold = 0;
+
+describe('BasicTable', function() {
+  beforeAll(function(done) {
+    // By loading this file it adds the component to the juju components.
+    YUI().use('basic-table', function() { done(); });
+  });
+
+  it('can render the table', function() {
+    const columns = [{
+      title: 'Column 1',
+      size: 3,
+      classes: ['class1', 'class2']
+    }, {
+      title: 'Column 2',
+      size: '4'
+    }];
+    const rows = [
+      ['row 1 column 1', 'row 1 column 2'],
+      ['row 2 column 1'],
+      ['row 3 column 1', 'row 3 column 2', 'row 3 column 3']
+    ];
+    const renderer = jsTestUtils.shallowRender(
+      <juju.components.BasicTable
+        columns={columns}
+        rows={rows} />, true);
+    const output = renderer.getRenderOutput();
+    const expected = (
+      <ul className="basic-table twelve-col">
+        <li className="twelve-col basic-table__header"
+          key={0}>
+          <div className="three-col class1 class2"
+            key={0}>
+            Column 1
+          </div>
+          <div className="last-col four-col"
+            key={1}>
+            Column 2
+          </div>
+        </li>
+        <li className="twelve-col basic-table__row"
+          key={1}>
+          <div className="three-col class1 class2"
+            key={0}>
+            row 1 column 1
+          </div>
+          <div className="last-col four-col"
+            key={1}>
+            row 1 column 2
+          </div>
+        </li>
+        <li className="twelve-col basic-table__row"
+          key={2}>
+          <div className="three-col class1 class2"
+            key={0}>
+            row 2 column 1
+          </div>
+          <div className="last-col four-col"
+            key={1}>
+          </div>
+        </li>
+        <li className="twelve-col basic-table__row"
+          key={3}>
+          <div className="three-col class1 class2"
+            key={0}>
+            row 3 column 1
+          </div>
+          <div className="last-col four-col"
+            key={1}>
+            row 3 column 2
+          </div>
+        </li>
+      </ul>);
+    expect(output).toEqualJSX(expected);
+  });
+});
