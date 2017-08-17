e29743e629b89199fe5019dcb804f4edb57425c5
Jeff Pihach <jeff.pihach@canonical.com>
2017-08-16 19:07:04 -0500

    Convert the user menu to use the new button-dropdown component.

--- jujugui/static/gui/src/app/components/user-menu/user-menu.js
+++ jujugui/static/gui/src/app/components/user-menu/user-menu.js
@@ -7,31 +7,9 @@
   If user is not logged in the user icon is replaced with a login button.
 */
 class UserMenu extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showDropdown: false
-    };
-  }
 
-  /**
-    Passed into the dropdown component to call when the user clicks outside
-    of it. We use this trigger to close the dropdown.
-    @param {Object} e The click event.
-  */
-  _handleDropdownClickOutside(e) {
-    // If they click the button again we don't want it to clse the menu in the
-    // clickoutside as the _toggleHelpMenu will handle that.
-    if (!ReactDOM.findDOMNode(this).contains(e.target)) {
-      this.setState({showDropdown: false});
-    }
-  }
-
-  /**
-    Clicking the help menu will toggle whether it's visibility.
-  */
   _toggleDropdown() {
-    this.setState({showDropdown: !this.state.showDropdown});
+    this.refs.buttonDropdown._toggleDropdown();
   }
 
   _handleProfileClick() {
@@ -44,81 +22,35 @@ class UserMenu extends React.Component {
     this._toggleDropdown();
   }
 
-  /**
-    Generate menu based on whether the button has been clicked.
-  */
-  _generateUserMenu() {
-    if (!this.state.showDropdown) {
-      return '';
-    }
-    const logoutLink = this.props.LogoutLink;
-    return (
-      <juju.components.DropdownMenu
-        handleClickOutside={this._handleDropdownClickOutside.bind(this)}>
-        <li className="dropdown-menu__list-item"
-          role="menuitem" tabIndex="0">
-          <a className="dropdown-menu__list-item-link"
-            role="button"
-            onClick={this._handleProfileClick.bind(this)}>Profile</a>
-        </li>
-        <li className="dropdown-menu__list-item"
-          role="menuitem" tabIndex="0">
-          <a className="dropdown-menu__list-item-link"
-            role="button"
-            onClick={this._handleAccountClick.bind(this)}>Account</a>
-        </li>
-        <li className="dropdown-menu__list-item"
-          role="menuitem" tabIndex="0">
-          {logoutLink}
-        </li>
-      </juju.components.DropdownMenu>
-    );
-  }
-
-  /**
-    Get class names based on whether the menu is shown.
-
-    @method _getClassNames
-  */
-  _getClassNames(isLogin) {
-    return classNames(
-      'header-menu__button',
-      {
-        'header-menu__button-with-text': isLogin,
-        'header-menu__show-menu': this.state.showDropdown
-      });
-  }
-
   render() {
     const controllerAPI = this.props.controllerAPI;
-    const loginEle = this.props.USSOLoginLink;
     const showLogin = controllerAPI && !controllerAPI.userIsAuthenticated;
-    let buttonContent;
-    let menu;
-
-    if (showLogin) {
-      buttonContent = loginEle;
-    } else {
-      buttonContent = (<juju.components.SvgIcon name="user_16"
-        className="header-menu__icon"
-        size="16" />);
-      menu = this._generateUserMenu();
-    }
     return (
-      <div className="header-menu">
-        <span className={this._getClassNames(showLogin)}
-          onClick={this._toggleDropdown.bind(this)}
-          role="button"
-          tabIndex="0"
-          aria-haspopup="true"
-          aria-owns="userMenu"
-          aria-controls="userMenu"
-          aria-expanded="false">
-          {buttonContent}
-        </span>
-        {menu}
-      </div>
-    );
+      <juju.components.ButtonDropdown
+        classes={['user-menu']}
+        ref="buttonDropdown"
+        icon={showLogin ? this.props.USSOLoginLink : 'user_16'}
+        disableDropdown={showLogin}
+        listItems={[
+          <li className="dropdown-menu__list-item"
+            role="menuitem" tabIndex="0" key="profile">
+            <a className="dropdown-menu__list-item-link"
+              role="button"
+              onClick={this._handleProfileClick.bind(this)}>Profile</a>
+          </li>,
+          <li className="dropdown-menu__list-item"
+            role="menuitem" tabIndex="0" key="account">
+            <a className="dropdown-menu__list-item-link"
+              role="button"
+              onClick={this._handleAccountClick.bind(this)}>Account</a>
+          </li>,
+          <li className="dropdown-menu__list-item"
+            role="menuitem" tabIndex="0" key="logout">
+            {this.props.LogoutLink}
+          </li>
+        ]}
+        tooltip="user"
+      />);
   }
 };
 
@@ -133,6 +65,5 @@ UserMenu.propTypes = {
 YUI.add('user-menu', function() {
   juju.components.UserMenu = UserMenu;
 }, '0.1.0', { requires: [
-  'dropdown-menu',
-  'svg-icon'
+  'dropdown-menu'
 ]});
