ef51ea67328699d1459d4c393ee5b1d7b6a9f8f7
Jeff Pihach <jeff.pihach@canonical.com>
2017-08-29 14:19:54 -0500

    Convert NotificationList/Item to modules.

--- jujugui/static/gui/src/app/components/notification-list/item/item.js
+++ jujugui/static/gui/src/app/components/notification-list/item/item.js
@@ -1,22 +1,8 @@
-/*
-This file is part of the Juju GUI, which lets users view and manage Juju
-environments within a graphical interface (https://launchpad.net/juju-gui).
-Copyright (C) 2015 Canonical Ltd.
-
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU Affero General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
-SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
-General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License along
-with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* Copyright (C) 2017 Canonical Ltd. */
 
 'use strict';
+
+const SvgIcon = require('../../svg-icon/svg-icon');
 
 class NotificationListItem extends React.Component {
   constructor() {
@@ -68,7 +54,7 @@ class NotificationListItem extends React.Component {
         <span tabIndex="0" role="button"
           className="notification-list-item__hide"
           onClick={this.hide.bind(this)}>
-          <juju.components.SvgIcon name="close_16"
+          <SvgIcon name="close_16"
             size="16" />
         </span>
       </li>);
@@ -83,10 +69,4 @@ NotificationListItem.propTypes = {
   type: PropTypes.string
 };
 
-YUI.add('notification-list-item', function() {
-  juju.components.NotificationListItem = NotificationListItem;
-}, '0.1.0', {
-  requires: [
-    'svg-icon'
-  ]
-});
+module.exports = NotificationListItem;
