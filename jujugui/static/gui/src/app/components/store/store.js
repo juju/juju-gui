b11b7a52cfd726a615ced38242fd6a7febffa363
Jeff Pihach <jeff.pihach@canonical.com>
2017-08-29 18:14:13 -0500

    Converted Store component.

--- jujugui/static/gui/src/app/components/store/store.js
+++ jujugui/static/gui/src/app/components/store/store.js
@@ -1,21 +1,4 @@
-/*
-This file is part of the Juju GUI, which lets users view and manage Juju
-environments within a graphical interface (https://launchpad.net/juju-gui).
-Copyright (C) 2015 Canonical Ltd.
-
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU Affero General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
-SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
-General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License along
-with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
+/* Copyright (C) 2017 Canonical Ltd. */
 'use strict';
 
 class Store extends React.Component {
@@ -263,7 +246,7 @@ class Store extends React.Component {
           View
       </span>);
     }
-    
+
     const kubernetes = (
       <div onClick={this._handleSearchClick.bind(this)}
         data-query="kubernetes"
@@ -980,6 +963,4 @@ Store.propTypes = {
   staticURL: PropTypes.string
 };
 
-YUI.add('store', function() {
-  juju.components.Store = Store;
-}, '0.1.0', { requires: []});
+module.exports = Store;
