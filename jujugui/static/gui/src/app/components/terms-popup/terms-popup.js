c4dca1c2e9b33a704877113c2207fad859309d67
Jeff Pihach <jeff.pihach@canonical.com>
2017-08-29 17:14:36 -0500

    Converted terms-popup

--- jujugui/static/gui/src/app/components/terms-popup/terms-popup.js
+++ jujugui/static/gui/src/app/components/terms-popup/terms-popup.js
@@ -1,29 +1,15 @@
-/*
-This file is part of the Juju GUI, which lets users view and manage Juju
-environments within a graphical interface (https://launchpad.net/juju-gui).
-Copyright (C) 2016 Canonical Ltd.
-
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU Affero General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
-SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
-General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License along
-with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
+/* Copyright (C) 2017 Canonical Ltd. */
 'use strict';
+
+const Spinner = require('../spinner/spinner');
+const Popup = require('../popup/popup');
 
 class TermsPopup extends React.Component {
   render() {
     let content;
     const terms = this.props.terms;
     if (terms.length === 0) {
-      content = <juju.components.Spinner />;
+      content = <Spinner />;
     } else {
       const termsList = terms.map(term => {
         return (
@@ -40,11 +26,11 @@ class TermsPopup extends React.Component {
         </div>);
     }
     return (
-      <juju.components.Popup
+      <Popup
         close={this.props.close}
         type="wide">
         {content}
-      </juju.components.Popup>);
+      </Popup>);
   }
 };
 
@@ -53,11 +39,4 @@ TermsPopup.propTypes = {
   terms: PropTypes.array.isRequired
 };
 
-YUI.add('terms-popup', function() {
-  juju.components.TermsPopup = TermsPopup;
-}, '0.1.0', {
-  requires: [
-    'loading-spinner',
-    'popup'
-  ]
-});
+module.exports = TermsPopup;
