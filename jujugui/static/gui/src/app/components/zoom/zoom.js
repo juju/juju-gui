d1f5d6266f9ef4dbbffc16bc00088fceeeae4d2f
Jeff Pihach <jeff.pihach@canonical.com>
2017-08-29 14:20:34 -0500

    Converted Zoom and ExpandingProgress to modules.

--- jujugui/static/gui/src/app/components/zoom/zoom.js
+++ jujugui/static/gui/src/app/components/zoom/zoom.js
@@ -1,22 +1,8 @@
-/*
-This file is part of the Juju GUI, which lets users view and manage Juju
-environments within a graphical interface (https://launchpad.net/juju-gui).
-Copyright (C) 2015 Canonical Ltd.
-
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU Affero General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
-SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
-General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License along
-with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* Copyright (C) 2017 Canonical Ltd. */
 
 'use strict';
+
+const SvgIcon = require('../svg-icon/svg-icon');
 
 class Zoom extends React.Component {
   /**
@@ -52,7 +38,7 @@ class Zoom extends React.Component {
           onClick={this._zoomIn.bind(this)}
           role="button"
           tabIndex="0">
-          <juju.components.SvgIcon name="add_16"
+          <SvgIcon name="add_16"
             className="zoom-in__icon"
             size="12" />
         </li>
@@ -60,7 +46,7 @@ class Zoom extends React.Component {
           onClick={this._zoomOut.bind(this)}
           role="button"
           tabIndex="0">
-          <juju.components.SvgIcon name="minus_16"
+          <SvgIcon name="minus_16"
             className="zoom-out__icon"
             size="12" />
         </li>
@@ -78,6 +64,4 @@ Zoom.defaultProps = {
   scaleJump: .2
 };
 
-YUI.add('zoom', function() {
-  juju.components.Zoom = Zoom;
-}, '0.1.0', { requires: []});
+module.exports = Zoom;
