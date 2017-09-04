a6324fe2c87b3414bf475e78d5f2a62e19462f33
Jeff Pihach <jeff.pihach@canonical.com>
2017-09-03 20:29:56 -0500

    Converted ButtonRow to module.

--- jujugui/static/gui/src/app/components/button-row/test-button-row.js
+++ jujugui/static/gui/src/app/components/button-row/test-button-row.js
@@ -1,34 +1,14 @@
-/*
-This file is part of the Juju GUI, which lets users view and manage Juju
-environments within a graphical interface (https://launchpad.net/juju-gui).
-Copyright (C) 2015 Canonical Ltd.
-
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU Affero General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
-SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
-General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License along
-with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
+/* Copyright (C) 2017 Canonical Ltd. */
 'use strict';
 
-var juju = {components: {}}; // eslint-disable-line no-unused-vars
+const React = require('react');
 
-chai.config.includeStack = true;
-chai.config.truncateThreshold = 0;
+const ButtonRow = require('./button-row');
+const GenericButton = require('../generic-button/generic-button');
+
+const jsTestUtils = require('../../utils/component-test-utils');
 
 describe('ButtonRow', function() {
-
-  beforeAll(function(done) {
-    // By loading this file it adds the component to the juju components.
-    YUI().use('button-row', function() { done(); });
-  });
 
   it('generates a button', function() {
     var callbackStub = sinon.stub();
@@ -38,17 +18,17 @@ describe('ButtonRow', function() {
       action: callbackStub
     }];
     var output = jsTestUtils.shallowRender(
-      <juju.components.ButtonRow
+      <ButtonRow
         buttons={buttons} />);
     assert.deepEqual(output.props.children, [
-      <juju.components.GenericButton
+      <GenericButton
         key="My button"
         action={callbackStub}
         disabled={undefined}
         submit={undefined}
         type="submit">
         My button
-      </juju.components.GenericButton>]);
+      </GenericButton>]);
   });
 
   it('sets a class when generating multiple buttons', function() {
@@ -63,25 +43,25 @@ describe('ButtonRow', function() {
       action: callbackStub
     }];
     var output = jsTestUtils.shallowRender(
-      <juju.components.ButtonRow
+      <ButtonRow
         buttons={buttons} />);
     var children = [
-      <juju.components.GenericButton
+      <GenericButton
         key="My button"
         action={callbackStub}
         disabled={undefined}
         submit={undefined}
         type="submit">
         My button
-      </juju.components.GenericButton>,
-      <juju.components.GenericButton
+      </GenericButton>,
+      <GenericButton
         key="Another button"
         action={callbackStub}
         disabled={undefined}
         submit={undefined}
         type="submit">
         Another button
-      </juju.components.GenericButton>
+      </GenericButton>
     ];
     assert.deepEqual(output,
       <div className="button-row button-row--multiple button-row--count-2">
