4fe8b21c86acbf34bdc67b859d1ab5ab80a7e4cc
Huw Wilkins <huw.wilkins@canonical.com>
2017-09-14 13:21:50 +1000

    Migrate controller api tests to the new suite.

--- karma.conf.js
+++ karma.conf.js
@@ -74,6 +74,7 @@ module.exports = function(config) {
       'jujugui/static/gui/src/test/test_entity_extension.js',
       'jujugui/static/gui/src/test/test_env_change_set.js',
       'jujugui/static/gui/src/test/test_env_api.js',
+      'jujugui/static/gui/src/test/test_controller_api.js',
       'jujugui/static/gui/src/test/test_viewport_module.js'
     ],
 
