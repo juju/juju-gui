aa65edeb24d653c95b9ea5d4f7d5be557ef651bd
Francesco Banconi <francesco.banconi@canonical.com>
2017-12-05 17:17:44 +0000

    inspector header testing: abstract out rendering into a render func.

--- jujugui/static/gui/src/app/components/inspector/header/test-header.js
+++ jujugui/static/gui/src/app/components/inspector/header/test-header.js
@@ -7,79 +7,56 @@ const InspectorHeader = require('./header');
 
 const jsTestUtils = require('../../../utils/component-test-utils');
 
-const entityId = 'cs:demo';
+fdescribe('InspectorHeader', function() {
 
-describe('InspectorHeader', function() {
+  // render creates and returns a renderer for the header component, optionally
+  // passing the provided parameters.
+  const render = (params={}) => {
+    const renderer = jsTestUtils.shallowRender(
+      <InspectorHeader
+        backCallback={params.backCallback || sinon.stub()}
+        changeState={params.changeState}
+        entityId={params.entityId || 'cs:demo'}
+        icon={params.icon}
+        title={params.title || 'Juju GUI'}
+        type={params.type}
+      />, true);
+    return {
+      instance: renderer.getMountedInstance(),
+      output: renderer.getRenderOutput()
+    };
+  };
 
   it('displays the provided title', function() {
-    const output = jsTestUtils.shallowRender(
-      <InspectorHeader
-        backCallback={sinon.stub()}
-        title="Juju GUI"
-        changeState={sinon.stub()}
-        entityId={entityId} />
-    );
+    const output = render().output;
     assert.equal(output.props.children[1].props.children, 'Juju GUI');
   });
 
   it('adds a class based on the provided type', function() {
-    const output = jsTestUtils.shallowRender(
-      <InspectorHeader
-        backCallback={sinon.stub()}
-        title="Juju GUI"
-        type="error"
-        changeState={sinon.stub()}
-        entityId={entityId} />
-    );
+    const output = render({type: 'error'}).output;
     assert.equal(output.props.className,
       'inspector-header inspector-header--type-error');
   });
 
   it('does not add a type class if it is not provided', function() {
-    const output = jsTestUtils.shallowRender(
-      <InspectorHeader
-        backCallback={sinon.stub()}
-        title="Juju GUI"
-        changeState={sinon.stub()}
-        entityId={entityId} />
-    );
+    const output = render().output;
     assert.equal(output.props.className, 'inspector-header');
   });
 
   it('displays the provided icon', function() {
-    const output = jsTestUtils.shallowRender(
-      <InspectorHeader
-        backCallback={sinon.stub()}
-        icon="icon.svg"
-        title="Juju GUI"
-        changeState={sinon.stub()}
-        entityId={entityId} />
-    );
+    const output = render({icon: 'icon.svg'}).output;
     assert.equal(output.props.children[2].props.children.props.src, 'icon.svg');
   });
 
   it('calls supplied callable when clicked', function() {
-    var callbackStub = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <InspectorHeader
-        backCallback={callbackStub}
-        title="Juju GUI"
-        changeState={sinon.stub()}
-        entityId={entityId} />
-    );
+    const callbackStub = sinon.stub();
+    const output = render({backCallback: callbackStub}).output;
     output.props.onClick();
     assert.equal(callbackStub.callCount, 1);
   });
 
-  fit('can render correctly with details inline list', () => {
-    const renderer = jsTestUtils.shallowRender(
-      <InspectorHeader
-        backCallback={sinon.stub()}
-        title="Juju GUI"
-        changeState={sinon.stub()}
-        entityId={entityId}
-      />, true);
-    const instance = renderer.getMountedInstance();
+  it('can render correctly with details inline list', () => {
+    const instance = render({changeState: sinon.stub()}).instance;
     const output = instance._renderHeaderLinks();
     const expected = (
       <ul className="inspector-header__inline-list">
@@ -95,5 +72,18 @@ describe('InspectorHeader', function() {
         </li>
       </ul>);
     expect(output).toEqualJSX(expected);
+  });
+
+  it('navigates to the getstarted page of a charm or bundle', () => {
+    const changeState = sinon.stub();
+    const event = {preventDefault: sinon.stub()};
+    const instance = render({changeState: changeState}).instance;
+    instance._navigateToGetStarted(event);
+    assert.strictEqual(changeState.callCount, 1, 'changeState call count');
+    assert.deepEqual(changeState.args[0], [{
+      postDeploymentPanel: {show: true, entityId: 'cs:demo'}
+    }]);
+    assert.strictEqual(event.preventDefault.callCount, 1, 'preventDefault call count');
+    assert.deepEqual(event.preventDefault.args[0], []);
   });
 });
