bd03e52e31d6f708d29c6a2388ece86456d55f10
Jeff Pihach <jeff.pihach@canonical.com>
2018-01-05 11:06:11 -0600

    Add tests for the new inspector header links.

--- jujugui/static/gui/src/app/components/inspector/header/test-header.js
+++ jujugui/static/gui/src/app/components/inspector/header/test-header.js
@@ -13,9 +13,14 @@ describe('InspectorHeader', () => {
     return jsTestUtils.shallowRender(
       <InspectorHeader
         backCallback={options.backCallback || sinon.stub()}
+        changeState={options.changeState}
+        charmId={options.charmId}
+        entityPath={options.entityPath}
+        hasGetStarted={options.hasGetStarted}
+        icon={options.icon}
+        showLinks={options.showLinks}
         title="Juju GUI"
-        type={options.type}
-        icon={options.icon} />);
+        type={options.type} />);
   }
 
   it('displays the provided title', () => {
@@ -44,5 +49,94 @@ describe('InspectorHeader', () => {
     const output = renderComponent({backCallback: callbackStub});
     output.props.onClick();
     assert.equal(callbackStub.callCount, 1);
+  });
+
+  it('by default the header links are disabled', () => {
+    const output = renderComponent();
+    assert.equal(output.props.children[3], undefined);
+  });
+
+  it('renders the Get Started and Charm Details links when requested', () => {
+    const output = renderComponent({
+      hasGetStarted: true,
+      showLinks: true
+    });
+    expect(output.props.children[3]).toEqualJSX(
+      <ul className="inspector-header__inline-list">
+        <li className='inspector-header__list-item'>
+          <a onClick={sinon.stub()}>
+            Get started
+          </a>
+        </li>
+        <li className="inspector-header__list-item">
+          <a onClick={sinon.stub()}>
+            Charm details
+          </a>
+        </li>
+      </ul>);
+  });
+
+  it('shows the Get Started link disabled if the charm is missind it', () => {
+    const output = renderComponent({
+      hasGetStarted: false,
+      showLinks: true
+    });
+    assert.equal(
+      output.props.children[3].props.children[0].props.className,
+      'inspector-header__list-item inspector-header__list-item--disabled');
+  });
+
+  it('clicking the Get Started link does nothing if it is disabled', () => {
+    const changeState = sinon.stub();
+    const output = renderComponent({
+      changeState,
+      charmId: 'charmid',
+      hasGetStarted: false,
+      showLinks: true
+    });
+    output.props.children[3].props.children[0].props.children.props.onClick({
+      preventDefault: sinon.stub(),
+      stopPropagation: sinon.stub()
+    });
+    assert.equal(changeState.callCount, 0);
+  });
+
+  it('clicking the Get Started link changes state to show the get started', () => {
+    const changeState = sinon.stub();
+    const charmId = 'cs:~hatch/ghost';
+    const output = renderComponent({
+      changeState,
+      charmId,
+      hasGetStarted: true,
+      showLinks: true
+    });
+    output.props.children[3].props.children[0].props.children.props.onClick({
+      preventDefault: sinon.stub(),
+      stopPropagation: sinon.stub()
+    });
+    assert.deepEqual(changeState.args[0], [{
+      postDeploymentPanel: {
+        show: true,
+        entityId: charmId
+      }
+    }]);
+  });
+
+  it('clicking the Charm Details link changes state to show the details', () => {
+    const changeState = sinon.stub();
+    const entityPath = 'u/hatch/ghost';
+    const output = renderComponent({
+      changeState,
+      entityPath,
+      hasGetStarted: true,
+      showLinks: true
+    });
+    output.props.children[3].props.children[1].props.children.props.onClick({
+      preventDefault: sinon.stub(),
+      stopPropagation: sinon.stub()
+    });
+    assert.deepEqual(changeState.args[0], [{
+      store: entityPath
+    }]);
   });
 });
