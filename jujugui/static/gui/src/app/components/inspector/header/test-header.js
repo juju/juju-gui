deb911af81cc8d719e97961cebbbeb00c8f706d9
Jeff Pihach <jeff.pihach@canonical.com>
2018-01-05 10:23:50 -0600

    Consolidate the inspector header renderer tests.

--- jujugui/static/gui/src/app/components/inspector/header/test-header.js
+++ jujugui/static/gui/src/app/components/inspector/header/test-header.js
@@ -7,49 +7,41 @@ const InspectorHeader = require('./header');
 
 const jsTestUtils = require('../../../utils/component-test-utils');
 
-describe('InspectorHeader', function() {
+describe('InspectorHeader', () => {
 
-  it('displays the provided title', function() {
-    const output = jsTestUtils.shallowRender(
+  function renderComponent(options = {}) {
+    return jsTestUtils.shallowRender(
       <InspectorHeader
-        backCallback={sinon.stub()}
-        title="Juju GUI" />);
+        backCallback={options.backCallback || sinon.stub()}
+        title="Juju GUI"
+        type={options.type}
+        icon={options.icon} />);
+  }
+
+  it('displays the provided title', () => {
+    const output = renderComponent();
     assert.equal(output.props.children[1].props.children, 'Juju GUI');
   });
 
-  it('adds a class based on the provided type', function() {
-    const output = jsTestUtils.shallowRender(
-      <InspectorHeader
-        backCallback={sinon.stub()}
-        title="Juju GUI"
-        type="error" />);
+  it('adds a class based on the provided type', () => {
+    const output = renderComponent({type: 'error'});
     assert.equal(output.props.className,
       'inspector-header inspector-header--type-error');
   });
 
-  it('does not add a type class if it is not provided', function() {
-    const output = jsTestUtils.shallowRender(
-      <InspectorHeader
-        backCallback={sinon.stub()}
-        title="Juju GUI" />);
+  it('does not add a type class if it is not provided', () => {
+    const output = renderComponent();
     assert.equal(output.props.className, 'inspector-header');
   });
 
-  it('displays the provided icon', function() {
-    const output = jsTestUtils.shallowRender(
-      <InspectorHeader
-        backCallback={sinon.stub()}
-        icon="icon.svg"
-        title="Juju GUI" />);
+  it('displays the provided icon', () => {
+    const output = renderComponent({icon: 'icon.svg'});
     assert.equal(output.props.children[2].props.children.props.src, 'icon.svg');
   });
 
-  it('calls supplied callable when clicked', function() {
-    var callbackStub = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <InspectorHeader
-        backCallback={callbackStub}
-        title="Juju GUI" />);
+  it('calls supplied callable when clicked', () => {
+    const callbackStub = sinon.stub();
+    const output = renderComponent({backCallback: callbackStub});
     output.props.onClick();
     assert.equal(callbackStub.callCount, 1);
   });
