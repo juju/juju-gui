c0ffbbf49c733874a1a86584f51faefdf20e38d6
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-07 16:18:18 +0100

    Add an store expert card.

---
+++ jujugui/static/gui/src/app/components/expert-store-card/expert-store-card.js
@@ -0,0 +1,42 @@
+/* Copyright (C) 2018 Canonical Ltd. */
+'use strict';
+
+const PropTypes = require('prop-types');
+const React = require('react');
+
+const EXPERTS = require('../expert-card/experts');
+const ExpertCard = require('../expert-card/expert-card');
+
+class ExpertStoreCard extends React.Component {
+  render() {
+    const expert = EXPERTS[this.props.expert];
+    if (!expert) {
+      return null;
+    }
+    return (
+      <ExpertCard
+        expert={expert}
+        classes={this.props.classes}
+        staticURL={this.props.staticURL}>
+        <div className="expert-store-card">
+          <p className="expert-store-card__description">
+            {expert.storeDescription}
+          </p>
+          <a className="button--inline-neutral"
+            href="http://jujucharms.com/experts/"
+            target="_blank">
+            Learn about Big Data expertise&hellip;
+          </a>
+        </div>
+      </ExpertCard>
+    );
+  }
+};
+
+ExpertStoreCard.propTypes = {
+  classes: PropTypes.array.isRequired,
+  expert: PropTypes.string.isRequired,
+  staticURL: PropTypes.string
+};
+
+module.exports = ExpertStoreCard;
