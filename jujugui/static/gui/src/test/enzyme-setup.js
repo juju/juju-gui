a95b2a4584d174bffb93dc9209504f6da4b74ef2
Jeff Pihach <hatched@users.noreply.github.com>
2018-03-09 10:19:13 +0100

    Update enzyme-setup.js
    Removed commented code

--- jujugui/static/gui/src/test/enzyme-setup.js
+++ jujugui/static/gui/src/test/enzyme-setup.js
@@ -2,9 +2,5 @@
 
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
-// const chai = require('chai');
-// const chaiEnzyme = require('chai-enzyme');
-
 
 Enzyme.configure({ adapter: new Adapter() });
-// chai.use(chaiEnzyme());
