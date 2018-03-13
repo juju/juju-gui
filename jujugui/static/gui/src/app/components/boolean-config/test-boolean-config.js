6f4db1c66a33b48ce231c53edd49870da4b956be
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-13 11:59:59 +1100

    Update boolean config tests to use enzyme.

--- jujugui/static/gui/src/app/components/boolean-config/test-boolean-config.js
+++ jujugui/static/gui/src/app/components/boolean-config/test-boolean-config.js
@@ -2,25 +2,32 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const BooleanConfig = require('./boolean-config');
 
-const jsTestUtils = require('../../utils/component-test-utils');
-
 describe('BooleanConfig', function() {
+  let option;
 
-  it('renders a checked input based on config prop', function() {
-    const option = {
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <BooleanConfig
+      config={options.config === undefined ? true : options.config}
+      disabled={options.disabled === undefined ? false : options.disabled}
+      label={options.label || 'Test'}
+      onChange={options.onChange || null}
+      option={options.option || option} />
+  );
+
+  beforeEach(() => {
+    option = {
       key: 'testcheck',
       description: 'it is a test config option'
     };
-    const output = jsTestUtils.shallowRender(
-      <BooleanConfig
-        config={true}
-        label="Test"
-        option={option} />
-    );
-    const input = output.props.children[0].props.children[1].props.children[0];
+  });
+
+  it('renders a checked input based on config prop', function() {
+    const wrapper = renderComponent();
+    const input = wrapper.find('.boolean-config--input');
     const expected = (
       <div className="boolean-config">
         <div className="boolean-config--toggle-container">
@@ -31,8 +38,8 @@ describe('BooleanConfig', function() {
               defaultChecked={true}
               disabled={false}
               id={option.key}
-              onChange={input.props.onChange}
-              onClick={input.props.onClick}
+              onChange={input.prop('onChange')}
+              onClick={input.prop('onClick')}
               type="checkbox" />
             <label
               className="boolean-config--label"
@@ -46,138 +53,28 @@ describe('BooleanConfig', function() {
         </div>
       </div>
     );
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('renders an unchecked input based on config prop', function() {
-    const option = {
-      key: 'testcheck',
-      description: 'it is a test config option'
-    };
-    const output = jsTestUtils.shallowRender(
-      <BooleanConfig
-        config={false}
-        label="Test"
-        option={option} />
-    );
-    const input = output.props.children[0].props.children[1].props.children[0];
-    expect(output).toEqualJSX(
-      <div className="boolean-config">
-        <div className="boolean-config--toggle-container">
-          <div className="boolean-config--title">Test</div>
-          <div className="boolean-config--toggle">
-            <input
-              className="boolean-config--input"
-              defaultChecked={false}
-              disabled={false}
-              id={option.key}
-              onChange={input.props.onChange}
-              onClick={input.props.onClick}
-              type="checkbox" />
-            <label
-              className="boolean-config--label"
-              htmlFor={option.key}>
-              <div className="boolean-config--handle"></div>
-            </label>
-          </div>
-        </div>
-        <div className="boolean-config--description"
-          dangerouslySetInnerHTML={{__html: option.description}}>
-        </div>
-      </div>);
+    const wrapper = renderComponent({ config: false });
+    assert.equal(wrapper.find('.boolean-config--input').prop('defaultChecked'), false);
   });
 
   it('supports string boolean config props (true)', function() {
-    const option = {
-      key: 'testcheck',
-      description: 'it is a test config option'
-    };
-    const output = jsTestUtils.shallowRender(
-      <BooleanConfig
-        config="True"
-        label="Test"
-        option={option} />
-    );
-    const input = output.props.children[0].props.children[1].props.children[0];
-    expect(output).toEqualJSX(
-      <div className="boolean-config">
-        <div className="boolean-config--toggle-container">
-          <div className="boolean-config--title">Test</div>
-          <div className="boolean-config--toggle">
-            <input
-              className="boolean-config--input"
-              defaultChecked={true}
-              disabled={false}
-              id={option.key}
-              onChange={input.props.onChange}
-              onClick={input.props.onClick}
-              type="checkbox" />
-            <label
-              className="boolean-config--label boolean-config--label-changed"
-              htmlFor={option.key}>
-              <div className="boolean-config--handle"></div>
-            </label>
-          </div>
-        </div>
-        <div className="boolean-config--description"
-          dangerouslySetInnerHTML={{__html: option.description}}>
-        </div>
-      </div>);
+    const wrapper = renderComponent({ config: 'True' });
+    assert.equal(wrapper.find('.boolean-config--input').prop('defaultChecked'), true);
   });
 
   it('supports string boolean config props (false)', function() {
-    const option = {
-      key: 'testcheck',
-      description: 'it is a test config option'
-    };
-    const output = jsTestUtils.shallowRender(
-      <BooleanConfig
-        config="False"
-        label="Test"
-        option={option} />
-    );
-    const input = output.props.children[0].props.children[1].props.children[0];
-    expect(output).toEqualJSX(
-      <div className="boolean-config">
-        <div className="boolean-config--toggle-container">
-          <div className="boolean-config--title">Test</div>
-          <div className="boolean-config--toggle">
-            <input
-              className="boolean-config--input"
-              defaultChecked={false}
-              disabled={false}
-              id={option.key}
-              onChange={input.props.onChange}
-              onClick={input.props.onClick}
-              type="checkbox" />
-            <label
-              className="boolean-config--label boolean-config--label-changed"
-              htmlFor={option.key}>
-              <div className="boolean-config--handle"></div>
-            </label>
-          </div>
-        </div>
-        <div className="boolean-config--description"
-          dangerouslySetInnerHTML={{__html: option.description}}>
-        </div>
-      </div>);
+    const wrapper = renderComponent({ config: 'False' });
+    assert.equal(wrapper.find('.boolean-config--input').prop('defaultChecked'), false);
   });
 
   it('can call an onChange method if supplied', function() {
     const onChange = sinon.stub();
-    const option = {
-      key: 'testcheck',
-      description: 'it is a test config option'
-    };
-    const output = jsTestUtils.shallowRender(
-      <BooleanConfig
-        config="False"
-        label="Test"
-        onChange={onChange}
-        option={option} />
-    );
-    const input = output.props.children[0].props.children[1].props.children[0];
-    input.props.onChange({
+    const wrapper = renderComponent({ onChange });
+    wrapper.find('.boolean-config--input').simulate('change', {
       target: {
         checked: true
       }
@@ -186,27 +83,7 @@ describe('BooleanConfig', function() {
   });
 
   it('can be disabled', function() {
-    const option = {
-      key: 'testcheck',
-      description: 'it is a test config option'
-    };
-    const output = jsTestUtils.shallowRender(
-      <BooleanConfig
-        config={true}
-        disabled={true}
-        label="Test"
-        option={option} />
-    );
-    const input = output.props.children[0].props.children[1].props.children[0];
-    const expected = (
-      <input
-        className="boolean-config--input"
-        defaultChecked={true}
-        disabled={true}
-        id={option.key}
-        onChange={input.props.onChange}
-        onClick={input.props.onClick}
-        type="checkbox" />);
-    expect(input).toEqualJSX(expected);
+    const wrapper = renderComponent({ disabled: true });
+    assert.equal(wrapper.find('.boolean-config--input').prop('disabled'), true);
   });
 });
