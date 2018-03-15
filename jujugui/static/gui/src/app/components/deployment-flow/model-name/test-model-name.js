d58998a7c8bd4fe26e6d6756523eb53f698845fe
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-16 09:39:24 +1100

    Update model name tests to use enzyme.

--- jujugui/static/gui/src/app/components/deployment-flow/model-name/test-model-name.js
+++ jujugui/static/gui/src/app/components/deployment-flow/model-name/test-model-name.js
@@ -2,35 +2,36 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const DeploymentModelName = require('./model-name');
 const GenericInput = require('../../generic-input/generic-input');
 
-const jsTestUtils = require('../../../utils/component-test-utils');
-
 describe('DeploymentModelName', () => {
   let acl;
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <DeploymentModelName
+      acl={options.acl || acl}
+      ddEntity={options.ddEntity}
+      modelName={options.modelName || 'mymodel'}
+      setModelName={options.setModelName || sinon.stub()} />
+  );
 
   beforeEach(() => {
     acl = {isReadOnly: sinon.stub().returns(false)};
   });
 
   it('can render', function() {
-    const renderer = jsTestUtils.shallowRender(
-      <DeploymentModelName
-        acl={acl}
-        ddEntity={null}
-        modelName="mymodel"
-        setModelName={sinon.stub()} />, true);
-    const output = renderer.getRenderOutput();
+    const wrapper = renderComponent();
     const expected = (
       <div className="six-col no-margin-bottom">
         <GenericInput
           disabled={false}
           key="modelName"
           label="Model name"
-          onBlur={sinon.stub()}
-          onChange={sinon.stub()}
+          onBlur={wrapper.find('GenericInput').prop('onBlur')}
+          onChange={wrapper.find('GenericInput').prop('onChange')}
           ref="modelName"
           required={true}
           validate={[{
@@ -44,76 +45,35 @@ describe('DeploymentModelName', () => {
           }]}
           value="mymodel" />
       </div>);
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('can render when read only', function() {
     acl.isReadOnly = sinon.stub().returns(true);
-    const renderer = jsTestUtils.shallowRender(
-      <DeploymentModelName
-        acl={acl}
-        ddEntity={null}
-        modelName="mymodel"
-        setModelName={sinon.stub()} />, true);
-    const output = renderer.getRenderOutput();
-    const expected = (
-      <div className="six-col no-margin-bottom">
-        <GenericInput
-          disabled={true}
-          key="modelName"
-          label="Model name"
-          onBlur={sinon.stub()}
-          onChange={sinon.stub()}
-          ref="modelName"
-          required={true}
-          validate={[{
-            regex: /\S+/,
-            error: 'This field is required.'
-          }, {
-            regex: /^([a-z0-9]([a-z0-9-]*[a-z0-9])?)?$/,
-            error: 'This field must only contain lowercase ' +
-              'letters, numbers, and hyphens. It must not start or ' +
-              'end with a hyphen.'
-          }]}
-          value="mymodel" />
-      </div>);
-    expect(output).toEqualJSX(expected);
+    const wrapper = renderComponent();
+    assert.equal(wrapper.find('GenericInput').prop('disabled'), true);
   });
 
   it('can derive the model name from the DD entity name', () => {
-    const renderer = jsTestUtils.shallowRender(
-      <DeploymentModelName
-        acl={acl}
-        ddEntity={{get: sinon.stub().returns('snazzy-bundle')}}
-        modelName="mymodel"
-        setModelName={sinon.stub()} />, true);
-    const output = renderer.getRenderOutput();
-    assert.equal(output.props.children.props.value, 'snazzy-bundle');
+    const wrapper = renderComponent({
+      ddEntity: {get: sinon.stub().returns('snazzy-bundle')}
+    });
+    assert.equal(wrapper.find('GenericInput').prop('value'), 'snazzy-bundle');
   });
 
   it('focuses on the model name field when loaded', () => {
-    const renderer = jsTestUtils.shallowRender(
-      <DeploymentModelName
-        acl={acl}
-        ddEntity={null}
-        modelName="mymodel"
-        setModelName={sinon.stub()} />, true);
-    const instance = renderer.getMountedInstance();
-    instance.refs = {modelName: {focus: sinon.stub()}};
+    const wrapper = renderComponent();
+    const instance = wrapper.instance();
+    const focus = sinon.stub();
+    instance.refs = {modelName: {focus}};
     instance.componentDidMount();
-    assert.equal(instance.refs.modelName.focus.callCount, 1);
+    assert.equal(focus.callCount, 1);
   });
 
   it('can update the model name on blur', () => {
     const setModelName = sinon.stub();
-    const renderer = jsTestUtils.shallowRender(
-      <DeploymentModelName
-        acl={acl}
-        ddEntity={null}
-        modelName="mymodel"
-        setModelName={setModelName} />, true);
-    const output = renderer.getRenderOutput();
-    output.props.children.props.onBlur({
+    const wrapper = renderComponent({ setModelName });
+    wrapper.find('GenericInput').simulate('blur', {
       currentTarget: {
         value: 'snazzy-bundle'
       }
@@ -124,28 +84,16 @@ describe('DeploymentModelName', () => {
 
   it('can update the model name on change', () => {
     const setModelName = sinon.stub();
-    const renderer = jsTestUtils.shallowRender(
-      <DeploymentModelName
-        acl={acl}
-        ddEntity={null}
-        modelName="mymodel"
-        setModelName={setModelName} />, true);
-    const output = renderer.getRenderOutput();
-    output.props.children.props.onChange('snazzy-bundle');
+    const wrapper = renderComponent({ setModelName });
+    wrapper.find('GenericInput').simulate('change', 'snazzy-bundle');
     assert.equal(setModelName.callCount, 1);
     assert.equal(setModelName.args[0][0], 'snazzy-bundle');
   });
 
   it('does not update the model name if there is no value', () => {
     const setModelName = sinon.stub();
-    const renderer = jsTestUtils.shallowRender(
-      <DeploymentModelName
-        acl={acl}
-        ddEntity={null}
-        modelName="mymodel"
-        setModelName={setModelName} />, true);
-    const output = renderer.getRenderOutput();
-    output.props.children.props.onBlur({
+    const wrapper = renderComponent({ setModelName });
+    wrapper.find('GenericInput').simulate('blur', {
       currentTarget: {
         value: ''
       }
