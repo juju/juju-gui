db5b4cb4b6708f19ca54d83f879bb9bfa68c7c1e
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-15 14:50:35 +1100

    Update cloud tests to use enzyme.

--- jujugui/static/gui/src/app/components/deployment-flow/cloud/test-cloud.js
+++ jujugui/static/gui/src/app/components/deployment-flow/cloud/test-cloud.js
@@ -2,15 +2,25 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const DeploymentCloud = require('./cloud');
 const Spinner = require('../../spinner/spinner');
 const SvgIcon = require('../../svg-icon/svg-icon');
 
-const jsTestUtils = require('../../../utils/component-test-utils');
-
 describe('DeploymentCloud', function() {
   let acl, cloudList, getCloudProviderDetails;
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <DeploymentCloud
+      acl={options.acl || acl}
+      addNotification={options.addNotification || sinon.stub()}
+      cloud={options.cloud || null}
+      controllerIsReady={options.controllerIsReady || sinon.stub().returns(true)}
+      getCloudProviderDetails={options.getCloudProviderDetails || getCloudProviderDetails}
+      listClouds={options.listClouds || sinon.stub().callsArgWith(0, null, cloudList)}
+      setCloud={options.setCloud || sinon.stub()} />
+  );
 
   beforeEach(() => {
     acl = {isReadOnly: sinon.stub().returns(false)};
@@ -57,23 +67,14 @@ describe('DeploymentCloud', function() {
   });
 
   it('can render', function() {
-    var renderer = jsTestUtils.shallowRender(
-      <DeploymentCloud
-        acl={acl}
-        addNotification={sinon.stub()}
-        cloud={null}
-        controllerIsReady={sinon.stub().returns(true)}
-        getCloudProviderDetails={getCloudProviderDetails}
-        listClouds={sinon.stub().callsArgWith(0, null, cloudList)}
-        setCloud={sinon.stub()} />, true);
-    var output = renderer.getRenderOutput();
-    var options = output.props.children[0].props.children;
+    const wrapper = renderComponent();
+    const options = wrapper.find('.deployment-cloud__cloud');
     var expected = (
       <div>
         <ul className="deployment-cloud__list">
           <li className="deployment-cloud__cloud four-col"
             key="google"
-            onClick={options[0].props.onClick}
+            onClick={options.at(0).prop('onClick')}
             role="button"
             tabIndex="0">
             <span className="deployment-cloud__cloud-logo">
@@ -85,7 +86,7 @@ describe('DeploymentCloud', function() {
           </li>
           <li className="deployment-cloud__cloud four-col"
             key="azure"
-            onClick={options[1].props.onClick}
+            onClick={options.at(1).prop('onClick')}
             role="button"
             tabIndex="0">
             <span className="deployment-cloud__cloud-logo">
@@ -97,7 +98,7 @@ describe('DeploymentCloud', function() {
           </li>
           <li className="deployment-cloud__cloud four-col last-col"
             key="aws"
-            onClick={options[2].props.onClick}
+            onClick={options.at(2).prop('onClick')}
             role="button"
             tabIndex="0">
             <span className="deployment-cloud__cloud-logo">
@@ -110,68 +111,43 @@ describe('DeploymentCloud', function() {
         </ul>
         {undefined}
       </div>);
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('can display the loading state', function() {
-    var renderer = jsTestUtils.shallowRender(
-      <DeploymentCloud
-        acl={acl}
-        addNotification={sinon.stub()}
-        cloud={null}
-        controllerIsReady={sinon.stub().returns(true)}
-        getCloudProviderDetails={getCloudProviderDetails}
-        listClouds={sinon.stub()}
-        setCloud={sinon.stub()} />, true);
-    var output = renderer.getRenderOutput();
+    const wrapper = renderComponent({ listClouds: sinon.stub() });
     var expected = (
-      <div>
-        <div className="deployment-cloud__loading">
-          <Spinner />
-        </div>
-        {undefined}
+      <div className="deployment-cloud__loading">
+        <Spinner />
       </div>);
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper.find('.deployment-cloud__loading'), expected);
   });
 
   it('can render with a chosen cloud', function() {
-    var renderer = jsTestUtils.shallowRender(
-      <DeploymentCloud
-        acl={acl}
-        addNotification={sinon.stub()}
-        cloud={{name: 'google', cloudType: 'gce'}}
-        controllerIsReady={sinon.stub().returns(true)}
-        getCloudProviderDetails={getCloudProviderDetails}
-        listClouds={sinon.stub().callsArgWith(0, null, {})}
-        setCloud={sinon.stub()} />, true);
-    var output = renderer.getRenderOutput();
+    const wrapper = renderComponent({
+      cloud: {name: 'google', cloudType: 'gce'},
+      listClouds: sinon.stub().callsArgWith(0, null, {})
+    });
     var expected = (
-      <div>
-        {undefined}
-        <div className="deployment-cloud__chosen">
-          <SvgIcon
-            height={33}
-            name="google"
-            width={256} />
-        </div>
+      <div className="deployment-cloud__chosen">
+        <SvgIcon
+          height={33}
+          name="google"
+          width={256} />
       </div>);
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper.find('.deployment-cloud__chosen'), expected);
+    assert.equal(wrapper.find('.deployment-cloud__list').length, 0);
   });
 
-  it('can select a cloud if there is only one', function() {
+  it('automatically selects a cloud if there is only one', function() {
     cloudList = {
       'google': {name: 'google'}
     };
     const setCloud = sinon.stub();
-    jsTestUtils.shallowRender(
-      <DeploymentCloud
-        acl={acl}
-        addNotification={sinon.stub()}
-        cloud={null}
-        controllerIsReady={sinon.stub().returns(true)}
-        getCloudProviderDetails={getCloudProviderDetails}
-        listClouds={sinon.stub().callsArgWith(0, null, cloudList)}
-        setCloud={setCloud} />);
+    renderComponent({
+      listClouds: sinon.stub().callsArgWith(0, null, cloudList),
+      setCloud
+    });
     assert.equal(setCloud.callCount, 1);
     assert.deepEqual(setCloud.args[0][0], {
       name: 'google'
@@ -180,17 +156,10 @@ describe('DeploymentCloud', function() {
 
   it('can select a cloud', function() {
     var setCloud = sinon.stub();
-    var renderer = jsTestUtils.shallowRender(
-      <DeploymentCloud
-        acl={acl}
-        addNotification={sinon.stub()}
-        cloud={null}
-        controllerIsReady={sinon.stub().returns(true)}
-        getCloudProviderDetails={getCloudProviderDetails}
-        listClouds={sinon.stub().callsArgWith(0, null, cloudList)}
-        setCloud={setCloud} />, true);
-    var output = renderer.getRenderOutput();
-    output.props.children[0].props.children[0].props.onClick();
+    const wrapper = renderComponent({
+      setCloud
+    });
+    wrapper.find('.deployment-cloud__cloud').at(0).simulate('click');
     assert.equal(setCloud.callCount, 1);
     assert.deepEqual(setCloud.args[0][0], {
       name: 'google',
@@ -200,15 +169,10 @@ describe('DeploymentCloud', function() {
 
   it('can handle errors getting clouds', function() {
     const addNotification = sinon.stub();
-    jsTestUtils.shallowRender(
-      <DeploymentCloud
-        acl={acl}
-        addNotification={addNotification}
-        cloud={null}
-        controllerIsReady={sinon.stub().returns(true)}
-        getCloudProviderDetails={getCloudProviderDetails}
-        listClouds={sinon.stub().callsArgWith(0, 'Uh oh!', null)}
-        setCloud={sinon.stub()} />);
+    renderComponent({
+      addNotification,
+      listClouds: sinon.stub().callsArgWith(0, 'Uh oh!', null)
+    });
     assert.equal(addNotification.callCount, 1);
     assert.deepEqual(addNotification.args[0][0], {
       title: 'unable to list clouds',
