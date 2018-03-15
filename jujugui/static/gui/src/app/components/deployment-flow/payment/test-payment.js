ba2b041e2c39e9500ed6da5ad73cfa3233918f41
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-16 10:02:34 +1100

    Update deployment payment tests to use enzyme.

--- jujugui/static/gui/src/app/components/deployment-flow/payment/test-payment.js
+++ jujugui/static/gui/src/app/components/deployment-flow/payment/test-payment.js
@@ -2,16 +2,30 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const PaymentMethodCard = require('../../payment/methods/card/card');
 const DeploymentPayment = require('./payment');
 const CreatePaymentUser = require('../../create-payment-user/create-payment-user');
 const Spinner = require('../../spinner/spinner');
 
-const jsTestUtils = require('../../../utils/component-test-utils');
-
 describe('DeploymentPayment', function() {
   let acl, getCountries, getUser, user;
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <DeploymentPayment
+      acl={options.acl || acl}
+      addNotification={options.addNotification || sinon.stub()}
+      createCardElement={options.createCardElement || sinon.stub()}
+      createToken={options.createToken || sinon.stub()}
+      createUser={options.createUser || sinon.stub()}
+      getCountries={options.getCountries || getCountries}
+      getUser={options.getUser || getUser}
+      paymentUser={options.paymentUser}
+      setPaymentUser={options.setPaymentUser || sinon.stub()}
+      username={options.username || 'spinach'}
+      validateForm={options.validateForm || sinon.stub()} />
+  );
 
   beforeEach(() => {
     acl = {isReadOnly: sinon.stub().returns(false)};
@@ -29,88 +43,42 @@ describe('DeploymentPayment', function() {
   });
 
   it('can display a loading spinner', function() {
-    const renderer = jsTestUtils.shallowRender(
-      <DeploymentPayment
-        acl={acl}
-        addNotification={sinon.stub()}
-        createCardElement={sinon.stub()}
-        createToken={sinon.stub()}
-        createUser={sinon.stub()}
-        getCountries={getCountries}
-        getUser={sinon.stub()}
-        setPaymentUser={sinon.stub()}
-        username="spinach"
-        validateForm={sinon.stub()} />, true);
-    const output = renderer.getRenderOutput();
+    const wrapper = renderComponent({ getUser: sinon.stub() });
     const expected = (
       <div className="deployment-payment">
         <Spinner />
       </div>);
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('can store the user details', function() {
     const setPaymentUser = sinon.stub();
-    jsTestUtils.shallowRender(
-      <DeploymentPayment
-        acl={acl}
-        addNotification={sinon.stub()}
-        createCardElement={sinon.stub()}
-        createToken={sinon.stub()}
-        createUser={sinon.stub()}
-        getCountries={getCountries}
-        getUser={getUser}
-        setPaymentUser={setPaymentUser}
-        username="spinach"
-        validateForm={sinon.stub()} />);
+    renderComponent({ setPaymentUser });
     assert.equal(setPaymentUser.callCount, 1);
     assert.deepEqual(setPaymentUser.args[0][0], user);
   });
 
   it('can display payment methods', function() {
-    const renderer = jsTestUtils.shallowRender(
-      <DeploymentPayment
-        acl={acl}
-        addNotification={sinon.stub()}
-        createCardElement={sinon.stub()}
-        createToken={sinon.stub()}
-        createUser={sinon.stub()}
-        getCountries={getCountries}
-        getUser={getUser}
-        paymentUser={user}
-        setPaymentUser={sinon.stub()}
-        username="spinach"
-        validateForm={sinon.stub()} />, true);
-    const output = renderer.getRenderOutput();
+    const wrapper = renderComponent({ paymentUser: user });
     const expected = (
-      <div className="deployment-payment">
-        <ul className="deployment-payment__methods twelve-col">
-          {[<li className="deployment-payment__method"
-            key="Company0">
-            <PaymentMethodCard
-              card={{name: 'Company'}} />
-          </li>]}
-        </ul>
-      </div>);
-    expect(output).toEqualJSX(expected);
+      <ul className="deployment-payment__methods twelve-col">
+        {[<li className="deployment-payment__method"
+          key="Company0">
+          <PaymentMethodCard
+            card={{name: 'Company'}} />
+        </li>]}
+      </ul>);
+    assert.compareJSX(wrapper.find('.deployment-payment__methods'), expected);
   });
 
   it('can display an error when getting users', function() {
     getUser = sinon.stub().callsArgWith(1, 'failed', null);
     const addNotification = sinon.stub();
     const setPaymentUser = sinon.stub();
-    jsTestUtils.shallowRender(
-      <DeploymentPayment
-        acl={acl}
-        addNotification={addNotification}
-        createCardElement={sinon.stub()}
-        createToken={sinon.stub()}
-        createUser={sinon.stub()}
-        getCountries={getCountries}
-        getUser={getUser}
-        setPaymentUser={setPaymentUser}
-        username="spinach"
-        validateForm={sinon.stub()} />);
+    renderComponent({
+      addNotification,
+      setPaymentUser
+    });
     assert.equal(addNotification.callCount, 1);
     assert.deepEqual(addNotification.args[0][0], {
       title: 'Could not load user info',
@@ -120,58 +88,28 @@ describe('DeploymentPayment', function() {
   });
 
   it('can display a new user form', function() {
-    const addNotification = sinon.stub();
-    const validateForm = sinon.stub();
-    const createUser = sinon.stub();
-    const createToken = sinon.stub();
-    const createCardElement = sinon.stub();
-    const renderer = jsTestUtils.shallowRender(
-      <DeploymentPayment
-        acl={acl}
-        addNotification={addNotification}
-        createCardElement={createCardElement}
-        createToken={createToken}
-        createUser={createUser}
-        getCountries={getCountries}
-        getUser={getUser}
-        paymentUser={null}
-        setPaymentUser={sinon.stub()}
-        username="spinach"
-        validateForm={validateForm} />, true);
-    const instance = renderer.getMountedInstance();
-    const output = renderer.getRenderOutput();
+    const wrapper = renderComponent();
     const expected = (
       <div className="deployment-payment">
         <CreatePaymentUser
           acl={acl}
-          addNotification={addNotification}
-          createCardElement={createCardElement}
-          createToken={createToken}
-          createUser={createUser}
+          addNotification={sinon.stub()}
+          createCardElement={sinon.stub()}
+          createToken={sinon.stub()}
+          createUser={sinon.stub()}
           getCountries={getCountries}
-          onUserCreated={instance._getUser}
+          onUserCreated={wrapper.find('CreatePaymentUser').prop('onUserCreated')}
           username="spinach"
-          validateForm={validateForm} />
+          validateForm={sinon.stub()} />
       </div>);
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('can abort requests when unmounting', function() {
     const abort = sinon.stub();
     getUser = sinon.stub().returns({abort: abort});
-    const component = jsTestUtils.shallowRender(
-      <DeploymentPayment
-        acl={acl}
-        addNotification={sinon.stub()}
-        createCardElement={sinon.stub()}
-        createToken={sinon.stub()}
-        createUser={sinon.stub()}
-        getCountries={getCountries}
-        getUser={getUser}
-        setPaymentUser={sinon.stub()}
-        username="spinach"
-        validateForm={sinon.stub()} />, true);
-    component.unmount();
+    const wrapper = renderComponent({ getUser });
+    wrapper.unmount();
     assert.equal(abort.callCount, 1);
   });
 });
