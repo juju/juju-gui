7b3bd67803e78209d773b93737f94fd2b7a075af
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-16 16:06:43 +1100

    Update config option tests to use enzyme.

--- jujugui/static/gui/src/app/components/entity-details/content/config-option/test-config-option.js
+++ jujugui/static/gui/src/app/components/entity-details/content/config-option/test-config-option.js
@@ -2,12 +2,16 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const EntityContentConfigOption = require('./config-option');
 
-const jsTestUtils = require('../../../../utils/component-test-utils');
-
 describe('EntityContentConfigOption', function() {
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <EntityContentConfigOption
+      option={options.option} />
+  );
 
   it('can render with a default value', function() {
     var option = {
@@ -16,10 +20,8 @@ describe('EntityContentConfigOption', function() {
       type: 'string',
       default: 'abc123'
     };
-    var output = jsTestUtils.shallowRender(
-      <EntityContentConfigOption
-        option={option} />);
-    assert.deepEqual(output,
+    const wrapper = renderComponent({ option });
+    const expected = (
       <div className="entity-content__config-option">
         <dt className="entity-content__config-name"
           id="charm-config-password">
@@ -38,6 +40,7 @@ describe('EntityContentConfigOption', function() {
             abc123
         </dd>
       </div>);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('can render without a default value', function() {
@@ -46,25 +49,7 @@ describe('EntityContentConfigOption', function() {
       description: 'Required password',
       type: 'string'
     };
-    var output = jsTestUtils.shallowRender(
-      <EntityContentConfigOption
-        option={option} />);
-    assert.deepEqual(output,
-      <div className="entity-content__config-option">
-        <dt className="entity-content__config-name"
-          id="charm-config-password">
-            password
-        </dt>
-        <dd className="entity-content__config-description">
-          <p>
-            <span className="entity-content__config-type">
-                ({'string'})
-            </span>
-            {' '}
-              Required password
-          </p>
-        </dd>
-        {undefined}
-      </div>);
+    const wrapper = renderComponent({ option });
+    assert.equal(wrapper.find('.entity-content__config-default').length, 0);
   });
 });
