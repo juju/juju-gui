781c32a6a32cc8defeac81841e0d4adaf2ce7c6d
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-16 16:43:53 +1100

    Update entity files tests to use enzyme.

--- jujugui/static/gui/src/app/components/entity-details/content/files/test-files.js
+++ jujugui/static/gui/src/app/components/entity-details/content/files/test-files.js
@@ -2,14 +2,21 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const EntityFiles = require('./files');
 
 const jsTestUtils = require('../../../../utils/component-test-utils');
-const testUtils = require('react-dom/test-utils');
 
 describe('EntityFiles', function() {
   var mockEntity;
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <EntityFiles
+      apiUrl={options.apiUrl || 'http://example.com/'}
+      entityModel={options.entityModel}
+      pluralize={sinon.stub().returns('files')} />
+  );
 
   beforeEach(function() {
     mockEntity = jsTestUtils.makeEntity(
@@ -22,13 +29,10 @@ describe('EntityFiles', function() {
 
   it('renders a list of files', function() {
     var apiUrl = 'https://api.jujucharms.com/charmstore/v4';
-    var renderer = jsTestUtils.shallowRender(
-      <EntityFiles
-        apiUrl={apiUrl}
-        entityModel={mockEntity}
-        pluralize={sinon.stub().returns('files')} />, true);
-    var output = renderer.getRenderOutput();
-    var instance = renderer.getMountedInstance();
+    const wrapper = renderComponent({
+      apiUrl,
+      entityModel: mockEntity
+    });
     var archiveUrl = `${apiUrl}/django/archive`;
     var fileItems = [
       <li className="p-list-tree__item" key="foo.zip">
@@ -44,7 +48,7 @@ describe('EntityFiles', function() {
         <button aria-controls="/bar" aria-expanded="false"
           className="p-list-tree__toggle"
           id="/bar-toggle"
-          onClick={instance._onDirectoryClick}
+          onClick={wrapper.find('button').prop('onClick')}
           role="tab">/bar</button>
         <ul aria-hidden="true" aria-labelledby="/bar-toggle"
           className="p-list-tree" id="/bar" role="tabpanel">
@@ -72,7 +76,7 @@ describe('EntityFiles', function() {
           {fileItems}
         </ul>
         <ul className="section__list">
-          <li className="section__list-item">
+          <li className="section__list-item entity-files__code-link">
             <a className="button--inline-neutral entity-files__link"
               href="https://code.launchpad.net/django/code"
               ref="codeLink"
@@ -90,29 +94,23 @@ describe('EntityFiles', function() {
         </ul>
       </div>
     );
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('renders for an empty/null list of files', function() {
     mockEntity.set('files', []);
-    var output = testUtils.renderIntoDocument(
-      <EntityFiles
-        apiUrl="http://example.com/"
-        entityModel={mockEntity}
-        pluralize={sinon.spy()} />
-    );
-    assert.equal(output.refs.files.children.length, 0);
+    const wrapper = renderComponent({
+      entityModel: mockEntity
+    });
+    assert.equal(wrapper.find('.p-list-tree__item').length, 0);
   });
 
   it('excludes the code link when url is not present', function() {
     mockEntity.set('code_source', null);
-    var output = testUtils.renderIntoDocument(
-      <EntityFiles
-        apiUrl="http://example.com/"
-        entityModel={mockEntity}
-        pluralize={sinon.spy()} />
-    );
-    assert.equal(output.refs.codeLink, undefined);
+    const wrapper = renderComponent({
+      entityModel: mockEntity
+    });
+    assert.equal(wrapper.find('.entity-files__code-link').length, 0);
   });
 
   // Check that the links for files included in the given entity are rendered
@@ -122,13 +120,11 @@ describe('EntityFiles', function() {
     mockEntity.set('id', entityId);
     const apiUrl = 'https://api.jujucharms.com/charmstore/v5';
     const expectedURL = apiUrl + expectedPath;
-    const output = testUtils.renderIntoDocument(
-      <EntityFiles
-        apiUrl={apiUrl}
-        entityModel={mockEntity}
-        pluralize={sinon.spy()} />
-    );
-    assert.equal(output.refs.files.children[0].children[0].href, expectedURL);
+    const wrapper = renderComponent({
+      apiUrl,
+      entityModel: mockEntity
+    });
+    assert.equal(wrapper.find('.link').at(0).prop('href'), expectedURL);
   };
 
   it('renders file URLs correctly for promulgated bundles', function() {
