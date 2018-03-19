fd84a884f2d13247b149e162e8bfe1e41bddc0fa
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-19 16:01:39 +1100

    Update expert store card tests to use enzyme.

--- jujugui/static/gui/src/app/components/expert-store-card/test-expert-store-card.js
+++ jujugui/static/gui/src/app/components/expert-store-card/test-expert-store-card.js
@@ -2,42 +2,33 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
-const EXPERTS = require('../expert-card/experts');
-const ExpertCard = require('../expert-card/expert-card');
 const ExpertStoreCard = require('../expert-store-card/expert-store-card');
 
-const jsTestUtils = require('../../utils/component-test-utils');
-
 describe('ExpertStoreCard', function() {
-  function renderComponent(options={}) {
-    return jsTestUtils.shallowRender(
-      <ExpertStoreCard
-        classes={options.classes || ['extra-class']}
-        expert={options.expert || 'spicule'}
-        staticURL="/media" />, true);
-  }
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <ExpertStoreCard
+      classes={options.classes || ['extra-class']}
+      expert={options.expert || 'spicule'}
+      staticURL="/media" />
+  );
 
   it('can render', () => {
-    const renderer = renderComponent();
-    const output = renderer.getRenderOutput();
+    const wrapper = renderComponent();
     const expected = (
-      <ExpertCard
-        classes={['extra-class']}
-        expert={EXPERTS['spicule']}
-        staticURL="/media">
-        <div className="expert-store-card">
-          <p className="expert-store-card__description">
-          Learn how Spicule and Canonical can help solve your Big Data
-          challenges with JAAS:
-          </p>
-          <a className="button--inline-neutral"
-            href="http://jujucharms.com/experts/"
-            target="_blank">
-            Learn about Big Data expertise&hellip;
-          </a>
-        </div>
-      </ExpertCard>);
-    expect(output).toEqualJSX(expected);
+      <div className="expert-store-card">
+        <p className="expert-store-card__description">
+        Learn how Spicule and Canonical can help solve your Big Data
+        challenges with JAAS:
+        </p>
+        <a className="button--inline-neutral"
+          href="http://jujucharms.com/experts/"
+          target="_blank">
+          Learn about Big Data expertise&hellip;
+        </a>
+      </div>);
+    assert.compareJSX(wrapper.find('.expert-store-card'), expected);
   });
 });
