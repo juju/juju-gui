ef41ad11efb0dd040e183ace2ed0f46a825ee3b5
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-19 15:04:51 +1100

    Update env size display tests to use enzyme.

--- jujugui/static/gui/src/app/components/env-size-display/test-env-size-display.js
+++ jujugui/static/gui/src/app/components/env-size-display/test-env-size-display.js
@@ -2,21 +2,21 @@
 'use strict';
 
 const React = require('react');
-const ReactDOM = require('react-dom');
+const enzyme = require('enzyme');
 
 const EnvSizeDisplay = require('./env-size-display');
 
-const testUtils = require('react-dom/test-utils');
-
-const renderIntoDocument = testUtils.renderIntoDocument;
-
-function queryComponentSelector(component, selector, all) {
-  var queryFn = (all) ? 'querySelectorAll' : 'querySelector';
-  return ReactDOM.findDOMNode(component)[queryFn](selector);
-}
-
 describe('EnvSizeDisplay', function() {
   let appState;
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <EnvSizeDisplay
+      appState={options.appState || appState}
+      machineCount={options.machineCount || 4}
+      pluralize={options.pluralize || sinon.stub()}
+      serviceCount={options.serviceCount || 3}
+      showStatus={options.showStatus === undefined ? true : options.showStatus} />
+  );
 
   beforeEach(function() {
     appState = {
@@ -33,51 +33,40 @@ describe('EnvSizeDisplay', function() {
     var pluralize = sinon.stub();
     pluralize.withArgs('application').returns('applications');
     pluralize.withArgs('machine').returns('machines');
-    var component = renderIntoDocument(
-      <EnvSizeDisplay
-        appState={appState}
-        machineCount={4}
-        pluralize={pluralize}
-        serviceCount={3}
-        showStatus={true} />);
+    const wrapper = renderComponent({
+      pluralize: pluralize
+    });
     assert.equal(
-      queryComponentSelector(
-        component, 'a[data-view=application]').innerText, '3 applications');
+      wrapper.find('a[data-view="application"]').text(),
+      '3 applications');
     assert.equal(
-      queryComponentSelector(
-        component, 'a[data-view=machines]').innerText, '4 machines');
+      wrapper.find('a[data-view="machines"]').text(),
+      '4 machines');
   });
 
   it('highlights active tab on initial render', function() {
-    var component = renderIntoDocument(
-      <EnvSizeDisplay
-        appState={appState}
-        machineCount={4}
-        pluralize={sinon.stub()}
-        serviceCount={3}
-        showStatus={true} />);
-    assert.notEqual(
-      queryComponentSelector(
-        component,
-        '.env-size-display__list-item.is-active a[data-view=machines]'),
-      null);
+    const wrapper = renderComponent();
+    const active = wrapper.find('.is-active a');
+    assert.equal(active.length, 1);
+    assert.equal(active.prop('data-view'), 'machines');
   });
 
   it('calls to change state when list item is clicked', function() {
-    var component = renderIntoDocument(
-      <EnvSizeDisplay
-        appState={appState}
-        machineCount={4}
-        pluralize={sinon.stub()}
-        serviceCount={3}
-        showStatus={true} />);
-    var serviceLink = queryComponentSelector(component,
-      'a[data-view=application]');
-    var machineLink = queryComponentSelector(component,
-      'a[data-view=machines]');
-    testUtils.Simulate.click(machineLink);
-    testUtils.Simulate.click(serviceLink);
-
+    const wrapper = renderComponent();
+    wrapper.find('a[data-view="machines"]').simulate('click', {
+      currentTarget: {
+        dataset: {
+          view: 'machines'
+        }
+      }
+    });
+    wrapper.find('a[data-view="application"]').simulate('click', {
+      currentTarget: {
+        dataset: {
+          view: 'application'
+        }
+      }
+    });
     assert.equal(appState.changeState.callCount, 2);
     assert.deepEqual(appState.changeState.getCall(0).args[0], {
       gui: {
@@ -88,58 +77,6 @@ describe('EnvSizeDisplay', function() {
     assert.deepEqual(appState.changeState.getCall(1).args[0], {
       gui: {
         machines: null,
-        status: null
-      }
-    });
-  });
-
-  it('highlights the tab which was clicked on', function() {
-    var component = renderIntoDocument(
-      <EnvSizeDisplay
-        appState={appState}
-        machineCount={4}
-        pluralize={sinon.stub()}
-        serviceCount={3}
-        showStatus={true} />);
-    var serviceLink = queryComponentSelector(component,
-      'a[data-view=application]');
-    var machineLink = queryComponentSelector(component,
-      'a[data-view=machines]');
-
-    testUtils.Simulate.click(machineLink);
-    assert.notEqual(
-      queryComponentSelector(
-        component,
-        '.env-size-display__list-item.is-active a[data-view=machines]'),
-      null);
-    assert.equal(appState.changeState.callCount, 1);
-    assert.deepEqual(appState.changeState.args[0][0], {
-      gui: {
-        machines: '',
-        status: null
-      }
-    });
-    delete appState.current.gui.machine;
-    delete appState.current.gui.status;
-    appState.current.gui.application = true;
-    testUtils.Simulate.click(serviceLink);
-    component = renderIntoDocument(
-      <EnvSizeDisplay
-        appState={appState}
-        machineCount={4}
-        pluralize={sinon.stub()}
-        serviceCount={3}
-        showStatus={true} />);
-    assert.notEqual(
-      queryComponentSelector(
-        component,
-        '.env-size-display__list-item.is-active a[data-view=application]'),
-      null);
-
-    assert.equal(appState.changeState.callCount, 2);
-    assert.deepEqual(appState.changeState.args[0][0], {
-      gui: {
-        machines: '',
         status: null
       }
     });
