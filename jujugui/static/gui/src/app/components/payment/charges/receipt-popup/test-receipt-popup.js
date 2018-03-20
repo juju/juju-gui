7e07ecc6e97dcce5387ccee867cf5363ec4fb266
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-21 10:16:34 +1100

    Update receipt popup to use enzyme.

--- jujugui/static/gui/src/app/components/payment/charges/receipt-popup/test-receipt-popup.js
+++ jujugui/static/gui/src/app/components/payment/charges/receipt-popup/test-receipt-popup.js
@@ -2,62 +2,40 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const ReceiptPopup = require('./receipt-popup');
-const Spinner = require('../../../spinner/spinner');
-const Popup = require('../../../popup/popup');
-
-const jsTestUtils = require('../../../../utils/component-test-utils');
 
 describe('ReceiptPopup', function() {
 
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <ReceiptPopup
+      addNotification={options.addNotification || sinon.stub()}
+      chargeId={options.chargeId || 'charge123'}
+      close={options.close || sinon.stub()}
+      getReceipt={options.getReceipt || sinon.stub()} />
+  );
+
   it('can display the loading spinner', function() {
-    const renderer = jsTestUtils.shallowRender(
-      <ReceiptPopup
-        addNotification={sinon.stub()}
-        chargeId="charge123"
-        close={close}
-        getReceipt={sinon.stub()} />, true);
-    const output = renderer.getRenderOutput();
-    const expected = (
-      <Spinner />);
-    expect(output.props.children).toEqualJSX(expected);
+    const wrapper = renderComponent();
+    assert.equal(wrapper.find('Spinner').length, 1);
   });
 
   it('can display the receipt', function() {
-    const close = sinon.stub();
     const getReceipt = sinon.stub().callsArgWith(1, null, '<html>...</html>');
-    const renderer = jsTestUtils.shallowRender(
-      <ReceiptPopup
-        addNotification={sinon.stub()}
-        chargeId="charge123"
-        close={close}
-        getReceipt={getReceipt} />, true);
-    const output = renderer.getRenderOutput();
-    const expected = (
-      <Popup
-        className="receipt-popup"
-        close={close}
-        type="wide">
-        <div className="receipt-popup__container">
-          <iframe height="100%"
-            src="data:text/html;charset=utf-8,%3Chtml%3E...%3C%2Fhtml%3E"
-            width="100%">
-          </iframe>
-        </div>
-      </Popup>);
-    expect(output).toEqualJSX(expected);
+    const wrapper = renderComponent({ getReceipt });
+    assert.equal(
+      wrapper.find('iframe').prop('src').includes('%3Chtml%3E...%3C%2Fhtml%3E'),
+      true);
   });
 
   it('can handle errors when getting the receipt', function() {
     const addNotification = sinon.stub();
     const getReceipt = sinon.stub().callsArgWith(1, 'Uh oh!', null);
-    jsTestUtils.shallowRender(
-      <ReceiptPopup
-        addNotification={addNotification}
-        chargeId="charge123"
-        close={sinon.stub()}
-        getReceipt={getReceipt} />);
+    renderComponent({
+      addNotification,
+      getReceipt
+    });
     assert.equal(addNotification.callCount, 1);
     assert.deepEqual(addNotification.args[0][0], {
       title: 'Could not load the receipt',
@@ -68,27 +46,16 @@ describe('ReceiptPopup', function() {
 
   it('can close the popup', function() {
     const close = sinon.stub();
-    const renderer = jsTestUtils.shallowRender(
-      <ReceiptPopup
-        addNotification={sinon.stub()}
-        chargeId="charge123"
-        close={close}
-        getReceipt={sinon.stub()} />, true);
-    const output = renderer.getRenderOutput();
-    output.props.close();
+    const wrapper = renderComponent({ close });
+    wrapper.props().close();
     assert.equal(close.callCount, 1);
   });
 
   it('can abort requests when unmounting', function() {
     const abort = sinon.stub();
     const getReceipt = sinon.stub().returns({abort: abort});
-    const renderer = jsTestUtils.shallowRender(
-      <ReceiptPopup
-        addNotification={sinon.stub()}
-        chargeId="charge123"
-        close={sinon.stub()}
-        getReceipt={getReceipt} />, true);
-    renderer.unmount();
+    const wrapper = renderComponent({ getReceipt });
+    wrapper.unmount();
     assert.equal(abort.callCount, 1);
   });
 });
