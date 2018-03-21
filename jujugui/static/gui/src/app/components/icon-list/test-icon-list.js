d7224b0b3248895015b52f362fee3abd77b24349
Francesco Banconi <francesco.banconi@canonical.com>
2018-03-21 08:37:43 +0000

    Improve handling of unknown charm series and add bionic

--- jujugui/static/gui/src/app/components/icon-list/test-icon-list.js
+++ jujugui/static/gui/src/app/components/icon-list/test-icon-list.js
@@ -17,10 +17,13 @@ describe('IconList', function() {
       }, {
         displayName: 'wordpress',
         id: 'cs:wordpress'
+      }, {
+        displayName: 'django',
+        iconPath: 'django.svg',
+        id: 'cs:new-series/django'
       }]}
       changeState={options.changeState || sinon.stub()}
-      generatePath={
-        options.generatePath || sinon.stub().returns('/charm/path')} />
+      generatePath={sinon.stub().returns('/charm/path')} />
   );
 
   it('can render', () => {
@@ -28,8 +31,7 @@ describe('IconList', function() {
     const links = wrapper.find('.icon-list__link');
     const expected = (
       <ul className="icon-list">
-        <li className="icon-list__item tooltip"
-          key="mysql">
+        <li className="icon-list__item tooltip" key="mysql">
           <a className="icon-list__link"
             href="/charm/path"
             onClick={links.at(0).prop('onClick')}>
@@ -43,8 +45,7 @@ describe('IconList', function() {
             </span>
           </a>
         </li>
-        <li className="icon-list__item tooltip"
-          key="wordpress">
+        <li className="icon-list__item tooltip" key="wordpress">
           <a className="icon-list__link"
             href="/charm/path"
             onClick={links.at(1).prop('onClick')}>
@@ -58,7 +59,34 @@ describe('IconList', function() {
             </span>
           </a>
         </li>
+        <li className="icon-list__item tooltip" key="django">
+          <a className="icon-list__link"
+            href="/charm/path"
+            onClick={links.at(0).prop('onClick')}>
+            <img alt='django'
+              className="icon-list__image"
+              src="django.svg" />
+            <span className="tooltip__tooltip">
+              <span className="tooltip__inner tooltip__inner--down">
+                django
+              </span>
+            </span>
+          </a>
+        </li>
       </ul>);
     assert.compareJSX(wrapper, expected);
   });
+
+  it('generates the correct paths', () => {
+    const wrapper = renderComponent();
+    const generatePath = wrapper.instance().props.generatePath;
+    assert.equal(generatePath.callCount, 3);
+    const args = generatePath.args;
+    assert.deepEqual(args[0], [{profile: null, search: null, store: 'mysql'}]);
+    assert.deepEqual(
+      args[1], [{profile: null, search: null, store: 'wordpress'}]);
+    // It was not possible to get the store state for an unknown series.
+    assert.deepEqual(args[2], [{profile: null, search: null, store: null}]);
+  });
+
 });
