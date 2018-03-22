befa2a0aceb066aa9d74c085f7f7a830304a7fd1
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-23 09:38:49 +1100

    Update machine view header tests to use enzyme.

--- jujugui/static/gui/src/app/components/machine-view/header/test-header.js
+++ jujugui/static/gui/src/app/components/machine-view/header/test-header.js
@@ -2,18 +2,33 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const shapeup = require('shapeup');
 
 const MachineViewHeader = require('./header');
-const GenericButton = require('../../generic-button/generic-button');
-const ButtonDropdown = require('../../button-dropdown/button-dropdown');
-const SvgIcon = require('../../svg-icon/svg-icon');
 
 const jsTestUtils = require('../../../utils/component-test-utils');
 
 describe('MachineViewHeader', function() {
   let acl, sendAnalytics;
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    // The component is wrapped to handle drag and drop, but we just want to
+    // test the internal component so we access it via DecoratedComponent.
+    <MachineViewHeader.DecoratedComponent
+      acl={options.acl || acl}
+      activeMenuItem={options.activeMenuItem}
+      canDrop={options.canDrop === undefined ? false : options.canDrop}
+      connectDropTarget={jsTestUtils.connectDropTarget}
+      droppable={options.droppable === undefined ? true : options.droppable}
+      isOver={options.isOver === undefined ? false : options.isOver}
+      menuItems={options.menuItems}
+      sendAnalytics={options.sendAnalytics || sendAnalytics}
+      title={options.title || 'Sandbox'}
+      toggle={options.toggle}
+      type={options.type || 'machine'} />
+  );
 
   beforeEach(() => {
     acl = shapeup.deepFreeze({isReadOnly: () => false});
@@ -21,18 +36,7 @@ describe('MachineViewHeader', function() {
   });
 
   it('can render', function() {
-    // The component is wrapped to handle drag and drop, but we just want to
-    // test the internal component so we access it via DecoratedComponent.
-    const output = jsTestUtils.shallowRender(
-      <MachineViewHeader.DecoratedComponent
-        acl={acl}
-        canDrop={false}
-        connectDropTarget={jsTestUtils.connectDropTarget}
-        droppable={true}
-        isOver={false}
-        sendAnalytics={sendAnalytics}
-        title="Sandbox"
-        type="machine" />);
+    const wrapper = renderComponent();
     const expected = (
       <div className="machine-view__header">
           Sandbox
@@ -43,109 +47,38 @@ describe('MachineViewHeader', function() {
           </div>
         </div>
       </div>);
-    assert.deepEqual(output, expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('can render in droppable mode', function() {
-    const output = jsTestUtils.shallowRender(
-      <MachineViewHeader.DecoratedComponent
-        acl={acl}
-        canDrop={false}
-        connectDropTarget={jsTestUtils.connectDropTarget}
-        droppable={true}
-        isOver={true}
-        sendAnalytics={sendAnalytics}
-        title="Sandbox"
-        type="machine" />);
-    const expected = (
-      <div className="machine-view__header machine-view__header--drop">
-        {output.props.children}
-      </div>);
-    assert.deepEqual(output, expected);
+    const wrapper = renderComponent({ canDrop: true });
+    assert.equal(
+      wrapper.prop('className').includes('machine-view__header--droppable'),
+      true);
   });
 
   it('can render in drop mode', function() {
-    const output = jsTestUtils.shallowRender(
-      <MachineViewHeader.DecoratedComponent
-        acl={acl}
-        canDrop={true}
-        connectDropTarget={jsTestUtils.connectDropTarget}
-        droppable={true}
-        isOver={false}
-        sendAnalytics={sendAnalytics}
-        title="Sandbox"
-        type="machine" />);
-    const expected = (
-      <div className="machine-view__header machine-view__header--droppable">
-        {output.props.children}
-      </div>);
-    assert.deepEqual(output, expected);
+    const wrapper = renderComponent({ isOver: true });
+    assert.equal(
+      wrapper.prop('className').includes('machine-view__header--drop'),
+      true);
   });
 
   it('can render with a menu', function() {
     const menuItems = [];
-    const output = jsTestUtils.shallowRender(
-      <MachineViewHeader.DecoratedComponent
-        acl={acl}
-        activeMenuItem="name"
-        canDrop={false}
-        connectDropTarget={jsTestUtils.connectDropTarget}
-        droppable={true}
-        isOver={false}
-        menuItems={menuItems}
-        sendAnalytics={sendAnalytics}
-        title="Sandbox"
-        type="machine" />);
-    const expected = (
-      <div className="machine-view__header">
-          Sandbox
-        <ButtonDropdown
-          activeItem="name"
-          classes={['machine-view__header-dropdown']}
-          listItems={menuItems} />
-        <div className="machine-view__header-drop-target">
-          <div className="machine-view__header-drop-message">
-              Create new {'machine'}
-          </div>
-        </div>
-      </div>);
-    assert.deepEqual(output, expected);
+    const wrapper = renderComponent({ menuItems });
+    const dropdown = wrapper.find('ButtonDropdown');
+    assert.equal(dropdown.length, 1);
+    assert.equal(dropdown.prop('listItems'), menuItems);
   });
 
   it('can render with a toggle', function() {
-    const action = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <MachineViewHeader.DecoratedComponent
-        acl={acl}
-        canDrop={false}
-        connectDropTarget={jsTestUtils.connectDropTarget}
-        droppable={true}
-        isOver={false}
-        sendAnalytics={sendAnalytics}
-        title="Sandbox"
-        toggle={{
-          action: action,
-          disabled: false,
-          toggleOn: true
-        }}
-        type="machine" />);
-    const expected = (
-      <div className="machine-view__header">
-          Sandbox
-        <GenericButton
-          action={action}
-          disabled={false}
-          type="inline-positive">
-          <SvgIcon
-            name="close_16_white"
-            size="16" />
-        </GenericButton>
-        <div className="machine-view__header-drop-target">
-          <div className="machine-view__header-drop-message">
-              Create new {'machine'}
-          </div>
-        </div>
-      </div>);
-    assert.deepEqual(output, expected);
+    const toggle = {
+      action: sinon.stub(),
+      disabled: false,
+      toggleOn: true
+    };
+    const wrapper = renderComponent({ toggle });
+    assert.equal(wrapper.find('GenericButton').length, 1);
   });
 });
