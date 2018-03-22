e6231a8a80b9c5dedd2edbd1a1bd616d638be081
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-22 17:32:24 +1100

    Update logout tests to use enzyme.

--- jujugui/static/gui/src/app/components/logout/test-logout.js
+++ jujugui/static/gui/src/app/components/logout/test-logout.js
@@ -2,105 +2,59 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const Logout = require('./logout');
 
-const jsTestUtils = require('../../utils/component-test-utils');
-
 describe('Logout', () => {
 
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <Logout
+      charmstoreLogoutUrl={options.charmstoreLogoutUrl || 'http://charmstorelogout'}
+      doCharmstoreLogout={options.doCharmstoreLogout || sinon.stub().returns(false)}
+      locationAssign={options.locationAssign || sinon.stub()}
+      logoutUrl={options.logoutUrl || 'http://logout'}
+      visible={options.visible === undefined ? true : options.visible} />
+  );
+
   it('renders properly for regular logout', () => {
-    const doCharmstoreLogout = sinon.stub().returns(false);
-    const logoutUrl = 'http://logout';
-    const charmstoreLogoutUrl = 'http://charmstorelogout';
-    const output = jsTestUtils.shallowRender(
-      <Logout
-        charmstoreLogoutUrl={charmstoreLogoutUrl}
-        doCharmstoreLogout={doCharmstoreLogout}
-        locationAssign={sinon.stub()}
-        logoutUrl={logoutUrl}
-        visible={true} />);
+    const wrapper = renderComponent();
     const expected = (
       <a className="logout-link dropdown-menu__list-item-link"
-        href={logoutUrl}
-        onClick={output.props.onClick}
+        href="http://logout"
+        onClick={wrapper.prop('onClick')}
         target="_self">Logout</a>
     );
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('renders properly for charmstore logout', () => {
     const doCharmstoreLogout = sinon.stub().returns(true);
-    const logoutUrl = 'http://logout';
-    const charmstoreLogoutUrl = 'http://charmstorelogout';
-    const output = jsTestUtils.shallowRender(
-      <Logout
-        charmstoreLogoutUrl={charmstoreLogoutUrl}
-        doCharmstoreLogout={doCharmstoreLogout}
-        locationAssign={sinon.stub()}
-        logoutUrl={logoutUrl}
-        visible={true} />);
-    const expected = (
-      <a className="logout-link dropdown-menu__list-item-link"
-        href={charmstoreLogoutUrl}
-        onClick={output.props.onClick}
-        target="_blank">Logout</a>);
-    expect(output).toEqualJSX(expected);
+    const wrapper = renderComponent({ doCharmstoreLogout });
+    assert.equal(wrapper.prop('href'), 'http://charmstorelogout');
   });
 
   it('can be hidden', () => {
-    const doCharmstoreLogout = sinon.stub().returns(false);
-    const logoutUrl = 'http://logout';
-    const charmstoreLogoutUrl = 'http://charmstorelogout';
-    const output = jsTestUtils.shallowRender(
-      <Logout
-        charmstoreLogoutUrl={charmstoreLogoutUrl}
-        doCharmstoreLogout={doCharmstoreLogout}
-        locationAssign={sinon.stub()}
-        logoutUrl={logoutUrl}
-        visible={false} />);
-    const classes = 'logout-link dropdown-menu__list-item-link ' +
-      'logout-link--hidden';
-    const expected = (
-      <a className={classes}
-        href={logoutUrl}
-        onClick={output.props.onClick}
-        target="_self">Logout</a>);
-    expect(output).toEqualJSX(expected);
+    const wrapper = renderComponent({ visible: false });
+    assert.equal(wrapper.prop('className').includes('logout-link--hidden'), true);
   });
 
   it('does not redirect to logout onClick for regular logout', () => {
-    const doCharmstoreLogout = sinon.stub().returns(false);
-    const logoutUrl = 'http://logout';
-    const charmstoreLogoutUrl = 'http://charmstorelogout';
     const locationAssign = sinon.stub();
-    const renderer = jsTestUtils.shallowRender(
-      <Logout
-        charmstoreLogoutUrl={charmstoreLogoutUrl}
-        doCharmstoreLogout={doCharmstoreLogout}
-        locationAssign={locationAssign}
-        logoutUrl={logoutUrl}
-        visible={false} />, true);
-    const instance = renderer.getMountedInstance();
-    instance._handleClick();
+    const wrapper = renderComponent({ locationAssign });
+    wrapper.simulate('click');
     assert.equal(locationAssign.callCount, 0);
   });
 
   it('redirects to logout onClick for charmstore logout', () => {
-    const doCharmstoreLogout = sinon.stub().returns(true);
-    const logoutUrl = 'http://logout';
-    const charmstoreLogoutUrl = 'http://charmstorelogout';
     const locationAssign = sinon.stub();
-    const renderer = jsTestUtils.shallowRender(
-      <Logout
-        charmstoreLogoutUrl={charmstoreLogoutUrl}
-        doCharmstoreLogout={doCharmstoreLogout}
-        locationAssign={locationAssign}
-        logoutUrl={logoutUrl}
-        visible={false} />, true);
-    const instance = renderer.getMountedInstance();
-    instance._handleClick();
+    const doCharmstoreLogout = sinon.stub().returns(true);
+    const wrapper = renderComponent({
+      doCharmstoreLogout,
+      locationAssign
+    });
+    wrapper.simulate('click');
     assert.equal(locationAssign.callCount, 1);
-    assert.equal(locationAssign.args[0][0], logoutUrl);
+    assert.equal(locationAssign.args[0][0], 'http://logout');
   });
 });
