a88cfd202d62cd9e6651e55a5e1bbdd14d93cae2
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-23 17:33:10 +1100

    Update popup tests to use enzyme.

--- jujugui/static/gui/src/app/components/popup/test-popup.js
+++ jujugui/static/gui/src/app/components/popup/test-popup.js
@@ -2,71 +2,53 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const Popup = require('./popup');
 const ButtonRow = require('../button-row/button-row');
 const GenericButton = require('../generic-button/generic-button');
-const Panel = require('../panel/panel');
 const SvgIcon = require('../svg-icon/svg-icon');
-
-const jsTestUtils = require('../../utils/component-test-utils');
 
 describe('Popup', () => {
 
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <Popup
+      buttons={options.buttons || ['one', 'two']}
+      close={options.close || sinon.stub()}
+      title={options.title || 'A title'}
+      type={options.type}>
+      {options.children || (<span>Content</span>)}
+    </Popup>
+  );
+
   it('can render', () => {
-    const buttons = ['one', 'two'];
-    const close = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <Popup
-        buttons={buttons}
-        close={close}
-        title="A title">
-        <span>Content</span>
-      </Popup>);
+    const wrapper = renderComponent();
     const expected = (
-      <Panel
-        instanceName="popup"
-        visible={true}>
-        <div className="popup__panel popup__panel--narrow">
-          <div className="popup__close">
-            <GenericButton
-              action={close}
-              type="inline-base">
-              <SvgIcon
-                name="close_16"
-                size="16" />
-            </GenericButton>
-          </div>
-          <h3 className="popup__title">
-            A title
-          </h3>
-          <span>Content</span>
-          <ButtonRow
-            buttons={buttons} />
+      <div className="popup__panel popup__panel--narrow">
+        <div className="popup__close">
+          <GenericButton
+            action={sinon.stub()}
+            type="inline-base">
+            <SvgIcon
+              name="close_16"
+              size="16" />
+          </GenericButton>
         </div>
-      </Panel>
+        <h3 className="popup__title">
+          A title
+        </h3>
+        <span>Content</span>
+        <ButtonRow
+          buttons={['one', 'two']} />
+      </div>
     );
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper.find('.popup__panel'), expected);
   });
 
   it('can set a type class', () => {
-    const output = jsTestUtils.shallowRender(
-      <Popup
-        type="wide">
-        <span>Content</span>
-      </Popup>);
-    const expected = (
-      <Panel
-        instanceName="popup"
-        visible={true}>
-        <div className="popup__panel popup__panel--wide">
-          {undefined}
-          {undefined}
-          <span>Content</span>
-          {undefined}
-        </div>
-      </Panel>
-    );
-    expect(output).toEqualJSX(expected);
+    const wrapper = renderComponent({ type: 'wide' });
+    assert.equal(
+      wrapper.find('.popup__panel').prop('className').includes('popup__panel--wide'),
+      true);
   });
 });
