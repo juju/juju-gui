894424d438cc1aec960145d139ad734f9f7dbb9c
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-23 11:24:55 +1100

    Update machine view machine unit tests to use enzyme.

--- jujugui/static/gui/src/app/components/machine-view/machine-unit/test-machine-unit.js
+++ jujugui/static/gui/src/app/components/machine-view/machine-unit/test-machine-unit.js
@@ -2,20 +2,34 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const shapeup = require('shapeup');
 
 const MachineViewMachineUnit = require('./machine-unit');
-const ButtonDropdown = require('../../button-dropdown/button-dropdown');
 
 const jsTestUtils = require('../../../utils/component-test-utils');
 
 describe('MachineViewMachineUnit', function() {
-  let acl, sendAnalytics, service, unit;
+  let acl, service, unit;
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    // The component is wrapped to handle drag and drop, but we just want to
+    // test the internal component so we access it via DecoratedComponent.
+    <MachineViewMachineUnit.DecoratedComponent
+      acl={options.acl || acl}
+      canDrag={options.canDrag === undefined ? false : options.canDrag}
+      connectDragSource={jsTestUtils.connectDragSource}
+      isDragging={options.isDragging === undefined ? false : options.isDragging}
+      machineType={options.machineType || 'machine'}
+      removeUnit={options.removeUnit || sinon.stub()}
+      sendAnalytics={options.sendAnalytics || sinon.stub()}
+      service={options.service || service}
+      unit={options.unit || unit} />
+  );
 
   beforeEach(function() {
     acl = shapeup.deepFreeze({isReadOnly: () => false});
-    sendAnalytics = sinon.stub();
     service = {
       get: function(val) {
         switch (val) {
@@ -38,21 +52,7 @@ describe('MachineViewMachineUnit', function() {
   });
 
   it('can render a machine', function() {
-    const removeUnit = sinon.stub();
-    const renderer = jsTestUtils.shallowRender(
-      // The component is wrapped to handle drag and drop, but we just want to
-      // test the internal component so we access it via DecoratedComponent.
-      <MachineViewMachineUnit.DecoratedComponent
-        acl={acl}
-        canDrag={false}
-        connectDragSource={jsTestUtils.connectDragSource}
-        isDragging={false}
-        machineType="machine"
-        removeUnit={removeUnit}
-        sendAnalytics={sendAnalytics}
-        service={service}
-        unit={unit} />, true);
-    const output = renderer.getRenderOutput();
+    const wrapper = renderComponent();
     const expected = (
       <li className={'machine-view__machine-unit ' +
         'machine-view__machine-unit--started'}>
@@ -66,147 +66,50 @@ describe('MachineViewMachineUnit', function() {
         {undefined}
         {undefined}
       </li>);
-    assert.deepEqual(output, expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('can render a container', function() {
-    const removeUnit = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <MachineViewMachineUnit.DecoratedComponent
-        acl={acl}
-        canDrag={false}
-        connectDragSource={jsTestUtils.connectDragSource}
-        isDragging={false}
-        machineType="container"
-        removeUnit={removeUnit}
-        sendAnalytics={sendAnalytics}
-        service={service}
-        unit={unit} />);
-    const expected = (
-      <li className={'machine-view__machine-unit ' +
-        'machine-view__machine-unit--started'}>
-        <span className="machine-view__machine-unit-icon">
-          <img
-            alt="django/7"
-            className="machine-view__machine-unit-icon-img"
-            src="icon.svg"
-            title="django/7" />
-        </span>
-        {'django/7'}
-        <ButtonDropdown
-          classes={['machine-view__machine-dropdown']}
-          listItems={[{
-            label: 'Destroy',
-            action: output.props.children[2].props.listItems[0].action
-          }]} />
-      </li>);
-    assert.deepEqual(output, expected);
+    const wrapper = renderComponent({ machineType: 'container' });
+    assert.equal(wrapper.find('ButtonDropdown').length, 1);
   });
 
   it('can disable the destroy when read only', function() {
     acl = shapeup.deepFreeze({isReadOnly: () => true});
-    const removeUnit = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <MachineViewMachineUnit.DecoratedComponent
-        acl={acl}
-        canDrag={false}
-        connectDragSource={jsTestUtils.connectDragSource}
-        isDragging={false}
-        machineType="container"
-        removeUnit={removeUnit}
-        sendAnalytics={sendAnalytics}
-        service={service}
-        unit={unit} />);
-    const expected = (
-      <ButtonDropdown
-        classes={['machine-view__machine-dropdown']}
-        listItems={[{
-          label: 'Destroy',
-          action: null
-        }]} />);
-    expect(output.props.children[2]).toEqualJSX(expected);
+    const wrapper = renderComponent({ machineType: 'container' });
+    assert.strictEqual(
+      wrapper.find('ButtonDropdown').prop('listItems')[0].action, null);
   });
 
   it('can display in dragged mode', function() {
-    const removeUnit = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <MachineViewMachineUnit.DecoratedComponent
-        acl={acl}
-        canDrag={false}
-        connectDragSource={jsTestUtils.connectDragSource}
-        isDragging={true}
-        machineType="container"
-        removeUnit={removeUnit}
-        sendAnalytics={sendAnalytics}
-        service={service}
-        unit={unit} />);
-    const expected = (
-      <li className={'machine-view__machine-unit ' +
-        'machine-view__machine-unit--dragged ' +
-        'machine-view__machine-unit--started'}>
-        {output.props.children}
-      </li>);
-    assert.deepEqual(output, expected);
+    const wrapper = renderComponent({ isDragging: true });
+    assert.equal(
+      wrapper.prop('className').includes('machine-view__machine-unit--dragged'),
+      true);
   });
 
   it('can display as uncommitted', function() {
     unit.deleted = true;
-    const removeUnit = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <MachineViewMachineUnit.DecoratedComponent
-        acl={acl}
-        canDrag={false}
-        connectDragSource={jsTestUtils.connectDragSource}
-        isDragging={false}
-        machineType="container"
-        removeUnit={removeUnit}
-        sendAnalytics={sendAnalytics}
-        service={service}
-        unit={unit} />);
-    const expected = (
-      <li className={'machine-view__machine-unit ' +
-        'machine-view__machine-unit--uncommitted'}>
-        {output.props.children}
-      </li>);
-    assert.deepEqual(output, expected);
+    const wrapper = renderComponent({ isDragging: true });
+    assert.equal(
+      wrapper.prop('className').includes('machine-view__machine-unit--uncommitted'),
+      true);
   });
 
   it('can display in draggable mode', function() {
-    const removeUnit = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <MachineViewMachineUnit.DecoratedComponent
-        acl={acl}
-        canDrag={true}
-        connectDragSource={jsTestUtils.connectDragSource}
-        isDragging={false}
-        machineType="container"
-        removeUnit={removeUnit}
-        sendAnalytics={sendAnalytics}
-        service={service}
-        unit={unit} />);
-    const expected = (
-      <li className={'machine-view__machine-unit ' +
-        'machine-view__machine-unit--draggable ' +
-        'machine-view__machine-unit--started'}>
-        {output.props.children}
-      </li>);
-    assert.deepEqual(output, expected);
+    const wrapper = renderComponent({ canDrag: true });
+    assert.equal(
+      wrapper.prop('className').includes('machine-view__machine-unit--draggable'),
+      true);
   });
 
   it('can remove a unit', function() {
     const removeUnit = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <MachineViewMachineUnit.DecoratedComponent
-        acl={acl}
-        canDrag={false}
-        connectDragSource={jsTestUtils.connectDragSource}
-        isDragging={false}
-        machineType="container"
-        removeUnit={removeUnit}
-        sendAnalytics={sendAnalytics}
-        service={service}
-        unit={unit} />);
-    output.props.children[2].props.listItems[0].action();
+    const wrapper = renderComponent({
+      machineType: 'container',
+      removeUnit
+    });
+    wrapper.find('ButtonDropdown').prop('listItems')[0].action();
     assert.equal(removeUnit.callCount, 1);
   });
 });
