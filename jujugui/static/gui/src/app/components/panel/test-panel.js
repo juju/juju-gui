1371e3910f36aaf94ff52be9b6470a73356a8cb0
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-23 17:23:51 +1100

    Update panel tests to use enzyme.

--- jujugui/static/gui/src/app/components/panel/test-panel.js
+++ jujugui/static/gui/src/app/components/panel/test-panel.js
@@ -2,128 +2,80 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const Panel = require('./panel');
 
-const jsTestUtils = require('../../utils/component-test-utils');
-
 describe('PanelComponent', function() {
 
+  const renderComponent = (options = {}) => {
+    const wrapper = enzyme.shallow(
+      <Panel
+        clickAction={options.clickAction}
+        focus={options.focus}
+        instanceName={options.instanceName || 'custom-instance-name'}
+        visible={options.visible === undefined ? true : options.visible}>
+        {options.children || (<div>child</div>)}
+      </Panel>,
+      { disableLifecycleMethods: true }
+    );
+    const instance = wrapper.instance();
+    instance.refs = {
+      content: {
+        focus: sinon.stub()
+      }
+    };
+    instance.componentDidMount();
+    return wrapper;
+  };
+
   it('generates a visible panel when visible flag is provided', function() {
-    var instanceName = 'custom-instance-name';
-    var renderer = jsTestUtils.shallowRender(
-      <Panel
-        instanceName={instanceName}
-        visible={true} />, true);
-    var instance = renderer.getMountedInstance();
-    var output = renderer.getRenderOutput();
+    const wrapper = renderComponent();
     var expected = (
       <div className="panel-component custom-instance-name"
-        onClick={instance._handleClick}
+        onClick={wrapper.prop('onClick')}
         ref="content"
         tabIndex="0">
         <div className="panel-component__inner"
-          onClick={instance._stopBubble}>
-          {undefined}
-        </div>
-      </div>);
-    expect(output).toEqualJSX(expected);
-  });
-
-  it('generates a hidden panel if visible flag is falsey', function() {
-    var instanceName = 'custom-instance-name';
-    var renderer = jsTestUtils.shallowRender(
-      <Panel
-        instanceName={instanceName}
-        visible={false} />, true);
-    var instance = renderer.getMountedInstance();
-    var output = renderer.getRenderOutput();
-    var expected = (
-      <div className="panel-component custom-instance-name hidden"
-        onClick={instance._handleClick}
-        ref="content"
-        tabIndex="0">
-        <div className="panel-component__inner"
-          onClick={instance._stopBubble}>
-          {undefined}
-        </div>
-      </div>);
-    expect(output).toEqualJSX(expected);
-  });
-
-  it('renders provided children components', function() {
-    var instanceName = 'custom-instance-name';
-    var renderer = jsTestUtils.shallowRender(
-      <Panel
-        instanceName={instanceName}
-        visible={true}>
-        <div>child</div>
-      </Panel>, true);
-    var instance = renderer.getMountedInstance();
-    var output = renderer.getRenderOutput();
-    const expected = (
-      <div className="panel-component custom-instance-name"
-        onClick={instance._handleClick}
-        ref="content"
-        tabIndex="0">
-        <div className="panel-component__inner"
-          onClick={instance._stopBubble}>
+          onClick={wrapper.find('.panel-component__inner').prop('onClick')}>
           <div>child</div>
         </div>
       </div>);
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
+  });
+
+  it('generates a hidden panel if visible flag is falsey', function() {
+    const wrapper = renderComponent({ visible: false });
+    assert.equal(wrapper.prop('className').includes('hidden'), true);
   });
 
   it('can call a function on click if provided', function() {
-    var instanceName = 'custom-instance-name';
     var clickAction = sinon.stub();
-    var renderer = jsTestUtils.shallowRender(
-      <Panel
-        clickAction={clickAction}
-        instanceName={instanceName}
-        visible={true} />, true);
-    var instance = renderer.getMountedInstance();
-    instance._handleClick();
+    const wrapper = renderComponent({ clickAction });
+    wrapper.simulate('click');
     assert.equal(clickAction.callCount, 1);
   });
 
   it('does not bubble clicks from the children', function() {
-    var instanceName = 'custom-instance-name';
     var clickAction = sinon.stub();
     var stopPropagation = sinon.stub();
-    var renderer = jsTestUtils.shallowRender(
-      <Panel
-        clickAction={clickAction}
-        instanceName={instanceName}
-        visible={true} />, true);
-    var output = renderer.getRenderOutput();
-    output.props.children.props.onClick({stopPropagation: stopPropagation});
+    const wrapper = renderComponent({ clickAction });
+    wrapper.find('.panel-component__inner').simulate('click', {
+      stopPropagation: stopPropagation
+    });
     assert.equal(stopPropagation.callCount, 1);
     assert.equal(clickAction.callCount, 0);
   });
 
   it('sets the keyboard focus when it loads', function() {
-    var focus = sinon.stub();
-    var renderer = jsTestUtils.shallowRender(
-      <Panel
-        instanceName="my-panel"
-        visible={true} />, true);
-    var instance = renderer.getMountedInstance();
-    instance.refs = {content: {focus: focus}};
-    instance.componentDidMount();
-    assert.equal(focus.callCount, 1);
+    const wrapper = renderComponent();
+    const instance = wrapper.instance();
+    assert.equal(instance.refs.content.focus.callCount, 1);
   });
 
   it('can not set the keyboard focus on load', function() {
-    var focus = sinon.stub();
-    var renderer = jsTestUtils.shallowRender(
-      <Panel
-        focus={false}
-        instanceName="my-panel"
-        visible={true} />, true);
-    var instance = renderer.getMountedInstance();
-    instance.refs = {content: {focus: focus}};
-    instance.componentDidMount();
-    assert.equal(focus.callCount, 0);
+    const wrapper = renderComponent({ focus: false });
+    const instance = wrapper.instance();
+    assert.equal(instance.refs.content.focus.callCount, 0);
   });
 });
