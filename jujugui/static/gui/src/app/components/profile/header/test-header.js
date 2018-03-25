c60f0245778e43c6e323efed9b0a1592272feaba
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-26 10:52:40 +1100

    Update profile header tests to use enzyme.

--- jujugui/static/gui/src/app/components/profile/header/test-header.js
+++ jujugui/static/gui/src/app/components/profile/header/test-header.js
@@ -2,14 +2,25 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const ProfileHeader = require('./header');
 const SvgIcon = require('../../svg-icon/svg-icon');
 
-const jsTestUtils = require('../../../utils/component-test-utils');
-
 describe('Profile Header', function() {
   let getUser;
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <ProfileHeader
+      changeState={options.changeState || sinon.stub()}
+      controllerIP={options.controllerIP || '1.2.3.4'}
+      getUser={options.getUser || getUser}
+      gisf={options.gisf === undefined ? true : options.gisf}
+      userInfo={options.userInfo || {
+        isCurrent: true,
+        profile: 'spinach'
+      }} />
+  );
 
   beforeEach(() => {
     getUser = sinon.stub().callsArgWith(1, null, {
@@ -20,27 +31,12 @@ describe('Profile Header', function() {
   });
 
   it('can render', () => {
-    const renderer = jsTestUtils.shallowRender(
-      <ProfileHeader
-        changeState={sinon.stub()}
-        controllerIP={'1.2.3.4'}
-        getUser={getUser}
-        gisf={true}
-        userInfo={{
-          isCurrent: true,
-          profile: 'spinach'
-        }} />, true);
-    const instance = renderer.getMountedInstance();
-    // Triggering a componentWillMount before grabbing the rendered output so
-    // that the getUser call will have returned and rendered the gravatar UI.
-    // The following test tests the 'not yet returned' state.
-    instance.componentWillMount();
-    const output = renderer.getRenderOutput();
+    const wrapper = renderComponent();
     const expected = (
       <div className="profile-header twelve-col">
         <div className="inner-wrapper profile-header__inner">
           <div className="profile-header__close link"
-            onClick={sinon.stub()}
+            onClick={wrapper.find('.profile-header__close').prop('onClick')}
             role="button"
             tabIndex="0">
             <SvgIcon
@@ -81,71 +77,22 @@ describe('Profile Header', function() {
         </div>
       </div>
     );
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('can render correctly for the non-logged in user', () => {
-    const renderer = jsTestUtils.shallowRender(
-      <ProfileHeader
-        changeState={sinon.stub()}
-        controllerIP={'1.2.3.4'}
-        getUser={getUser}
-        gisf={true}
-        userInfo={{
-          isCurrent: false,
-          profile: 'notspinach'
-        }} />, true);
-    const instance = renderer.getMountedInstance();
-    instance.componentWillMount();
-    const output = renderer.getRenderOutput();
-    const expected = (
-      <div className="profile-header twelve-col">
-        <div className="inner-wrapper profile-header__inner">
-          <div className="profile-header__close link"
-            onClick={sinon.stub()}
-            role="button"
-            tabIndex="0">
-            <SvgIcon
-              name="close_16"
-              size="20" />
-          </div>
-          <span className="profile-header__avatar">
-            <img alt="Gravatar"
-              className="profile-header__avatar-gravatar"
-              src="https://www.gravatar.com/avatar/id123" />
-          </span>
-          <ul className="profile-header__meta">
-            <li className="profile-header__username">
-              <h1>
-                notspinach
-              </h1>
-            </li>
-            <li><strong>Geoffrey Spinach</strong></li>
-            <li>spinach@example.com</li>
-          </ul>
-        </div>
-      </div>
-    );
-    expect(output).toEqualJSX(expected);
+    const wrapper = renderComponent({
+      userInfo: {
+        isCurrent: false,
+        profile: 'notspinach'
+      }
+    });
+    assert.equal(wrapper.find('.profile-header__menu').length, 0);
+    assert.equal(wrapper.find('.tooltip__tooltip').length, 0);
   });
 
   it('displays a different link list for non-jaas', () => {
-    const renderer = jsTestUtils.shallowRender(
-      <ProfileHeader
-        changeState={sinon.stub()}
-        controllerIP={'1.2.3.4'}
-        getUser={getUser}
-        gisf={false}
-        userInfo={{
-          isCurrent: true,
-          profile: 'spinach'
-        }} />, true);
-    const instance = renderer.getMountedInstance();
-    // Triggering a componentWillMount before grabbing the rendered output so
-    // that the getUser call will have returned and rendered the gravatar UI.
-    // The following test tests the 'not yet returned' state.
-    instance.componentWillMount();
-    const output = renderer.getRenderOutput();
+    const wrapper = renderComponent({ gisf: false });
     const expected = (
       <ul className="profile-header__menu">
         <li key="controller">
@@ -156,62 +103,32 @@ describe('Profile Header', function() {
         <li key="home"><a href="https://jujucharms.com/about">Juju Home</a></li>
       </ul>
     );
-    expect(output.props.children.props.children[3]).toEqualJSX(expected);
+    assert.compareJSX(wrapper.find('.profile-header__menu'), expected);
   });
 
   it('displays a hidden gravatar until the user request has returned', () => {
     // For the test where it has returned successfully see the above tests.
-    const output = jsTestUtils.shallowRender(
-      <ProfileHeader
-        changeState={sinon.stub()}
-        getUser={() => {}}
-        userInfo={{
-          isCurrent: true,
-          profile: 'spinach'
-        }} />);
+    const wrapper = renderComponent({ getUser: sinon.stub() });
     assert.equal(
-      output.props.children.props.children[1].props.className,
-      'profile-header__avatar tooltip profile-header__avatar--default ' +
-      'profile-header__avatar--hidden');
+      wrapper.find('.profile-header__avatar').prop('className').includes(
+        'profile-header__avatar--hidden'),
+      true);
   });
 
   it('displays the fallback gravatar if the user request fails', () => {
-    const renderer = jsTestUtils.shallowRender(
-      <ProfileHeader
-        changeState={sinon.stub()}
-        getUser={(u, c) => c(null, null)}
-        userInfo={{
-          isCurrent: true,
-          profile: 'spinach'
-        }} />, true);
-    renderer.getMountedInstance().componentWillMount();
-    const output = renderer.getRenderOutput();
-    const expected = (
-      <span className="profile-header__avatar tooltip profile-header__avatar--default">
-        <a href="http://gravatar.com/"
-          target="_blank">
-          <span className="profile-header__avatar-overlay"></span>
-        </a>
-        <span className="tooltip__tooltip">
-          <span className="tooltip__inner tooltip__inner--down">
-            Edit your <strong>Gravatar</strong>
-          </span>
-        </span>
-      </span>);
-    expect(output.props.children.props.children[1]).toEqualJSX(expected);
+    const wrapper = renderComponent({
+      getUser: sinon.stub().callsArgWith(1, null, null)
+    });
+    assert.equal(
+      wrapper.find('.profile-header__avatar').prop('className').includes(
+        'profile-header__avatar--default'),
+      true);
   });
 
   it('can close the profile', () => {
     const changeState = sinon.stub();
-    const output = jsTestUtils.shallowRender(
-      <ProfileHeader
-        changeState={changeState}
-        getUser={getUser}
-        userInfo={{
-          isCurrent: true,
-          profile: 'spinach'
-        }} />);
-    output.props.children.props.children[0].props.onClick();
+    const wrapper = renderComponent({ changeState });
+    wrapper.find('.profile-header__close').props().onClick();
     assert.equal(changeState.callCount, 1);
     assert.deepEqual(changeState.args[0][0], {
       hash: null,
