9cafc71f8dedd4ea578e120c4c6c4ad960665e69
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-26 09:59:27 +1100

    Update profile credential list delete to use enzyme.

--- jujugui/static/gui/src/app/components/profile/credential-list/delete/test-delete.js
+++ jujugui/static/gui/src/app/components/profile/credential-list/delete/test-delete.js
@@ -2,62 +2,57 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const Popup = require('../../../popup/popup');
 const ProfileCredentialListDelete = require('./delete');
 
-const jsTestUtils = require('../../../../utils/component-test-utils');
-
 describe('ProfileCredentialListDelete', () => {
-  function renderComponent(options = {}) {
-    const renderer = jsTestUtils.shallowRender(
-      <ProfileCredentialListDelete
-        addNotification={options.addNotification || sinon.stub()}
-        credential="google_foo@external_admin"
-        onCancel={options.onCancel || sinon.stub()}
-        onCredentialDeleted={options.onCredentialDeleted || sinon.stub()}
-        revokeCloudCredential={
-          options.revokeCloudCredential || sinon.stub()} />, true);
-    return {
-      renderer,
-      output: renderer.getRenderOutput(),
-      instance: renderer.getMountedInstance()
-    };
-  }
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <ProfileCredentialListDelete
+      addNotification={options.addNotification || sinon.stub()}
+      credential="google_foo@external_admin"
+      onCancel={options.onCancel || sinon.stub()}
+      onCredentialDeleted={options.onCredentialDeleted || sinon.stub()}
+      revokeCloudCredential={
+        options.revokeCloudCredential || sinon.stub()} />
+  );
 
   it('can render', () => {
-    const comp = renderComponent();
+    const wrapper = renderComponent();
     const expected = (
-      <Popup
-        buttons={[{
-          title: 'Cancel',
-          action: sinon.stub(),
-          type: 'inline-neutral'
-        }, {
-          title: 'Continue',
-          action: sinon.stub(),
-          type: 'destructive'
-        }]}
-        title="Remove credentials">
-        <p>
-          Are you sure you want to remove these credentials?
-        </p>
-      </Popup>);
-    expect(comp.output).toEqualJSX(expected);
+      <div>
+        <Popup
+          buttons={[{
+            title: 'Cancel',
+            action: sinon.stub(),
+            type: 'inline-neutral'
+          }, {
+            title: 'Continue',
+            action: sinon.stub(),
+            type: 'destructive'
+          }]}
+          title="Remove credentials">
+          <p>
+            Are you sure you want to remove these credentials?
+          </p>
+        </Popup>
+      </div>);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('can cancel', () => {
     const onCancel = sinon.stub();
-    const comp = renderComponent({ onCancel });
-    comp.output.props.buttons[0].action();
+    const wrapper = renderComponent({ onCancel });
+    wrapper.find('Popup').prop('buttons')[0].action();
     assert.equal(onCancel.callCount, 1);
   });
 
   it('can remove credentials', () => {
     const revokeCloudCredential = sinon.stub().callsArgWith(1, null);
     const onCredentialDeleted = sinon.stub();
-    const comp = renderComponent({ revokeCloudCredential, onCredentialDeleted });
-    comp.output.props.buttons[1].action();
+    const wrapper = renderComponent({ revokeCloudCredential, onCredentialDeleted });
+    wrapper.find('Popup').prop('buttons')[1].action();
     assert.equal(revokeCloudCredential.callCount, 1);
     assert.equal(onCredentialDeleted.callCount, 1);
     assert.equal(onCredentialDeleted.args[0][0], 'google_foo@external_admin');
@@ -67,9 +62,9 @@ describe('ProfileCredentialListDelete', () => {
     const revokeCloudCredential = sinon.stub().callsArgWith(1, 'Uh oh!');
     const onCredentialDeleted = sinon.stub();
     const addNotification = sinon.stub();
-    const comp = renderComponent(
+    const wrapper = renderComponent(
       { addNotification, revokeCloudCredential, onCredentialDeleted });
-    comp.output.props.buttons[1].action();
+    wrapper.find('Popup').prop('buttons')[1].action();
     assert.equal(addNotification.callCount, 1);
     assert.deepEqual(addNotification.args[0][0], {
       title: 'Unable to revoke the cloud credential',
