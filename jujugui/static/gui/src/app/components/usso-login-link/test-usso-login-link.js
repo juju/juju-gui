35b9996d77d936cedd877c7782c83983bd54bdbf
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-26 17:00:25 +1100

    Update usso login link tests to use enzyme.

--- jujugui/static/gui/src/app/components/usso-login-link/test-usso-login-link.js
+++ jujugui/static/gui/src/app/components/usso-login-link/test-usso-login-link.js
@@ -2,53 +2,52 @@
 'use strict';
 
 const React = require('react');
+const enzyme = require('enzyme');
 
 const USSOLoginLink = require('./usso-login-link');
 const GenericButton = require('../generic-button/generic-button');
 
-const jsTestUtils = require('../../utils/component-test-utils');
-const testUtils = require('react-dom/test-utils');
-
 describe('USSOLoginLink', () => {
+
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <USSOLoginLink
+      addNotification={options.addNotification || sinon.stub()}
+      displayType={options.displayType || 'text'}
+      gisf={options.gisf}
+      loginToController={options.loginToController || sinon.stub()}
+      sendPost={options.sendPost || sinon.stub()}>
+      {options.children}
+    </USSOLoginLink>
+  );
+
   it('can render a text link', () => {
-    const output = jsTestUtils.shallowRender(
-      <USSOLoginLink
-        addNotification={sinon.stub()}
-        displayType={'text'}
-        loginToController={sinon.stub()} />);
+    const wrapper = renderComponent();
     const expected = (
       <div className="usso-login">
         <a className="usso-login__action"
-          onClick={output.props.children.props.onClick}
+          onClick={wrapper.find('.usso-login__action').prop('onClick')}
           target="_blank">
             Login
         </a>
       </div>);
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('calls loginToController on click for text link', () => {
     const loginToController = sinon.stub();
-    const output = testUtils.renderIntoDocument(
-      <USSOLoginLink
-        addNotification={sinon.stub()}
-        displayType={'text'}
-        loginToController={loginToController} />, true);
-    testUtils.Simulate.click(
-      testUtils.findRenderedDOMComponentWithTag(output, 'a'));
+    const wrapper = renderComponent({ loginToController });
+    wrapper.find('.usso-login__action').simulate('click');
     assert.equal(loginToController.callCount, 1);
   });
 
   it('handles errors when logging in', () => {
     const addNotification = sinon.stub();
     const loginToController = sinon.stub().callsArgWith(0, 'Uh oh!');
-    const output = testUtils.renderIntoDocument(
-      <USSOLoginLink
-        addNotification={addNotification}
-        displayType={'text'}
-        loginToController={loginToController} />, true);
-    testUtils.Simulate.click(
-      testUtils.findRenderedDOMComponentWithTag(output, 'a'));
+    const wrapper = renderComponent({
+      addNotification,
+      loginToController
+    });
+    wrapper.find('.usso-login__action').simulate('click');
     assert.equal(addNotification.callCount, 1);
     assert.deepEqual(addNotification.args[0][0], {
       title: 'cannot log into the controller',
@@ -58,63 +57,39 @@ describe('USSOLoginLink', () => {
   });
 
   it('can render a button link', () => {
-    const component = jsTestUtils.shallowRender(
-      <USSOLoginLink
-        addNotification={sinon.stub()}
-        displayType={'button'}
-        gisf={false}
-        loginToController={sinon.stub()}
-        sendPost={sinon.stub()} />, true);
-    const output = component.getRenderOutput();
+    const wrapper = renderComponent({ displayType: 'button' });
     var expected = (
       <div className="usso-login">
         <GenericButton
-          action={component.getMountedInstance().handleLogin}
+          action={wrapper.find('GenericButton').prop('action')}
           extraClasses="usso-login__action"
           type="positive" >
           Sign up/Log in with USSO
         </GenericButton>
       </div>);
-    expect(output).toEqualJSX(expected);
+    assert.compareJSX(wrapper, expected);
   });
 
   it('can render a button link with custom content', () => {
-    const component = jsTestUtils.shallowRender(
-      <USSOLoginLink
-        addNotification={sinon.stub()}
-        displayType={'button'}
-        gisf={false}
-        loginToController={sinon.stub()}
-        sendPost={sinon.stub()}>
-          Scooby Doo
-      </USSOLoginLink>, true);
-    const output = component.getRenderOutput();
-    assert.equal(output.props.children.props.children, 'Scooby Doo');
+    const wrapper = renderComponent({
+      children: 'Scooby Doo',
+      displayType: 'button'
+    });
+    assert.equal(wrapper.find('GenericButton').children().text(), 'Scooby Doo');
   });
 
   it('can render a text link with custom content', () => {
-    const component = jsTestUtils.shallowRender(
-      <USSOLoginLink
-        addNotification={sinon.stub()}
-        displayType={'text'}
-        gisf={false}
-        loginToController={sinon.stub()}
-        sendPost={sinon.stub()}>
-          Scooby Doo
-      </USSOLoginLink>, true);
-    const output = component.getRenderOutput();
-    assert.equal(output.props.children.props.children, 'Scooby Doo');
+    const wrapper = renderComponent({ children: 'Scooby Doo' });
+    assert.equal(wrapper.find('.usso-login__action').text(), 'Scooby Doo');
   });
 
   it('calls loginToController on click for button link', () => {
     const loginToController = sinon.stub();
-    const output = testUtils.renderIntoDocument(
-      <USSOLoginLink
-        addNotification={sinon.stub()}
-        displayType={'button'}
-        loginToController={loginToController} />, true);
-    testUtils.Simulate.click(
-      testUtils.findRenderedDOMComponentWithTag(output, 'button'));
+    const wrapper = renderComponent({
+      displayType: 'button',
+      loginToController
+    });
+    wrapper.find('GenericButton').props().action();
     assert.equal(loginToController.callCount, 1);
   });
 });
