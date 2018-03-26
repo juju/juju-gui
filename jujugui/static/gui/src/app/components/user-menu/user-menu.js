f13a1d5cdf36032f0313479114c9aac53d6f207e
Huw Wilkins <huw.wilkins@canonical.com>
2018-03-26 16:50:33 +1100

    Update user menu tests to use enzyme.

--- jujugui/static/gui/src/app/components/user-menu/user-menu.js
+++ jujugui/static/gui/src/app/components/user-menu/user-menu.js
@@ -6,27 +6,29 @@ const React = require('react');
 
 const ButtonDropdown = require('../button-dropdown/button-dropdown');
 /**
-  Provides a user menu to the header - shows Profile, Account and Logout links.
+  Provides a user menu to the header - shows Profile, help and Logout links.
   If user is not logged in the user icon is replaced with a login button.
 */
 const UserMenu = props => {
   const controllerAPI = props.controllerAPI;
   const showLogin = controllerAPI && !controllerAPI.userIsAuthenticated;
   return (
-    <ButtonDropdown
-      classes={['user-menu']}
-      disableDropdown={showLogin}
-      icon={showLogin ? props.USSOLoginLink : 'user_16'}
-      listItems={[{
-        action: props.navigateUserProfile,
-        label: 'Profile'
-      }, {
-        action: props.showHelp,
-        label: 'GUI help'
-      }, {
-        element: props.LogoutLink
-      }]}
-      tooltip={showLogin ? '' : 'user'} />);
+    <div>
+      <ButtonDropdown
+        classes={['user-menu']}
+        disableDropdown={showLogin}
+        icon={showLogin ? props.USSOLoginLink : 'user_16'}
+        listItems={[{
+          action: props.navigateUserProfile,
+          label: 'Profile'
+        }, {
+          action: props.showHelp,
+          label: 'GUI help'
+        }, {
+          element: props.LogoutLink
+        }]}
+        tooltip={showLogin ? '' : 'user'} />
+    </div>);
 };
 
 UserMenu.propTypes = {
