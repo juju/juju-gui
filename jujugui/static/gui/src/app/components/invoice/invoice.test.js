178693988d74392fea51c61e205caad9b6abf755
Barry McGee <barry.mcgee@integralcode.co.uk>
2018-04-05 13:51:22 +0100

    Update tests to use Enzyme

--- jujugui/static/gui/src/app/components/invoice/invoice.test.js
+++ jujugui/static/gui/src/app/components/invoice/invoice.test.js
@@ -1,19 +1,21 @@
-/* Copyright (C) 2018 Canonical Ltd. */
-
+/* Copyright (C) 2017 Canonical Ltd. */
 'use strict';
-const React = require('react');
-const Invoice = require('./invoice');
 
-const jsTestUtils = require('../../utils/component-test-utils');
+const React = require('react');
+const enzyme = require('enzyme');
+
+const Invoice = require('./revenue-statement');
 
 describe('Invoice', function() {
 
-  function renderComponent(options={}) {
-    return jsTestUtils.shallowRender(
-      <Invoice />, true);
-  }
+  const renderComponent = (options = {}) => enzyme.shallow(
+    <Invoice />
+  );
 
   it('can render', () => {
-    renderComponent();
+    const wrapper = renderComponent();
+    assert.equal(wrapper.find('.revenue-statement__inner').length,
+      1, 
+      'Revenue statement inner wrapper');
   });
 });
