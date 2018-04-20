77f12865ab9770eb5869378fb3ed0e4e6f37daf8
Jeff Pihach <jeff.pihach@canonical.com>
2018-04-20 10:54:13 -0600

    Update tests for SelectionPlan component.

--- jujugui/static/gui/src/app/components/deployment-flow/pricing/support-selection/test-support-selection.js
+++ jujugui/static/gui/src/app/components/deployment-flow/pricing/support-selection/test-support-selection.js
@@ -10,11 +10,20 @@ const DeploymentSupportSelectionPlan = require('./plan/plan');
 describe('DeploymentSupportSelection', () => {
 
   const renderComponent = (options = {}) => enzyme.shallow(
-    <DeploymentSupportSelection />
+    <DeploymentSupportSelection
+      getSLAMachineRates={options.getSLAMachineRates || sinon.stub()} />
   );
 
-  it('can render', function() {
-    const wrapper = renderComponent();
+  it('can render', () => {
+    const data = {
+      advanced: '0.110',
+      essential: '0.011',
+      standard: '0.055',
+      unsupported: '0.000'
+    };
+    const wrapper = renderComponent({
+      getSLAMachineRates: cb => cb(data)
+    });
     const plans = wrapper.find('DeploymentSupportSelectionPlan');
     const expected = (
       <div className="deployment-support-selection equal-height">
@@ -22,7 +31,7 @@ describe('DeploymentSupportSelection', () => {
           classes={[]}
           cost="8.75"
           features={['8hx5d ticked']}
-          hourPrice="0.00856"
+          hourPrice="0.011"
           key="Essential"
           onSelect={plans.at(0).prop('onSelect')}
           selected={false}
@@ -34,7 +43,7 @@ describe('DeploymentSupportSelection', () => {
             '10x5 phone support',
             '2hr critical response'
           ]}
-          hourPrice="0.02853"
+          hourPrice="0.055"
           key="Standard"
           onSelect={plans.at(1).prop('onSelect')}
           selected={false}
@@ -46,7 +55,7 @@ describe('DeploymentSupportSelection', () => {
             '24x7 phone support',
             '1hr critical response'
           ]}
-          hourPrice="0.05707"
+          hourPrice="0.110"
           key="Advanced"
           onSelect={plans.at(2).prop('onSelect')}
           selected={false}
@@ -55,8 +64,21 @@ describe('DeploymentSupportSelection', () => {
     assert.compareJSX(wrapper, expected);
   });
 
-  it('can select a plan', function() {
+  it('shows the spinner while requesting SLA rates', () => {
     const wrapper = renderComponent();
+    assert.equal(wrapper.find('Spinner').length, 1);
+  });
+
+  it('can select a plan', () => {
+    const data = {
+      advanced: '0.110',
+      essential: '0.011',
+      standard: '0.055',
+      unsupported: '0.000'
+    };
+    const wrapper = renderComponent({
+      getSLAMachineRates: cb => cb(data)
+    });
     const instance = wrapper.instance();
     const plan = wrapper.find('DeploymentSupportSelectionPlan').at(1);
     assert.equal(plan.prop('selected'), false);
