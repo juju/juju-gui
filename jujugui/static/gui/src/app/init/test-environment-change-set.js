299ebbbee4f04bea42bfd40192307564360bb296
Huw Wilkins <huw.wilkins@canonical.com>
2018-07-03 14:43:16 +1000

    Test.

--- jujugui/static/gui/src/app/init/test-environment-change-set.js
+++ jujugui/static/gui/src/app/init/test-environment-change-set.js
@@ -18,7 +18,6 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 /* eslint-disable */
-const ECS = require('./environment-change-set');
 const testUtils = require('./testing-utils');
 const User = require('../user/user');
 const utils = require('./utils');
