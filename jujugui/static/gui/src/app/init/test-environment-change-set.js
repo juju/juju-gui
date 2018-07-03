37f362b14c8913367a72fa5a737cad53f9568720
Huw Wilkins <huw.wilkins@canonical.com>
2018-07-03 18:07:16 +1000

    Test.

--- jujugui/static/gui/src/app/init/test-environment-change-set.js
+++ jujugui/static/gui/src/app/init/test-environment-change-set.js
@@ -18,18 +18,13 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
-/* eslint-disable */
 const ECS = require('./environment-change-set');
-const testUtils = require('./testing-utils');
-const User = require('../user/user');
-const utils = require('./utils');
-/* eslint-enable */
 
 describe('Environment Change Set', function() {
-  var Y, ecs, envObj, dbObj, models, _cleanups;
+  var ecs, dbObj, models;
 
   beforeAll(function(done) {
-    Y = YUI(GlobalConfig).use([], function(Y) {
+    YUI(GlobalConfig).use([], function(Y) {
       window.yui = Y;
       require('../yui-modules');
       window.yui.use(window.MODULES, function() {
@@ -40,40 +35,13 @@ describe('Environment Change Set', function() {
   });
 
   beforeEach(function() {
-    _cleanups = [];
-    const getMockStorage = function() {
-      return new function() {
-        return {
-          store: {},
-          setItem: function(name, val) { this.store['name'] = val; },
-          getItem: function(name) { return this.store['name'] || null; }
-        };
-      };
-    };
-    const userClass = new User({sessionStorage: getMockStorage()});
-    userClass.controller = {user: 'user', password: 'password'};
     dbObj = new models.Database({getECS: sinon.stub().returns({changeSet: {}})});
-    dbObj.fireEvent = sinon.stub();
     ecs = new ECS({
       db: dbObj
     });
-    envObj = new Y.juju.environments.GoEnvironment({
-      connection: new testUtils.SocketStub(),
-      user: userClass,
-      ecs: ecs
-    });
-    sinon.stub(envObj, '_addCharm');
-    sinon.stub(envObj, '_deploy');
-    sinon.stub(envObj, '_set_config');
-    sinon.stub(envObj, '_add_relation');
-    sinon.stub(envObj, '_addMachines');
   });
 
   afterEach(function() {
-    _cleanups.forEach(cleanup => cleanup && cleanup());
-    dbObj.reset();
-    dbObj.destroy();
-    envObj.destroy();
   });
 
   describe('ECS methods', function() {
