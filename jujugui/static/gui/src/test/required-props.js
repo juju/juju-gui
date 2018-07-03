0ebdfcdb06b31ad3783ba5522a3e235ea7b3ccac
Huw Wilkins <huw.wilkins@canonical.com>
2018-07-04 09:15:43 +1000

    Test.

--- jujugui/static/gui/src/test/required-props.js
+++ jujugui/static/gui/src/test/required-props.js
@@ -1,25 +1,3 @@
 /* Copyright (C) 2017 Canonical Ltd. */
 
 'use strict';
-
-// Validate prop type errors.
-
-beforeEach(function() {
-  spyOn(console, 'error').and.callThrough(); // eslint-disable-line no-undef
-});
-
-afterEach(function() {
-  console.error.calls.all().forEach(call => {
-    let error = call.args[0];
-    if (!error) {
-      return;
-    }
-    if (error.toString) {
-      // This must be a raised Error().
-      error = error.toString();
-    }
-    if (error.startsWith('Warning: Failed prop type:')) {
-      fail(`\x1b[31m${error}\x1b[0m'`);
-    }
-  });
-});
