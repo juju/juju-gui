cb94a1cf87a2afeebd7c9b697d10099d0c4c8226
Huw Wilkins <huw.wilkins@canonical.com>
2018-07-03 15:22:18 +1000

    Test.

--- jujugui/static/gui/src/app/init/test-utils.js
+++ jujugui/static/gui/src/app/init/test-utils.js
@@ -7,24 +7,6 @@ const testUtils = require('./testing-utils');
 
 describe('init utils', () => {
   describe('createSocketURL', () => {
-    it('honors socket_protocol and uuid', () => {
-      const expected = [
-        'ws://',
-        window.location.hostname,
-        ':',
-        window.location.port,
-        '/juju/api/example.com/17070/1234-1234'
-      ].join('');
-      const url = utils.createSocketURL({
-        apiAddress: 'http://api.example.com/',
-        template: '/juju/api/$server/$port/$uuid',
-        protocol: 'ws',
-        uuid: '1234-1234',
-        server: 'example.com',
-        port: '17070'
-      });
-      assert.strictEqual(url, expected);
-    });
 
     it('honors a fully qualified provided socket URL', () => {
       const url = utils.createSocketURL({
