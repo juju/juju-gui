8dd2165a9a14ea0ca3a9303d89c3941d09be7c45
Huw Wilkins <huw.wilkins@canonical.com>
2018-08-20 10:39:04 +1000

    Update the cookie notification tests to check the returned component.

--- jujugui/static/gui/src/app/init/test-cookie-util.js
+++ jujugui/static/gui/src/app/init/test-cookie-util.js
@@ -1,62 +1,69 @@
 /* Copyright (C) 2017 Canonical Ltd. */
 'use strict';
 
+const React = require('react');
+const enzyme = require('enzyme');
+
 const cookieUtil = require('./cookie-util');
+const Notification = require('../components/notification/notification');
 
 describe('CookieUtil', () => {
-
-  let container = null;
-  let fakeDocument = null;
-
-  function createContainer() {
-    const container = document.createElement('div');
-    container.setAttribute('id', 'cookie-container');
-    document.body.appendChild(container);
-    return container;
-  }
-
-  function removeContainer(container) {
-    container.remove();
-  }
+  let fakeDocument, state;
 
   beforeEach(() => {
-    container = createContainer();
     fakeDocument = {
-      cookie: '',
-      getElementById: document.getElementById.bind(document)
+      cookie: ''
+    };
+    state = {
+      dispatch: sinon.stub()
     };
   });
 
   afterEach(() => {
-    removeContainer(container);
-    container = null;
-    fakeDocument = null;
     localStorage.removeItem('disable-cookie');
   });
 
-  it('calling check makes the node visible', () => {
-    assert.equal(container.children.length > 0, false);
-    cookieUtil.check(document);
-    assert.equal(container.children.length > 0, true);
+  it('calling check returns the notification', () => {
+    // Wrap the output so we can check what is returned, not render the containing
+    // Notification component.
+    const wrapper = enzyme.shallow(
+      <div>
+        {cookieUtil.check(fakeDocument, state)}
+      </div>);
+    const content = (
+      <span>
+        We use cookies to improve your experience. By your continued use
+        of this application you accept such use. To change your settings
+        please&nbsp;
+        <a href="http://www.ubuntu.com/privacy-policy#cookies"
+          target="_blank">
+          see our policy
+        </a>
+      </span>);
+    const expected = (
+      <Notification
+        content={content}
+        dismiss={wrapper.find('Notification').prop('dismiss')}
+        extraClasses="p-notification--center-bottom" />);
+    assert.compareJSX(wrapper.find('Notification'), expected);
   });
 
   it('closing the banner sets the cookie', () => {
     const date = new Date('January 12, 2025');
     assert.equal(fakeDocument.cookie, '');
-    cookieUtil.close(fakeDocument);
+    cookieUtil.close(fakeDocument, state);
     assert.equal(fakeDocument.cookie, `_cookies_accepted=true; expiry=${date}`);
   });
 
   it('the cookie prevents the node from being made visible', () => {
     const date = new Date('January 12, 2025');
     fakeDocument.cookie = `_cookies_accepted=true; expiry=${date}`;
-    cookieUtil.check(fakeDocument);
-    assert.equal(container.children.length, 0);
+    assert.strictEqual(cookieUtil.check(fakeDocument, state), null);
   });
 
   it('the custom setting prevents the node from being made visible', () => {
     localStorage.setItem('disable-cookie', 'true');
     cookieUtil.check(fakeDocument);
-    assert.equal(container.children.length, 0);
+    assert.strictEqual(cookieUtil.check(fakeDocument, state), null);
   });
 });
