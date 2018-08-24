7c496cbc3b924d6fa25f15b2a48cb787d0545f59
Barry McGee <barry.mcgee@integralcode.co.uk>
2018-08-24 12:44:24 +0100

    Update Revenue Statement test to add Snapshot testing

--- jujugui/static/gui/src/app/components/revenue-statement/test-revenue-statement.js
+++ jujugui/static/gui/src/app/components/revenue-statement/test-revenue-statement.js
@@ -1,4 +1,3 @@
-/* Copyright (C) 2017 Canonical Ltd. */
 'use strict';
 
 const React = require('react');
@@ -7,15 +6,10 @@ const enzyme = require('enzyme');
 const RevenueStatement = require('./revenue-statement');
 
 describe('Revenue statement', function() {
-
-  const renderComponent = (options = {}) => enzyme.shallow(
-    <RevenueStatement />
-  );
+  const renderComponent = (options = {}) => enzyme.shallow(<RevenueStatement />);
 
   it('can render', () => {
-    const wrapper = renderComponent();
-    assert.equal(wrapper.find('.revenue-statement__inner').length,
-      1,
-      'Revenue statement inner wrapper');
+    const wrapper = renderComponent({ gisf: true });
+    expect(wrapper).toMatchSnapshot();
   });
 });
