2fc251afe48e59d69cc833d57e047e926105f812
Jeff Pihach <jeff.pihach@canonical.com>
2018-09-04 14:21:12 -0600

    Update the styling to match design mockup.

--- jujugui/static/gui/src/app/components/added-services-list/label/label.js
+++ jujugui/static/gui/src/app/components/added-services-list/label/label.js
@@ -5,7 +5,7 @@ const PropTypes = require('prop-types');
 const React = require('react');
 
 const AddedServicesLabel = props =>
-  <li className="inspector-view__list-item" key={props.name}>{props.name}</li>;
+  <li className="inspector-view__label-item">{props.name}</li>;
 
 AddedServicesLabel.propTypes = {
   name: PropTypes.string.isRequired
