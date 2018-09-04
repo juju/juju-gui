94e1cae3fa7f5ea06a3f26051453aa35f1345cc1
Jeff Pihach <jeff.pihach@canonical.com>
2018-09-04 13:50:55 -0600

    Group applications in a bundle under a bundle name label in the added services list.

--- jujugui/static/gui/src/app/components/added-services-list/added-services-list.js
+++ jujugui/static/gui/src/app/components/added-services-list/added-services-list.js
@@ -6,25 +6,72 @@ const React = require('react');
 const shapeup = require('shapeup');
 
 const AddedServicesListItem = require('./item/item');
+const AddedServicesLabel = require('./label/label');
+
+const addToObj = (obj, key, value) => {
+  if (!obj[key]) {
+    obj[key] = [];
+  }
+  obj[key].push(value);
+};
 
 class AddedServicesList extends React.Component {
+
+  /**
+    Generate a new AddedServicesListItem component and return it.
+    @param {Object} service The service instance to fetch data from.
+    @returns {Function} The AddedServicesListItem.
+  */
+  _newListItem(service) {
+    return (
+      <AddedServicesListItem
+        // We use the 'name' instead of the 'id' here because when a
+        // ghost service is added it uses the ghost id structure instead
+        // of the final deployed service id structure and we want react
+        // to treat them as the same record instead of re-rendering
+        // when they key changes.
+        changeState={this.props.changeState}
+        hovered={service.get('id') === this.props.hoveredId}
+        key={service.get('name')}
+        ref={'AddedServicesListItem-' + service.get('id')}
+        service={service}
+        serviceModule={this.props.serviceModule} />);
+  }
+
+  /**
+    Generate the list of the labels and applications.
+    @param {Object} services A YUI ModelList of applications.
+    @returns {Array} A list of applications and label components to render.
+  */
   generateItemList(services) {
-    var items = [];
+    const grouped = {};
+    // Group all of the applications by the bundleURL if one exists.
     services.each(service => {
-      items.push(
-        <AddedServicesListItem
-          // We use the 'name' instead of the 'id' here because when a
-          // ghost service is added it uses the ghost id structure instead
-          // of the final deployed service id structure and we want react
-          // to treat them as the same record instead of re-rendering
-          // when they key changes.
-          changeState={this.props.changeState}
-          hovered={service.get('id') === this.props.hoveredId}
-          key={service.get('name')}
-          ref={'AddedServicesListItem-' + service.get('id')}
-          service={service}
-          serviceModule={this.props.serviceModule} />);
+      const bundleURL = service.get('annotations').bundleURL;
+      if (bundleURL) {
+        addToObj(grouped, bundleURL, service);
+        return;
+      }
+      addToObj(grouped, 'solo', service);
     });
+    const items = [];
+    // We'll put the solo ones at the top for now, this can be moved later.
+    if (grouped.solo) {
+      grouped.solo.forEach(app => {
+        items.push(this._newListItem(app));
+      });
+      delete grouped.solo;
+    }
+    for (let key in grouped) {
+      // A bundleURL is in the format elasticsearch-cluster/bundle/17 so just
+      // create a label using the bundle name itself.
+      items.push(<AddedServicesLabel name={key.split('/')[0]} />);
+      // Now that the label has been added, loop through the applications in that
+      // bundle.
+      grouped[key].forEach(app => {
+        items.push(this._newListItem(app));
+      });
+    }
     return items;
   }
 
