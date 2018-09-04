c46a8ea4086be29b3031ef3b7067b5d509ee146f
Barry McGee <barry.mcgee@integralcode.co.uk>
2018-09-04 10:46:38 +0100

    Add test-rs-details-item.js

---
+++ jujugui/static/gui/src/app/components/revenue-statement/rs-details-item/test-rs-details-item.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const React = require('react');
+const enzyme = require('enzyme');
+
+const RsDetailsItem = require('./rs-details-item');
+
+describe('Revenue statement details item', function() {
+  const renderComponent = (options = {}) =>
+    enzyme.shallow(
+      <RsDetailsItem amount="99.99" name="EntityName" plan="Plan A" share="99" />
+    );
+
+  it('can render', () => {
+    const wrapper = renderComponent({ gisf: true });
+    expect(wrapper).toMatchSnapshot();
+  });
+});
