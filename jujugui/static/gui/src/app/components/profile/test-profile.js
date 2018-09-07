259e632c08e79ace22f588d36fbba0e18ed33bcc
Barry McGee <barry.mcgee@integralcode.co.uk>
2018-09-07 11:22:21 +0100

    Update render <Profile> to Snapshot test

--- jujugui/static/gui/src/app/components/profile/test-profile.js
+++ jujugui/static/gui/src/app/components/profile/test-profile.js
@@ -12,40 +12,40 @@ const Profile = require('./profile');
 const Panel = require('../panel/panel');
 
 describe('Profile', function() {
-
-  const renderComponent = (options = {}) => enzyme.shallow(
-    <Profile
-      acl={options.acl || acl}
-      activeSection={options.activeSection || undefined}
-      addNotification={sinon.stub()}
-      addToModel={options.addToModel || sinon.stub()}
-      bakery={{}}
-      baseURL="/gui/"
-      changeState={options.changeState || sinon.stub()}
-      charmstore={{
-        getDiagramURL: sinon.stub(),
-        getMacaroon: sinon.stub(),
-        list: sinon.stub(),
-        url: '/charmstore'
-      }}
-      controllerAPI={controllerAPI}
-      controllerIP={'1.2.3.4'}
-      controllerIsReady={sinon.stub()}
-      controllerUser={options.controllerUser || 'spinach'}
-      destroyModel={sinon.stub()}
-      facadesExist={true}
-      generatePath={options.generatePath || sinon.stub()}
-      getModelName={options.getModelName || sinon.stub()}
-      getUser={options.getUser || sinon.stub()}
-      gisf={true}
-      payment={options.payment}
-      sendAnalytics={sinon.stub()}
-      showPay={options.showPay || false}
-      storeUser={options.storeUser || sinon.stub()}
-      stripe={options.stripe}
-      switchModel={sinon.stub()}
-      userInfo={options.userInfo || userInfo} />
-  );
+  const renderComponent = (options = {}) =>
+    enzyme.shallow(
+      <Profile
+        acl={options.acl || acl}
+        activeSection={options.activeSection || undefined}
+        addNotification={sinon.stub()}
+        addToModel={options.addToModel || sinon.stub()}
+        bakery={{}}
+        baseURL="/gui/"
+        changeState={options.changeState || sinon.stub()}
+        charmstore={{
+          getDiagramURL: sinon.stub(),
+          getMacaroon: sinon.stub(),
+          list: sinon.stub(),
+          url: '/charmstore'
+        }}
+        controllerAPI={controllerAPI}
+        controllerIP={'1.2.3.4'}
+        controllerIsReady={sinon.stub()}
+        controllerUser={options.controllerUser || 'spinach'}
+        destroyModel={sinon.stub()}
+        facadesExist={true}
+        generatePath={options.generatePath || sinon.stub()}
+        getModelName={options.getModelName || sinon.stub()}
+        getUser={options.getUser || sinon.stub()}
+        gisf={true}
+        payment={options.payment}
+        sendAnalytics={sinon.stub()}
+        showPay={options.showPay || false}
+        storeUser={options.storeUser || sinon.stub()}
+        stripe={options.stripe}
+        switchModel={sinon.stub()}
+        userInfo={options.userInfo || userInfo} />
+    );
 
   // Return the sectionsMap stored in the given component.
   function getSectionsMap(wrapper) {
@@ -57,7 +57,7 @@ describe('Profile', function() {
   let acl, controllerAPI, userInfo;
 
   beforeEach(() => {
-    acl = shapeup.deepFreeze({isReadOnly: () => false});
+    acl = shapeup.deepFreeze({ isReadOnly: () => false });
     controllerAPI = {
       getCloudCredentialNames: sinon.stub(),
       listClouds: sinon.stub(),
@@ -73,40 +73,8 @@ describe('Profile', function() {
   });
 
   it('can render', () => {
-    const wrapper = renderComponent();
-    const expected = (
-      <div>
-        <Panel
-          instanceName="profile"
-          visible={true}>
-          <ProfileHeader
-            changeState={sinon.stub()}
-            controllerIP={'1.2.3.4'}
-            getUser={sinon.stub()}
-            gisf={true}
-            userInfo={userInfo} />
-          <div className="twelve-col">
-            <div className="profile__content inner-wrapper">
-              <ProfileNavigation
-                activeSection="models"
-                changeState={sinon.stub()}
-                sectionsMap={new Map()} />
-              <ProfileModelList
-                acl={acl}
-                addNotification={sinon.stub()}
-                baseURL="/gui/"
-                changeState={sinon.stub()}
-                destroyModel={sinon.stub()}
-                facadesExist={true}
-                listModelsWithInfo={sinon.stub()}
-                switchModel={sinon.stub()}
-                userInfo={userInfo} />
-            </div>
-          </div>
-        </Panel>
-      </div>
-    );
-    assert.compareJSX(wrapper, expected);
+    const wrapper = renderComponent({ gisf: true });
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('does not show the payments section when the flag is off', () => {
@@ -116,20 +84,20 @@ describe('Profile', function() {
   });
 
   it('shows the payments section when the flag is on', () => {
-    const wrapper = renderComponent({showPay: true});
+    const wrapper = renderComponent({ showPay: true });
     const sectionsMap = getSectionsMap(wrapper);
     assert.isObject(sectionsMap.get('payment'));
   });
 
   it('hides certain sections when viewing others profile pages', () => {
-    const wrapper = renderComponent({controllerUser: 'foo'});
+    const wrapper = renderComponent({ controllerUser: 'foo' });
     const sectionsMap = getSectionsMap(wrapper);
     const allowedKeys = ['charms', 'bundles'];
     assert.deepEqual(Array.from(sectionsMap.keys()), allowedKeys);
   });
 
   it('correctly parses the URL', () => {
-    const wrapper = renderComponent({activeSection: 'credentials/aws_test'});
+    const wrapper = renderComponent({ activeSection: 'credentials/aws_test' });
     const instance = wrapper.instance();
     assert.deepEqual(instance._getSectionInfo(), {
       full: 'credentials/aws_test',
@@ -149,5 +117,4 @@ describe('Profile', function() {
       sub: null
     });
   });
-
 });
