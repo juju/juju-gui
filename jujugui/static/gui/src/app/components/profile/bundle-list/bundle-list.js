4a3c51a4390e91d287b40ede97d1bff97488efd8
Barry McGee <barry.mcgee@integralcode.co.uk>
2018-09-14 12:04:50 +0100

    Update styling & snapshot test for ProfileBundleList

--- jujugui/static/gui/src/app/components/profile/bundle-list/bundle-list.js
+++ jujugui/static/gui/src/app/components/profile/bundle-list/bundle-list.js
@@ -96,12 +96,13 @@ class ProfileBundleList extends React.Component {
   */
   _generateTitle() {
     return (
-      <h2 className="profile__title">
-        {this.props.isActiveUsersProfile ? 'My' : 'Their'} bundles
-        <span className="profile__title-count">
-          ({(this.state.data || []).length})
-        </span>
-      </h2>);
+      <div className="v1">
+        <h2 className="profile__title">
+          {this.props.isActiveUsersProfile ? 'My' : 'Their'} bundles
+          <span className="profile__title-count">({(this.state.data || []).length})</span>
+        </h2>
+      </div>
+    );
   }
 
   /**
