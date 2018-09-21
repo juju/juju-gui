ccd77024059c8c533329b19a378127c385ffd6cc
Huw Wilkins <huw.wilkins@canonical.com>
2018-09-21 10:19:20 +0200

    Update the application list to use the value store.

--- jujugui/static/gui/src/app/components/shared/status/application-list/test-application-list.js
+++ jujugui/static/gui/src/app/components/shared/status/application-list/test-application-list.js
@@ -7,7 +7,7 @@ const enzyme = require('enzyme');
 const StatusApplicationList = require('./application-list');
 
 describe('StatusApplicationList', () => {
-  let applications;
+  let applications, units;
 
   const renderComponent = (options = {}) => enzyme.shallow(
     <StatusApplicationList
@@ -18,23 +18,70 @@ describe('StatusApplicationList', () => {
         options.generateApplicationURL || sinon.stub().returns('http://example.com')}
       generateCharmURL={options.generateCharmURL || sinon.stub()}
       onCharmClick={options.onCharmClick || sinon.stub()}
-      statusFilter={options.statusFilter} />
+      statusFilter={options.statusFilter}
+      units={options.units || units} />
   );
 
   beforeEach(() => {
-    applications = [{
-      getAttrs: sinon.stub().returns({
-        charm: '~who/xenial/django-42',
-        exposed: true,
-        icon: 'django.svg',
-        id: 'django',
-        name: 'django',
-        pending: false,
-        status: {current: 'active'},
-        units: [],
-        workloadVersion: '1.10'
-      })
-    }];
+    applications = {
+      etcd: {
+        modelUUID: '32c9c2db-0955-459a-8201-539657ef0da1',
+        name: 'etcd',
+        exposed: false,
+        charmURL: 'cs:~containers/etcd-126',
+        ownerTag: '',
+        life: 'alive',
+        minUnits: 0,
+        constraints: {
+          'root-disk': 8192
+        },
+        config: {
+          channel: '3.2/stable'
+        },
+        subordinate: false,
+        status: {
+          current: 'waiting',
+          message: 'waiting for machine',
+          since: '2018-09-18T12:31:14.763574172Z',
+          version: ''
+        },
+        workloadVersion: ''
+      }
+    };
+    units = {
+      'etcd/0': {
+        modelUUID: '32c9c2db-0955-459a-8201-539657ef0da1',
+        name: 'etcd/0',
+        application: 'etcd',
+        series: 'xenial',
+        charmURL: 'cs:~containers/etcd-126',
+        publicAddress: '13.211.141.188',
+        privateAddress: '172.31.6.46',
+        machineID: '2',
+        ports: [{
+          protocol: 'tcp',
+          number: 2379
+        }],
+        portRanges: [{
+          fromPort: 2379,
+          toPort: 2379,
+          protocol: 'tcp'
+        }],
+        subordinate: false,
+        workloadStatus: {
+          current: 'active',
+          message: 'Healthy with 3 known peers',
+          since: '2018-09-18T12:37:52.738056612Z',
+          version: ''
+        },
+        agentStatus: {
+          current: 'idle',
+          message: '',
+          since: '2018-09-18T12:43:05.056981166Z',
+          version: '2.4.3'
+        }
+      }
+    };
   });
 
   it('renders', () => {
