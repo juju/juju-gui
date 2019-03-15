f64b6e3bf20360db6b7df9f8fd1deb1ed2a9aabf
Jeff Pihach <jeff.pihach@canonical.com>
2019-03-15 11:34:35 -0600

    Add script for generating a version.json file.

---
+++ scripts/version.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CURRENT_VERSION=`sed -n -e '/current_version =/ s/.*\= *// p' .bumpversion.cfg`
+CURRENT_COMMIT=`git rev-parse HEAD`
+
+echo "{\"version\": \"$CURRENT_VERSION\", \"commit\": \"$CURRENT_COMMIT\"}" > static/version.json
