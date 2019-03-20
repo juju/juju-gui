9802a9eaccee44fb19dfd052e9e04318a02e008a
Jeff Pihach <jeff.pihach@canonical.com>
2019-03-20 13:41:21 -0600

    Move generated version.json file to the static/build directory.

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,4 +3,4 @@
 CURRENT_VERSION=`sed -n -e '/current_version =/ s/.*\= *// p' .bumpversion.cfg`
 CURRENT_COMMIT=`git rev-parse HEAD`
 
-echo "{\"version\": \"$CURRENT_VERSION\", \"commit\": \"$CURRENT_COMMIT\"}" > static/version.json
+echo "{\"version\": \"$CURRENT_VERSION\", \"commit\": \"$CURRENT_COMMIT\"}" > static/build/version.json
