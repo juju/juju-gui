66b3b1ff16d224bac58421c2dd058acd20a983ca
Jeff Pihach <jeff.pihach@canonical.com>
2019-04-23 15:27:06 -0600

    Convert cookie util to use js-cookie.

--- gui/src/app/init/cookie-util.js
+++ gui/src/app/init/cookie-util.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const React = require('react');
+const cookie = require('js-cookie');
 
 const Notification = require('../components/notification/notification');
 
@@ -11,9 +12,9 @@ const Notification = require('../components/notification/notification');
   @param {Object} doc Reference to the window.document.
   @param {Object} state Reference to the app state.
 */
-function check(doc, state) {
-  if (shouldShowNotification(doc)) {
-    return _renderNotification(doc, state);
+function check(state) {
+  if (shouldShowNotification()) {
+    return _renderNotification(state);
   } else {
     return null;
   }
@@ -21,65 +22,28 @@ function check(doc, state) {
 
 /**
   Check whether the cookie notice should be visible.
-  @param {Object} doc Reference to the window.document.
   @returns {Boolean} Whether the notification should be showed.
 */
-function shouldShowNotification(doc) {
-  return _getCookie(doc, '_cookies_accepted') !== 'true' &&
+function shouldShowNotification() {
+  return cookie.get('_cookies_accepted') !== 'true' &&
     !localStorage.getItem('disable-cookie');
 }
 
 /**
   Close the cookie usage warning and set a cookie to denote user agreement.
-  @param {Object} doc Reference to the window.document.
   @param {Object} state Reference to the app state.
 */
-function close(doc, state) {
-  _setCookie(doc, '_cookies_accepted', 'true', new Date('January 12, 2025'));
+function close(state) {
+  cookie.set('_cookies_accepted', true, {expiry: new Date('January 12, 2025')});
   // Dispatch the state so that the check is done again and the notice no longer displayed.
   state.dispatch();
 }
 
 /**
-  Get the value for a cookie.
-  @param {Object} doc Reference to the window.document.
-  @param cookie {String} The name of the cookie.
-  @param {Object} document Reference to the window.document.
-  @return {String} The value of the cookie.
-*/
-function _getCookie(doc, cookie) {
-  let value = null;
-  doc.cookie.split(';').some(pair => {
-    const parts = pair.split('=');
-    if (parts[0].trim() === cookie) {
-      value = parts.length == 2 && parts[1].trim() || '';
-      return true;
-    }
-  });
-  return value;
-}
-
-/**
-  Set the value for a cookie.
-  @param {Object} doc Reference to the window.document.
-  @param cookie {String} The name of the cookie.
-  @param value {String} The value of the cookie.
-  @param expiry {String} The expiry for the cookie.
-*/
-function _setCookie(doc, cookie, value, expiry) {
-  const pairs = [`${cookie}=${value}`];
-  if (expiry) {
-    pairs.push(`expiry=${expiry}`);
-  }
-  doc.cookie = pairs.join('; ');
-}
-
-/**
   Render the notification.
-  @param {Object} doc Reference to the window.document.
   @param {Object} state Reference to the app state.
 */
-function _renderNotification(doc, state) {
+function _renderNotification(state) {
   const content = (
     <span>
       We use cookies to improve your experience. By your continued use
@@ -94,7 +58,7 @@ function _renderNotification(doc, state) {
   return (
     <Notification
       content={content}
-      dismiss={close.bind(this, doc, state)}
+      dismiss={close.bind(this, state)}
       extraClasses="p-notification--center-bottom" />);
 }
 
