eeed28be6f0d65e0efb8176829277d05c4726952
Jeff Pihach <jeff.pihach@canonical.com>
2019-05-09 11:06:10 -0600

    lint

--- webapp/blueprint.py
+++ webapp/blueprint.py
@@ -34,9 +34,7 @@ def root():
 
 @gui.route("/docs/<path:path>")
 def docs(path=""):
-    return flask.redirect(
-        urljoin("https://docs.jujucharms.com", path)
-    )
+    return flask.redirect(urljoin("https://docs.jujucharms.com", path))
 
 
 @gui.route("/big-data")
@@ -59,9 +57,7 @@ def jaas():
     if flask.request.path == "/home":
         return flask.redirect(JAAS_URL)
     else:
-        return flask.redirect(
-            urljoin(JAAS_URL, flask.request.path)
-        )
+        return flask.redirect(urljoin(JAAS_URL, flask.request.path))
 
 
 @gui.route("/new")
